fa4345e1f54aebf85b113441b0929ba4f4451b25
jijoong.moon <jijoong.moon@samsung.com>
2019-11-07 09:29:52 +0900

    Add initialize DeepQ with gym (CartPole-v0)
    Add DeepQ for CartPole-v0
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	 [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: jijoong.moon <jijoong.moon@samsung.com>

---
+++ DeepQ/include/neuralnet.h
@@ -0,0 +1,34 @@
+#ifndef __NEURALNET_H__
+#define __NEURALNET_H__
+
+#include "matrix.h"
+#include <vector>
+
+namespace Network {
+
+class NeuralNetwork {
+public:
+  NeuralNetwork(){};
+  ~NeuralNetwork(){};
+
+  double getLoss();
+  void setLoss(double l);
+
+  void init(int input, int hidden, int output, double rate);
+  Matrix forwarding(std::vector<double> input);
+  void backwarding(std::vector<double> expectedOutput);
+
+  NeuralNetwork &copy(NeuralNetwork const &from);
+
+private:
+  Matrix X, W1, H, W2, Y, B1, B2, Y2, dJdB1, dJdB2, dJdW1, dJdW2;
+
+  int inputNeuron;
+  int outputNeuron;
+  int hiddenNeuron;
+  double learning_rate;
+  double loss;
+};
+}
+
+#endif
