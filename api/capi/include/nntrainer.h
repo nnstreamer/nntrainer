0286ee84fb4a8594b28222c61b9e27592c6f2d49
jijoong.moon <jijoong.moon@samsung.com>
2020-04-03 14:16:11 +0900

    [API] Prototype of C API & test for Neural Network Model
    - Add C API to construct / destruct Neural Network Model object
    . ml_nnmodel_construct()
    . ml_nnmodel_destruct()
    
    - Add test cases
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	 [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: jijoong.moon <jijoong.moon@samsung.com>

---
+++ api/capi/include/nntrainer.h
@@ -0,0 +1,91 @@
+/**
+ * Copyright (C) 2020 Samsung Electronics Co., Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * @file nntrainer.h
+ * @date 02 April 2020
+ * @brief NNTrainer C-API Header.
+ *        This allows to construct and control NNTrainer Model.
+ * @see	https://github.com/nnstreamer/nntrainer
+ * @author Jijoong Moon <jijoong.moon@samsung.com>
+ * @bug No known bugs except for NYI items
+ */
+
+#ifndef __TIZEN_MACHINELEARNING_NNTRAINER_H__
+#define __TIZEN_MACHINELEARNING_NNTRAINER_H__
+
+#include <stdbool.h>
+#include <stddef.h>
+
+#include <tizen_error.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+/**
+ * @addtogroup CAPI_ML_NNTRAINER_MODULE
+ * @{
+ */
+
+/**
+ * @brief A handle of an NNTrainer model.
+ * @since_tizen 5.5
+ */
+typedef void *ml_nnmodel_h;
+
+/**
+ * @brief Enumeration for the error codes of NNTrainer.
+ * @since_tizen 5.5
+ */
+typedef enum {
+  ML_ERROR_NONE = TIZEN_ERROR_NONE,                                   /**< Success! */
+  ML_ERROR_INVALID_PARAMETER = TIZEN_ERROR_INVALID_PARAMETER,         /**< Invalid parameter */
+  ML_ERROR_UNKNOWN = TIZEN_ERROR_UNKNOWN,                             /**< Unknown error */
+  ML_ERROR_TIMED_OUT = TIZEN_ERROR_TIMED_OUT,                         /**< Time out */
+  ML_ERROR_NOT_SUPPORTED = TIZEN_ERROR_NOT_SUPPORTED,                 /**< The feature is not supported */
+  ML_ERROR_PERMISSION_DENIED = TIZEN_ERROR_PERMISSION_DENIED,         /**< Permission denied */
+  ML_ERROR_OUT_OF_MEMORY = TIZEN_ERROR_OUT_OF_MEMORY,                 /**< Out of memory (Since 6.0) */
+  ML_ERROR_CANNOT_ASSIGN_ADDRESS = TIZEN_ERROR_CANNOT_ASSIGN_ADDRESS, /**< Cannot assign requested address */
+  ML_ERROR_BAD_ADDRESS = TIZEN_ERROR_BAD_ADDRESS,                     /**< Bad Address */
+} ml_error_e;
+
+/**
+ * @brief Constructs the neural network model.
+ * @details Use this function to create Neural Netowrk Model.
+ * @since_tizen 5.5
+ * @param[in] model The NNTrainer Model handler from the given description.
+ * @return @c 0 on success. Otherwise a negative error value.
+ * @retval #ML_ERROR_NONE Successful.
+ * @retval #ML_ERROR_CANNOT_ASSIGN_ADDRESS Cannot assign object.
+ */
+int ml_nnmodel_construct(ml_nnmodel_h *model);
+
+/**
+ * @brief Destructs the neural network model.
+ * @details Use this function to delete Neural Netowrk Model.
+ * @since_tizen 5.5
+ * @param[in] model The NNTrainer model handler from the given description.
+ * @return @c 0 on success. Otherwise a negative error value.
+ * @retval #ML_ERROR_NONE Successful.
+ * @retval #ML_ERROR_INVALID_PARAMETER Invalid Parameter.
+ */
+int ml_nnmodel_destruct(ml_nnmodel_h model);
+
+/**
+ * @}
+ */
+#ifdef __cplusplus
+}
+
+#endif /* __cplusplus */
+#endif /* __TIZEN_MACHINELEARNING_NNTRAINER_H__ */
---
+++ test/tizen_capi/unittest_tizen_capi.cpp
@@ -0,0 +1,60 @@
+/**
+ * Copyright (C) 2019 Samsung Electronics Co., Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * @file        unittest_tizen_capi.cc
+ * @date        03 April 2020
+ * @brief       Unit test utility.
+ * @see         https://github.com/nnstreamer/nntrainer
+ * @author      Jijoong Moon <jijoong.moon@samsung.com>
+ * @bug         No known bugs
+ */
+#include <gtest/gtest.h>
+#include <nntrainer.h>
+
+/**
+ * @brief Neural Network Model Contruct / Destruct Test (possitive test )
+ * @return 0 success, -EADDRNOTAVIL if falied, -EFAULT if failed, -EVALID if failed.
+ */
+TEST(nntrainer_nnmodel_construct_deconstruct, nntrainer_01_p) {
+  ml_nnmodel_h handle;
+  int status;
+  status = ml_nnmodel_construct(&handle);
+  EXPECT_EQ(status, ML_ERROR_NONE);
+  status = ml_nnmodel_destruct(handle);
+  EXPECT_EQ(status, ML_ERROR_NONE);
+}
+
+/**
+ * @brief Neural Network Model Destruct Test (negative test )
+ * @return 0 success, -EINVAL if failed.
+ */
+TEST(nntrainer_nnmodel_construct_deconstruct, nntrainer_02_n) {
+  ml_nnmodel_h handle = NULL;
+  int status;
+  status = ml_nnmodel_destruct(handle);
+  EXPECT_EQ(status, ML_ERROR_INVALID_PARAMETER);
+}
+
+/**
+ * @brief Main gtest
+ */
+int main(int argc, char **argv) {
+  int result = -1;
+
+  testing::InitGoogleTest(&argc, argv);
+
+  result = RUN_ALL_TESTS();
+
+  return result;
+}
