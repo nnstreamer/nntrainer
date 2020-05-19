2e2aaa89399f9c550795c822dd900b036b88be57
Sangjung Woo <sangjung.woo@samsung.com>
2020-05-19 16:13:03 +0900

    [Unit Test] Add checkValidation() test case of each Layer class
    This patch adds checkValidation() method test case of each Layer class.
    
    Signed-off-by: Sangjung Woo <sangjung.woo@samsung.com>

--- test/unittest/unittest_nntrainer_internal.cpp
+++ test/unittest/unittest_nntrainer_internal.cpp
@@ -325,6 +325,19 @@ TEST(nntrainer_InputLayer, setActivation_02_n) {
 }
 
 /**
+ * @brief Input Layer
+ */
+TEST(nntrainer_InputLayer, checkValidation_01_p) {
+  int status = ML_ERROR_NONE;
+  nntrainer::InputLayer layer;
+  layer.initialize(1, 1, 1, false, true);
+  layer.setActivation(nntrainer::ACT_TANH);
+
+  status = layer.checkValidation();
+  EXPECT_EQ(status, ML_ERROR_NONE);
+}
+
+/**
  * @brief Fully Connected Layer
  */
 TEST(nntrainer_FullyConnectedLayer, initialize_01_p) {
@@ -469,6 +482,20 @@ TEST(nntrainer_FullyConnectedLayer, setCost_02_n) {
 }
 
 /**
+ * @brief FullyConnected Layer
+ */
+TEST(nntrainer_FullyConnectedLayer, checkValidation_01_p) {
+  int status = ML_ERROR_NONE;
+  nntrainer::FullyConnectedLayer layer;
+
+  layer.initialize(1, 1, 1, false, true);
+  layer.setActivation(nntrainer::ACT_RELU);
+
+  status = layer.checkValidation();
+  EXPECT_EQ(status, ML_ERROR_NONE);
+}
+
+/**
  * @brief Batch Normalization Layer
  */
 TEST(nntrainer_BatchNormalizationLayer, initialize_01_p) {
@@ -528,6 +555,20 @@ TEST(nntrainer_BatchNormalizationLayer, setActivation_02_n) {
   nntrainer::BatchNormalizationLayer layer;
   status = layer.setActivation(nntrainer::ACT_UNKNOWN);
   EXPECT_EQ(status, ML_ERROR_INVALID_PARAMETER);
+}
+
+/**
+ * @brief Batch Normalization Layer
+ */
+TEST(nntrainer_BatchNormalizationLayer, checkValidation_01_p)
+{
+  int status = ML_ERROR_NONE;
+  nntrainer::BatchNormalizationLayer layer;
+  layer.initialize(1, 1, 1, false, true);
+  layer.setActivation(nntrainer::ACT_RELU);
+
+  status = layer.checkValidation();
+  EXPECT_EQ(status, ML_ERROR_NONE);
 }
 
 TEST(nntrainer_TensorDim, setTensorDim_01_p) {
