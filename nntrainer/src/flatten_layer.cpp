c6cd9cce394d9a5e8868b028c489f0fa3575f511
jijoong.moon <jijoong.moon@samsung.com>
2020-06-16 13:15:56 +0900

    [ Flatten ] forwarding / copy for Flatten Layer
    This PR provides the fowarding/copy function of Flatten Layer
    . implement forwarding function
    . implement copy function
    
    Resolves:
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	 [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: jijoong.moon <jijoong.moon@samsung.com>

--- nntrainer/src/flatten_layer.cpp
+++ nntrainer/src/flatten_layer.cpp
@@ -23,13 +23,32 @@ namespace nntrainer {
 
 int FlattenLayer::initialize(bool last) {
   int status = ML_ERROR_NONE;
-  // NYI
+  if (input_dim.getDataLen() == 1) {
+    ml_logw("Warning: the length of previous layer dimension is one");
+  }
+  if (input_dim.batch() <= 0 || input_dim.height() <= 0 ||
+      input_dim.width() <= 0 || input_dim.channel() <= 0) {
+    ml_loge("Error: Dimension must be greater than 0");
+    return ML_ERROR_INVALID_PARAMETER;
+  }
+
+  this->last_layer = last;
+
+  output_dim.batch(input_dim.batch());
+  output_dim.channel(1);
+  output_dim.height(1);
+  output_dim.width(input_dim.getFeatureLen());
+
+  hidden = Tensor(output_dim);
+
   return status;
 }
 
 Tensor FlattenLayer::forwarding(Tensor in, int &status) {
-  // NYI
-  return in;
+  memcpy(hidden.getData(), in.getData(),
+         in.getDim().getDataLen() * sizeof(float));
+
+  return hidden;
 }
 
 Tensor FlattenLayer::forwarding(Tensor in, Tensor output, int &status) {
@@ -42,7 +61,14 @@ Tensor FlattenLayer::backwarding(Tensor in, int iteration) {
 }
 
 void FlattenLayer::copy(std::shared_ptr<Layer> l) {
-  // NYI
+  std::shared_ptr<FlattenLayer> from =
+    std::static_pointer_cast<FlattenLayer>(l);
+  this->input.copy(from->input);
+  this->hidden.copy(from->hidden);
+  this->dim = from->dim;
+  this->input_dim = from->input_dim;
+  this->output_dim = from->output_dim;
+  this->last_layer = from->last_layer;
 }
 
 } /* namespace nntrainer */
