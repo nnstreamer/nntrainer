73747e4c827c8beaae1c6c4c3305855710c474af
Jihoon Lee <jhoon.it.lee@samsung.com>
2020-07-21 11:35:52 +0900

    [Example] Add drawing part
    This PR adds drawing parti of example/customShortcut
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

--- Applications/Tizen_native/CustomShortcut/inc/data.h
+++ Applications/Tizen_native/CustomShortcut/inc/data.h
@@ -11,6 +11,8 @@
 
 #include <Elementary.h>
 #include <app.h>
+#include <cairo.h>
+#include <cairo/cairo-evas-gl.h>
 #include <dlog.h>
 #include <efl_extension.h>
 #include <tizen.h>
@@ -22,19 +24,76 @@
 
 #define EDJ_PATH "edje/main.edj"
 
+typedef enum _DRAW_MODE {
+  INFER = 0,
+  TRAIN_SMILE,
+  TRAIN_SAD,
+} DRAW_MODE;
+
+int MAX_TRIES = 5;
 typedef struct appdata {
   Evas_Object *win;
   Evas_Object *conform;
   Evas_Object *label;
   Evas_Object *naviframe;
+  Elm_Object_Item *nf_it;
   Eext_Circle_Surface *circle_nf;
   Elm_Object_Item *home;
   Evas_Object *layout;
+
+  /**< drawing related */
+  Evas_Object *canvas;   /**< image object that cairo surface flushes to */
+  unsigned char *pixels; /**< actual pixel data */
+  Evas_Coord x_offset;   /**< x starting point of canvas position */
+  Evas_Coord y_offset;   /**< y starting point of canvas position */
+  int width;             /**< width of the canvas */
+  int height;            /**< height of the canvas */
+
+  cairo_surface_t *cr_surface; /**< cairo surface for the canvas */
+  cairo_t *cr;                 /**< cairo engine for the canvas */
+  DRAW_MODE mode;              /**< drawing mode of current canvas */
+  int tries;                   /**< tells how many data has been labeled */
+
   char edj_path[PATH_MAX];
 } appdata_s;
 
+/**
+ * @brief separate route path from route
+ * @note this function copies @a source, and change ':' to '\0' and return start
+ * of the route and data accordingly
+ * @param[in] source raw source that comes like "route_target:data\0"
+ * @param[out] route route part of the string, free after use
+ * @param[out] data returns pointer of raw source, freed when route is freed. do
+ * not free.
+ * @param[out] length of data len
+ * @retval 0 if no error
+ */
+int parse_route(const char *source, char **route, char **data);
+
 #if !defined(PACKAGE)
 #define PACKAGE "org.example.nntrainer-example-custom-shortcut"
+#endif
+
+#if !defined(_D)
+#define _D(fmt, arg...)                                                    \
+  dlog_print(DLOG_DEBUG, LOG_TAG, "[%s:%d] " fmt "\n", __func__, __LINE__, \
+             ##arg)
+#endif
+
+#if !defined(_I)
+#define _I(fmt, arg...) \
+  dlog_print(DLOG_INFO, LOG_TAG, "[%s:%d] " fmt "\n", __func__, __LINE__, ##arg)
+#endif
+
+#if !defined(_W)
+#define _W(fmt, arg...) \
+  dlog_print(DLOG_WARN, LOG_TAG, "[%s:%d] " fmt "\n", __func__, __LINE__, ##arg)
+#endif
+
+#if !defined(_E)
+#define _E(fmt, arg...)                                                    \
+  dlog_print(DLOG_DEBUG, LOG_TAG, "[%s:%d] " fmt "\n", __func__, __LINE__, \
+             ##arg)
 #endif
 
 #endif /* __nntrainer_example_custom_shortcut_data_H__ */
---
+++ Applications/Tizen_native/CustomShortcut/src/data.c
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: Apache-2.0-only
+/**
+ * Copyright (C) 2020 Jijoong Moon <jijoong.moon@samsung.com>
+ *
+ * @file data.c
+ * @date 21 Jul 2020
+ * @brief TIZEN Native Example App data entry with NNTrainer/CAPI.
+ * @see  https://github.com/nnstreamer/nntrainer
+ * @author Jihoon Lee <jhoon.it.lee@samsung.com>
+ * @bug No known bugs except for NYI items
+ *
+ */
+#include "data.h"
+
+#include <string.h>
+
+int parse_route(const char *source, char **route, char **data) {
+  char *dst = strdup(source);
+  const char sep = ':';
+  char *i;
+  bool find_data = false;
+
+  if (route == NULL || data == NULL) {
+    free(dst);
+    return APP_ERROR_INVALID_PARAMETER;
+  }
+
+  *route = dst;
+
+  for (i = dst; *i != '\0'; ++i) {
+    if (*i == sep) {
+      *i = '\0';
+      *data = i + 1;
+      find_data = true;
+      break;
+    }
+  }
+
+  if (!find_data) {
+    *data = NULL;
+  }
+
+  return APP_ERROR_NONE;
+}
