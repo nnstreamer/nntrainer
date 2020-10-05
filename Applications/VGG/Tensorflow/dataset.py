ab3e597fc488e760344e4e65ea85ad024fff924e
jijoong.moon <jijoong.moon@samsung.com>
2020-10-05 20:56:40 +0900

    [ Application ] NNTrainer Validation with VGG
    This PR includes:
      - NNTrainer Validation with VGG Model.
      - Input is cifar100.
        . 100 Classes, 450 Images for train, 50 Images for validation
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	 [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: jijoong.moon <jijoong.moon@samsung.com>

---
+++ Applications/VGG/Tensorflow/dataset.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# SPDX-License-Identifier: Apache-2.0
+#
+# Copyright (C) 2020 Jijoong Moon <jijoong.moon@samsung.com>
+#
+# @file	dataset.py
+# @date	15 July 2020
+# @brief	This is for mnist input generation
+# @see		https://github.com/nnstreamer/nntrainer
+# @author	Jijoong Moon <jijoong.moon@samsung.com>
+# @bug		No known bugs except for NYI items
+#
+#
+
+import struct
+import os
+import numpy as np
+
+TOTAL_TRAIN_DATA_SIZE=100
+TOTAL_LABEL_SIZE=100
+TOTAL_VAL_DATA_SIZE=20
+FEATURE_SIZE = 3072
+
+def get_data_info(target):
+    t_data_size = TOTAL_TRAIN_DATA_SIZE
+    v_data_size = TOTAL_VAL_DATA_SIZE
+    return t_data_size, v_data_size, TOTAL_LABEL_SIZE, FEATURE_SIZE
+
+##
+# @brief load input data from file
+# @return (InputVector, InputLabel, Validation Vector, ValidationLabel)
+def load_data(target):
+    # data_size = TOTAL_TRAIN_DATA_SIZE;
+    d_size = get_data_info(target)
+
+    if target == "validation":
+        t_buf_size = d_size[0]
+        v_buf_size = d_size[1]
+    else:
+        t_buf_size = d_size[0]*TOTAL_LABEL_SIZE
+        v_buf_size = d_size[1]*TOTAL_LABEL_SIZE
+
+    InputVector = np.zeros((t_buf_size, FEATURE_SIZE),dtype=np.float32)
+    InputLabel = np.zeros((t_buf_size, TOTAL_LABEL_SIZE),dtype=np.float32)
+
+    ValVector = np.zeros((v_buf_size,FEATURE_SIZE),dtype=np.float32)
+    ValLabel = np.zeros((v_buf_size, TOTAL_LABEL_SIZE),dtype=np.float32)
+
+    #read Input & Label
+
+    fin = open('vgg_valSet.dat','rb')
+    for i in range(v_buf_size):
+        for j in range(FEATURE_SIZE):
+            data_str = fin.read(4)
+            ValVector[i,j] = struct.unpack('f',data_str)[0]
+        for j in range(TOTAL_LABEL_SIZE):
+            data_str = fin.read(4)
+            ValLabel[i,j] = struct.unpack('f',data_str)[0]
+    fin.close()
+
+    # we are using same training data for validation to check how internal implementation is working
+    fin=open('vgg_trainingSet.dat','rb')
+    for i in range(t_buf_size):
+        for j in range(FEATURE_SIZE):
+            data_str = fin.read(4)
+            InputVector[i,j] = struct.unpack('f',data_str)[0]
+        for j in range(TOTAL_LABEL_SIZE):
+            data_str = fin.read(4)
+            InputLabel[i,j] = struct.unpack('f',data_str)[0]
+    fin.close()
+
+    return InputVector, InputLabel, ValVector, ValLabel
---
+++ Applications/VGG/jni/Android.mk
@@ -0,0 +1,50 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+# ndk path
+ifndef ANDROID_NDK
+$(error ANDROID_NDK is not defined!)
+endif
+
+ifndef NNTRAINER_ROOT
+NNTRAINER_ROOT := $(LOCAL_PATH)/../../..
+endif
+
+NNTRAINER_INCLUDES := $(NNTRAINER_ROOT)/nntrainer/include \
+	$(NNTRAINER_ROOT)/api \
+	$(NNTRAINER_ROOT)/api/capi/include/platform
+
+LOCAL_MODULE := nntrainer
+LOCAL_SRC_FILES := $(NNTRAINER_ROOT)/libs/$(TARGET_ARCH_ABI)/libnntrainer.so
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := app_utils
+LOCAL_SRC_FILES := $(NNTRAINER_ROOT)/Applications/utils/libs/$(TARGET_ARCH_ABI)/libapp_utils.so
+APP_UTILS_INCLUDES := $(NNTRAINER_ROOT)/Applications/utils/jni/includes
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_ARM_NEON := true
+LOCAL_CFLAGS += -std=c++14 -Ofast -mcpu=cortex-a53 -Ilz4-nougat/lib
+LOCAL_LDFLAGS += -Llz4-nougat/lib/obj/local/$(TARGET_ARCH_ABI)/
+LOCAL_CXXFLAGS += -std=c++14
+LOCAL_CFLAGS += -pthread -fopenmp -fexceptions
+LOCAL_LDFLAGS += -fopenmp -fexceptions
+LOCAL_MODULE_TAGS := optional
+LOCAL_ARM_MODE := arm
+LOCAL_MODULE := nntrainer_vgg
+LOCAL_LDLIBS := -llog
+
+LOCAL_SRC_FILES := main.cpp
+
+LOCAL_SHARED_LIBRARIES := nntrainer app_utils
+
+LOCAL_C_INCLUDES += $(NNTRAINER_INCLUDES) $(APP_UTILS_INCLUDES)
+
+include $(BUILD_EXECUTABLE)
