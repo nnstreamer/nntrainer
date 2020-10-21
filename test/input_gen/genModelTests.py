2ce35e25cfd7465ed2732964c0250b2f43b9fc50
Jihoon Lee <jhoon.it.lee@samsung.com>
2020-10-21 19:20:05 +0900

    [Test] Add ParamTest scaffolding for model tests
    Add Parameterized test scaffolding for integrated tests with minor
    changes
    
    **Additional Changes proposed in this PR:**
    - Add `TensorDim::TensorDim(const std::string &shape)
    - Move `Tensor::epsilon` to public
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    cc: Parichay Kapoor <pk.kapoor@samsung.com>
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

--- test/input_gen/genModelTests.py
+++ test/input_gen/genModelTests.py
@@ -10,7 +10,7 @@
 
 import warnings
 
-from .recorder import KerasRecorder
+from recorder import KerasRecorder
 
 with warnings.catch_warnings():
     warnings.filterwarnings("ignore", category=FutureWarning)
@@ -26,10 +26,28 @@ if __name__ == "__main__":
     d = K.layers.Activation("softmax")(c)
 
     KerasRecorder(
-        file_name="a.info",
+        file_name="fc_softmax_mse.info",
         inputs=inp,
         outputs=[inp, a, b, c, d],
         input_shape=(3, 3),
         label_shape=(3, 10),
-        loss_fn=tf.keras.losses.CategoricalCrossentropy(),
-    ).run(2)
+        loss_fn=tf.keras.losses.MeanSquaredError(),
+    ).run(10)
+
+    inp = K.Input(shape=(3, 3))
+    a = K.layers.Dense(10)(inp)
+    b = K.layers.Activation("relu")(a)
+    c = K.layers.Dense(10)(b)
+    d = K.layers.Activation("relu")(c)
+    e = K.layers.Dense(2)(d)
+    f = K.layers.Activation("relu")(e)
+
+    KerasRecorder(
+        file_name="fc_relu_mse.info",
+        inputs=inp,
+        outputs=[inp, a, b, c, d, e, f],
+        input_shape=(3, 3),
+        label_shape=(3, 2),
+        loss_fn=tf.keras.losses.MeanSquaredError(),
+        optimizer=tf.keras.optimizers.SGD(lr=0.001)
+    ).run(10)
