72c7f5d17a39e83aeaa409f7de3eaf7a092efe53
Jihoon Lee <jhoon.it.lee@samsung.com>
2020-11-11 18:43:35 +0900

    [Optimizer] Add enum type factory
    This patch adds integer factory to align with capi
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

--- nntrainer/optimizers/optimizer_factory.h
+++ nntrainer/optimizers/optimizer_factory.h
@@ -14,10 +14,20 @@
 #define __OPTIMIZER_FACTORY_H__
 #ifdef __cplusplus
 
+#include <optimizer.h>
 #include <optimizer_internal.h>
 
 namespace nntrainer {
 
+using OptType = ml::train::OptimizerType;
+
+/**
+ * @brief change Optimizer Type to string
+ *
+ * @param type type to change
+ * @return const std::string string representation of the type
+ */
+const std::string optimizerIntToStrType(const OptType &type);
 /**
  * @brief Factory creator with copy constructor
  */
@@ -25,9 +35,20 @@ std::unique_ptr<Optimizer> createOptimizer(const std::string &type,
                                            const Optimizer &opt);
 
 /**
+ * @brief Factory creator with copy constructor using enum(integer)
+ */
+std::unique_ptr<Optimizer> createOptimizer(const OptType &type,
+                                           const Optimizer &opt);
+
+/**
  * @brief Factory creator with constructor
  */
 std::unique_ptr<Optimizer> createOptimizer(const std::string &type);
+
+/**
+ * @brief Factory creator with constructor using enum(integer)
+ */
+std::unique_ptr<Optimizer> createOptimizer(const OptType &type);
 
 } // namespace nntrainer
 
