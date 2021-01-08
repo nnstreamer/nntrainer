1eab8f7060cf879012535f0ab9b7882faf85eb26
Jihoon Lee <jhoon.it.lee@samsung.com>
2021-01-08 14:28:25 +0900

    [SimpleShot] Add scaffolding for the application
    This patch adds simpleshot directory to application.
    Nothing's present yet just a simple structures
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

---
+++ Applications/SimpleShot/task_runner.cpp
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2020 Jihoon Lee <jhoon.it.lee@samsung.com>
+ *
+ * @file	task_runner.cpp
+ * @date	08 Jan 2021
+ * @brief	task runner for the simpleshot demonstration
+ * @see		https://github.com/nnstreamer/nntrainer
+ * @author	Jihoon Lee <jhoon.it.lee@samsung.com>
+ * @bug		No known bugs except for NYI items
+ */
+
+#include <iostream>
+
+/**
+ * @brief main runner
+ *
+ * @return int
+ */
+int main(int argc, char **argv) { std::cout << "hello world\n"; }
