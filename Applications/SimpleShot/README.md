2a2798e2fd2d660340746f76dd3ed7a4f82e7ed8
Jihoon Lee <jhoon.it.lee@samsung.com>
2021-01-08 14:28:25 +0900

    [SimpleShot] Add scaffolding for the application
    This patch adds simpleshot directory to application.
    Nothing's present yet just a simple structures
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

---
+++ Applications/SimpleShot/README.md
@@ -0,0 +1,6 @@
+Pratical demonstration of SimpleShot few tasks including real examples
+
+Reference. [Wang, Yan, et al. 2019](https://arxiv.org/abs/1911.04623)
+
+# How to use
+TBA
---
+++ Applications/SimpleShot/test/mock_test.cpp
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2020 Jihoon Lee <jhoon.it.lee@samsung.com>
+ *
+ * @file	mock_test.cpp
+ * @date	08 Jan 2021
+ * @brief	mock test for simpleshot application
+ * @see		https://github.com/nnstreamer/nntrainer
+ * @author	Jihoon Lee <jhoon.it.lee@samsung.com>
+ * @note this will be deleted or moved as the application is building up
+ * @bug		No known bugs except for NYI items
+ */
+
+#include <gtest/gtest.h>
+
+TEST(sample_test, test_01_p) { EXPECT_TRUE(true); }
