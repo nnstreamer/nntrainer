258de3877d998d32e2ed71b3342969de25c1bc46
Jihoon Lee <jhoon.it.lee@samsung.com>
2021-04-01 19:31:21 +0900

    [Compiler] Add concept of the compiler
    This patch adds a concept of the compiler and class declaration.
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

--- nntrainer/compiler/compiler.h
+++ nntrainer/compiler/compiler.h
@@ -4,13 +4,48 @@
  *
  * @file compiler.h
  * @date 01 April 2021
- * @brief NNTrainer compiler that reads and generates excutable graph
+ * @brief NNTrainer compiler that reads and generates executable graph
+ * @details
+ * Graph is convertible either to iostream, representation, executable by
+ * appropriate compiler and interpreter
+ *         +--------+
+ *         |iostream|
+ *         +--+-----+
+ *            |  ^
+ * operator<< |  |
+ *            |  |
+ *        (Interpreter)
+ *            |  |
+ *            |  | operator>>
+ *            |  |
+ *    +-------+--+--------+
+ *    |GraphRepresentation|
+ *    +-------+-----------+
+ *            |  ^
+ *  compile() |  |
+ *            |  |
+ *         (Compiler)
+ *            |  |
+ *            |  | decompile()
+ *            v  |
+ *      +--------+------+
+ *      |ExecutableGraph|
+ *      +---------------+
+ *
  * @see	https://github.com/nnstreamer/nntrainer
  * @author Jihoon Lee <jhoon.it.lee@samsung.com>
  * @bug No known bugs except for NYI items
  */
 
 namespace nntrainer {
+
+class ExecutableGraph;
+
+class GraphRepresentation;
+
+class GraphCompiler;
+
+class GraphInterpreter;
 
 /**
  * @brief a simple scaffoliding function
