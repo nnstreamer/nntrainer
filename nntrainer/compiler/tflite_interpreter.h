6dc048c27616222b1c967ffe10067255f471889b
Jihoon Lee <jhoon.it.lee@samsung.com>
2021-04-12 20:15:30 +0900

    [Skeleton] Add Tflite interpreter skeleton
    This patch adds tflite interpreter skeleton
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

--- nntrainer/compiler/tflite_interpreter.h
+++ nntrainer/compiler/tflite_interpreter.h
@@ -9,5 +9,50 @@
  * @author Jihoon Lee <jhoon.it.lee@samsung.com>
  * @bug No known bugs except for NYI items
  */
+#ifndef __TFLITE_INTERPRETER_H__
+#define __TFLITE_INTERPRETER_H__
 
-#include <tf_schema_generated.h>
+#include <interpreter.h>
+
+#include <app_context.h>
+namespace nntrainer {
+
+/**
+ * @brief tflite graph interpreter class
+ *
+ */
+class TfliteInterpreter : public GraphInterpreter {
+public:
+  /**
+   * @brief Construct a new tflite Graph Interpreter object
+   *
+   * @param app_context_ app context to create layers
+   */
+  TfliteInterpreter(const AppContext &app_context_ = AppContext::Global()) :
+    app_context(app_context_) {}
+
+  /**
+   * @brief Destroy the Tflite Interpreter object
+   *
+   */
+  virtual ~TfliteInterpreter() = default;
+
+  /**
+   * @copydoc GraphInterpreter::serialize(const std::string &out)
+   */
+  void serialize(std::shared_ptr<const GraphRepresentation> representation,
+                 const std::string &out) override;
+
+  /**
+   * @copydoc GraphInterpreter::deserialize(const std::string &in)
+   */
+  std::shared_ptr<GraphRepresentation>
+  deserialize(const std::string &in) override;
+
+private:
+  AppContext app_context;
+};
+
+} // namespace nntrainer
+
+#endif // __TFLITE_INTERPRETER_H__
