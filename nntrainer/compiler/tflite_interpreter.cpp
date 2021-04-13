019aad7371f3433935f86391692a834b561d92e8
Jihoon Lee <jhoon.it.lee@samsung.com>
2021-04-13 16:58:05 +0900

    [tflite] implement serialize
    This patch implements serialize with bunch of NYI functions, structures
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

--- nntrainer/compiler/tflite_interpreter.cpp
+++ nntrainer/compiler/tflite_interpreter.cpp
@@ -12,14 +12,21 @@
 #include <tflite_interpreter.h>
 
 #include <fstream>
+#include <memory>
 #include <string>
 
 #include <tf_schema_generated.h>
 
 #include <nntrainer_error.h>
+#include <node_exporter.h>
+#include <tensor.h>
+
+#define UNUSED(x) x __attribute__((unused))
+#define WILL(x) ;
 
 static constexpr const char *FUNC_TAG = "[TFLITE INTERPRETER] ";
 
+namespace nntrainer {
 namespace {
 /**
  * @brief after finishing building, call this to safe to a file
@@ -42,32 +49,110 @@ void builder2file(const flatbuffers::FlatBufferBuilder &builder,
   os.write((char *)builder.GetBufferPointer(), builder.GetSize());
   os.close();
 }
-} // namespace
 
-namespace nntrainer {
+/**
+ * @brief tensorflow operational node representation. This class contains,
+ * information to build operation flatbuffer
+ *
+ */
+class TfOpNode {
+private:
+  std::vector<std::shared_ptr<Var_Grad>> inputs;
+  std::vector<std::shared_ptr<Var_Grad>> outputs;
+  std::vector<std::shared_ptr<Var_Grad>> weights;
+
+  tflite::BuiltinOperator ops_type;
+
+  /// retrieve this from export_to
+  flatbuffers::Offset<void> builtin_ops;
+  tflite::BuiltinOptions builtin_option_type;
+};
+
+using TfOpNodes = std::vector<TfOpNode>;
+
+/**
+ * @brief tensorflow operation index map, this class manages operation index
+ * mapping
+ *
+ */
+class TfOpIdxMap {
+public:
+  TfOpIdxMap(std::vector<TfOpNode> nodes){};
+
+private:
+  /**
+   * @brief Bidirectional Index map
+   *
+   * @tparam T type of a underyling value
+   */
+  template <typename T> class BidirectionalIndexMap {
+    std::unordered_map<T, unsigned int> data2index; /**< data -> index map */
+    std::vector<T> index2data;                      /**< index -> data map */
+  };
+
+  float empty_buffer[0]; /**< unintialized tensor points to this buffer */
+
+  BidirectionalIndexMap<float *> buffer_map; /**< underlying buffer map */
+  BidirectionalIndexMap<tflite::BuiltinOperator> opcode_map; /**< opcode map */
+  BidirectionalIndexMap<Var_Grad *> variable_map;            /**< tensor map */
+};
+
+TfOpNodes
+buildOpNodes(std::shared_ptr<const GraphRepresentation> representation) {
+  /** NYI! */
+  return TfOpNodes();
+}
+
+flatbuffers::Offset<flatbuffers::Vector<flatbuffers::Offset<tflite::Buffer>>>
+buildBuffers(const TfOpIdxMap &map, flatbuffers::FlatBufferBuilder &fbb) {
+  /** NYI! */
+  return flatbuffers::Offset<
+    flatbuffers::Vector<flatbuffers::Offset<tflite::Buffer>>>();
+}
+
+flatbuffers::Offset<
+  flatbuffers::Vector<flatbuffers::Offset<tflite::OperatorCode>>>
+buildOperatorCodes(const TfOpIdxMap &map, flatbuffers::FlatBufferBuilder &fbb) {
+  /** NYI! */
+  return flatbuffers::Offset<
+    flatbuffers::Vector<flatbuffers::Offset<tflite::OperatorCode>>>();
+};
+
+flatbuffers::Offset<flatbuffers::Vector<flatbuffers::Offset<tflite::SubGraph>>>
+buildSubGraph(const TfOpNodes &nodes, const TfOpIdxMap &map,
+              flatbuffers::FlatBufferBuilder &fbb) {
+  /** NYI! */
+  return flatbuffers::Offset<
+    flatbuffers::Vector<flatbuffers::Offset<tflite::SubGraph>>>();
+}
+
+} // namespace
 
 void TfliteInterpreter::serialize(
   std::shared_ptr<const GraphRepresentation> representation,
   const std::string &out) {
   /// @todo check if graph is finalized
-  flatbuffers::FlatBufferBuilder fb_builder;
+  flatbuffers::FlatBufferBuilder fbb;
 
-  /// @todo parse subgraph
-  /// 1. in&outs&weights
-  /// 2. buffer
-  /// 2. ops
-  /// 3. op_codes
+  auto opNodes = buildOpNodes(representation);
+  TfOpIdxMap map(opNodes); /// build TfOpIdxMap
 
-  auto desc = fb_builder.CreateString("This file is generated from NNTrainer");
+  auto UNUSED(opcodes) = buildOperatorCodes(map, fbb);
+  auto UNUSED(buffers) = buildBuffers(map, fbb);
+  auto UNUSED(subgraph) = buildSubGraph(opNodes, map, fbb);
+  auto desc = fbb.CreateString("This file is generated from NNTrainer");
 
-  tflite::ModelBuilder model_builder(fb_builder);
+  tflite::ModelBuilder model_builder(fbb);
+
+  WILL(model_builder.add_operator_codes(opcode_offset));
+  WILL(model_builder.add_buffers(buffers));
+  WILL(model_builder.add_subgraphs(subgraph));
   model_builder.add_version(3);
   model_builder.add_description(desc);
   auto model = model_builder.Finish();
 
-  fb_builder.Finish(model, tflite::ModelIdentifier());
-
-  builder2file(fb_builder, out);
+  fbb.Finish(model, tflite::ModelIdentifier());
+  builder2file(fbb, out);
 }
 
 std::shared_ptr<GraphRepresentation>
