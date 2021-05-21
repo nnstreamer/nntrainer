58aec74973cfebdd4d5137c02f2b4557e906ecfd
Parichay Kapoor <pk.kapoor@samsung.com>
2021-05-21 20:07:23 +0900

    [split layer] Add forwarding and backwarding
    Add forwarding and backwarding for the split layer
    
    **Self evaluation:**
    1. Build test: [x]Passed [ ]Failed [ ]Skipped
    2. Run test: [x]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Parichay Kapoor <pk.kapoor@samsung.com>

--- nntrainer/layers/split_layer.cpp
+++ nntrainer/layers/split_layer.cpp
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 /**
- * Copyright (C) 2020 Jijoong Moon <jijoong.moon@samsung.com>
+ * Copyright (C) 2021 Parichay Kapoor <pk.kapoor@samsung.com>
  *
  * @file   split_layer.cpp
  * @date   21 May 2021
@@ -46,6 +46,14 @@ int SplitLayer::initialize(Manager &manager) {
     return ML_ERROR_INVALID_PARAMETER;
   }
 
+  /**
+   * The split is only done along the split_dimension dimension.
+   * For example, consider input dimension [b,c,h,w],
+   * 1. split_dimension = 0, output_dim = [1,c,h,w], num_outputs = b
+   * 2. split_dimension = 1, output_dim = [b,1,h,w], num_outputs = c
+   * 3. split_dimension = 2, output_dim = [b,c,1,w], num_outputs = h
+   * 4. split_dimension = 3, output_dim = [b,c,h,1], num_outputs = w
+   */
   const TensorDim &in_dim = input_dim[0];
   setNumOutputs(in_dim.getTensorDim(split_dimension));
 
@@ -59,6 +67,10 @@ int SplitLayer::initialize(Manager &manager) {
   /**
    * Setup input_reshape_helper to which input will be reshaped in forwarding
    * to facilitate easier processing.
+   *
+   * The helper shape consolidates all the dimensions before the split_dimension
+   * together and all the dimensions after the split_dimension to faciliate
+   * easier splitting of the data.
    */
   input_reshape_helper = {1, 1, 1, 1};
   for (unsigned int idx = 0; idx < split_dimension; ++idx) {
@@ -84,15 +96,53 @@ int SplitLayer::initialize(Manager &manager) {
 }
 
 void SplitLayer::forwarding(bool training) {
-  /**
-   * NYI
-   */
+  Tensor &input_ = net_input[0]->getVariableRef();
+
+  input_.reshape(input_reshape_helper);
+
+  for (unsigned int idx = 0; idx < getNumOutputs(); idx++) {
+    Tensor &output_ = net_hidden[0]->getVariableRef();
+    output_.reshape(output_reshape_helper);
+
+    for (unsigned int batch = 0; batch < input_.batch(); batch++) {
+      const Tensor source_tensor = Tensor::Map(
+        input_.getAddress(batch, 0, idx, 0), input_reshape_helper.width(),
+        {1, 1, 1, input_reshape_helper.width()});
+      Tensor dest_tensor = Tensor::Map(
+        output_.getAddress(batch, 0, idx, 0), output_reshape_helper.width(),
+        {1, 1, 1, output_reshape_helper.width()});
+      dest_tensor.copy(source_tensor);
+    }
+
+    output_.reshape(output_dim[idx]);
+  }
+
+  input_.reshape(input_dim[0]);
 }
 
 void SplitLayer::calcDerivative() {
-  /**
-   * NYI
-   */
+  Tensor &input_ = net_input[0]->getGradientRef();
+
+  input_.reshape(input_reshape_helper);
+
+  for (unsigned int idx = 0; idx < getNumOutputs(); idx++) {
+    Tensor &output_ = net_hidden[0]->getGradientRef();
+    output_.reshape(output_reshape_helper);
+
+    for (unsigned int batch = 0; batch < input_.batch(); batch++) {
+      Tensor dest_tensor = Tensor::Map(input_.getAddress(batch, 0, idx, 0),
+                                       input_reshape_helper.width(),
+                                       {1, 1, 1, input_reshape_helper.width()});
+      const Tensor source_tensor = Tensor::Map(
+        output_.getAddress(batch, 0, idx, 0), output_reshape_helper.width(),
+        {1, 1, 1, output_reshape_helper.width()});
+      dest_tensor.copy(source_tensor);
+    }
+
+    output_.reshape(output_dim[idx]);
+  }
+
+  input_.reshape(input_dim[0]);
 }
 
 void SplitLayer::setProperty(const PropertyType type,
@@ -101,7 +151,7 @@ void SplitLayer::setProperty(const PropertyType type,
 
   switch (type) {
   case PropertyType::split_dimension: {
-    if (!split_dimension.empty()) {
+    if (!value.empty()) {
       status = setUint(split_dimension, value);
       throw_status(status);
     }
