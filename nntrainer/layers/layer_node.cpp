e4a7b5438e5619224b37056893a9a8efc24ed9c3
Jihoon Lee <jhoon.it.lee@samsung.com>
2021-05-24 19:14:33 +0900

    [LayerNode] Add print function
    This patch adds simple print function to be used for debugging
    Note that, input_layers are from `layer_internal` for now, but it's
    private so it is commented out
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

--- nntrainer/layers/layer_node.cpp
+++ nntrainer/layers/layer_node.cpp
@@ -22,4 +22,23 @@ std::unique_ptr<LayerNode> createLayerNode(const std::string &type) {
   return std::make_unique<LayerNode>(createLayer(type));
 }
 
+std::ostream &operator<<(std::ostream &out, const LayerNode &l) {
+  out << "[" << l.getName() << '/' << l.getType() << "]\n";
+  auto print_vector = [&out](const std::vector<std::string> &layers,
+                             const std::string &title) {
+    out << title << "[" << layers.size() << "] ";
+    for (auto &layer : layers) {
+      out << layer << ' ';
+    }
+    out << '\n';
+  };
+
+  print_vector(l.input_layers, " input_layers");
+  print_vector(l.output_layers, "output_layers");
+  /// comment intended here,
+  // print_vector(l.getObject()->input_layers, " input_layers");
+  // print_vector(l.getObject()->output_layers, "output_layers");
+  return out;
+}
+
 }; // namespace nntrainer
