8431720b19f0ddcccf0ac4f8c1ebc040f73baa2b
Jihoon Lee <jhoon.it.lee@samsung.com>
2021-06-11 13:42:13 +0900

    [Plugin/Test] Add preset plugin for general test
    This extracts common test to be used multiple times.
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

---
+++ Applications/Custom/LayerPlugin/layer_plugin_common_test.h
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2021 Jihoon Lee <jhoon.it.lee@samsung.com>
+ *
+ * @file   layer_plugin_common_test.h
+ * @date   11 June 2021
+ * @brief  This file contains the parameterized common test of layer plugin
+ * @see    https://github.com/nnstreamer/nntrainer
+ * @author Jihoon Lee <jhoon.it.lee@samsung.com>
+ * @bug    No known bugs except for NYI items
+ *
+ */
+#include <string>
+#include <tuple>
+
+#include <gtest/gtest.h>
+
+/// @todo check if below headers are part of devel header
+#include <app_context.h>
+#include <layer.h>
+
+#ifndef __LAYER_PLUGIN_COMMON_TEST_H__
+#define __LAYER_PLUGIN_COMMON_TEST_H__
+
+static const char *NNTRAINER_PATH = std::getenv("NNTRAINER_PATH");
+
+/**
+ * @brief layer plugin common test cases
+ * @todo expand this to typed_test_p for other types of example (eg) optimizer
+ *
+ */
+class LayerPluginCommonTest
+  : public ::testing::TestWithParam<std::tuple<std::string, std::string>> {
+
+public:
+  /**
+   * @brief SetUp test cases here
+   *
+   */
+  virtual void SetUp() {
+    ASSERT_NE(NNTRAINER_PATH, nullptr)
+      << "NNTRAINER_PATH environment value must be set";
+
+    const auto &params = GetParam();
+    plugin_lib_name = std::get<0>(params);
+    layer_type_name = std::get<1>(params);
+    ac = nntrainer::AppContext();
+  };
+
+  /**
+   * @brief do here if any memory needs to be released
+   *
+   */
+  virtual void TearDown(){};
+
+protected:
+  nntrainer::AppContext ac;    /**< AppContext          */
+  std::string plugin_lib_name; /**< plugin library name */
+  std::string layer_type_name; /**< layer type name */
+};
+#endif // __LAYER_PLUGIN_COMMON_TEST_H__
