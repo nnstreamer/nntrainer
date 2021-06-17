baf99159ed45e6e930ab2ac49a8b704459f27d79
Parichay Kapoor <pk.kapoor@samsung.com>
2021-06-17 14:52:27 +0900

    [networkgraph] Network graph updated for Layer V2
    Network graph updated to work with LayerV2
    This involves settings input dimension in InitContext, and setting
    up RunContext for each layer before their execution.
    
    Further, some helper functions are also added in LayerNode.
    
    Signed-off-by: Parichay Kapoor <pk.kapoor@samsung.com>

--- nntrainer/graph/network_graph.h
+++ nntrainer/graph/network_graph.h
@@ -262,8 +262,10 @@ public:
    * @param init_context Init layer context to create run context
    * @param run_context Run layer context to be created
    */
-  static void updateRunContext(std::shared_ptr<Manager> &manager,
-                               const std::shared_ptr<LayerNode> &lnode);
+  static std::vector<Var_Grad *>
+  updateRunContext(std::shared_ptr<Manager> &manager,
+                   const std::shared_ptr<LayerNode> &lnode,
+                   const std::vector<Var_Grad *> &inputs);
 
 private:
   std::map<std::string, std::string> sub_in_out; /** This is map to identify
