1c7b7eb3ec78210406bb055910c78842b119c337
Parichay Kapoor <pk.kapoor@samsung.com>
2021-06-23 16:04:39 +0900

    [losslayer] Update loss layer with LayerV2 design
    This patch update loss layer with LayerV2 design.
    In order to limit the interface to the updated Layer interface,
    different type of loss layers are split into individual classes
    extending LossLayer class. LossLayer class is an abstract class
    providing some common utility functions to all the loss functions.
    
    Also added loss to RunLayerContext, which allows setting and getting
    loss. However, note that although this loss value can be set/get by any
    layer for themselves, it will only be used by the model for loss layers.
    
    Signed-off-by: Parichay Kapoor <pk.kapoor@samsung.com>
    
    [loss layer v2] loss layer v2
    
    loss layer v2
    
    Signed-off-by: Parichay Kapoor <pk.kapoor@samsung.com>

---
+++ nntrainer/layers/loss/cross_entropy_softmax_loss_layer.cpp
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2021 Parichay Kapoor <pk.kapoor@samsung.com>
+ *
+ * @file   cross_entropy_Softmax_loss_layer.cpp
+ * @date   24 June 2021
+ * @brief  This is MSE Loss Layer Class of Neural Network
+ * @see    https://github.com/nnstreamer/nntrainer
+ * @author Parichay Kapoor <pk.kapoor@samsung.com>
+ * @bug    No known bugs except for NYI items
+ *
+ */
+
+#include <cmath>
+
+#include <cross_entropy_softmax_loss_layer.h>
+
+#include <acti_func.h>
+#include <lazy_tensor.h>
+#include <util_func.h>
+
+namespace nntrainer {
+
+static constexpr size_t SINGLE_INOUT_IDX = 0;
+
+void CrossEntropySoftmaxLossLayer::forwarding(RunLayerContext &context,
+                                              bool training) {
+  Tensor &hidden_ = context.getOutput(SINGLE_INOUT_IDX);
+  // TODO: try Tensor & - it should work
+  Tensor y = context.getInput(SINGLE_INOUT_IDX);
+
+  // fill the output
+  hidden_ = y.apply(ActiFunc::softmax, hidden_);
+
+  if (context.isLabelAvailable(SINGLE_INOUT_IDX)) {
+    Tensor &y2 = context.getLabel(SINGLE_INOUT_IDX);
+    l = y2.multiply(hidden_.apply(logFloat)).sum_by_batch().multiply(-1);
+
+    // update the loss value
+    updateLoss(context, l);
+  }
+}
+
+void CrossEntropySoftmaxLossLayer::calcDerivative(RunLayerContext &context) {
+  Tensor &ret_derivative = context.getOutgoingDerivative(SINGLE_INOUT_IDX);
+  Tensor &y2 = context.getIncomingDerivative(SINGLE_INOUT_IDX);
+  Tensor &y = context.getInput(SINGLE_INOUT_IDX);
+
+  Tensor ret;
+
+  /// @note y and ret_derivative can be same here, so this has to be out-place
+  /// operation
+  // TODO: verify y and ret_derivative must not be same as loss layer is not
+  // working in-place
+  y.apply(ActiFunc::softmax, ret);
+  ret.subtract(y2, ret_derivative);
+  if (ret_derivative.divide_i(ret.batch()) != ML_ERROR_NONE) {
+    throw std::runtime_error("[CrossEntropySoftmaxLossLayer::calcDerivative] "
+                             "Error when calculating loss");
+  }
+}
+
+} // namespace nntrainer
