77e94ace3f1a7e62c05810087f7a1a87d9f4524f
Parichay Kapoor <pk.kapoor@samsung.com>
2021-06-23 16:04:39 +0900

    [losslayer] Update loss layer with LayerV2 design
    This patch update loss layer with LayerV2 design.
    In order to limit the interface to the updated Layer interface,
    different type of loss layers are split into individual classes
    extending LossLayer class. LossLayer class is an abstract class
    providing some common utility functions to all the loss functions.
    
    Also added loss to RunLayerContext, which allows setting and getting
    loss. However, note that although this loss value can be set/get by any
    layer for themselves, it will only be used by the model for loss layers.
    
    Signed-off-by: Parichay Kapoor <pk.kapoor@samsung.com>
    
    [loss layer v2] loss layer v2
    
    loss layer v2
    
    Signed-off-by: Parichay Kapoor <pk.kapoor@samsung.com>

---
+++ nntrainer/layers/loss/cross_entropy_loss_layer.h
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2021 Parichay Kapoor <pk.kapoor@samsung.com>
+ *
+ * @file   cross_entropy_loss_layer.h
+ * @date   24 June 2021
+ * @brief  This is Cross Entropy Loss Layer Class of Neural Network
+ * @see    https://github.com/nnstreamer/nntrainer
+ * @author Parichay Kapoor <pk.kapoor@samsung.com>
+ * @bug    No known bugs except for NYI items
+ *
+ */
+
+#ifndef __CROSS_ENTROPY_LOSS_LAYER_H__
+#define __CROSS_ENTROPY_LOSS_LAYER_H__
+#ifdef __cplusplus
+
+#include <loss_layer.h>
+
+namespace nntrainer {
+
+/**
+ * @class   CrossEntropyLossLayer
+ * @brief   Cross Entropy Loss Layer
+ */
+class CrossEntropyLossLayer : public LossLayer {
+public:
+  /**
+   * @brief     Constructor of Cross Entropy Loss Layer
+   */
+  CrossEntropyLossLayer() : LossLayer() {}
+
+  /**
+   * @brief     Destructor of Cross Entropy Loss Layer
+   */
+  ~CrossEntropyLossLayer() = default;
+
+  /**
+   * @copydoc Layer::forwarding(RunLayerContext &context, bool training)
+   */
+  void forwarding(RunLayerContext &context, bool training) override {
+    return exception::not_supported("Cross Entropy not supported without softmax or sigmoid");
+  }
+
+  /**
+   * @copydoc Layer::calcDerivative(RunLayerContext &context)
+   */
+  void calcDerivative(RunLayerContext &context) override {
+    return exception::not_supported("Cross Entropy not supported without softmax or sigmoid");
+  }
+
+  /**
+   * @copydoc Layer::getType()
+   */
+  const std::string getType() const override { return CrossEntropyLossLayer::type; };
+
+  inline static const std::string type = "cross_entropy_loss";
+};
+} // namespace nntrainer
+
+#endif /* __cplusplus */
+#endif /* __CROSS_ENTROPY_LOSS_LAYER_H__ */
+
---
+++ nntrainer/layers/loss/mse_loss_layer.cpp
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2021 Parichay Kapoor <pk.kapoor@samsung.com>
+ *
+ * @file   mse_loss_layer.cpp
+ * @date   24 June 2021
+ * @brief  This is MSE Loss Layer Class of Neural Network
+ * @see    https://github.com/nnstreamer/nntrainer
+ * @author Parichay Kapoor <pk.kapoor@samsung.com>
+ * @bug    No known bugs except for NYI items
+ *
+ */
+
+#include <mse_loss_layer.h>
+#include <lazy_tensor.h>
+
+namespace nntrainer {
+
+static constexpr size_t SINGLE_INOUT_IDX = 0;
+
+void MSELossLayer::forwarding(RunLayerContext &context, bool training) {
+  Tensor &hidden_ = context.getOutput(SINGLE_INOUT_IDX);
+  // TODO: try Tensor & - it should work
+  Tensor y = context.getInput(SINGLE_INOUT_IDX);
+  Tensor l;
+
+  // fill the output
+  hidden_.fill(y);
+  // y2 <- y2 - y;
+  if (context.isLabelAvailable(SINGLE_INOUT_IDX)) {
+    Tensor &y2 = context.getLabel(SINGLE_INOUT_IDX);
+    Tensor residual = y2.subtract(y);
+    l = residual.chain().multiply_i(residual).average().run();
+    updateLoss(context, l);
+  }
+}
+
+void MSELossLayer::calcDerivative(RunLayerContext &context) {
+  Tensor &ret_derivative = context.getOutgoingDerivative(SINGLE_INOUT_IDX);
+  Tensor &y2 = context.getIncomingDerivative(SINGLE_INOUT_IDX);
+  Tensor &y = context.getInput(SINGLE_INOUT_IDX);
+
+  y.subtract(y2, ret_derivative);
+  ret_derivative.multiply_i(2);
+  if (ret_derivative.divide_i(y.length()) != ML_ERROR_NONE) {
+    throw std::runtime_error(
+      "[MSELossLayer::calcDerivative] Error when calculating loss");
+  }
+}
+
+} // namespace nntrainer
