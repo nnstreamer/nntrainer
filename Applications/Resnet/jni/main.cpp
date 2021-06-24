acf847e72a3e8befc41605639697c2217f3ec0ba
Jihoon Lee <jhoon.it.lee@samsung.com>
2021-06-24 14:13:00 +0900

    [Skeleton] Add resnet application skeleton
    This patch adds resnet application skeleton
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

---
+++ Applications/Resnet/jni/main.cpp
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2020 Jihoon Lee <jhoon.it.lee@samsung.com>
+ *
+ * @file   main.cpp
+ * @date   24 Jun 2021
+ * @brief  task runner for the resnet
+ * @see    https://github.com/nnstreamer/nntrainer
+ * @author Jihoon Lee <jhoon.it.lee@samsung.com>
+ * @bug    No known bugs except for NYI items
+ */
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include <vector>
+
+#include <layer.h>
+
+int main() {
+  std::cout << "Hello world\n";
+
+  return 0;
+}
