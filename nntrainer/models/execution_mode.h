73e3e3c9f6bb773ded422a791396ea6c89fc9af6
Parichay Kapoor <pk.kapoor@samsung.com>
2021-06-25 19:24:16 +0900

    [executionMode] Added mode of execution
    Added mode of execution. This would be passed to the layers
    as well as used internally than just keeping bool training to denote
    the current mode of execution.
    The header can be kept separate so as to be able to import it whereever
    needed without being header heavy.
    
    Signed-off-by: Parichay Kapoor <pk.kapoor@samsung.com>

---
+++ nntrainer/models/execution_mode.h
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2021 Parichay Kapoor <pk.kapoor@samsung.com>
+ *
+ * @file   execution_mode.h
+ * @date   25 June 2020
+ * @see	   https://github.com/nnstreamer/nntrainer
+ * @author Parichay Kapoor <pk.kapoor@samsung.com>
+ * @bug	   No known bugs except for NYI items
+ * @brief  This is mode of executions
+ *
+ */
+
+#ifndef __EXECUTION_MODE_H__
+#define __EXECUTION_MODE_H__
+
+namespace nntrainer {
+
+/**
+ * @brief   class telling the execution mode of the model/operation
+ */
+enum class ExecutionMode {
+  TRAIN,     /** Training mode, label is necessary */
+  INFERENCE, /** Inference mode, label is optional */
+  VALIDATE   /** Validate mode, label is necessary */
+}
+
+}; // namespace nntrainer
+
+#endif // __EXECUTION_MODE_H__
