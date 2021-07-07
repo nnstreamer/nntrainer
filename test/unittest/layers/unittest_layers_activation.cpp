c8cf673d50b3d36faeb294cb85255b61e4eb38ab
Parichay Kapoor <pk.kapoor@samsung.com>
2021-07-07 15:10:31 +0900

    [test] Add semantic tests for activation layer
    Add semantic tests for activation layer
    
    Signed-off-by: Parichay Kapoor <pk.kapoor@samsung.com>

---
+++ test/unittest/layers/unittest_layers_activation.cpp
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2021 Parichay Kapoor <pk.kapoor@samsung.com>
+ *
+ * @file unittest_layers_pooling.cpp
+ * @date 7 July 2021
+ * @brief Activation Layer Test
+ * @see	https://github.com/nnstreamer/nntrainer
+ * @author Parichay Kapoor <pk.kapoor@samsung.com>
+ * @bug No known bugs except for NYI items
+ */
+#include <tuple>
+
+#include <gtest/gtest.h>
+
+#include <activation_layer.h>
+#include <layers_common_tests.h>
+
+auto semantic_activation_relu = LayerSemanticsParamType(
+  nntrainer::createLayer<nntrainer::ActivationLayer>,
+  nntrainer::ActivationLayer::type, {"activation=relu"}, 0, false);
+
+auto semantic_activation_sigmoid = LayerSemanticsParamType(
+  nntrainer::createLayer<nntrainer::ActivationLayer>,
+  nntrainer::ActivationLayer::type, {"activation=sigmoid"}, 0, false);
+
+auto semantic_activation_softmax = LayerSemanticsParamType(
+  nntrainer::createLayer<nntrainer::ActivationLayer>,
+  nntrainer::ActivationLayer::type, {"activation=softmax"}, 0, false);
+
+auto semantic_activation_tanh = LayerSemanticsParamType(
+  nntrainer::createLayer<nntrainer::ActivationLayer>,
+  nntrainer::ActivationLayer::type, {"activation=tanh"}, 0, false);
+
+auto semantic_activation_none = LayerSemanticsParamType(
+  nntrainer::createLayer<nntrainer::ActivationLayer>,
+  nntrainer::ActivationLayer::type, {"activation=none"}, 0, false);
+
+INSTANTIATE_TEST_CASE_P(ActivationNone, LayerSemantics,
+                        ::testing::Values(semantic_activation_relu,
+                                          semantic_activation_sigmoid,
+                                          semantic_activation_softmax,
+                                          semantic_activation_tanh,
+                                          semantic_activation_none));
