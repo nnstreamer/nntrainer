accd073073c163b296dd5be98b346fc6b36e9cd3
Parichay Kapoor <kparichay@gmail.com>
2021-07-12 03:21:29 +0900

    [layer] Enable split layer for V2 design
    Enable split layer for v2 design.
    
    Signed-off-by: Parichay Kapoor <kparichay@gmail.com>

---
+++ test/unittest/layers/unittest_layers_split.cpp
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2021 Parichay Kapoor <pk.kapoor@samsung.com>
+ *
+ * @file unittest_layers_fully_connected.cpp
+ * @date 12 June 2021
+ * @brief Split Layer Test
+ * @see	https://github.com/nnstreamer/nntrainer
+ * @author Parichay Kapoor <pk.kapoor@samsung.com>
+ * @bug No known bugs except for NYI items
+ */
+#include <tuple>
+
+#include <gtest/gtest.h>
+
+#include <layers_common_tests.h>
+#include <split_layer.h>
+
+auto semantic_split =
+  LayerSemanticsParamType(nntrainer::createLayer<nntrainer::SplitLayer>,
+                          nntrainer::SplitLayer::type, {}, {}, 0, false);
+
+INSTANTIATE_TEST_CASE_P(Split, LayerSemantics,
+                        ::testing::Values(semantic_split));
