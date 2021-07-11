2d24517c87d22be7d2faaa8887462913cfe3755f
Parichay Kapoor <kparichay@gmail.com>
2021-07-12 04:03:58 +0900

    [layer] Update concat layer to V2
    Update concat layer to V2 design.
    
    Signed-off-by: Parichay Kapoor <kparichay@gmail.com>

--- nntrainer/layers/concat_layer.cpp
+++ nntrainer/layers/concat_layer.cpp
@@ -21,92 +21,98 @@
 
 namespace nntrainer {
 
-int ConcatLayer::initialize(Manager &manager) {
-  int status = ML_ERROR_NONE;
+static constexpr size_t SINGLE_INOUT_IDX = 0;
+
+void ConcatLayer::finalize(InitLayerContext &context) {
   unsigned int channel = 0;
 
-  if (getNumInputs() == 0) {
-    ml_loge("Error: number of inputs are not initialized");
-    return ML_ERROR_INVALID_PARAMETER;
-  }
+  auto const &input_dims = context.getInputDimensions();
+  const TensorDim &input_dim_0 = input_dims[SINGLE_INOUT_IDX];
+  channel += input_dim_0.channel();
+  for (unsigned int idx = 1; idx < context.getNumInputs(); ++idx) {
+    const TensorDim &dim = input_dims[idx];
 
-  const TensorDim &d = input_dim[0];
-  channel += d.channel();
-  for (unsigned int idx = 1; idx < getNumInputs(); ++idx) {
-    const TensorDim &dim = input_dim[idx];
-
-    for (unsigned int i = 2; i < d.rank(); ++i) {
-      if (d[i] != dim[i])
+    for (unsigned int i = 2; i < input_dim_0.rank(); ++i) {
+      if (input_dim_0[i] != dim[i])
         throw std::runtime_error("Error: concat layer requires same "
                                  "shape from  all input layers");
     }
-    channel += input_dim[idx].channel();
+    channel += dim.channel();
   }
 
-  output_dim[0] = input_dim[0];
-  output_dim[0].channel(channel);
+  TensorDim output_dim = input_dim_0;
+  output_dim.channel(channel);
 
-  return status;
+  context.setOutputDimensions({output_dim});
 }
 
-void ConcatLayer::forwarding(bool training) {
-  Tensor &hidden_ = net_hidden[0]->getVariableRef();
+void ConcatLayer::forwarding(RunLayerContext &context, bool training) {
+  Tensor &hidden_ = context.getOutput(SINGLE_INOUT_IDX);
+  const TensorDim &output_dim = hidden_.getDim();
 
 #ifdef DEBUG
+  const TensorDim &input_dim = context.getInput(SINGLE_INOUT_IDX).getDim();
   unsigned int channel = 0;
-  const TensorDim &d = net_input[0]->getDim();
-  channel += d.channel();
-  for (unsigned int idx = 1; idx < getNumInputs(); ++idx) {
-    const TensorDim &dim = net_input[idx]->getDim();
+  channel += input_dim.channel();
+  for (unsigned int idx = 1; idx < context.getNumInputs(); ++idx) {
+    const TensorDim &dim = context.getInput(idx).getDim();
 
-    for (unsigned int i = 2; i < d.rank(); ++i) {
-      if (d[i] != dim[i])
+    for (unsigned int i = 2; i < input_dim.rank(); ++i) {
+      if (input_dim[i] != dim[i])
         throw std::runtime_error("Error: concat layer requires same "
                                  "shape from  all input layers");
     }
-    channel += input_dim[idx].channel();
+    channel += dim.channel();
   }
 
-  if (channel != output_dim[0].channel())
+  if (channel != output_dim.channel())
     throw std::runtime_error(
       "Error: Sum of channel of input layers is not same with output channel");
 #endif
 
-  unsigned int f_size = output_dim[0].getFeatureLen();
+  unsigned int f_size = output_dim.getFeatureLen();
 
   /**
    * @todo avoid copy by creating input here as a shared_tensor of the output
    * here and then this layer can be in_place as well
    */
-  for (unsigned int b = 0; b < input_dim[0].batch(); ++b) {
+  for (unsigned int b = 0; b < output_dim.batch(); ++b) {
     unsigned int position = 0;
-    for (unsigned int idx = 0; idx < getNumInputs(); ++idx) {
-      TensorDim in_dim = net_input[idx]->getDim();
-      memcpy(
-        hidden_.getAddress(b * f_size + position),
-        net_input[idx]->getVariable().getAddress(b * in_dim.getFeatureLen()),
-        in_dim.getFeatureLen() * sizeof(float));
+    for (unsigned int idx = 0; idx < context.getNumInputs(); ++idx) {
+      Tensor &input_ = context.getInput(idx);
+      TensorDim in_dim = input_.getDim();
+      memcpy(hidden_.getAddress(b * f_size + position),
+             input_.getAddress(b * in_dim.getFeatureLen()),
+             in_dim.getFeatureLen() * sizeof(float));
       position += in_dim.getFeatureLen();
     }
   }
 }
 
-void ConcatLayer::calcDerivative() {
-  TensorDim d = net_hidden[0]->getDim();
+void ConcatLayer::calcDerivative(RunLayerContext &context) {
+  Tensor &derivative_ = context.getIncomingDerivative(SINGLE_INOUT_IDX);
+  TensorDim d = derivative_.getDim();
 
   unsigned int position = 0;
-  for (unsigned int idx = 0; idx < getNumInputs(); ++idx) {
-    TensorDim in_dim = input_dim[idx];
+  for (unsigned int idx = 0; idx < context.getNumInputs(); ++idx) {
+    Tensor &ret_ = context.getOutgoingDerivative(idx);
+    TensorDim in_dim = ret_.getDim();
 
     for (unsigned int b = 0; b < in_dim.batch(); ++b) {
       // TODO: replace with tensor::copy/fill
-      memcpy(
-        net_input[idx]->getGradient().getAddress(b * in_dim.getFeatureLen()),
-        net_hidden[0]->getGradient().getAddress(b * d.getFeatureLen() +
-                                                position),
-        in_dim.getFeatureLen() * sizeof(float));
+      memcpy(ret_.getAddress(b * in_dim.getFeatureLen()),
+             derivative_.getAddress(b * d.getFeatureLen() + position),
+             in_dim.getFeatureLen() * sizeof(float));
     }
     position += in_dim.getFeatureLen();
+  }
+}
+
+void ConcatLayer::setProperty(const std::vector<std::string> &values) {
+  if (!values.empty()) {
+    std::string msg = "[ConcatLayer] Unknown Layer Properties count " +
+                      std::to_string(values.size());
+    throw exception::not_supported(msg);
   }
 }
 
