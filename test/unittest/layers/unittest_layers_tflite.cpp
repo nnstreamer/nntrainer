7644fcc29225893b31966667616a7d0236074595
Parichay Kapoor <kparichay@gmail.com>
2021-07-12 12:03:34 +0900

    [layer] Enable tflite layer for V2
    This patch enables tflite layer for V2 design.
    
    Signed-off-by: Parichay Kapoor <kparichay@gmail.com>

---
+++ test/unittest/layers/unittest_layers_tflite.cpp
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2021 Parichay Kapoor <pk.kapoor@samsung.com>
+ *
+ * @file unittest_layers_tflite.cpp
+ * @date 12 June 2021
+ * @brief TfLite Layer Test
+ * @see	https://github.com/nnstreamer/nntrainer
+ * @author Parichay Kapoor <pk.kapoor@samsung.com>
+ * @bug No known bugs except for NYI items
+ */
+#include <tuple>
+
+#include <gtest/gtest.h>
+
+#include <layers_common_tests.h>
+#include <tflite_layer.h>
+
+auto semantic_tflite = LayerSemanticsParamType(
+  nntrainer::createLayer<nntrainer::TfLiteLayer>, nntrainer::TfLiteLayer::type,
+  {"modelfile=../test/test_models/models/add.tflite"}, {}, 0, false);
+
+INSTANTIATE_TEST_CASE_P(TfLite, LayerSemantics,
+                        ::testing::Values(semantic_tflite));
