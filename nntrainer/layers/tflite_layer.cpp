d75dde1c993e25c69b60b2db99c76592a4d3e62d
Parichay Kapoor <kparichay@gmail.com>
2021-07-12 12:03:34 +0900

    [layer] Enable tflite layer for V2
    This patch enables tflite layer for V2 design.
    
    Signed-off-by: Parichay Kapoor <kparichay@gmail.com>

--- nntrainer/layers/tflite_layer.cpp
+++ nntrainer/layers/tflite_layer.cpp
@@ -10,6 +10,7 @@
  * @bug    No known bugs except for NYI items
  */
 
+#include <layer_internal.h>
 #include <nntrainer_error.h>
 #include <nntrainer_log.h>
 #include <tflite_layer.h>
@@ -39,73 +40,85 @@ void TfLiteLayer::setDimensions(const std::vector<int> &tensor_idx_list,
   }
 }
 
-int TfLiteLayer::initialize(Manager &manager) {
+void TfLiteLayer::finalize(InitLayerContext &context) {
   tflite::ops::builtin::BuiltinOpResolver resolver;
 
   model = tflite::FlatBufferModel::BuildFromFile(modelfile.c_str());
   if (!model)
-    return ML_ERROR_INVALID_PARAMETER;
+    throw std::invalid_argument("Failed to build tflite model");
 
   tflite::InterpreterBuilder(*model.get(), resolver)(&interpreter);
   if (!interpreter)
-    return ML_ERROR_INVALID_PARAMETER;
+    throw std::invalid_argument("Failed to build tflite interpreter");
 
   if (interpreter->AllocateTensors() != kTfLiteOk)
     throw std::runtime_error("Failed to allocate tensors!");
 
   std::vector<TensorDim> dims;
   setDimensions(interpreter->inputs(), dims, false);
-  setDimensions(interpreter->outputs(), output_dim, true);
+  const std::vector<TensorDim> &input_dims = context.getInputDimensions();
 
-  if (input_dim.size() && input_dim[0].getTensorDim(0) != 0) {
-    if (dims.size() != input_dim.size())
-      throw std::invalid_argument(
-        "Provided number of input dimensions mismatch");
-
-    for (size_t idx = 0; idx < dims.size(); idx++) {
-      if (dims[idx] != input_dim[idx])
-        throw std::invalid_argument("Input dimensions mismatch");
-    }
-  } else {
-    input_dim.resize(dims.size());
-    std::copy(dims.begin(), dims.end(), input_dim.begin());
-  }
-
-  return ML_ERROR_NONE;
-}
-
-void TfLiteLayer::setProperty(const PropertyType type,
-                              const std::string &value) {
-  switch (type) {
-  case PropertyType::modelfile: {
-    if (!value.empty())
-      modelfile = value;
-  } break;
-  default:
-    LayerV1::setProperty(type, value);
-    break;
-  }
-}
-
-void TfLiteLayer::forwarding(bool training) {
-#ifdef DEBUG
-  if (net_input.size() != input_dim.size())
+  if (dims.size() != input_dims.size())
     throw std::invalid_argument("Provided number of input dimensions mismatch");
 
-  for (unsigned int idx = 0; idx < input_dim.size(); idx++) {
-    if (net_input[idx]->getDim() != input_dim[idx])
+  for (size_t idx = 0; idx < dims.size(); idx++) {
+    if (dims[idx] != input_dims[idx])
       throw std::invalid_argument("Input dimensions mismatch");
   }
-#endif
+
+  std::vector<TensorDim> output_dims;
+  setDimensions(interpreter->outputs(), output_dims, true);
+  context.setOutputDimensions(output_dims);
+}
+
+void TfLiteLayer::setProperty(const std::vector<std::string> &values) {
+  /// @todo: deprecate this in favor of loadProperties
+  for (unsigned int i = 0; i < values.size(); ++i) {
+    std::string key;
+    std::string value;
+    std::stringstream ss;
+
+    if (getKeyValue(values[i], key, value) != ML_ERROR_NONE) {
+      throw std::invalid_argument("Error parsing the property: " + values[i]);
+    }
+
+    if (value.empty()) {
+      ss << "value is empty: key: " << key << ", value: " << value;
+      throw std::invalid_argument(ss.str());
+    }
+
+    /// @note this calls derived setProperty if available
+    setProperty(key, value);
+  }
+}
+
+void TfLiteLayer::setProperty(const std::string &type_str,
+                              const std::string &value) {
+  using PropertyType = LayerV1::PropertyType;
+  LayerV1::PropertyType type =
+    static_cast<LayerV1::PropertyType>(parseLayerProperty(type_str));
+
+  switch (type) {
+  case PropertyType::modelfile: {
+    modelfile = value;
+  } break;
+  default:
+    std::string msg = "[TfLiteLayer] Unknown Layer Property Key for value " +
+                      std::string(value);
+    throw exception::not_supported(msg);
+  }
+}
+
+void TfLiteLayer::forwarding(RunLayerContext &context, bool training) {
   auto in_indices = interpreter->inputs();
-  for (size_t idx = 0; idx < net_input.size(); idx++)
+  for (size_t idx = 0; idx < in_indices.size(); idx++)
     interpreter->tensor(in_indices[idx])->data.raw =
-      (char *)net_input[idx]->getVariableRef().getData();
+      reinterpret_cast<char *>(context.getInput(idx).getData());
 
   auto out_indices = interpreter->outputs();
   for (size_t idx = 0; idx < out_indices.size(); idx++) {
     interpreter->tensor(out_indices[idx])->data.raw =
-      reinterpret_cast<char *>(net_hidden[idx]->getVariableRef().getData());
+      reinterpret_cast<char *>(context.getOutput(idx).getData());
   }
 
   int status = interpreter->Invoke();
@@ -129,14 +142,7 @@ void TfLiteLayer::forwarding(bool training) {
 #endif
 }
 
-void TfLiteLayer::copy(std::shared_ptr<LayerV1> l) {
-  LayerV1::copy(l);
-
-  std::shared_ptr<TfLiteLayer> from = std::static_pointer_cast<TfLiteLayer>(l);
-  this->modelfile = from->modelfile;
-}
-
-void TfLiteLayer::calcDerivative() {
+void TfLiteLayer::calcDerivative(RunLayerContext &context) {
   throw exception::not_supported(
     "calcDerivative is not supported for tflite layer");
 }
