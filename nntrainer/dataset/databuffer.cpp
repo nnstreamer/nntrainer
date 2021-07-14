4c3289a36d2997bf537d53cf0ddfea3cd71ef07c
Jihoon Lee <jhoon.it.lee@samsung.com>
2021-07-15 01:42:49 +0900

    [Dataset] Remove unused part from refactoring
    This patch removes parts that are no longer used from the refactoring
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

--- nntrainer/dataset/databuffer.cpp
+++ nntrainer/dataset/databuffer.cpp
@@ -62,183 +62,11 @@ public:
 
 constexpr char USER_DATA[] = "user_data";
 
-DataBuffer::DataBuffer(DatasetType type) :
-  data_buffer_type(type),
-  batch_producer(),
-  data_q(),
-  label_q(),
-  queue_status(DataStatus::DATA_NOT_READY),
-  buf_size(0),
-  cur_bufsize(0),
-  class_num(0),
-  batch_size(0),
-  samples_per_epoch(0),
-  remaining_samples_per_epoch(0),
-  is_running(false),
-  initialized(false),
-  producer(nullptr),
-  db_props(new Props()) {
-  rng.seed(getSeed());
-};
-
 DataBuffer::DataBuffer(std::unique_ptr<DataProducer> &&producer_) :
   producer(std::move(producer_)),
-  db_props(new Props()) {
-  rng.seed(getSeed());
-}
+  db_props(new Props()) {}
 
 DataBuffer::~DataBuffer(){};
-
-int DataBuffer::rangeRandom(int min, int max) {
-  std::uniform_int_distribution<int> dist(min, max);
-  return dist(rng);
-}
-
-int DataBuffer::run() {
-  if (queue_status == DataStatus::DATA_ERROR)
-    return ML_ERROR_INVALID_PARAMETER;
-
-  if (initialized) {
-    this->is_running = true;
-    this->batch_producer = std::thread(&DataBuffer::updateData, this);
-    if (consumer_exception_ptr) {
-      try {
-        std::rethrow_exception(consumer_exception_ptr);
-      } catch (const std::exception &ex) {
-        std::cout << ex.what() << "\n";
-        return ML_ERROR_INVALID_PARAMETER;
-      }
-    }
-  } else {
-    ml_loge("Error: Training Data Set is not valid");
-    return ML_ERROR_INVALID_PARAMETER;
-  }
-
-  return ML_ERROR_NONE;
-}
-
-int DataBuffer::clear() {
-  int status = ML_ERROR_NONE;
-  setStateAndNotify(DataStatus::DATA_NOT_READY);
-  is_running = false;
-  if (initialized && true == batch_producer.joinable())
-    batch_producer.join();
-  this->data_q.clear();
-  this->label_q.clear();
-  this->cur_bufsize = 0;
-  this->remaining_samples_per_epoch = samples_per_epoch;
-  return status;
-}
-
-bool DataBuffer::getDataFromBuffer(float *out, float *label) {
-  using QueueType = std::vector<std::vector<float>>;
-
-  auto wait_for_data_fill = [](std::mutex &ready_mutex,
-                               std::condition_variable &cv, DataStatus &flag,
-                               const unsigned int batch_size,
-                               QueueType &queue) {
-    while (true) {
-      std::unique_lock<std::mutex> ul(ready_mutex);
-      cv.wait(ul, [&]() -> bool { return flag != DataStatus::DATA_NOT_READY; });
-      if (flag == DataStatus::DATA_ERROR || flag == DataStatus::DATA_END)
-        return queue.size() < batch_size ? false : true;
-
-      if (flag == DataStatus::DATA_READY && queue.size() >= batch_size)
-        return true;
-    }
-
-    throw std::logic_error("[getDataFromBuffer] control should not reach here");
-  };
-
-  auto fill_bundled_data_from_queue =
-    [](std::mutex &q_lock, QueueType &q, const unsigned int batch_size,
-       const unsigned int feature_size, float *buf) {
-      for (unsigned int b = 0; b < batch_size; ++b)
-        std::copy(q[b].begin(), q[b].begin() + feature_size,
-                  buf + b * feature_size);
-
-      q_lock.lock();
-      q.erase(q.begin(), q.begin() + batch_size);
-      q_lock.unlock();
-    };
-
-  /// facade that wait for the databuffer to be filled and pass it to outparam
-  /// note that batch_size is passed as an argument because it can vary by
-  /// DatasetDataUsageType::BUF_TYPE later...
-  auto fill_out_params =
-    [&](std::mutex &ready_mutex, std::condition_variable &cv, DataStatus &flag,
-        QueueType &data_q, QueueType &label_q, const unsigned int batch_size,
-        unsigned int &cur_bufsize) {
-      if (!wait_for_data_fill(ready_mutex, cv, flag, batch_size, data_q)) {
-        return false;
-      }
-
-      fill_bundled_data_from_queue(data_lock, data_q, batch_size,
-                                   this->input_dim.getFeatureLen(), out);
-      fill_bundled_data_from_queue(data_lock, label_q, batch_size,
-                                   this->class_num, label);
-
-      cur_bufsize -= batch_size;
-      return true;
-    };
-
-  return fill_out_params(status_mutex, cv_status, queue_status, data_q, label_q,
-                         batch_size, cur_bufsize);
-}
-
-int DataBuffer::setClassNum(unsigned int num) {
-  int status = ML_ERROR_NONE;
-  if (num == 0) {
-    ml_loge("Error: number of class should be bigger than 0");
-    initialized = false;
-    return ML_ERROR_INVALID_PARAMETER;
-  }
-  class_num = num;
-  return status;
-}
-
-int DataBuffer::setBatchSize(unsigned int size) {
-  int status = ML_ERROR_NONE;
-  if (size == 0) {
-    ml_loge("Error: batch size must be greater than 0");
-    initialized = false;
-    return ML_ERROR_INVALID_PARAMETER;
-  }
-  batch_size = size;
-  return status;
-}
-
-int DataBuffer::init() {
-  if (batch_size == 0) {
-    ml_loge("Error: batch size must be greater than 0");
-    initialized = false;
-    return ML_ERROR_INVALID_PARAMETER;
-  }
-
-  if (buf_size < batch_size) {
-    if (buf_size > 1) {
-      ml_logw("Dataset buffer size reset to be at least batch size");
-    }
-    buf_size = batch_size;
-  }
-
-  if (!class_num) {
-    ml_loge("Error: number of class must be set");
-    initialized = false;
-    return ML_ERROR_INVALID_PARAMETER;
-  }
-
-  if (!this->input_dim.getFeatureLen()) {
-    ml_loge("Error: feature size must be set");
-    initialized = false;
-    return ML_ERROR_INVALID_PARAMETER;
-  }
-
-  this->cur_bufsize = 0;
-
-  setStateAndNotify(DataStatus::DATA_NOT_READY);
-  return ML_ERROR_NONE;
-}
 
 std::future<std::shared_ptr<BatchQueue>>
 DataBuffer::startFetchWorker(const std::vector<TensorDim> &input_dims,
@@ -291,14 +119,11 @@ DataBuffer::batcher(const std::vector<TensorDim> &input_dims,
   return producer->finalize(input_dims, label_dims);
 }
 
-int DataBuffer::setFeatureSize(const TensorDim &indim) {
-  int status = ML_ERROR_NONE;
-  input_dim = indim;
-  return status;
-}
-
 void DataBuffer::displayProgress(const int count, float loss) {
   int barWidth = 20;
+  /** this is temporary measure, will be getting this as an argument */
+  int batch_size = 1;
+  int samples_per_epoch = 0;
 
   std::stringstream ssInt;
   ssInt << count * batch_size;
@@ -376,19 +201,4 @@ void DataBuffer::setProperty(const std::vector<std::string> &values) {
       << "[DataBuffer] Failed to set property";
   }
 }
-
-int DataBuffer::setGeneratorFunc(datagen_cb func, void *user_data) {
-  return ML_ERROR_NOT_SUPPORTED;
-}
-
-int DataBuffer::setDataFile(const std::string &path) {
-  return ML_ERROR_NOT_SUPPORTED;
-}
-
-void DataBuffer::setStateAndNotify(const DataStatus status) {
-  std::lock_guard<std::mutex> lgtrain(status_mutex);
-  queue_status = status;
-  cv_status.notify_all();
-}
-
 } /* namespace nntrainer */
