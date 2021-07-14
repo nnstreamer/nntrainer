47561a156b83f98f8905fe2fb33c40ddcaed4832
Parichay Kapoor <pk.kapoor@samsung.com>
2021-07-14 21:10:50 +0900

    [layer] Update NNStreamer layer to V2
    Update NNStreamer layer to V2 design.
    
    Signed-off-by: Parichay Kapoor <pk.kapoor@samsung.com>

---
+++ test/unittest/layers/unittest_layers_nnstreamer.cpp
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2021 Parichay Kapoor <pk.kapoor@samsung.com>
+ *
+ * @file unittest_layers_nnstreamer.cpp
+ * @date 12 June 2021
+ * @brief NNStreamer Layer Test
+ * @see	https://github.com/nnstreamer/nntrainer
+ * @author Parichay Kapoor <pk.kapoor@samsung.com>
+ * @bug No known bugs except for NYI items
+ */
+#include <tuple>
+
+#include <gtest/gtest.h>
+
+#include <layers_common_tests.h>
+#include <nnstreamer_layer.h>
+
+auto semantic_nnstreamer = LayerSemanticsParamType(
+  nntrainer::createLayer<nntrainer::NNStreamerLayer>,
+  nntrainer::NNStreamerLayer::type,
+  {"modelfile=../test/test_models/models/add.tflite"}, 0, false);
+
+INSTANTIATE_TEST_CASE_P(NNStreamer, LayerSemantics,
+                        ::testing::Values(semantic_nnstreamer));
