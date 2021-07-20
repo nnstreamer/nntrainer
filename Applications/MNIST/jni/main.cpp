0625de480f72b087744760dc3cf0779291cabbe3
Parichay Kapoor <pk.kapoor@samsung.com>
2021-07-20 14:49:48 +0900

    [application] MNIST application
    Update MNIST application benchmark values based on the updated weight
    initialization.
    
    Signed-off-by: Parichay Kapoor <pk.kapoor@samsung.com>

--- Applications/MNIST/jni/main.cpp
+++ Applications/MNIST/jni/main.cpp
@@ -89,7 +89,6 @@ std::string data_path;
 
 float training_loss = 0.0;
 float validation_loss = 0.0;
-float last_batch_loss = 0.0;
 
 std::string filename = "mnist_trainingSet.dat";
 
@@ -241,9 +240,8 @@ int getBatch_val(float **outVec, float **outLabel, bool *last,
 
 #if defined(APP_VALIDATE)
 TEST(MNIST_training, verify_accuracy) {
-  // EXPECT_FLOAT_EQ(training_loss, 2.3230426);
-  // EXPECT_FLOAT_EQ(validation_loss, 2.3045228);
-  // EXPECT_FLOAT_EQ(last_batch_loss, 2.2880380153656006);
+  EXPECT_FLOAT_EQ(training_loss, 2.3031187);
+  EXPECT_FLOAT_EQ(validation_loss, 2.2951343);
 }
 #endif
 
