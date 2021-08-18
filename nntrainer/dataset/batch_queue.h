6593150ef8d14356e78e75359b5e481074fd9b53
Jihoon Lee <jhoon.it.lee@samsung.com>
2021-08-18 14:20:40 +0900

    [Dataset] Add mt support to iteration Queue
    This path adds async support to iteration queue and ending mechanism
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

--- nntrainer/dataset/batch_queue.h
+++ nntrainer/dataset/batch_queue.h
@@ -20,6 +20,7 @@
 #include <memory>
 #include <queue>
 #include <shared_mutex>
+#include <stdexcept>
 #include <tuple>
 
 #include <data_iteration.h>
@@ -100,6 +101,74 @@ private:
 };
 
 /**
+ * @brief Thread Safe Queue implementation dedicated for the non-owing pointer
+ *
+ * @tparam original type of the view (T * will be pushed and pop)
+ */
+template <typename T> class ViewQueue {
+public:
+  /**
+   * @brief Construct a new queue
+   */
+  ViewQueue() : q() {}
+
+  /**
+   * @brief push data to queue
+   *
+   * @param data data to put
+   */
+  void push(T *data) {
+    {
+      std::unique_lock<std::shared_mutex> lk(q_mutex);
+      q.push(data);
+    }
+
+    q_cv.notify_one();
+  }
+
+  /**
+   * @brief pop data from the queue, wait if empty
+   * @note when fail to get, this will return nullptr (eg) when interrupt
+   * happens)
+   * @return T* view of the data
+   */
+  T *waitAndPop() {
+    std::unique_lock<std::shared_mutex> lk(q_mutex);
+    q_cv.wait(lk, [this] { return !q.empty(); });
+    auto ptr = q.front();
+    q.pop();
+
+    return ptr;
+  }
+
+  /**
+   * @brief check if current queue is empty
+   *
+   * @return bool true if empty
+   */
+  bool isEmpty() const {
+    std::shared_lock<std::shared_mutex> lk(q_mutex);
+    return q.empty();
+  }
+
+  /**
+   * @brief check if current queue is empty
+   *
+   * @return bool true if empty
+   */
+  typename std::queue<T *>::size_type size() const {
+    std::shared_lock<std::shared_mutex> lk(q_mutex);
+    return q.size();
+  }
+
+private:
+  mutable std::shared_mutex q_mutex;
+  std::condition_variable_any q_cv;
+
+  std::queue<T *> q;
+};
+
+/**
  * @brief A view container that calls a callback on destruct
  * @note the callback must be noexcept, and the given underlying data must
  * outlive the lifetime of this class
@@ -112,10 +181,9 @@ public:
    * @brief Construct a new Scoped View object
    *
    * @param data_ reference of the underlying data
-   * @param on_notify_ callback to be called on exit, this is not copied but
-   * reused
+   * @param on_notify_ callback to be called on exit
    */
-  ScopedView(T *data_, std::function<void(void)> &&on_notify_) :
+  ScopedView(T *data_, std::function<void(void)> &&on_notify_ = nullptr) :
     data(data_),
     on_notify(std::forward<std::function<void(void)>>(on_notify_)) {}
 
@@ -126,14 +194,27 @@ public:
   ScopedView &operator=(ScopedView &&rhs) = default;
 
   /**
+   * @brief check if scoped view contains any underlying data
+   *
+   * @return bool if data is empty
+   */
+  bool isEmpty() { return data == nullptr; }
+
+  /**
    * @brief Destroy the Scoped View object, callback is called at this time
    *
    */
   ~ScopedView() {
     try {
-      on_notify();
+      if (std::uncaught_exceptions()) {
+        /// NYI, add on_error handler here
+      } else {
+        if (on_notify) {
+          on_notify();
+        }
+      }
     } catch (...) {
-      ml_loge("while notifiying, error happened");
+      ml_loge("while handling on_notify or on_error, error happened");
     }
   }
 
@@ -159,7 +240,7 @@ private:
 
 /**
  * @brief Iteration queue that owns the buffer for input / labels
- * @detail
+ * @details
  *
  * - requestEmpty() will give a ScopedView<sample>
  *     Destructing the returned object will notify the iteration that is done
@@ -169,8 +250,15 @@ private:
  *     Destructing this will notify the queue that is done used (internally
  * calls IterationQueue::markEmpty())
  *
+ * @details For an iteration there can be four state.
+ * 1. The buffer is empty, waiting to be filled (will be in empty_q)
+ * 2. The buffer is being filled sample by sample, waiting to be marked as
+ * filled.
+ * 3. The buffer is filled, waiting to be served (will be in filled_q)
+ * 4. The buffer is being served, waiting to be marked as emptied.
  * @todo apply this to the databuffer
- * @todo prepare thread safe queue and apply
+ * @todo handle error case: 1. when ScopedView<Sample> has met throw
+ *                          2. when ScopedView<Iteration> has met throw
  */
 class IterationQueue {
 public:
@@ -188,19 +276,29 @@ public:
                  const std::vector<ml::train::TensorDim> &label_dims);
 
   /**
+   * @brief Destroy the Iteration Queue object
+   *
+   */
+  ~IterationQueue();
+
+  /**
    * @brief request empty sample from the queue.
-   * @note There is race condition between requesting empty, race condition with
-   * mark_ready should be handled by using MT_safe queue.
-   * @return ScopedView<Sample> sample view. Destroying the returned object will
+   * @note User must check if ScopedView actually has a value by calling
+   * copedView::isEmpty()
+   * @return ScopedView<Sample> sample view. ScopedView::isEmpty() == true
+   * if there is no more data coming. Destroying the returned object will
    * signal the queue that the sample is filled.
    */
   ScopedView<Sample> requestEmpty();
 
   /**
    * @brief request filled iteration from the queue.
-   * @note race condition here can be handled by using MT_safe queue
-   * @return ScopedView<Iteration> Ieration view. Destroying the returned object
-   * will signal the queue that the sample is done using.
+   * @note User must check if ScopedView actually has a value by calling
+   * copedView::isEmpty()
+   * @return ScopedView<Iteration> Ieration view. ScopedView::isEmpty() == true
+   * if there is no more data coming. Destroying the returned object will
+   * signal the queue that the sample is done using.
+   *
    */
   ScopedView<Iteration> requestFilled();
 
@@ -217,6 +315,17 @@ public:
    * @return unsigned int size of batch
    */
   unsigned int batch() { return iterations.front().get().batch(); }
+
+  /**
+   * @brief notifyEndOfRequest, when the producing by requestEmpty has finished.
+   * @note It is important that the owner of this class must ensure that there
+   * will be no more requestEmpty call after this. This means that, in case of
+   * multiple workers, the manager of the worker(producer) must know every
+   * producer has finished. and call this function other than each worker call
+   * this function.
+   *
+   */
+  void notifyEndOfRequestEmpty();
 
 private:
   /**
@@ -261,6 +370,15 @@ private:
     void markSampleFilled() /** noexcept */;
 
     /**
+     * @brief update end sample to the given iterator and mark last
+     * @note after updating end iterator, this can be markFilled() if every
+     * sample is already filled
+     *
+     * @param iterator non-inclusive iterator to mark the last
+     */
+    void setEndSample(std::vector<Sample>::iterator sample_iterator);
+
+    /**
      * @brief get underlying iteration
      *
      * @return Iteration& iteration
@@ -268,10 +386,22 @@ private:
     Iteration &get() { return iteration; }
 
   private:
-    mutable std::mutex notify_mutex;
-    Iteration iteration;
-    IterationQueue *iq;
-    unsigned int num_observed;
+    unsigned int num_observed; /**< number of observed samples which were passed
+                                  to the callee and notified done filling */
+    mutable std::mutex
+      notify_mutex;      /**< mutex which should be locked when try to notify */
+    Iteration iteration; /**< underlying iteration that this class owns */
+    IterationQueue *iq;  /**< view of iteration queue */
+  };
+
+  /**
+   * @brief Queue running state enum
+   *
+   */
+  enum class FlowState {
+    FLOW_STATE_OPEN = 0,           /**< nothing */
+    FLOW_STATE_STOP_REQUESTED = 1, /**< request stop */
+    FLOW_STATE_STOPPED = 2,        /**< queue is fully stopped */
   };
 
   /**
@@ -288,14 +418,25 @@ private:
    */
   void markEmpty(MarkableIteration *iteration) /** noexcept */;
 
-  std::vector<MarkableIteration> iterations; /** allocated iterations */
-  MarkableIteration *being_filled; /**< iteration that is being filled now */
+  std::vector<MarkableIteration> iterations; /**< allocated iterations */
+  MarkableIteration *being_filled; /**< last iteration that is being filled */
+  std::vector<Sample>::iterator
+    current_iterator; /**< current sample iteration of being_filled */
 
-  std::vector<Sample>::iterator current_iterator;
+  mutable std::mutex empty_mutex; /**< mutex to be used when it is mutually
+                                     exclusive to the requesting empty slots */
+  unsigned int
+    num_being_filled; /**< number of iteration that is in being_filled state */
+  mutable std::mutex
+    filled_mutex; /**< mutex to be used when it is mutually exclusive to the
+                     requesting filled slots */
+  std::condition_variable_any
+    notify_emptied_cv;  /**< conditional variable to wait based on the
+                           num_being_filled */
+  FlowState flow_state; /**< flow state of the queue */
 
-  /// @todo use mt safe queue
-  std::queue<MarkableIteration *> empty_q;  /** iterations to be filled */
-  std::queue<MarkableIteration *> filled_q; /** iterations to be served */
+  ViewQueue<MarkableIteration> empty_q;  /**< iterations to be filled */
+  ViewQueue<MarkableIteration> filled_q; /**< iterations to be served */
 };
 
 } // namespace nntrainer
