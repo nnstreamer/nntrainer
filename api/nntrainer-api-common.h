8605dba9f50062154d78802e7a46530053a9683e
Jihoon Lee <jhoon.it.lee@samsung.com>
2021-08-19 17:43:55 +0900

    [CAPI] Update description of ml_train_datagen_cb
    This patch updates ml_train_datagen_cb with newly added api sets and
    clarify the parameter @a last means.
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

--- api/nntrainer-api-common.h
+++ api/nntrainer-api-common.h
@@ -77,7 +77,7 @@ typedef enum {
  *
  * @details The user of the API must provide this callback function to supply
  * data to the model and register the callback with
- * ml_train_dataset_create_with_generator(). The model will call this callback
+ * ml_train_dataset_add_generator(). The model will call this callback
  * whenever it needs more data. This function should provide a single element of
  * input and label data in the passed containers. The containers passed by the
  * caller will already be allocated with sufficient space to contain the data.
@@ -95,13 +95,15 @@ typedef enum {
  * number of samples are set as a property for this dataset. In this case, last
  * is only used for verification purposes. If total number of samples for the
  * dataset is unknown, this function will be called in sequence.
+ * @note @a last has to be set true when filling the last @a input, @a label.
  * @param[out] input Container to hold all the input data. Should not be freed
  * by the user.
  * @param[out] label Container to hold corresponding label data. Should not be
  * freed by the user.
  * @param[out] last Container to notify if data is finished. Set true if no more
  * data to provide, else set false. Should not be freed by the user.
- * @param[in] user_data User application's private data.
+ * @param[in] user_data User application's private data passed along with
+ * ml_train_dataset_add_generator().
  * @return @c 0 on success. Otherwise a negative error value.
  * @retval #ML_ERROR_NONE Successful.
  * @retval #ML_ERROR_INVALID_PARAMETER Invalid parameter.
@@ -123,10 +125,11 @@ typedef enum {
  *
  * // This sample fills inputs and labels with random data.
  * srand(0);
- * if (count > num_samples) {
+ * if (count >= num_samples) {
  *   *last = true;
  *   // handle preparation for start of next epoch
- *   return ML_ERROR_NONE;
+ * } else {
+ *   *last = false;
  * }
  *
  * // Fill input data
@@ -153,17 +156,23 @@ typedef enum {
  * Below is an example of the usage of this sample:
  * @code
  * int status;
+ * void * user_data;
  * ml_train_dataset_h handle;
+ * status = ml_train_dataset_create(&handle);
+ * if (status != ML_ERROR_NONE) {
+ *    // handle error case.
+ *    return status;
+ * }
  *
- * status = ml_train_dataset_create_with_generator(&handle,
- *      rand_dataset_generator, NULL, NULL);
+ * status = ml_train_dataset_add_generator(dataset,
+ *      ML_TRAIN_DATASET_MODE_TRAIN, getBatch_train, user_data);
  * if (status != ML_ERROR_NONE) {
  *    // handle error case.
  *    return status;
  * }
  *
  * // Destroy the handle if not added to a model.
- * status = ml_train_layer_destroy(handle);
+ * status = ml_train_dataset_destroy(handle);
  * if (status != ML_ERROR_NONE) {
  *    // handle error case
  *    return status;
