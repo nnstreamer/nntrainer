0bdc71e5d83de5856a7b6048776af20c24c6bad6
Parichay Kapoor <pk.kapoor@samsung.com>
2021-10-08 20:36:58 +0900

    [test] Added unittests for LSTM
    This patch adds unittests for LSTM layer
    1. single and multi timesteps
    2. with and without return sequences
    3. with setting activations differently
    
    Signed-off-by: Parichay Kapoor <pk.kapoor@samsung.com>

--- test/unittest/layers/unittest_layers_lstm.cpp
+++ test/unittest/layers/unittest_layers_lstm.cpp
@@ -21,3 +21,42 @@ auto semantic_lstm =
                           nntrainer::LSTMLayer::type, {"unit=1"}, 0, false, 1);
 
 INSTANTIATE_TEST_CASE_P(LSTM, LayerSemantics, ::testing::Values(semantic_lstm));
+
+auto lstm_single_step = LayerGoldenTestParamType(
+  nntrainer::createLayer<nntrainer::LSTMLayer>, {"unit=5"}, "3:1:1:7",
+  "lstm_single_step.nnlayergolden", LayerGoldenTestParamOptions::DEFAULT);
+
+auto lstm_multi_step = LayerGoldenTestParamType(
+  nntrainer::createLayer<nntrainer::LSTMLayer>, {"unit=5"}, "3:1:4:7",
+  "lstm_multi_step.nnlayergolden", LayerGoldenTestParamOptions::DEFAULT);
+
+auto lstm_single_step_seq = LayerGoldenTestParamType(
+  nntrainer::createLayer<nntrainer::LSTMLayer>,
+  {"unit=5", "return_sequences=true"}, "3:1:1:7",
+  "lstm_single_step_seq.nnlayergolden", LayerGoldenTestParamOptions::DEFAULT);
+
+auto lstm_multi_step_seq = LayerGoldenTestParamType(
+  nntrainer::createLayer<nntrainer::LSTMLayer>,
+  {"unit=5", "return_sequences=true"}, "3:1:4:7",
+  "lstm_multi_step_seq.nnlayergolden", LayerGoldenTestParamOptions::DEFAULT);
+
+auto lstm_multi_step_seq_act_orig = LayerGoldenTestParamType(
+  nntrainer::createLayer<nntrainer::LSTMLayer>,
+  {"unit=5", "return_sequences=true", "hidden_state_activation=tanh",
+   "recurrent_activation=sigmoid"},
+  "3:1:4:7", "lstm_multi_step_seq.nnlayergolden",
+  LayerGoldenTestParamOptions::DEFAULT);
+
+auto lstm_multi_step_seq_act = LayerGoldenTestParamType(
+  nntrainer::createLayer<nntrainer::LSTMLayer>,
+  {"unit=5", "return_sequences=true", "hidden_state_activation=sigmoid",
+   "recurrent_activation=tanh"},
+  "3:1:4:7", "lstm_multi_step_seq_act.nnlayergolden",
+  LayerGoldenTestParamOptions::DEFAULT);
+
+INSTANTIATE_TEST_CASE_P(LSTM, LayerGoldenTest,
+                        ::testing::Values(lstm_single_step, lstm_multi_step,
+                                          lstm_single_step_seq,
+                                          lstm_multi_step_seq,
+                                          lstm_multi_step_seq_act_orig,
+                                          lstm_multi_step_seq_act));
