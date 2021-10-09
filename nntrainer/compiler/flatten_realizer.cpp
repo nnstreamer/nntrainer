42015d4643625e36464a7a4d9eb7a4c5fd231a33
Jihoon Lee <jhoon.it.lee@samsung.com>
2021-10-09 17:26:47 +0900

    [Graph/recurrent] Add concept of realizer
    This patch add graph realizer. Graph realizer will preprocess graph
    which can be effectively done as a lowering process of compile
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

---
+++ nntrainer/compiler/flatten_realizer.cpp
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2021 Jihoon Lee <jhoon.it.lee@samsung.com>
+ *
+ * @file flatten_realizer.h
+ * @date 09 October 2021
+ * @brief NNTrainer graph realizer which realizes flatten=true to actual node
+ * @see	https://github.com/nnstreamer/nntrainer
+ * @author Jihoon Lee <jhoon.it.lee@samsung.com>
+ * @bug No known bugs except for NYI items
+ */
+#include <flatten_realizer.h>
+
+#include <layer_node.h>
+
+namespace nntrainer {
+
+FlattenRealizer::~FlattenRealizer() {}
+
+GraphRepresentation
+FlattenRealizer::realize(const GraphRepresentation &reference) {
+  /// NYI
+  return reference;
+}
+} // namespace nntrainer
