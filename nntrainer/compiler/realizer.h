400238cbd2d7d7f5df5088e333d80e0781253c46
Jihoon Lee <jhoon.it.lee@samsung.com>
2021-10-09 17:26:47 +0900

    [Graph/recurrent] Add concept of realizer
    This patch add graph realizer. Graph realizer will preprocess graph
    which can be effectively done as a lowering process of compile
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

---
+++ nntrainer/compiler/realizer.h
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2021 Jihoon Lee <jhoon.it.lee@samsung.com>
+ *
+ * @file realizer.h
+ * @date 09 October 2021
+ * @brief NNTrainer graph realizer which preprocess graph representation as a
+ * lowering process of compile
+ * @see	https://github.com/nnstreamer/nntrainer
+ * @author Jihoon Lee <jhoon.it.lee@samsung.com>
+ * @bug No known bugs except for NYI items
+ */
+#ifndef __REALIZER_H__
+#define __REALIZER_H__
+
+#include <memory>
+#include <vector>
+
+namespace nntrainer {
+
+class LayerNode;
+using GraphRepresentation = std::vector<std::shared_ptr<LayerNode>>;
+
+/**
+ * @brief Graph realizer class
+ *
+ */
+class GraphRealizer {
+public:
+  /**
+   * @brief Destroy the Graph Realizer object
+   *
+   */
+  virtual ~GraphRealizer() {}
+
+  /**
+   * @brief graph realizer creates a new graph based on the reference
+   *
+   */
+  virtual GraphRepresentation realize(const GraphRepresentation &reference) = 0;
+};
+
+} // namespace nntrainer
+
+#endif // __REALIZER_H__
