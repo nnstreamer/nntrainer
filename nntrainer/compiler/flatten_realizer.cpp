bbdff326a3b23ef6fb812d44fa73cfaaa476526b
Jihoon Lee <jhoon.it.lee@samsung.com>
2021-10-12 17:37:51 +0900

    [Realizer] Apply flatten realizer
    This patch applies flatten realizer to model compile. Later, neuralnet
    will not have model_graph until compile() is being called
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

--- nntrainer/compiler/flatten_realizer.cpp
+++ nntrainer/compiler/flatten_realizer.cpp
@@ -11,8 +11,8 @@
  */
 #include <flatten_realizer.h>
 
-#include <layer_node.h>
 #include <flatten_layer.h>
+#include <layer_node.h>
 
 namespace nntrainer {
 
@@ -20,7 +20,23 @@ FlattenRealizer::~FlattenRealizer() {}
 
 GraphRepresentation
 FlattenRealizer::realize(const GraphRepresentation &reference) {
-  /// NYI
-  return reference;
+  GraphRepresentation processed;
+  processed.reserve(reference.size());
+
+  for (auto &node : reference) {
+    /// @note: [node] type=flatten; flatten=true; is awkward but allowed.
+    /// There is no reason to prohibit this.
+    processed.push_back(node);
+    if (node->getFlatten() && !node->getDistribute()) {
+      auto layer_name = node->getName();
+      auto flatten_node =
+        createLayerNode(FlattenLayer::type, {"name=" + layer_name});
+      node->setProperty({"flatten=false"});
+      node->setProperty({"name=" + layer_name + "/flatten_realized"});
+      processed.push_back(std::move(flatten_node));
+    }
+  }
+
+  return processed;
 }
 } // namespace nntrainer
