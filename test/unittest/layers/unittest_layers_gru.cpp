fa1f332eac1367d599b50d9aaebe66ef3689df4e
Parichay Kapoor <pk.kapoor@samsung.com>
2021-10-13 14:38:24 +0900

    [test/layers] Add gru layer testing
    This patch added gru layer unittest for layer golden tests.
    
    Signed-off-by: Parichay Kapoor <pk.kapoor@samsung.com>

--- test/unittest/layers/unittest_layers_gru.cpp
+++ test/unittest/layers/unittest_layers_gru.cpp
@@ -21,3 +21,42 @@ auto semantic_gru =
                           nntrainer::GRULayer::type, {"unit=1"}, 0, false, 1);
 
 INSTANTIATE_TEST_CASE_P(GRU, LayerSemantics, ::testing::Values(semantic_gru));
+
+auto gru_single_step = LayerGoldenTestParamType(
+  nntrainer::createLayer<nntrainer::GRULayer>, {"unit=5"}, "3:1:1:7",
+  "gru_single_step.nnlayergolden", LayerGoldenTestParamOptions::DEFAULT);
+
+auto gru_multi_step = LayerGoldenTestParamType(
+  nntrainer::createLayer<nntrainer::GRULayer>, {"unit=5"}, "3:1:4:7",
+  "gru_multi_step.nnlayergolden", LayerGoldenTestParamOptions::DEFAULT);
+
+auto gru_single_step_seq = LayerGoldenTestParamType(
+  nntrainer::createLayer<nntrainer::GRULayer>,
+  {"unit=5", "return_sequences=true"}, "3:1:1:7",
+  "gru_single_step_seq.nnlayergolden", LayerGoldenTestParamOptions::DEFAULT);
+
+auto gru_multi_step_seq = LayerGoldenTestParamType(
+  nntrainer::createLayer<nntrainer::GRULayer>,
+  {"unit=5", "return_sequences=true"}, "3:1:4:7",
+  "gru_multi_step_seq.nnlayergolden", LayerGoldenTestParamOptions::DEFAULT);
+
+auto gru_multi_step_seq_act_orig = LayerGoldenTestParamType(
+  nntrainer::createLayer<nntrainer::GRULayer>,
+  {"unit=5", "return_sequences=true", "hidden_state_activation=tanh",
+   "recurrent_activation=sigmoid"},
+  "3:1:4:7", "gru_multi_step_seq.nnlayergolden",
+  LayerGoldenTestParamOptions::DEFAULT);
+
+auto gru_multi_step_seq_act = LayerGoldenTestParamType(
+  nntrainer::createLayer<nntrainer::GRULayer>,
+  {"unit=5", "return_sequences=true", "hidden_state_activation=sigmoid",
+   "recurrent_activation=tanh"},
+  "3:1:4:7", "gru_multi_step_seq_act.nnlayergolden",
+  LayerGoldenTestParamOptions::DEFAULT);
+
+INSTANTIATE_TEST_CASE_P(GRU, LayerGoldenTest,
+                        ::testing::Values(gru_single_step, gru_multi_step,
+                                          gru_single_step_seq,
+                                          gru_multi_step_seq,
+                                          gru_multi_step_seq_act_orig,
+                                          gru_multi_step_seq_act));
