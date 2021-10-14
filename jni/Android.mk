1f87ab9afb437dd066c88267a40bb9632d524c4d
Jihoon Lee <jhoon.it.lee@samsung.com>
2021-10-14 20:06:23 +0900

    [Realizer] Implement slice realizer
    This patch implements slice realizer and it's test
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

--- jni/Android.mk
+++ jni/Android.mk
@@ -194,6 +194,7 @@ NNTRAINER_SRCS := $(NNTRAINER_ROOT)/nntrainer/models/neuralnet.cpp \
                   $(NNTRAINER_ROOT)/nntrainer/compiler/flatten_realizer.cpp \
                   $(NNTRAINER_ROOT)/nntrainer/compiler/recurrent_realizer.cpp \
                   $(NNTRAINER_ROOT)/nntrainer/compiler/remap_realizer.cpp \
+                  $(NNTRAINER_ROOT)/nntrainer/compiler/slice_realizer.cpp \
                   $(NNTRAINER_ROOT)/nntrainer/app_context.cpp
 
 ifeq ($(ENABLE_TFLITE_INTERPRETER), 1)
---
+++ nntrainer/compiler/slice_realizer.h
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2021 Jihoon Lee <jhoon.it.lee@samsung.com>
+ *
+ * @file slice_realizer.h
+ * @date 14 October 2021
+ * @brief NNTrainer graph realizer which slice the graph representation
+ * @see	https://github.com/nnstreamer/nntrainer
+ * @author Jihoon Lee <jhoon.it.lee@samsung.com>
+ * @bug No known bugs except for NYI items
+ */
+#ifndef __SLICE_REALIZER_H__
+#define __SLICE_REALIZER_H__
+
+#include <memory>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+#include <realizer.h>
+
+namespace nntrainer {
+
+/**
+ * @brief Graph realizer class which slice graph representation
+ *
+ */
+class SliceRealizer final : public GraphRealizer {
+public:
+  /**
+   * @brief Construct a new Slice Realizer object
+   *
+   * @param start_layers start layers
+   * @param end_layers end layers
+   */
+  SliceRealizer(const std::vector<std::string> &start_layers,
+                const std::vector<std::string> &end_layers);
+
+  /**
+   * @brief Destroy the Graph Realizer object
+   *
+   */
+  ~SliceRealizer();
+
+  /**
+   * @brief graph realizer creates a new graph based on the reference
+   * @note for each layer in start_layers, start dfs, if traversal meets an end
+   * layers, node is added to an ordered set.
+   * @throw std::invalid_argument if created GraphRepresentation is empty
+   *
+   */
+  GraphRepresentation realize(const GraphRepresentation &reference) override;
+
+private:
+  std::vector<std::string> start_layers;
+  std::unordered_set<std::string> end_layers;
+};
+
+} // namespace nntrainer
+
+#endif // __SLICE_REALIZER_H__
