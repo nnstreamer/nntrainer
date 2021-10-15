6005b8e4c7cdeca0c6ada66aea11208d9191cceb
Jihoon Lee <jhoon.it.lee@samsung.com>
2021-10-15 15:05:15 +0900

    [Test] Make model test to take arbitrary nn
    This patch updates model test to accept arbitrary neuralnetwork instead
    of ini only
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

--- test/unittest/models/models_golden_test.h
+++ test/unittest/models/models_golden_test.h
@@ -17,8 +17,13 @@
 
 #include <tuple>
 
+#include <functional>
 #include <ini_wrapper.h>
 #include <tensor_dim.h>
+
+namespace nntrainer {
+class NeuralNetwork;
+}
 
 /**
  * @brief Test Option for the unittest models
@@ -32,6 +37,13 @@ typedef enum {
   ALL = COMPARE | SAVE_AND_LOAD_INI /**< Set every option */
 } ModelTestOption;
 
+using ModelGoldenTestParamType =
+  std::tuple<std::function<std::unique_ptr<nntrainer::NeuralNetwork>()>
+             /**<NeuralNetwork Creator */,
+             const std::string /**< Golden file name */,
+             const nntrainer::TensorDim /**< InputDimension */,
+             const unsigned int /**< Number of Iterations */,
+             ModelTestOption /**< Options which test to run */>;
 /**
  * @brief nntrainerModelTest fixture for parametrized test
  *
@@ -40,51 +52,108 @@ typedef enum {
  * @param int Iteration
  */
 class nntrainerModelTest
-  : public ::testing::TestWithParam<
-      std::tuple<const nntrainer::IniWrapper /**< Model Architecture */,
-                 const nntrainer::TensorDim /**< InputDimension */,
-                 const unsigned int /**< Number of Iterations */,
-                 ModelTestOption /**< Options which test to run */>> {
+  : public ::testing::TestWithParam<ModelGoldenTestParamType> {
 
 protected:
+  /**
+   * @brief Construct a new nntrainer Model Test object
+   *
+   */
   nntrainerModelTest() :
     iteration(0),
     name(""),
     options(ModelTestOption::NO_THROW_RUN) {}
+
+  /**
+   * @brief Destroy the nntrainer Model Test object
+   *
+   */
+  virtual ~nntrainerModelTest() {}
+
+  /**
+   * @brief Set the Up object
+   *
+   */
   virtual void SetUp() {
-    auto param = GetParam();
+    auto &param = GetParam();
+    nn_creator = std::get<0>(param);
 
-    ini = std::get<0>(param);
     /// remove the test number after double __
-    name = ini.getName();
-    name = name.substr(0, name.find("__"));
+    name = std::get<1>(param);
 
-    label_dim = std::get<1>(param);
-    iteration = std::get<2>(param);
-    options = std::get<3>(param);
-    ini.save_ini();
+    label_dim = std::get<2>(param);
+    iteration = std::get<3>(param);
+    options = std::get<4>(param);
   }
 
-  virtual void TearDown() { ini.erase_ini(); }
+  /**
+   * @brief Tear down the test case
+   *
+   */
+  virtual void TearDown() {}
 
-  std::string getIniName() { return ini.getIniName(); }
+  /**
+   * @brief Create a Model object
+   *
+   * @return std::unique_ptr<nntrainer::NeuralNetwork> created model
+   */
+  std::unique_ptr<nntrainer::NeuralNetwork> createModel() {
+    return nn_creator();
+  }
+
+  /**
+   * @brief Get the Name object
+   *
+   * @return std::string name
+   */
   std::string getName() { return name; }
-  std::string getGoldenName() { return name + ".info"; }
+
+  /**
+   * @brief Get the Golden Name object
+   *
+   * @return std::string
+   */
+  std::string getGoldenName() {
+    return name.substr(0, name.find("__")) + ".info";
+  }
+
+  /**
+   * @brief Get the number of iteration
+   *
+   * @return int integer
+   */
   int getIteration() { return iteration; };
+
+  /**
+   * @brief Get the Label Dim object
+   *
+   * @return nntrainer::TensorDim label dimension
+   */
   nntrainer::TensorDim getLabelDim() { return label_dim; }
 
+  /**
+   * @brief query if compare test should be conducted
+   *
+   * @return bool true if test should be done
+   */
   bool shouldCompare() {
     return (options & ModelTestOption::COMPARE) == ModelTestOption::COMPARE;
   }
+
+  /**
+   * @brief query if saveload ini test should be done
+   *
+   * @return bool true if test should be done
+   */
   bool shouldSaveLoadIniTest() {
     return options & ModelTestOption::SAVE_AND_LOAD_INI;
   }
 
 private:
+  std::function<std::unique_ptr<nntrainer::NeuralNetwork>()> nn_creator;
   nntrainer::TensorDim label_dim;
   int iteration;
   std::string name;
-  nntrainer::IniWrapper ini;
   ModelTestOption options;
 };
 
@@ -96,10 +165,9 @@ private:
  * @param int Iteration
  * @param options options
  */
-std::tuple<const nntrainer::IniWrapper, const nntrainer::TensorDim,
-           const unsigned int, ModelTestOption>
-mkModelTc(const nntrainer::IniWrapper &ini, const std::string &label_dim,
-          const unsigned int iteration,
-          ModelTestOption options = ModelTestOption::ALL);
+ModelGoldenTestParamType
+mkModelIniTc(const nntrainer::IniWrapper &ini, const std::string &label_dim,
+             const unsigned int iteration,
+             ModelTestOption options = ModelTestOption::ALL);
 
 #endif // __MODELS_GOLDEN_TEST_H__
