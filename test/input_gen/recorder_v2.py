1768487406b73ce85e13d7595684c90e0829063e
Jihoon Lee <jhoon.it.lee@samsung.com>
2021-10-19 19:00:56 +0900

    [Test] Add model v2 test generator
    This patch add model v2 test generator using pytorch.
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

---
+++ test/input_gen/recorder_v2.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: Apache-2.0
+##
+# Copyright (C) 2021 Jihoon Lee <jhoon.it.lee@samsung.com>
+#
+# @file recorder_v2.py
+# @date 18 October 2021
+# @brief Generate tc from given torch model
+# @author Jihoon lee <jhoon.it.lee@samsung.com>
+
+import os
+import random
+import torch
+import numpy as np
+
+from transLayer_v2 import params_translated
+
+SEED = 1234
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+
+__all__ = ["record_v2", "inspect_file"]
+
+
+def _get_writer(file):
+    def write_fn(*items):
+        for item in items:
+            np.array([item.numel()], dtype="int32").tofile(file)
+            item.detach().cpu().numpy().tofile(file)
+
+        return items
+
+    return write_fn
+
+
+def _rand_like(*shapes, scale=1, rand="int"):
+    shape_to_np = (
+        lambda shape: np.random.randint(0, 10, shape).astype(dtype=np.float32)
+        if rand == "int"
+        else np.random.rand(*shape).astype(dtype=np.float32)
+    )
+
+    np_array = map(shape_to_np, shapes)
+    return [torch.tensor(t * scale) for t in np_array]
+
+##
+# @brief record a torch model
+# @param iteration number of iteration to record
+# @param input_dims dimensions to record including batch (list of tuple)
+# @param label_dims dimensions to record including batch (list of tuple)
+# @param name golden name
+def record_v2(model, iteration, input_dims, label_dims, name):
+    ## file format is as below
+    # [<number of iteration(int)> <Iteration> <Iteration>...<Iteration>]
+    # Each iteration contains
+    # [<input(Tensors)><Label(Tensors)><Parameters(Tensors)><Output(Tensors)>]
+    # Each tensor contains
+    # [<num_elements(int32)><data_point(float32)>...<data_point(float32)>]
+
+    file_name = name + ".nnmodelgolden"
+    if os.path.isfile(file_name):
+        print("Warning: the file %s is being truncated and overwritten" % file_name)
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+
+    print(*(model.named_parameters()))
+
+    def record_iteration(write_fn):
+        inputs = _rand_like(*input_dims, rand="float")
+        labels = _rand_like(*label_dims, rand="float")
+        output, loss = model(inputs, labels)
+        write_fn(*inputs)
+        write_fn(*labels)
+        write_fn(*(t for _, t in params_translated(model)))
+        write_fn(output)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    with open(file_name, "wb") as f:
+        # write number of iterations
+        np.array([iteration], dtype="int32").tofile(f)
+
+        write_fn = _get_writer(f)
+        for _ in range(iteration):
+            record_iteration(write_fn)
+
+
+##
+# @brief inpsect if file is created correctly
+# @note this just checks if offset is corretly set, The result have to inspected
+# manually
+def inspect_file(file_name):
+    with open(file_name, "rb") as f:
+        sz = int.from_bytes(f.read(4), byteorder="little")
+        if not sz:
+            return
+        print("num_iter: ", sz)
+        while True:
+            sz = int.from_bytes(f.read(4), byteorder="little")
+            if not sz:
+                break
+            print("size: ", sz)
+            print(np.fromfile(f, dtype="float32", count=sz))
+
---
+++ test/input_gen/transLayer_v2.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: Apache-2.0
+##
+# Copyright (C) 2021 Jihoon Lee <jhoon.it.lee@samsung.com>
+#
+# @file transLayer_v2.py
+# @date 19 October 2021
+# @brief Rewrite parameters in the order consistent with nntrainer for the torch model
+# @author Jihoon lee <jhoon.it.lee@samsung.com>
+
+import torch
+
+__all__ = ["params_translated"]
+
+# type to parameter mapper containing (classes, function)
+handler_book = []
+
+##
+# Decorater to register class mapping to a function.
+# This is to imitate function overloadding
+def register_for_(classes):
+    for already_registered_classes, _ in handler_book:
+        for cls_ in classes:
+            if isinstance(cls_, already_registered_classes):
+                raise ValueError("class is already registered %s" % cls_.__name__)
+
+    def wrapper(func):
+        handler_book.append((classes, func))
+        return func
+
+    return wrapper
+
+
+def default_translate_(model):
+    yield from model.named_parameters(recurse=False)
+
+
+@register_for_(torch.nn.LSTMCell)
+def lstm_translate(model):
+    params = [(name, tensor.detach()) for name, tensor in model.named_parameters()]
+    bias = ("bias", params[2][1] + params[3][1])
+    # hidden, input -> input, hidden
+    def transpose_(weight):
+        return (weight[0], weight[1].transpose(1, 0))
+
+    new_params = [transpose_(params[0]), transpose_(params[1]), bias]
+    yield from new_params
+
+
+def translate(model):
+    for child in model.children():
+        for registered_classes, fn in handler_book:
+            if isinstance(child, registered_classes):
+                yield from fn(child)
+            else:
+                yield from translate(child)
+    yield from default_translate_(model)
+
+
+params_translated = translate
+
