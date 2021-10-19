25152a3e69e5ef56ef930ad640b03d17ef1f696c
Jihoon Lee <jhoon.it.lee@samsung.com>
2021-10-19 17:32:39 +0900

    [Test] Add models v2 test
    This patch proposes models v2 test which is much lighter.
    
    **Purpose**
    1. Lighter model test as layer golden test compensates for it.
    2. Easier debugging especially by checking offset, and moving metatdat
    3. Give greater flexibility to the golden test generator on the model
    architecture
    4. Not saving golden file but making it on the fly on the builder side
    
    **Major Difference**
    1. Gradient comparison is skipped as it is automatically checked at the
    next iteration.
    2. Add metadata about num_iterations to file
    3. Add size check while reading the golden file
    4. Delete redundant information that can be infered.
    5. Every iteration has it's own input information.
    
    **Golden Format**
    ```
        ## file format is as below
        # [<number of iteration(int)> <Iteration> <Iteration>...<Iteration>]
        # Each iteration contains
        # [<input(Tensors)><Label(Tensors)><Parameters(Tensors)><Output(Tensors)>]
        # Each tensor contains
        # [<num_elements(int32)><data_point(float32)>...<data_point(float32)>]
    ```
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

--- test/unittest/models/models_test_utils.cpp
+++ test/unittest/models/models_test_utils.cpp
@@ -12,9 +12,12 @@
  */
 #include <gtest/gtest.h>
 
+#include <memory>
+
 #include <models_test_utils.h>
 #include <multiout_layer.h>
 #include <nntrainer_test_util.h>
+#include <tensor.h>
 
 /**
  * @brief Get the Models Path object
@@ -24,6 +27,16 @@
  */
 static const std::string getModelsPath(const std::string &file_name) {
   return getResPath(file_name, {"test", "unittest_models"});
+}
+
+using namespace nntrainer;
+
+static sharedConstTensors toSharedTensors(const std::vector<Tensor> &ts) {
+  sharedConstTensors sts;
+  sts.reserve(ts.size());
+  std::transform(ts.begin(), ts.end(), std::back_inserter(sts),
+                 [](const auto &ts) { return MAKE_SHARED_TENSOR(ts); });
+  return sts;
 }
 
 /**
@@ -41,8 +54,9 @@ static void verify(const nntrainer::Tensor &actual,
     std::cout
       << "============================================================\n";
     std::cout << "\033[1;33m" << error_msg << "\033[0m\n";
-    std::cout << "\033[1;33mcurrent\033[0m  " << actual
-              << "\033[1;33mexpected\033[0m  " << expected;
+    std::cout << "\033[1;33mcurrent\033[0m  " << actual.getName() << " - "
+              << actual << "\033[1;33mexpected\033[0m  " << expected.getName()
+              << " - " << expected;
 
     if (actual.getDim() == expected.getDim()) {
       nntrainer::Tensor diff = actual.subtract(expected);
@@ -80,6 +94,87 @@ static void verify(const std::vector<nntrainer::Tensor> &actual,
   }
 }
 
+/**
+ * @brief Iteration abstraction for golden test v2
+ *
+ */
+class IterationForGolden {
+public:
+  IterationForGolden(NeuralNetwork *nn) : nn(nn) {
+    auto in_dims = nn->getInputDimension();
+    auto out_dims = nn->getOutputDimension();
+
+    inputs = std::vector<Tensor>(in_dims.begin(), in_dims.end());
+    labels = std::vector<Tensor>(out_dims.begin(), out_dims.end());
+    expected_outputs = std::vector<Tensor>(out_dims.begin(), out_dims.end());
+
+    NetworkGraphType model_graph = nn->getNetworkGraph();
+    for (auto it = model_graph.cbegin(); it != model_graph.cend(); ++it) {
+      auto const &lnode = *it;
+      auto &rc = lnode->getRunContext();
+      for (unsigned int i = 0; i < rc.getNumWeights(); ++i) {
+        if (rc.isWeightDependent(i)) {
+          continue;
+        }
+        Tensor &t = rc.getWeight(i);
+        weights.push_back(t);
+        expected_weights.push_back(t.clone());
+      }
+    }
+  }
+
+  /**
+   * @brief test and match with the golden
+   *
+   * @param iteration iteration number
+   * @param f  file stream which contains golden data
+   * @param test_weights test weights if true else fill weights from read
+   */
+  void test(unsigned iteration, std::ifstream &f, bool test_weights) {
+    auto read_tensors = [&f](std::vector<Tensor> &ts) {
+      std::for_each(ts.begin(), ts.end(), [&f](Tensor &t) {
+        sizeCheckedReadTensor(t, f, "read_failed for " + t.getName());
+      });
+    };
+
+    auto to_tensors = [](sharedConstTensors &sts) {
+      std::vector<Tensor> ts;
+      ts.reserve(sts.size());
+      std::transform(sts.begin(), sts.end(), std::back_inserter(ts),
+                     [](const auto &ts) { return *ts; });
+      return ts;
+    };
+
+    read_tensors(inputs);
+    read_tensors(labels);
+    read_tensors(expected_weights);
+    read_tensors(expected_outputs);
+
+    if (test_weights) {
+      verify(weights, expected_weights, " weights");
+    } else {
+      for (unsigned int i = 0; i < weights.size(); ++i) {
+        weights.at(i).fill(expected_weights.at(i));
+      }
+    }
+
+    auto shared_inputs = toSharedTensors(inputs);
+    auto shared_labels = toSharedTensors(labels);
+
+    auto out = nn->forwarding(shared_inputs, shared_labels);
+    verify(to_tensors(out), expected_outputs, " output");
+    nn->backwarding(iteration);
+  }
+
+private:
+  NeuralNetwork *nn;
+  std::vector<Tensor> inputs;
+  std::vector<Tensor> labels;
+  std::vector<Tensor> weights;
+  std::vector<Tensor> expected_weights;
+  std::vector<Tensor> expected_outputs;
+};
+
 NodeWatcher::NodeWatcher(const NodeType &node) : node(node) {
   unsigned int num_weights = node->getNumWeights();
   try {
@@ -88,10 +183,13 @@ NodeWatcher::NodeWatcher(const NodeType &node) : node(node) {
     std::cout << "Cannot set layer " << node->getType() << " trainable";
   }
 
+  auto &rc = node->getRunContext();
   for (unsigned int i = 0; i < num_weights; ++i) {
     // const nntrainer::Weight &w = node->getWeightObject(i);
     // expected_weights.push_back(w.clone());
-    expected_weights.push_back(node->getWeightWrapper(i).clone());
+    if (!rc.isWeightDependent(i)) {
+      expected_weights.push_back(node->getWeightWrapper(i).clone());
+    }
   }
 
   for (auto &out_dim : node->getOutputDimensions()) {
@@ -106,8 +204,11 @@ NodeWatcher::NodeWatcher(const NodeType &node) : node(node) {
 NodeWatcher::NodeWatcher() : node(nullptr) {}
 
 void NodeWatcher::readLayerWeight(std::ifstream &f) {
+  auto &rc = node->getRunContext();
   for (unsigned int i = 0; i < node->getNumWeights(); ++i) {
-    /// @note below is harrasing the fact the tensor shares same base memory
+    if (rc.isWeightDependent(i)) {
+      continue;
+    }
     node->getWeight(i).read(f);
   }
 }
@@ -323,14 +424,62 @@ void GraphWatcher::validateFor(const nntrainer::TensorDim &label_shape) {
   }
 
   /**
-   * This inference is to ensure that inference runs with/without optimizations
-   * for various kinds of models
+   * This inference is to ensure that inference runs with/without
+   * optimizations for various kinds of models
    */
   EXPECT_NO_THROW(nn->inference(input, false));
   /** run inference again which frees the memory */
   EXPECT_NO_THROW(nn->inference(input, true));
   /** run inference again which will force to allocate memory again */
   EXPECT_NO_THROW(nn->inference(input, true));
+}
+
+void GraphWatcher::compareFor_V2(const std::string &reference) {
+
+  auto file = nntrainer::checkedOpenStream<std::ifstream>(
+    getModelsPath(reference), std::ios::in | std::ios::binary);
+
+  unsigned int num_iter;
+  nntrainer::checkedRead(file, (char *)&num_iter, sizeof(unsigned));
+  std::cout << "num iter: " << num_iter << '\n';
+
+  IterationForGolden ifg(nn.get());
+
+  for (unsigned int i = 0; i < num_iter; ++i) {
+    std::cout << "iteration: " << i << std::endl;
+    ifg.test(i, file, i != 0);
+  }
+}
+
+void GraphWatcher::validateFor_V2() {
+  auto in_dims = nn->getInputDimension();
+  auto out_dims = nn->getOutputDimension();
+
+  std::vector<Tensor> inputs(in_dims.begin(), in_dims.end());
+  std::vector<Tensor> labels(out_dims.begin(), out_dims.end());
+
+  auto shared_inputs = toSharedTensors(inputs);
+  auto shared_labels = toSharedTensors(labels);
+
+  if (loss_nodes.size()) {
+    EXPECT_NO_THROW(nn->forwarding(shared_inputs, shared_labels));
+  } else {
+    EXPECT_NO_THROW(nn->forwarding(shared_inputs, {}));
+  }
+
+  if (loss_nodes.size()) {
+    EXPECT_NO_THROW(nn->backwarding(0));
+  }
+
+  /**
+   * This inference is to ensure that inference runs with/without
+   * optimizations for various kinds of models
+   */
+  EXPECT_NO_THROW(nn->inference(shared_inputs, false));
+  /** run inference again which frees the memory */
+  EXPECT_NO_THROW(nn->inference(shared_inputs, true));
+  /** run inference again which will force to allocate memory again */
+  EXPECT_NO_THROW(nn->inference(shared_inputs, true));
 }
 
 std::array<nntrainer::Tensor, 2>
