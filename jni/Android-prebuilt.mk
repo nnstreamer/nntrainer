0fddf38fc7734bf93de8d572a96d7f7dd916ad54
Jihoon Lee <jhoon.it.lee@samsung.com>
2021-10-22 21:22:53 +0900

    [Android] disable nnstreamer layer
    This patch disables nnstreamer backbone because of the package issue for
    android build
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

--- jni/Android-prebuilt.mk
+++ jni/Android-prebuilt.mk
@@ -7,3 +7,12 @@ LOCAL_SRC_FILES := $(LOCAL_PATH)/lib/arm64-v8a/libccapi-nntrainer.so
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 
 include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := nntrainer
+
+LOCAL_SRC_FILES := $(LOCAL_PATH)/lib/$(TARGET_ARCH_ABI)/libnntrainer.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include $(LOCAL_PATH)/include/nntrainer
+
+include $(PREBUILT_SHARED_LIBRARY)
