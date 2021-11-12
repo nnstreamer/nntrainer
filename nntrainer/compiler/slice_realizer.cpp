2a9c79571b6b70be1ab425e2cccdeb59df6495a6
Parichay Kapoor <pk.kapoor@samsung.com>
2021-11-12 14:34:30 +0900

    [graph] Bug fix for slice realizer
    Below bugs have been fixed for the slice realizer:
    - end layers were being added using children which were not set yet
    - the dfs_stack never ignored nodes which had already been visited which
    was leading to infinite loop
    - an invalid graph given with loops would be stuck in infinite loop in
    the DFS
    
    Signed-off-by: Parichay Kapoor <pk.kapoor@samsung.com>

--- nntrainer/compiler/slice_realizer.cpp
+++ nntrainer/compiler/slice_realizer.cpp
@@ -30,10 +30,10 @@ SliceRealizer::realize(const GraphRepresentation &reference) {
     NodeInfo(std::shared_ptr<LayerNode> node) :
       node(node),
       is_visited(false),
-      is_added(false) {}
+      to_be_added(false) {}
     std::shared_ptr<LayerNode> node; /**< set this if not visited */
     bool is_visited;                 /**< set this if visited */
-    bool is_added;                   /**< set this if added */
+    bool to_be_added;                   /**< set this if it is to be added */
     std::vector<std::string> children;
     std::vector<std::string> path;
     /**< path is the tracing result from start to current node
@@ -43,7 +43,10 @@ SliceRealizer::realize(const GraphRepresentation &reference) {
     LayerNode *operator->() { return node.get(); }
   };
 
-  std::unordered_map<std::string, NodeInfo> mp; /// map point
+  /** @note mp has to be ordered map to keep the ordering of the nodes in the
+   * graph */
+  std::map<std::string, NodeInfo> mp; /// map point
+
   std::transform(
     reference.begin(), reference.end(), std::inserter(mp, mp.end()),
     [](std::shared_ptr<LayerNode> node) {
@@ -53,22 +56,7 @@ SliceRealizer::realize(const GraphRepresentation &reference) {
   auto cur_start_layers = start_layers;
   auto cur_end_layers = end_layers;
 
-  if (start_layers.empty()) {
-    for (auto &node : reference) {
-      if (node->getNumInputConnections() == 0) {
-        cur_start_layers.push_back(node->getName());
-      }
-    }
-  }
-
-  if (end_layers.empty()) {
-    for (auto &node : mp) {
-      if (node.second.children.size() == 0) {
-        cur_end_layers.insert(node.first);
-      }
-    }
-  }
-
+  /** setup children before filling in the end layers */
   std::for_each(reference.begin(), reference.end(),
                 [&mp](std::shared_ptr<LayerNode> node) {
                   auto node_name = node->getName();
@@ -77,15 +65,29 @@ SliceRealizer::realize(const GraphRepresentation &reference) {
                   };
                 });
 
-  GraphRepresentation processed;
-
-  auto update_processed = [&processed, &mp](const std::string &name) {
-    auto &node_info = mp.at(name);
-    if (!node_info.is_added) {
-      processed.push_back(node_info.node);
-      node_info.is_added = true;
+  if (cur_start_layers.empty()) {
+    for (auto &node : mp) {
+      if (node.second.node->getNumInputConnections() == 0) {
+        cur_start_layers.push_back(node.second.node->getName());
+      }
     }
-  };
+  }
+
+  if (cur_end_layers.empty()) {
+    for (auto &node : mp) {
+      if (node.second.children.size() == 0) {
+        cur_end_layers.insert(node.first);
+      }
+    }
+  }
+
+  if (cur_start_layers.empty()) {
+    throw std::runtime_error("No start layer is found, graph has a loop.");
+  }
+
+  if (cur_end_layers.empty()) {
+    throw std::runtime_error("No start layer is found, graph has a loop.");
+  }
 
   std::vector<std::string> dfs_stack(cur_start_layers.rbegin(),
                                      cur_start_layers.rend());
@@ -95,23 +97,43 @@ SliceRealizer::realize(const GraphRepresentation &reference) {
     return iter != cur_end_layers.end();
   };
 
+  auto update_processed = [&mp](const std::string &name) {
+    auto &node_info = mp.at(name);
+    node_info.to_be_added = true;
+  };
+
   while (!dfs_stack.empty()) {
     auto &node_info = mp.at(dfs_stack.back());
-    auto &path = node_info.path;
-    path.push_back(node_info->getName());
-    if (is_end_node(node_info->getName())) {
-      std::for_each(path.begin(), path.end(), update_processed);
+    dfs_stack.pop_back();
+
+    /** if end node or added, add the current stack */
+    if (node_info.to_be_added || is_end_node(node_info->getName())) {
+      node_info.to_be_added = true;
+      std::for_each(node_info.path.begin(), node_info.path.end(),
+                    update_processed);
     }
 
-    dfs_stack.pop_back();
+    /** if already visited, skip */
+    if (node_info.is_visited) {
+      continue;
+    }
+
+    auto &path = node_info.path;
     node_info.is_visited = true;
+    path.push_back(node_info->getName());
 
     auto &children = node_info.children;
     std::for_each(children.begin(), children.end(),
                   [&path, &mp](const auto &name) { mp.at(name).path = path; });
 
-    /// @todo: stop inserting to the dfs stack if children->isAdded == true
-    dfs_stack.insert(dfs_stack.end(), children.rbegin(), children.rend());
+    dfs_stack.insert(dfs_stack.end(), children.begin(), children.end());
+  }
+
+  GraphRepresentation processed;
+  for (auto &node : mp) {
+    if (node.second.to_be_added) {
+      processed.push_back(node.second.node);
+    }
   }
 
   NNTR_THROW_IF(processed.empty(), std::invalid_argument)
