3b0f886706477891afc4fc6338f07a6be88e9942
Parichay Kapoor <pk.kapoor@samsung.com>
2021-11-15 14:16:35 +0900

    [graph] Reduce metadata overhead of slice realizer
    Slice realizer was storing the path for each node from the start nodes
    to themselves. This can be big, and also comes with a overhead of
    storing and copying these paths for each node.
    
    This patch updates the implementation of the dfs to a recursive approach
    which maintains the stack of the nodes in the memory which represents
    the path till now.
    
    Signed-off-by: Parichay Kapoor <pk.kapoor@samsung.com>

--- nntrainer/compiler/slice_realizer.cpp
+++ nntrainer/compiler/slice_realizer.cpp
@@ -9,6 +9,7 @@
  * @author Jihoon Lee <jhoon.it.lee@samsung.com>
  * @bug No known bugs except for NYI items
  */
+
 #include <layer_node.h>
 #include <slice_realizer.h>
 
@@ -33,19 +34,15 @@ SliceRealizer::realize(const GraphRepresentation &reference) {
       to_be_added(false) {}
     std::shared_ptr<LayerNode> node; /**< set this if not visited */
     bool is_visited;                 /**< set this if visited */
-    bool to_be_added;                   /**< set this if it is to be added */
+    bool to_be_added;                /**< set this if it is to be added */
     std::vector<std::string> children;
-    std::vector<std::string> path;
-    /**< path is the tracing result from start to current node
-      eg) if traversal has started from a -> b -> c -> d.
-      The path has {"a", "b", "c", "d"} */
 
     LayerNode *operator->() { return node.get(); }
   };
 
   /** @note mp has to be ordered map to keep the ordering of the nodes in the
    * graph */
-  std::map<std::string, NodeInfo> mp; /// map point
+  std::unordered_map<std::string, NodeInfo> mp; /// map point
 
   std::transform(
     reference.begin(), reference.end(), std::inserter(mp, mp.end()),
@@ -86,61 +83,69 @@ SliceRealizer::realize(const GraphRepresentation &reference) {
   }
 
   if (cur_end_layers.empty()) {
-    throw std::runtime_error("No start layer is found, graph has a loop.");
+    throw std::runtime_error("No end layer is found, graph has a loop.");
   }
 
-  std::vector<std::string> dfs_stack(cur_start_layers.rbegin(),
-                                     cur_start_layers.rend());
+  std::vector<std::string> dfs_stack;
 
+  /** if the give node is the end node in the graph */
   auto is_end_node = [&cur_end_layers](const std::string &name) {
     auto iter = cur_end_layers.find(name);
     return iter != cur_end_layers.end();
   };
 
+  /** add node to be included to subgraph */
   auto update_processed = [&mp](const std::string &name) {
     auto &node_info = mp.at(name);
     node_info.to_be_added = true;
   };
 
-  while (!dfs_stack.empty()) {
-    auto &node_info = mp.at(dfs_stack.back());
-    dfs_stack.pop_back();
+  /** dfs function to perform depth-first search recursively with tracking */
+  std::function<void(const std::string &name)> dfs =
+    [&dfs, &mp, &dfs_stack, &is_end_node,
+     &update_processed](const std::string &name) {
+      auto &node_info = mp.at(name);
+      /** if node already added or end node, add the current stack to be added
+       * to the subgraph */
+      if (node_info.to_be_added || is_end_node(name)) {
+        std::for_each(dfs_stack.begin(), dfs_stack.end(), update_processed);
+        update_processed(name);
+      }
 
-    /** if end node or added, add the current stack */
-    if (node_info.to_be_added || is_end_node(node_info->getName())) {
-      node_info.to_be_added = true;
-      std::for_each(node_info.path.begin(), node_info.path.end(),
-                    update_processed);
-    }
+      /** if node is visited, return */
+      if (node_info.is_visited) {
+        return;
+      }
 
-    /** if already visited, skip */
-    if (node_info.is_visited) {
-      continue;
-    }
+      node_info.is_visited = true;
+      dfs_stack.push_back(name);
+      /** run dfs on all the children */
+      for (auto const &child : node_info.children) {
+        dfs(child);
+      }
+      dfs_stack.pop_back();
+    };
 
-    auto &path = node_info.path;
-    node_info.is_visited = true;
-    path.push_back(node_info->getName());
-
-    auto &children = node_info.children;
-    std::for_each(children.begin(), children.end(),
-                  [&path, &mp](const auto &name) { mp.at(name).path = path; });
-
-    dfs_stack.insert(dfs_stack.end(), children.begin(), children.end());
+  /** run dfs from all the starting layers */
+  for (auto &name : cur_start_layers) {
+    dfs(name);
   }
 
-  GraphRepresentation processed;
-  for (auto &node : mp) {
-    if (node.second.to_be_added) {
-      processed.push_back(node.second.node);
+  /** created the subgraph */
+  GraphRepresentation subgraph;
+  /** @note: iterate over reference than over mp to ensure the correct ordering
+   * of layers */
+  for (auto &node : reference) {
+    if (mp[node->getName()].to_be_added) {
+      subgraph.push_back(node);
     }
   }
 
-  NNTR_THROW_IF(processed.empty(), std::invalid_argument)
+  NNTR_THROW_IF(subgraph.empty(), std::invalid_argument)
     << "After slice, there is no node left, please check if configuration is "
        "correct";
 
-  return processed;
+  return subgraph;
 }
 
 } // namespace nntrainer
