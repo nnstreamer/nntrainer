01ee5fef75cb248fe94e593c6c75b9756f8589f7
Parichay Kapoor <pk.kapoor@samsung.com>
2021-11-18 17:40:01 +0900

    [test] Enable layer node test
    Enable the layer node test.
    
    Signed-off-by: Parichay Kapoor <pk.kapoor@samsung.com>

--- test/unittest/layers/unittest_layer_node.cpp
+++ test/unittest/layers/unittest_layer_node.cpp
@@ -9,20 +9,24 @@
  * @author      Parichay Kapoor <pk.kapoor@samsung.com>
  * @bug         No known bugs
  */
+
 #include <gtest/gtest.h>
+
+#include <fc_layer.h>
+#include <layer_node.h>
+#include <nntrainer_error.h>
+#include <tensor_dim.h>
+#include <var_grad.h>
 
 /**
  * @brief test distribute property
  */
 TEST(nntrainer_LayerNode, setDistribute_01_p) {
-  int status = ML_ERROR_NONE;
-
   auto lnode = nntrainer::createLayerNode(nntrainer::FullyConnectedLayer::type);
 
   EXPECT_EQ(false, lnode->getDistribute());
 
-  status = lnode->setProperty({"distribute=true"});
-  EXPECT_EQ(status, ML_ERROR_NONE);
+  EXPECT_NO_THROW(lnode->setProperty({"distribute=true"}));
 
   EXPECT_EQ(true, lnode->getDistribute());
 }
@@ -31,64 +35,54 @@ TEST(nntrainer_LayerNode, setDistribute_01_p) {
  * @brief test flatten property
  */
 TEST(nntrainer_LayerNode, setFlatten_01_p) {
-  int status = ML_ERROR_NONE;
-
   auto lnode = nntrainer::createLayerNode(nntrainer::FullyConnectedLayer::type);
-  status = lnode->setProperty({"flatten=true"});
-  EXPECT_EQ(status, ML_ERROR_NONE);
+  EXPECT_NO_THROW(lnode->setProperty({"flatten=true"}));
 }
 
 /**
  * @brief test finalize with wrong context
  */
 TEST(nntrainer_LayerNode, finalize_01_n) {
-  int status = ML_ERROR_NONE;
-
   auto lnode = nntrainer::createLayerNode(nntrainer::FullyConnectedLayer::type);
-  EXPECT_THROW(lnode->finalize(), std::runtime_error);
+  EXPECT_ANY_THROW(lnode->finalize());
+}
+
+/**
+ * @brief test finalize with wrong context
+ */
+TEST(nntrainer_LayerNode, finalize_02_n) {
+  auto lnode = nntrainer::createLayerNode(nntrainer::FullyConnectedLayer::type);
+  lnode->setProperty({"name=abc"});
+  EXPECT_ANY_THROW(lnode->finalize());
+}
+
+/**
+ * @brief test finalize with wrong context
+ */
+TEST(nntrainer_LayerNode, finalize_03_n) {
+  auto lnode = nntrainer::createLayerNode(nntrainer::FullyConnectedLayer::type);
+  lnode->setProperty({"input_shape=1:1:1"});
+  EXPECT_ANY_THROW(lnode->finalize());
 }
 
 /**
  * @brief test finalize with right context
  */
-TEST(nntrainer_LayerNode, finalize_02_p) {
-  int status = ML_ERROR_NONE;
-
+TEST(nntrainer_LayerNode, finalize_04_p) {
   auto lnode = nntrainer::createLayerNode(nntrainer::FullyConnectedLayer::type);
-  lnode->setProperty("input_shape=1:1:1");
+  lnode->setProperty({"input_shape=1:1:1", "name=abc", "unit=4"});
   EXPECT_NO_THROW(lnode->finalize());
 }
 
 /**
  * @brief test double finalize
  */
-TEST(nntrainer_LayerNode, finalize_01_n) {
-  int status = ML_ERROR_NONE;
-
+TEST(nntrainer_LayerNode, finalize_05_n) {
   auto lnode = nntrainer::createLayerNode(nntrainer::FullyConnectedLayer::type);
-  lnode->setProperty("input_shape=1:1:1");
+  lnode->setProperty({"input_shape=1:1:1", "name=abc", "unit=4"});
   EXPECT_NO_THROW(lnode->finalize());
-  EXPECT_THROW(lnode->finalize(), std::runtime_error);
-}
-
-/**
- * @brief Main gtest
- */
-int main(int argc, char **argv) {
-  int result = -1;
-
-  try {
-    testing::InitGoogleTest(&argc, argv);
-  } catch (...) {
-    std::cerr << "Error duing IniGoogleTest" << std::endl;
-    return 0;
-  }
-
-  try {
-    result = RUN_ALL_TESTS();
-  } catch (...) {
-    std::cerr << "Error duing RUN_ALL_TSETS()" << std::endl;
-  }
-
-  return result;
+  nntrainer::Var_Grad input =
+    nntrainer::Var_Grad(nntrainer::TensorDim({1, 1, 1, 1}));
+  lnode->configureRunContext({}, {&input}, {}, {});
+  EXPECT_ANY_THROW(lnode->finalize());
 }
