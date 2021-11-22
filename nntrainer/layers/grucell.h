df73f8e8a6a838ca0f16eeb5c3c6b2e1c0f0e5f2
hyeonseok lee <hs89.lee@samsung.com>
2021-11-22 15:46:17 +0900

    [grucell] Implement grucell
     - Support add with non-contiguous tensor
     - Implement grucell which is run only 1 timestep
     - Todo:
       1. Make it more efficient with strided tensor(Reduce copy tensor)
       2. Reduce temporary tensor or do inplace operation
    
    Self evaluation:
    
    Build test: [X]Passed [ ]Failed [ ]Skipped
    Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: hyeonseok lee <hs89.lee@samsung.com>

---
+++ nntrainer/layers/grucell.h
@@ -0,0 +1,136 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2021 hyeonseok lee <hs89.lee@samsung.com>
+ *
+ * @file   grucell.h
+ * @date   28 Oct 2021
+ * @brief  This is Gated Recurrent Unit Cell Layer Class of Neural Network
+ * @see	   https://github.com/nnstreamer/nntrainer
+ * @author hyeonseok lee <hs89.lee@samsung.com>
+ * @bug    No known bugs except for NYI items
+ *
+ */
+
+#ifndef __GRUCELL_H__
+#define __GRUCELL_H__
+#ifdef __cplusplus
+
+#include <acti_func.h>
+#include <common_properties.h>
+#include <layer_impl.h>
+
+namespace nntrainer {
+
+/**
+ * @class   GRUCellLayer
+ * @brief   GRUCellLayer
+ */
+class GRUCellLayer : public LayerImpl {
+public:
+  /**
+   * @brief     Constructor of GRUCellLayer
+   */
+  GRUCellLayer();
+
+  /**
+   * @brief     Destructor of GRUCellLayer
+   */
+  ~GRUCellLayer() = default;
+
+  /**
+   *  @brief  Move constructor.
+   *  @param[in] GRUCellLayer &&
+   */
+  GRUCellLayer(GRUCellLayer &&rhs) noexcept = default;
+
+  /**
+   * @brief  Move assignment operator.
+   * @parma[in] rhs GRUCellLayer to be moved.
+   */
+  GRUCellLayer &operator=(GRUCellLayer &&rhs) = default;
+
+  /**
+   * @copydoc Layer::finalize(InitLayerContext &context)
+   */
+  void finalize(InitLayerContext &context) override;
+
+  /**
+   * @copydoc Layer::forwarding(RunLayerContext &context, bool training)
+   */
+  void forwarding(RunLayerContext &context, bool training) override;
+
+  /**
+   * @copydoc Layer::calcDerivative(RunLayerContext &context)
+   */
+  void calcDerivative(RunLayerContext &context) override;
+
+  /**
+   * @copydoc Layer::calcGradient(RunLayerContext &context)
+   */
+  void calcGradient(RunLayerContext &context) override;
+
+  /**
+   * @copydoc Layer::exportTo(Exporter &exporter, ExportMethods method)
+   */
+  void exportTo(Exporter &exporter, const ExportMethods &method) const override;
+
+  /**
+   * @copydoc Layer::getType()
+   */
+  const std::string getType() const override { return GRUCellLayer::type; };
+
+  /**
+   * @copydoc Layer::supportBackwarding()
+   */
+  bool supportBackwarding() const override { return true; }
+
+  /**
+   * @copydoc Layer::setProperty(const PropertyType type, const std::string
+   * &value)
+   */
+  void setProperty(const std::vector<std::string> &values) override;
+
+  /**
+   * @copydoc Layer::setBatch(RunLayerContext &context, unsigned int batch)
+   */
+  void setBatch(RunLayerContext &context, unsigned int batch) override;
+
+  inline static const std::string type = "grucell";
+
+private:
+  static constexpr unsigned int NUM_GATE = 3;
+
+  /**
+   * Unit: number of output neurons
+   * HiddenStateActivation: activation type for hidden state. default is tanh
+   * RecurrentActivation: activation type for recurrent. default is sigmoid
+   * DropOutRate: dropout rate
+   * MaxTimeStep: Maximum timestep of gru
+   * TimeStep: timestep for which gru should operate
+   *
+   * */
+  std::tuple<props::Unit, props::HiddenStateActivation,
+             props::RecurrentActivation, props::DropOutRate, props::MaxTimestep,
+             props::Timestep>
+    grucell_props;
+  std::array<unsigned int, 7> wt_idx; /**< indices of the weights */
+
+  /**
+   * @brief     activation function for h_t : default is sigmoid
+   */
+  ActiFunc acti_func;
+
+  /**
+   * @brief     activation function for recurrent : default is tanh
+   */
+  ActiFunc recurrent_acti_func;
+
+  /**
+   * @brief     to protect overflow
+   */
+  float epsilon;
+};
+} // namespace nntrainer
+
+#endif /* __cplusplus */
+#endif /* __GRUCELL_H__ */
