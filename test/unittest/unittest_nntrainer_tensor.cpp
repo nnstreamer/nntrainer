e79d67e862fedf46a7dc70718d2b0cc74fbaee15
Jihoon Lee <jhoon.it.lee@samsung.com>
2021-12-15 19:52:51 +0900

    [tensor] Add tensor::split
    This patch add tensor split for wider use
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

--- test/unittest/unittest_nntrainer_tensor.cpp
+++ test/unittest/unittest_nntrainer_tensor.cpp
@@ -3054,6 +3054,142 @@ TEST(nntrainer_Tensor, initialize_08_p) {
   EXPECT_EQ(golden, t);
 }
 
+TEST(nntrainer_Tensor, split_p) {
+  {
+    nntrainer::TensorDim ref_dim(3, 2, 4, 5);
+    nntrainer::Tensor t = ranged(3, 2, 4, 5);
+    std::vector<nntrainer::Tensor> answer;
+    answer.reserve(3);
+    {
+      float answer_data[] = {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,
+                             10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+                             20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+                             30, 31, 32, 33, 34, 35, 36, 37, 38, 39};
+      answer.emplace_back(ml::train::TensorDim{1, 2, 4, 5}, answer_data);
+    }
+    {
+      float answer_data[] = {40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+                             50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+                             60, 61, 62, 63, 64, 65, 66, 67, 68, 69,
+                             70, 71, 72, 73, 74, 75, 76, 77, 78, 79};
+      answer.emplace_back(ml::train::TensorDim{1, 2, 4, 5}, answer_data);
+    }
+    {
+      float answer_data[] = {80,  81,  82,  83,  84,  85,  86,  87,  88,  89,
+                             90,  91,  92,  93,  94,  95,  96,  97,  98,  99,
+                             100, 101, 102, 103, 104, 105, 106, 107, 108, 109,
+                             110, 111, 112, 113, 114, 115, 116, 117, 118, 119};
+      answer.emplace_back(ml::train::TensorDim{1, 2, 4, 5}, answer_data);
+    }
+    EXPECT_EQ(t.split(3, 0), answer);
+  }
+  {
+    nntrainer::TensorDim ref_dim(3, 2, 4, 5);
+    nntrainer::Tensor t = ranged(3, 2, 4, 5);
+    std::vector<nntrainer::Tensor> answer;
+    answer.reserve(2);
+    {
+      float answer_data[] = {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11,
+                             12, 13, 14, 15, 16, 17, 18, 19, 40, 41, 42, 43,
+                             44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55,
+                             56, 57, 58, 59, 80, 81, 82, 83, 84, 85, 86, 87,
+                             88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99};
+      answer.emplace_back(ml::train::TensorDim{3, 1, 4, 5}, answer_data);
+    }
+    {
+      float answer_data[] = {20,  21,  22,  23,  24,  25,  26,  27,  28,  29,
+                             30,  31,  32,  33,  34,  35,  36,  37,  38,  39,
+                             60,  61,  62,  63,  64,  65,  66,  67,  68,  69,
+                             70,  71,  72,  73,  74,  75,  76,  77,  78,  79,
+                             100, 101, 102, 103, 104, 105, 106, 107, 108, 109,
+                             110, 111, 112, 113, 114, 115, 116, 117, 118, 119};
+      answer.emplace_back(ml::train::TensorDim{3, 1, 4, 5}, answer_data);
+    }
+    EXPECT_EQ(t.split(2, 1), answer);
+  }
+  {
+    nntrainer::TensorDim ref_dim(3, 2, 4, 5);
+    nntrainer::Tensor t = ranged(3, 2, 4, 5);
+    std::vector<nntrainer::Tensor> answer;
+    answer.reserve(2);
+    {
+      float answer_data[] = {
+        0,  1,  2,  3,  4,  5,   6,   7,   8,   9,   20,  21,  22,  23,  24,
+        25, 26, 27, 28, 29, 40,  41,  42,  43,  44,  45,  46,  47,  48,  49,
+        60, 61, 62, 63, 64, 65,  66,  67,  68,  69,  80,  81,  82,  83,  84,
+        85, 86, 87, 88, 89, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109};
+      answer.emplace_back(ml::train::TensorDim{3, 2, 2, 5}, answer_data);
+    }
+    {
+      float answer_data[] = {
+        10, 11, 12, 13, 14, 15,  16,  17,  18,  19,  30,  31,  32,  33,  34,
+        35, 36, 37, 38, 39, 50,  51,  52,  53,  54,  55,  56,  57,  58,  59,
+        70, 71, 72, 73, 74, 75,  76,  77,  78,  79,  90,  91,  92,  93,  94,
+        95, 96, 97, 98, 99, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119};
+      answer.emplace_back(ml::train::TensorDim{3, 2, 2, 5}, answer_data);
+    }
+    EXPECT_EQ(t.split(2, 2), answer);
+  }
+  {
+    nntrainer::TensorDim ref_dim(3, 2, 4, 5);
+    nntrainer::Tensor t = ranged(3, 2, 4, 5);
+    std::vector<nntrainer::Tensor> answer;
+    answer.reserve(5);
+    {
+      float answer_data[] = {0,  5,  10, 15, 20,  25,  30,  35,
+                             40, 45, 50, 55, 60,  65,  70,  75,
+                             80, 85, 90, 95, 100, 105, 110, 115};
+      answer.emplace_back(ml::train::TensorDim{3, 2, 4, 1}, answer_data);
+    }
+    {
+      float answer_data[] = {1,  6,  11, 16, 21,  26,  31,  36,
+                             41, 46, 51, 56, 61,  66,  71,  76,
+                             81, 86, 91, 96, 101, 106, 111, 116};
+      answer.emplace_back(ml::train::TensorDim{3, 2, 4, 1}, answer_data);
+    }
+    {
+      float answer_data[] = {2,  7,  12, 17, 22,  27,  32,  37,
+                             42, 47, 52, 57, 62,  67,  72,  77,
+                             82, 87, 92, 97, 102, 107, 112, 117};
+      answer.emplace_back(ml::train::TensorDim{3, 2, 4, 1}, answer_data);
+    }
+    {
+      float answer_data[] = {3,  8,  13, 18, 23,  28,  33,  38,
+                             43, 48, 53, 58, 63,  68,  73,  78,
+                             83, 88, 93, 98, 103, 108, 113, 118};
+      answer.emplace_back(ml::train::TensorDim{3, 2, 4, 1}, answer_data);
+    }
+    {
+      float answer_data[] = {4,  9,  14, 19, 24,  29,  34,  39,
+                             44, 49, 54, 59, 64,  69,  74,  79,
+                             84, 89, 94, 99, 104, 109, 114, 119};
+      answer.emplace_back(ml::train::TensorDim{3, 2, 4, 1}, answer_data);
+    }
+    EXPECT_EQ(t.split(5, 3), answer);
+  }
+  {
+    nntrainer::TensorDim ref_dim(1, 1, 4, 6);
+    nntrainer::Tensor t = ranged(1, 1, 4, 6);
+    std::vector<nntrainer::Tensor> answer;
+    answer.reserve(2);
+    {
+      float answer_data[] = {0, 1, 2, 6, 7, 8, 12, 13, 14, 18, 19, 20};
+      answer.emplace_back(ml::train::TensorDim{1, 1, 4, 3}, answer_data);
+    }
+    {
+      float answer_data[] = {3, 4, 5, 9, 10, 11, 15, 16, 17, 21, 22, 23};
+      answer.emplace_back(ml::train::TensorDim{1, 1, 4, 3}, answer_data);
+    }
+    EXPECT_EQ(t.split(2, 3), answer);
+  }
+}
+
+// TEST(nntrainer_Tensor, split_n) {
+//     using namespace nntrainer;
+//     Tensor t = ranged(3, 2, 4, 5);
+//     t.split(3, 0);
+// }
+
 int main(int argc, char **argv) {
   int result = -1;
 
