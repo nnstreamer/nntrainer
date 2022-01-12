43bcc0131880a53be0eb71ee7e192eeedea453e0
Sijun Park <sijun1.park@samsung.com>
2022-01-12 14:52:03 +0900

    file writing update for Roomid
    applying to the Roomid application, open and close file in each epoch
    Signed-off-by: Sijun Park <sijun1.park@samsung.com>

--- nntrainer/models/neuralnet.cpp
+++ nntrainer/models/neuralnet.cpp
@@ -638,6 +638,10 @@ int NeuralNetwork::train_run() {
       checkedOpenStream<std::ofstream>(save_train_log_path, std::ios::out);
   }
 
+  train_log_file << "진행률 : 0%" << "\n";
+  train_log_file.close();
+
+
   auto const &outputs = model_graph.getOutputTensors();
   auto in_dims = model_graph.getInputDimension();
   auto label_dims = model_graph.getOutputDimension();
@@ -713,7 +717,7 @@ int NeuralNetwork::train_run() {
     stat.num_iterations++;
   };
 
-  auto train_epoch_end = [this, &train_log_file](RunStats &stat,
+  auto train_epoch_end = [this](RunStats &stat,
                                                  DataBuffer &buffer) {
     stat.loss /= static_cast<float>(stat.num_iterations);
     auto &save_path = std::get<props::SavePath>(model_flex_props);
@@ -721,10 +725,22 @@ int NeuralNetwork::train_run() {
       save(save_path, ml::train::ModelFormat::MODEL_FORMAT_BIN);
     }
 
-    if (train_log_file.good()) {
-      train_log_file << "#" << epoch_idx << "/" << getEpochs()
-                     << " - Training Loss: " << stat.loss << "\n";
+
+    std::ofstream train_log_file;
+    auto &save_train_log_path =
+          std::get<props::SaveTrainLogPath>(model_flex_props);
+    if (!save_train_log_path.empty()) {
+      train_log_file =
+      checkedOpenStream<std::ofstream>(save_train_log_path, std::ios::out);
     }
+
+
+      train_log_file << "진행률 : " << epoch_idx*100/getEpochs() << "%\n";
+      //train_log_file << "progress : " << epoch_idx << "/" << getEpochs() << "\n";
+      train_log_file << "#" << epoch_idx << "/" << getEpochs() << " - Training Loss: " << stat.loss << "\n";
+      train_log_file.close();
+
+
     std::cout << "#" << epoch_idx << "/" << getEpochs()
               << " - Training Loss: " << stat.loss;
     ml_logi("# %d / %d - Training Loss: %f", epoch_idx, getEpochs(), stat.loss);
@@ -794,9 +810,9 @@ int NeuralNetwork::train_run() {
   /** Clear the set inputs and labels */
   model_graph.setInputsLabels({}, {});
 
-  if (!save_train_log_path.empty()) {
-    train_log_file.close();
-  }
+  //if (!save_train_log_path.empty()) {
+  //  train_log_file.close();
+  //}
   return status;
 }
 
