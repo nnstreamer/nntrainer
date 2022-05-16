dbba00623dc962e29ae823262362618d15fa0931
jijoong.moon <jijoong.moon@samsung.com>
2022-05-16 20:10:11 +0900

    [ Application ] Update Logistic Regression to use CCAPI
    This patch includes
     . Update Logistic Regression to use CCAPIs
     . Update Inference process
     . static link about openmp
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	 [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: jijoong.moon <jijoong.moon@samsung.com>

--- Applications/LogisticRegression/jni/Android.mk
+++ Applications/LogisticRegression/jni/Android.mk
@@ -11,16 +11,9 @@ ifndef NNTRAINER_ROOT
 NNTRAINER_ROOT := $(LOCAL_PATH)/../../..
 endif
 
- ML_API_COMMON_INCLUDES := ${NNTRAINER_ROOT}/ml_api_common/include
-NNTRAINER_INCLUDES := $(NNTRAINER_ROOT)/nntrainer \
-	$(NNTRAINER_ROOT)/nntrainer/dataset \
-	$(NNTRAINER_ROOT)/nntrainer/models \
-	$(NNTRAINER_ROOT)/nntrainer/layers \
-	$(NNTRAINER_ROOT)/nntrainer/compiler \
-	$(NNTRAINER_ROOT)/nntrainer/graph \
-	$(NNTRAINER_ROOT)/nntrainer/optimizers \
-	$(NNTRAINER_ROOT)/nntrainer/tensor \
-	$(NNTRAINER_ROOT)/nntrainer/utils \
+ML_API_COMMON_INCLUDES := ${NNTRAINER_ROOT}/ml_api_common/include
+
+NNTRAINER_INCLUDES := $(NNTRAINER_ROOT)/nntrainer/include \
 	$(NNTRAINER_ROOT)/api \
 	$(NNTRAINER_ROOT)/api/ccapi/include \
 	${ML_API_COMMON_INCLUDES}
@@ -43,12 +36,12 @@ LOCAL_ARM_NEON := true
 LOCAL_CFLAGS += -std=c++17 -Ofast -mcpu=cortex-a53 -Ilz4-nougat/lib
 LOCAL_LDFLAGS += -Llz4-nougat/lib/obj/local/$(TARGET_ARCH_ABI)/
 LOCAL_CXXFLAGS += -std=c++17 -frtti
-LOCAL_CFLAGS += -pthread -fexceptions -fopenmp
+LOCAL_CFLAGS += -pthread -fexceptions -fopenmp -static-openmp
 LOCAL_LDFLAGS += -fexceptions
 LOCAL_MODULE_TAGS := optional
 LOCAL_ARM_MODE := arm
 LOCAL_MODULE := nntrainer_logistic
-LOCAL_LDLIBS := -llog -landroid -fopenmp
+LOCAL_LDLIBS := -llog -landroid -fopenmp -static-openmp
 
 LOCAL_SRC_FILES := main.cpp
 
