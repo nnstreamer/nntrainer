db02b64811195e24fea4c55b84627948bfd2aee5
hyunil park <hyunil46.park@samsung.com>
2022-11-09 15:40:50 +0900

    [subplugin] Add constructing nntrainer and InputTensorsInfo class
    - Construct nntrainer with config file
    - Deconstruct nntrainer
    - Load model config
    - Compile, initialize and setDataset
    - Add InputTensorsInfo to construct dataset memory
    
    **Self evaluation:**
    1. Build test:   [X]Passed [ ]Failed [ ]Skipped
    2. Run test:     [ ]Passed [ ]Failed [X]Skipped
    
    Signed-off-by: hyunil park <hyunil46.park@samsung.com>

--- nnstreamer/tensor_trainer/tensor_trainer_nntrainer.cc
+++ nnstreamer/tensor_trainer/tensor_trainer_nntrainer.cc
@@ -22,12 +22,15 @@
 #include "ml-api-common.h"
 #include "nnstreamer.h"
 #include "nnstreamer_plugin_api.h"
+#include "nnstreamer_plugin_api_filter.h"
 #include "tensor_trainer_nntrainer.hh"
 
 #include <iostream>
-#define cout_log(x)
-std::cout << __FILE__ << ":" << __LINE__ << ":" << __func__ << ":" << #x << ":"
-          << x << std::endl;
+
+#define UNUSED(expr) \
+  do {               \
+    (void)(expr);    \
+  } while (0)
 
 /**
  * @brief startup constructor
@@ -39,19 +42,107 @@ void init_subplugin_nntrainer(void) __attribute__((constructor));
  */
 void fini_subplugin_nntrainer(void) __attribute__((destructor));
 
+static void nntrainer_model_destructor(const GstTensorFilterProperties *prop,
+                                       void **private_data) {
+  NNTrainer::NNTrainerTrain *nntrainer =
+    static_cast<NNTrainer::NNTrainerTrain *>(*private_data);
+
+  if (!nntrainer)
+    return;
+  delete nntrainer;
+  *private_data = NULL;
+}
+
+#if 0 /** To avoid build errors : defined but not used */
+static void nntrainer_model_train(const GstTensorFilterProperties *prop,
+                                  void **private_data) {
+  NNTrainer::NNTrainerTrain *nntrainer =
+    static_cast<NNTrainer::NNTrainerTrain *>(*private_data);
+  if (!nntrainer) {
+    std::cerr << "Failed get model" << std::endl;
+  }
+
+  try {
+    nntrainer->trainModel();
+  } catch (std::exception &e) {
+    std::cerr << "Error" << typeid(e).name() << "," << e.what() << std::endl;
+  }
+}
+#endif
+
+void NNTrainer::NNTrainerTrain::trainModel() {
+  try {
+    model->train();
+    training_loss = model->getTrainingLoss();
+    validation_loss = model->getValidationLoss();
+  } catch (std::exception &e) {
+    std::cerr << "Error" << typeid(e).name() << "," << e.what() << std::endl;
+  }
+}
+
+void NNTrainer::NNTrainerTrain::createModel() {
+
+  try {
+    model = ml::train::createModel(ml::train::ModelType::NEURAL_NET);
+    model->load(model_config,
+                ml::train::ModelFormat::MODEL_FORMAT_INI_WITH_BIN);
+  } catch (std::exception &e) {
+    std::cerr << "Failed loading model configuration file" << typeid(e).name()
+              << e.what() << std::endl;
+  }
+
+  try {
+    model->compile();
+    model->initialize();
+    model->setDataset(ml::train::DatasetModeType::MODE_TRAIN, dataset_train);
+    model->setDataset(ml::train::DatasetModeType::MODE_VALID, dataset_valid);
+  } catch (std::exception &e) {
+    std::cerr << "Error" << typeid(e).name() << "," << e.what() << std::endl;
+  }
+}
+
+NNTrainer::NNTrainerTrain::NNTrainerTrain(const std::string &_model_config) :
+  model_config(_model_config) {
+  createModel();
+}
+
+static int
+nntrainer_model_construct_with_conf(const GstTensorFilterProperties *prop,
+                                    void **private_data) {
+
+  NNTrainer::NNTrainerTrain *nntrainer =
+    static_cast<NNTrainer::NNTrainerTrain *>(*private_data);
+  if (!nntrainer)
+    nntrainer_model_destructor(prop, private_data);
+
+  try {
+    nntrainer =
+      new NNTrainer::NNTrainerTrain("/home/hyunil/nnstreamer/mnist.ini");
+  } catch (std::exception &e) {
+    std::cerr << "Error" << typeid(e).name() << "," << e.what() << std::endl;
+  }
+
+  return 0;
+}
+
+static int nntrainer_model_construct(const GstTensorFilterProperties *prop,
+                                     void **private_data) {
+  int status = nntrainer_model_construct_with_conf(prop, private_data);
+
+  return status;
+}
+
 static gchar subplugin_name[] = "nntrainer";
 
 /** GstTensorFilterFramework will be changed */
 static GstTensorFilterFramework NNS_Trainer_support_nntrainer = {
   .version = GST_TENSOR_FILTER_FRAMEWORK_V0,
-  .open = NULL,  //.constructr
-  .close = NULL, //.destructor
-  //.train = NULL
+  .open = nntrainer_model_construct, //.construct = nntrainer_model_construct,
+  .close =
+    nntrainer_model_destructor, //.destructor = nntrainer_model_destructor,
+  //.train = nntrainer_model_train
 };
 
-/** member function and variable of NNS_Trainer_support_nntrainer will be
- * changed
- */
 void init_subplugin_nntrainer(void) {
   NNS_Trainer_support_nntrainer.name = subplugin_name;
   NNS_Trainer_support_nntrainer.allow_in_place = FALSE;
