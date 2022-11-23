6f82ee6b781f824a3c399fbd5dc8abd27384432b
Seungbaek Hong <sb92.hong@samsung.net>
2022-11-23 19:44:10 +0900

    [utils] Add getRealpath function
    Since the "realpath" function is not available in the Windows operating system, the "getRealpath" function has been added and replaced so that it can be used in the Windows operating system.
    
    - Add "getRealpath" function to utils.
    
    **Self evaluation:**
    1. Build test: [x]Passed []Failed []Skipped
    2. Run test: [x]Passed []Failed []Skipped
    
    Signed-off-by: Seungbaek Hong <sb92.hong@samsung.net>

--- nntrainer/models/model_loader.cpp
+++ nntrainer/models/model_loader.cpp
@@ -470,7 +470,7 @@ int ModelLoader::loadFromConfig(std::string config, NeuralNetwork &model) {
 
   model_file_context = std::make_unique<AppContext>();
 
-  auto config_realpath_char = realpath(config.c_str(), nullptr);
+  auto config_realpath_char = getRealpath(config.c_str(), nullptr);
   if (config_realpath_char == nullptr) {
     ml_loge("failed to resolve config path to absolute path, reason: %s",
             strerror(errno));
