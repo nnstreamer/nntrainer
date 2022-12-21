21380912db1ea9f1cb0aebd49c4a19938eb88694
Jiho Chu <jiho.chu@samsung.com>
2022-12-21 11:36:42 +0900

    [Utils] Add log timestamp type
    This patch adds types fo log timestamp type.
    
    NNTRAINER_LOG_TIMESTAMP_SEC: year, mon, day, hour, min, sec
    NNTRAINER_LOG_TIMESTAMP_MS: milliseconds from log start
    
    Signed-off-by: Jiho Chu <jiho.chu@samsung.com>

--- nntrainer/nntrainer_logger.cpp
+++ nntrainer/nntrainer_logger.cpp
@@ -67,7 +67,7 @@ Logger::Cleanup::~Cleanup() {
 
 Logger::~Logger() { outputstream.close(); }
 
-Logger::Logger() {
+Logger::Logger() : ts_type(NNTRAINER_LOG_TIMESTAMP_SEC) {
   struct tm now;
   getLocaltime(&now);
   std::stringstream ss;
@@ -90,9 +90,8 @@ Logger::Logger() {
 void Logger::log(const std::string &message,
                  const nntrainer_loglevel loglevel) {
   std::lock_guard<std::mutex> guard(smutex);
-  struct tm now;
-  getLocaltime(&now);
   std::stringstream ss;
+
   switch (loglevel) {
   case NNTRAINER_LOG_INFO:
     ss << "[NNTRAINER INFO  ";
@@ -110,11 +109,24 @@ void Logger::log(const std::string &message,
     break;
   }
 
-  ss << std::dec << (now.tm_year + 1900) << '-' << std::setfill('0')
-     << std::setw(2) << (now.tm_mon + 1) << '-' << std::setfill('0')
-     << std::setw(2) << now.tm_mday << ' ' << std::setfill('0') << std::setw(2)
-     << now.tm_hour << ':' << std::setfill('0') << std::setw(2) << now.tm_min
-     << ':' << std::setfill('0') << std::setw(2) << now.tm_sec << ']';
+  if (ts_type == NNTRAINER_LOG_TIMESTAMP_MS) {
+    static auto start = std::chrono::system_clock::now().time_since_epoch();
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+                std::chrono::system_clock::now().time_since_epoch() - start)
+                .count();
+
+    ss << "[ " << ms << " ]";
+  } else if (ts_type == NNTRAINER_LOG_TIMESTAMP_SEC) {
+    struct tm now;
+    getLocaltime(&now);
+
+    ss << std::dec << (now.tm_year + 1900) << '-' << std::setfill('0')
+       << std::setw(2) << (now.tm_mon + 1) << '-' << std::setfill('0')
+       << std::setw(2) << now.tm_mday << ' ' << std::setfill('0')
+       << std::setw(2) << now.tm_hour << ':' << std::setfill('0')
+       << std::setw(2) << now.tm_min << ':' << std::setfill('0') << std::setw(2)
+       << now.tm_sec << ']';
+  }
 
   outputstream << ss.str() << " " << message << std::endl;
 }
