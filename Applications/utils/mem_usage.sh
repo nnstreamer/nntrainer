8d656f11b92fd3c4b9b965936b5897fd40a4225b
Jiho Chu <jiho.chu@samsung.com>
2022-12-27 16:16:17 +0900

    [Utils] Modify memory usage script
    It uses /proc/$PID/smaps information to check current memory usage.
    
    Please refer doc:
    https://man7.org/linux/man-pages/man5/proc.5.html
    
    Signed-off-by: Jiho Chu <jiho.chu@samsung.com>

--- Applications/utils/mem_usage.sh
+++ Applications/utils/mem_usage.sh
@@ -16,30 +16,29 @@ log="\nPID - $process_id :"
 outlog=""
 count=0
 while true; do
-    if [ -d "/proc/$process_id" ]; then
-	output=`grep VmHWM /proc/$process_id/status`
-	read -ra mem_str <<< "$output"
-	count=$((count + 1))
-	mem_usage=$((${mem_str[1]}))
-	cpu_output=""
-	n=`nproc`
-	i=0
-	while [ "$i" -lt $n ]
-	do
-	    cpu_freq=`cat /sys/devices/system/cpu/cpu$i/cpufreq/scaling_cur_freq`
-	    cpu_output="$cpu_output $cpu_freq"
-	    i=$(expr $i + 1)
-	done
-	outlog="$outlog$count $mem_usage kB $cpu_output\n"
-	let mem_peak='mem_usage > mem_peak ? mem_usage : mem_peak'
-	sleep 1s # configure the sleep time if the program does not run long enough
-    else
-	break
-    fi
+	if [ -d "/proc/$process_id" ]; then
+		count=$((count + 1))
+    # Use smaps to get more accurate memory usage (refer: https://man7.org/linux/man-pages/man5/proc.5.html)
+		mem_usage=`cat /proc/$process_id/smaps | grep -E "Rss" | awk '{sum += $2;}END{print sum;}'`
+		cpu_output=""
+		n=`nproc`
+		i=0
+		while [ "$i" -lt $n ]
+		do
+			cpu_freq=`cat /sys/devices/system/cpu/cpu$i/cpufreq/scaling_cur_freq`
+			cpu_output="$cpu_output $cpu_freq"
+			i=$(expr $i + 1)
+		done
+		outlog="$outlog$count $mem_usage kB $cpu_output\n"
+		let mem_peak='mem_usage > mem_peak ? mem_usage : mem_peak'
+		sleep 1s # configure the sleep time if the program does not run long enough
+	else
+		break
+	fi
 done
 
 if [ -d "/proc/$process_id" ]; then
-    kill -9 $process_id
+	kill -9 $process_id
 fi
 
 log="$log: $mem_peak kB"
