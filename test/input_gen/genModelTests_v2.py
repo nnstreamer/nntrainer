aa8a535050fdcb4515c2b38c265e2bc5137f8c31
Seungbaek Hong <sb92.hong@samsung.com>
2022-12-29 15:40:33 +0900

    [test] add test case when a specific layer is non-trainable
    Add a test case when a specific layer is non-trainable.
    
    - Add a test case when the output fc layer is set to non-trainable.
    
    **Self evaluation:**
    1. Build test: [x]Passed []Failed []Skipped
    2. Run test: [x]Passed []Failed []Skipped
    
    Signed-off-by: Seungbaek Hong <sb92.hong@samsung.com>

--- test/input_gen/genModelTests_v2.py
+++ test/input_gen/genModelTests_v2.py
@@ -296,12 +296,9 @@ class NonTrainableFC(torch.nn.Module):
         self.fc3 = torch.nn.Linear(10, 2)
         self.loss = torch.nn.MSELoss()
         # determine which layer to set to non-trainable
-        if idx == 1:
-            for param in self.fc1.parameters():
-                param.requires_grad = False
-        elif idx == 2:
-            for param in self.fc2.parameters():
-                param.requires_grad = False
+        fc_layer_list = [self.fc1, self.fc2, self.fc3]
+        for param in fc_layer_list[idx-1].parameters():
+            param.requires_grad = False
 
     def forward(self, inputs, labels):
         out = torch.relu(self.fc1(inputs[0]))
@@ -529,6 +526,16 @@ if __name__ == "__main__":
         label_dims=[(3,2)],
         name="non_trainable_fc_idx2"
     )
+
+    non_trainable_fc_idx3 = NonTrainableFC(idx=3)
+    record_v2(
+        non_trainable_fc_idx3,
+        iteration=2,
+        input_dims=[(3,3)],
+        input_dtype=[float],
+        label_dims=[(3,2)],
+        name="non_trainable_fc_idx3"
+    )
     
     # Function to check the created golden test file
-    inspect_file("fc_relu_decay.nnmodelgolden")
+    inspect_file("non_trainable_fc_idx3.nnmodelgolden")
