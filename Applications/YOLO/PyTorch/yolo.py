76c21463c729fd2ef651e454b511898dfd0bf9a7
Seungbaek Hong <sb92.hong@samsung.com>
2023-03-03 14:04:27 +0900

    [Application] add object detection example using pytorch
    Add YOLO v2 object detection example using pytorch.
    
    It was implemented in the same way as the YOLO v2 model,
    but only the backbone model was made into a simpler cnn
    model to make it easier to configure the initial version
    of YOLO to be supported by NNTrainer.
    
    Signed-off-by: Seungbaek Hong <sb92.hong@samsung.com>

---
+++ Applications/YOLO/PyTorch/yolo.py
@@ -0,0 +1,58 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2023 Seungbaek Hong <sb92.hong@samsung.com>
+#
+# @file yolo.py
+# @date 8 March 2023
+# @brief Define simple yolo model, but not original darknet.
+#
+# @author Seungbaek Hong <sb92.hong@samsung.com>
+
+import torch
+import torch.nn as nn
+
+##
+# @brief define simple yolo model (not original darknet)
+class YoloV2_light(nn.Module): 
+    def __init__(self, 
+                 num_classes,
+                 anchors=\
+                 [(1.3221, 1.73145), (3.19275, 4.00944), (5.05587, 8.09892), (9.47112, 4.84053), (11.2364, 10.0071)]):
+        
+        super(YoloV2_light, self).__init__()              
+        self.num_classes = num_classes
+        self.anchors = anchors
+        self.stage1_conv1 = nn.Sequential(nn.Conv2d(3, 32, 3, 1, 1), nn.BatchNorm2d(32),
+                                          nn.LeakyReLU(0.1), nn.MaxPool2d(2, 2))
+        self.stage1_conv2 = nn.Sequential(nn.Conv2d(32, 64, 3, 1, 1), nn.BatchNorm2d(64),
+                                          nn.LeakyReLU(0.1), nn.MaxPool2d(2, 2))
+        self.stage1_conv3 = nn.Sequential(nn.Conv2d(64, 128, 3, 1, 1), nn.BatchNorm2d(128),
+                                          nn.LeakyReLU(0.1))
+        self.stage1_conv4 = nn.Sequential(nn.Conv2d(128, 64, 1, 1, 0), nn.BatchNorm2d(64),
+                                          nn.LeakyReLU(0.1))
+        self.stage1_conv5 = nn.Sequential(nn.Conv2d(64, 128, 3, 1, 1), nn.BatchNorm2d(128),
+                                          nn.LeakyReLU(0.1), nn.MaxPool2d(2, 2))
+        self.stage1_conv6 = nn.Sequential(nn.Conv2d(128, 256, 3, 1, 1), nn.BatchNorm2d(256),
+                                          nn.LeakyReLU(0.1))
+        self.stage1_conv7 = nn.Sequential(nn.Conv2d(256, 128, 1, 1, 0), nn.BatchNorm2d(128),
+                                          nn.LeakyReLU(0.1))
+        self.stage1_conv8 = nn.Sequential(nn.Conv2d(128, 256, 3, 1, 1), nn.BatchNorm2d(256),
+                                          nn.LeakyReLU(0.1), nn.MaxPool2d(2, 2))
+        self.stage1_conv9 = nn.Sequential(nn.Conv2d(256, 512, 3, 1, 1), nn.BatchNorm2d(512),
+                                          nn.LeakyReLU(0.1))
+        self.stage1_conv10 = nn.Sequential(nn.Conv2d(512, 256, 1, 1, 0), nn.BatchNorm2d(256),
+                                           nn.LeakyReLU(0.1), nn.MaxPool2d(2, 2))
+        self.out_conv = nn.Conv2d(256, len(self.anchors) * (5 + num_classes), 1, 1, 0)
+
+    def forward(self, input):
+        output = self.stage1_conv1(input)
+        output = self.stage1_conv2(output)
+        output = self.stage1_conv3(output)
+        output = self.stage1_conv4(output)
+        output = self.stage1_conv5(output)
+        output = self.stage1_conv6(output)
+        output = self.stage1_conv7(output)
+        output = self.stage1_conv8(output)
+        output = self.stage1_conv9(output)
+        output = self.stage1_conv10(output)
+        output = self.out_conv(output)
+        return output
