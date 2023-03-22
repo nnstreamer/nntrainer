42f78c9b8cd7352030c6ee250587b3821084ec8c
jijoong.moon <jijoong.moon@samsung.com>
2023-03-22 16:52:38 +0900

    add
    Describe a commit content (Until 80 colums per line) in detail ASAP.
    
    **Changes proposed in this PR:**
    - Added TOC generator for README.md
    
    Resolves:
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	 [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: jijoong.moon <jijoong.moon@samsung.com>

--- nntrainer/graph/network_graph.h
+++ nntrainer/graph/network_graph.h
@@ -312,7 +312,7 @@ public:
    */
   void allocateWeights() {
     tensor_manager->allocateWeights(
-      std::get<2>(backward_iter_end->getExecutionOrder()));
+      std::get<3>(backward_iter_end->getExecutionOrder()));
   }
 
   /**
