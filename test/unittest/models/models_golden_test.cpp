0ea7f24d38e1a6ef2ecf27167f75e05948e39cc0
DongHak Park <donghak.park@samsung.com>
2023-04-04 15:21:26 +0900

    [AHub] Fix AHub Defect
    Fix Ahub Defect
    - change stderror to stderror_r
    
    Signed-off-by: DongHak Park <donghak.park@samsung.com>

--- test/unittest/models/models_golden_test.cpp
+++ test/unittest/models/models_golden_test.cpp
@@ -110,8 +110,10 @@ TEST_P(nntrainerModelTest, model_test_save_load_compare) {
       new nntrainer::NeuralNetwork());
     nn->load(saved_ini_name, ml::train::ModelFormat::MODEL_FORMAT_INI);
     if (remove(saved_ini_name.c_str())) {
-      std::cerr << "remove ini " << saved_ini_name
-                << "failed, reason: " << strerror(errno);
+      const size_t error_buflen = 100;
+      char error_buf[error_buflen];
+      std::cerr << "remove ini " << saved_ini_name << "failed, reason: "
+                << strerror_r(errno, error_buf, error_buflen);
     }
     return nn;
   };
@@ -140,8 +142,10 @@ TEST_P(nntrainerModelTest, model_test_save_load_verify) {
       new nntrainer::NeuralNetwork());
     nn->load(saved_ini_name, ml::train::ModelFormat::MODEL_FORMAT_INI);
     if (remove(saved_ini_name.c_str())) {
-      std::cerr << "remove ini " << saved_ini_name
-                << "failed, reason: " << strerror(errno);
+      const size_t error_buflen = 100;
+      char error_buf[error_buflen];
+      std::cerr << "remove ini " << saved_ini_name << "failed, reason: "
+                << strerror_r(errno, error_buf, error_buflen);
     }
     return nn;
   };
