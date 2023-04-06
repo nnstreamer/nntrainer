195b4417830258a688637d71583a34134a3c3b6b
hyeonseok lee <hs89.lee@samsung.com>
2023-04-06 20:22:53 +0900

    [Application] implement yolo v2 loss backwarding
     - Implement yolo v2 loss layer backwarding
    
    Signed-off-by: hyeonseok lee <hs89.lee@samsung.com>

--- Applications/YOLO/jni/yolo_v2_loss.cpp
+++ Applications/YOLO/jni/yolo_v2_loss.cpp
@@ -76,7 +76,12 @@ float mse(nntrainer::Tensor &pred, nntrainer::Tensor &ground_truth) {
  */
 void msePrime(nntrainer::Tensor &pred, nntrainer::Tensor &ground_truth,
               nntrainer::Tensor &outgoing_derivative) {
-  /** NYI */
+  pred.subtract(ground_truth, outgoing_derivative);
+  float divider = ((float)pred.size()) / 2;
+  if (outgoing_derivative.divide_i(divider) != ML_ERROR_NONE) {
+    throw std::runtime_error(
+      "[YoloV2LossLayer::calcDerivative] Error when calculating loss");
+  }
 }
 
 /**
@@ -200,8 +205,57 @@ std::vector<nntrainer::Tensor> calc_iou_grad(
   nntrainer::Tensor &bbox1_height, nntrainer::Tensor &is_xy_min_max,
   nntrainer::Tensor &intersection_width, nntrainer::Tensor &intersection_height,
   nntrainer::Tensor &unions) {
-  /** NYI */
-  return {nntrainer::Tensor()};
+  nntrainer::Tensor intersection =
+    intersection_width.multiply(intersection_height);
+
+  // 1. calculate intersection local gradient [f'(x)]
+  nntrainer::Tensor intersection_width_relu_prime =
+    intersection_width.apply(nntrainer::ActiFunc::reluPrime);
+  nntrainer::Tensor intersection_height_relu_prime =
+    intersection_height.apply(nntrainer::ActiFunc::reluPrime);
+
+  nntrainer::Tensor intersection_x2_local_grad =
+    intersection_width_relu_prime.multiply(intersection_height);
+  nntrainer::Tensor intersection_y2_local_grad =
+    intersection_height_relu_prime.multiply(intersection_width);
+  nntrainer::Tensor intersection_x1_local_grad =
+    intersection_x2_local_grad.multiply(-1.0);
+  nntrainer::Tensor intersection_y1_local_grad =
+    intersection_y2_local_grad.multiply(-1.0);
+
+  nntrainer::Tensor intersection_local_grad = nntrainer::Tensor::cat(
+    {intersection_x1_local_grad, intersection_y1_local_grad,
+     intersection_x2_local_grad, intersection_y2_local_grad},
+    3);
+  intersection_local_grad.multiply_i(is_xy_min_max);
+
+  // 2. calculate union local gradient [g'(x)]
+  nntrainer::Tensor bbox1_x1_grad = bbox1_height.multiply(-1.0);
+  nntrainer::Tensor bbox1_y1_grad = bbox1_width.multiply(-1.0);
+  nntrainer::Tensor bbox1_x2_grad = bbox1_height;
+  nntrainer::Tensor bbox1_y2_grad = bbox1_width;
+  nntrainer::Tensor bbox1_grad = nntrainer::Tensor::cat(
+    {bbox1_x1_grad, bbox1_y1_grad, bbox1_x2_grad, bbox1_y2_grad}, 3);
+
+  nntrainer::Tensor unions_local_grad =
+    bbox1_grad.subtract(intersection_local_grad);
+
+  // 3. calculate iou local gradient [(f'(x)g(x) - f(x)g'(x))/(g(x)^2)]
+  nntrainer::Tensor lhs = intersection_local_grad.multiply(unions);
+  nntrainer::Tensor rhs = unions_local_grad.multiply(intersection);
+  nntrainer::Tensor iou_grad = lhs.subtract(rhs);
+  iou_grad.divide_i(unions);
+  iou_grad.divide_i(unions);
+
+  // 3. multiply with incoming derivative
+  iou_grad.multiply_i(confidence_gt_grad);
+
+  auto splitted_iou_grad = iou_grad.split({1, 1, 1, 1}, 3);
+  std::vector<nntrainer::Tensor> ret = {
+    splitted_iou_grad[0].add(splitted_iou_grad[2]),
+    splitted_iou_grad[1].add(splitted_iou_grad[3]), splitted_iou_grad[2],
+    splitted_iou_grad[3]};
+  return ret;
 }
 
 YoloV2LossLayer::YoloV2LossLayer() :
@@ -433,6 +487,14 @@ void YoloV2LossLayer::forwarding(nntrainer::RunLayerContext &context,
   nntrainer::Tensor &iou_mask =
     context.getTensor(wt_idx[YoloV2LossParams::iou_mask]);
 
+  bbox_x_gt.setValue(0);
+  bbox_y_gt.setValue(0);
+  bbox_w_gt.setValue(0);
+  bbox_h_gt.setValue(0);
+
+  confidence_gt.setValue(0);
+  class_gt.setValue(0);
+
   // init mask
   bbox_class_mask.setValue(0);
   iou_mask.setValue(0.5);
@@ -476,7 +538,156 @@ void YoloV2LossLayer::forwarding(nntrainer::RunLayerContext &context,
 }
 
 void YoloV2LossLayer::calcDerivative(nntrainer::RunLayerContext &context) {
-  /** NYI */
+  nntrainer::Tensor &bbox_x_pred =
+    context.getTensor(wt_idx[YoloV2LossParams::bbox_x_pred]);
+  nntrainer::Tensor &bbox_x_pred_grad =
+    context.getTensorGrad(wt_idx[YoloV2LossParams::bbox_x_pred]);
+  nntrainer::Tensor &bbox_y_pred =
+    context.getTensor(wt_idx[YoloV2LossParams::bbox_y_pred]);
+  nntrainer::Tensor &bbox_y_pred_grad =
+    context.getTensorGrad(wt_idx[YoloV2LossParams::bbox_y_pred]);
+  nntrainer::Tensor &bbox_w_pred =
+    context.getTensor(wt_idx[YoloV2LossParams::bbox_w_pred]);
+  nntrainer::Tensor &bbox_w_pred_grad =
+    context.getTensorGrad(wt_idx[YoloV2LossParams::bbox_w_pred]);
+  nntrainer::Tensor &bbox_h_pred =
+    context.getTensor(wt_idx[YoloV2LossParams::bbox_h_pred]);
+  nntrainer::Tensor &bbox_h_pred_grad =
+    context.getTensorGrad(wt_idx[YoloV2LossParams::bbox_h_pred]);
+
+  nntrainer::Tensor &confidence_pred =
+    context.getTensor(wt_idx[YoloV2LossParams::confidence_pred]);
+  nntrainer::Tensor &confidence_pred_grad =
+    context.getTensorGrad(wt_idx[YoloV2LossParams::confidence_pred]);
+  nntrainer::Tensor &class_pred =
+    context.getTensor(wt_idx[YoloV2LossParams::class_pred]);
+  nntrainer::Tensor &class_pred_grad =
+    context.getTensorGrad(wt_idx[YoloV2LossParams::class_pred]);
+
+  nntrainer::Tensor &bbox_w_pred_anchor =
+    context.getTensor(wt_idx[YoloV2LossParams::bbox_w_pred_anchor]);
+  nntrainer::Tensor &bbox_h_pred_anchor =
+    context.getTensor(wt_idx[YoloV2LossParams::bbox_h_pred_anchor]);
+
+  nntrainer::Tensor &bbox_x_gt =
+    context.getTensor(wt_idx[YoloV2LossParams::bbox_x_gt]);
+  nntrainer::Tensor &bbox_y_gt =
+    context.getTensor(wt_idx[YoloV2LossParams::bbox_y_gt]);
+  nntrainer::Tensor &bbox_w_gt =
+    context.getTensor(wt_idx[YoloV2LossParams::bbox_w_gt]);
+  nntrainer::Tensor &bbox_h_gt =
+    context.getTensor(wt_idx[YoloV2LossParams::bbox_h_gt]);
+
+  nntrainer::Tensor &confidence_gt =
+    context.getTensor(wt_idx[YoloV2LossParams::confidence_gt]);
+  nntrainer::Tensor &class_gt =
+    context.getTensor(wt_idx[YoloV2LossParams::class_gt]);
+
+  nntrainer::Tensor &bbox_class_mask =
+    context.getTensor(wt_idx[YoloV2LossParams::bbox_class_mask]);
+  nntrainer::Tensor &iou_mask =
+    context.getTensor(wt_idx[YoloV2LossParams::iou_mask]);
+
+  nntrainer::Tensor &bbox1_width =
+    context.getTensor(wt_idx[YoloV2LossParams::bbox1_width]);
+  nntrainer::Tensor &bbox1_height =
+    context.getTensor(wt_idx[YoloV2LossParams::bbox1_height]);
+  nntrainer::Tensor &is_xy_min_max =
+    context.getTensor(wt_idx[YoloV2LossParams::is_xy_min_max]);
+  nntrainer::Tensor &intersection_width =
+    context.getTensor(wt_idx[YoloV2LossParams::intersection_width]);
+  nntrainer::Tensor &intersection_height =
+    context.getTensor(wt_idx[YoloV2LossParams::intersection_height]);
+  nntrainer::Tensor &unions =
+    context.getTensor(wt_idx[YoloV2LossParams::unions]);
+
+  nntrainer::Tensor bbox_pred = nntrainer::Tensor::cat(
+    {bbox_x_pred, bbox_y_pred, bbox_w_pred_anchor, bbox_h_pred_anchor}, 3);
+  nntrainer::Tensor masked_bbox_pred = bbox_pred.multiply(bbox_class_mask);
+  nntrainer::Tensor masked_confidence_pred = confidence_pred.multiply(iou_mask);
+  nntrainer::Tensor masked_class_pred = class_pred.multiply(bbox_class_mask);
+
+  nntrainer::Tensor bbox_gt =
+    nntrainer::Tensor::cat({bbox_x_gt, bbox_y_gt, bbox_w_gt, bbox_h_gt}, 3);
+  nntrainer::Tensor masked_bbox_gt = bbox_gt.multiply(bbox_class_mask);
+  nntrainer::Tensor masked_confidence_gt = confidence_gt.multiply(iou_mask);
+  nntrainer::Tensor masked_class_gt = class_gt.multiply(bbox_class_mask);
+
+  nntrainer::Tensor masked_bbox_pred_grad;
+  nntrainer::Tensor masked_confidence_pred_grad;
+  nntrainer::Tensor masked_confidence_gt_grad;
+  nntrainer::Tensor masked_class_pred_grad;
+
+  nntrainer::Tensor confidence_gt_grad;
+
+  msePrime(masked_bbox_pred, masked_bbox_gt, masked_bbox_pred_grad);
+  msePrime(masked_confidence_pred, masked_confidence_gt,
+           masked_confidence_pred_grad);
+  msePrime(masked_confidence_gt, masked_confidence_pred,
+           masked_confidence_gt_grad);
+  msePrime(masked_class_pred, masked_class_gt, masked_class_pred_grad);
+
+  masked_bbox_pred_grad.multiply_i(5);
+
+  nntrainer::Tensor bbox_pred_grad;
+
+  masked_bbox_pred_grad.multiply(bbox_class_mask, bbox_pred_grad);
+  masked_confidence_pred_grad.multiply(iou_mask, confidence_pred_grad);
+  masked_confidence_gt_grad.multiply(iou_mask, confidence_gt_grad);
+  masked_class_pred_grad.multiply(bbox_class_mask, class_pred_grad);
+
+  std::vector<nntrainer::Tensor> splitted_bbox_pred_grad =
+    bbox_pred_grad.split({1, 1, 1, 1}, 3);
+  bbox_x_pred_grad.copyData(splitted_bbox_pred_grad[0]);
+  bbox_y_pred_grad.copyData(splitted_bbox_pred_grad[1]);
+  bbox_w_pred_grad.copyData(splitted_bbox_pred_grad[2]);
+  bbox_h_pred_grad.copyData(splitted_bbox_pred_grad[3]);
+
+  std::vector<nntrainer::Tensor> bbox_pred_iou_grad =
+    calc_iou_grad(confidence_gt_grad, bbox1_width, bbox1_height, is_xy_min_max,
+                  intersection_width, intersection_height, unions);
+  bbox_x_pred_grad.add_i(bbox_pred_iou_grad[0]);
+  bbox_y_pred_grad.add_i(bbox_pred_iou_grad[1]);
+  bbox_w_pred_grad.add_i(bbox_pred_iou_grad[2]);
+  bbox_h_pred_grad.add_i(bbox_pred_iou_grad[3]);
+
+  /**
+   * @brief calculate gradient for applying anchors to bounding box
+   * @details Let say bbox_pred as x, anchor as c indicated that anchor is
+   * constant for bbox_pred and bbox_pred_anchor as y. Then we can denote y =
+   * sqrt(cx). Partial derivative of y with respect to x will be
+   * sqrt(c)/(2*sqrt(x)) which is equivalent to sqrt(cx)/(2x) and we can replace
+   * sqrt(cx) with y.
+   * @note divide by bbox_pred(x) will not be executed because bbox_pred_grad
+   * will be multiply by bbox_pred(x) soon after.
+   */
+  bbox_w_pred_grad.multiply_i(bbox_w_pred_anchor);
+  bbox_h_pred_grad.multiply_i(bbox_h_pred_anchor);
+  /** intended comment */
+  // bbox_w_pred_grad.divide_i(bbox_w_pred);
+  // bbox_h_pred_grad.divide_i(bbox_h_pred);
+  bbox_w_pred_grad.divide_i(2);
+  bbox_h_pred_grad.divide_i(2);
+
+  sigmoid.run_prime_fn(bbox_x_pred, bbox_x_pred, bbox_x_pred_grad,
+                       bbox_x_pred_grad);
+  sigmoid.run_prime_fn(bbox_y_pred, bbox_y_pred, bbox_y_pred_grad,
+                       bbox_y_pred_grad);
+  /** intended comment */
+  // bbox_w_pred_grad.multiply_i(bbox_w_pred);
+  // bbox_h_pred_grad.multiply_i(bbox_h_pred);
+  sigmoid.run_prime_fn(confidence_pred, confidence_pred, confidence_pred_grad,
+                       confidence_pred_grad);
+  softmax.run_prime_fn(class_pred, class_pred, class_pred_grad,
+                       class_pred_grad);
+
+  nntrainer::Tensor outgoing_derivative_ = nntrainer::Tensor::cat(
+    {bbox_x_pred_grad, bbox_y_pred_grad, bbox_w_pred_grad, bbox_h_pred_grad,
+     confidence_pred_grad, class_pred_grad},
+    3);
+  nntrainer::Tensor &outgoing_derivative =
+    context.getOutgoingDerivative(SINGLE_INOUT_IDX);
+  outgoing_derivative.copyData(outgoing_derivative_);
 }
 
 void YoloV2LossLayer::exportTo(nntrainer::Exporter &exporter,
