f5ad9fc48a6f6c9056061bc19303b6589341dccc
jijoong.moon <jijoong.moon@samsung.com>
2023-04-07 15:37:38 +0900

    [NNStreamer] fix input dimension with MAX RANK with 8
    This pr fixes the limit of rank whic greater than 4.
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	 [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: jijoong.moon <jijoong.moon@samsung.com>

--- nntrainer/layers/nnstreamer_layer.cpp
+++ nntrainer/layers/nnstreamer_layer.cpp
@@ -85,12 +85,6 @@ static int nnst_info_to_tensor_dim(ml_tensors_info_h &out_res, TensorDim &dim) {
   if (status != ML_ERROR_NONE)
     return status;
 
-  if (ML_TENSOR_RANK_LIMIT > ml::train::TensorDim::MAXDIM) {
-    for (uint i = ml::train::TensorDim::MAXDIM; i < ML_TENSOR_RANK_LIMIT; i++)
-      if (dim_[i] > 1)
-        return ML_ERROR_NOT_SUPPORTED;
-  }
-
   for (size_t i = 0; i < ml::train::TensorDim::MAXDIM; i++)
     dim.setTensorDim(i, dim_[i]);
 
