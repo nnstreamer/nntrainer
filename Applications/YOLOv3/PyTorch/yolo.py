24fe1e01ee0879ec4f33c7cc1800ebd003e65c58
Seungbaek Hong <sb92.hong@samsung.com>
2023-06-08 15:37:27 +0900

    [Wait for #2221][Application] YOLOv3 implementation for forwarding in torch
    Added YOLO v3 pytorch implementation for forwarding in pytorch.
    
    Signed-off-by: Seungbaek Hong <sb92.hong@samsung.com>

--- Applications/YOLOv3/PyTorch/yolo.py
+++ Applications/YOLOv3/PyTorch/yolo.py
@@ -26,6 +26,7 @@ def get_util_path():
 sys.path.append(get_util_path())
 from torchconverter import save_bin
 
+
 class ConvBlock(nn.Module):
     def __init__(self, num_channel_in, num_channel_out, kernel_size, stride, padding):
         super(ConvBlock, self).__init__()
@@ -66,24 +67,28 @@ class Darknet53(nn.Module):
     def __init__(self):
         super(Darknet53, self).__init__()
 
-        self.fe_modules = nn.Sequential()
-        self.fe_modules.append(ConvBlock(3, 32, 3, 1, 1))
-        self.fe_modules.append(ConvBlock(32, 64, 3, 2, 1))
-        self.fe_modules.append(DarkNetBlock(64, 1))
-        self.fe_modules.append(ConvBlock(64, 128, 3, 2, 1))
-        self.fe_modules.append(DarkNetBlock(128, 2))
-        self.fe_modules.append(ConvBlock(128, 256, 3, 2, 1))
-        self.fe_modules.append(DarkNetBlock(256, 8))
-        self.fe_modules.append(ConvBlock(256, 512, 3, 2, 1))
-        self.fe_modules.append(DarkNetBlock(512, 8))
-        self.fe_modules.append(ConvBlock(512, 1024, 3, 2, 1))
-        self.fe_modules.append(DarkNetBlock(1024, 4))        
+        self.module_list = nn.Sequential()
+        self.module_list.append(ConvBlock(3, 32, 3, 1, 1))
+        self.module_list.append(ConvBlock(32, 64, 3, 2, 1))
+        self.module_list.append(DarkNetBlock(64, 1))
+        self.module_list.append(ConvBlock(64, 128, 3, 2, 1))
+        self.module_list.append(DarkNetBlock(128, 2))
+        self.module_list.append(ConvBlock(128, 256, 3, 2, 1))
+        self.module_list.append(DarkNetBlock(256, 8))
+        self.module_list.append(ConvBlock(256, 512, 3, 2, 1))
+        self.module_list.append(DarkNetBlock(512, 8))
+        self.module_list.append(ConvBlock(512, 1024, 3, 2, 1))
+        self.module_list.append(DarkNetBlock(1024, 4))
 
     def forward(self, x):
-        for module in self.fe_modules:
+        for i, module in enumerate(self.module_list):
             x = module(x)
+            if i == 6:
+                route_1 = x
+            elif i == 8:
+                route_2 = x
 
-        return x
+        return route_1, route_2, x
 
     def load_weights_of_convblock(self, block, weights, pointer):
         # load bias of batch norm
@@ -116,7 +121,7 @@ class Darknet53(nn.Module):
         pointer += num_weights
         
         return pointer
-        
+
     def load_pretrained_weights(self, path):
         with open(path, "rb") as f:
             # read header info (useless in this func)
@@ -134,7 +139,100 @@ class Darknet53(nn.Module):
                         pointer = self.load_weights_of_convblock(conv_block, weights, pointer)
 
 
+class YoloV3(nn.Module):
+    def __init__(self, num_classes, pretrained_darknet_path=''):
+        super(YoloV3, self).__init__()
+        self.num_classes = num_classes
+
+        # load darknet53
+        self.backbone = Darknet53()
+        if pretrained_darknet_path:
+            self.backbone.load_pretrained_weights(pretrained_darknet_path)
+
+        # feature pyramid for route3 (large object)
+        self.fp3 = nn.Sequential(
+            ConvBlock(1024, 512, 1, 1, 0),
+            ConvBlock(512, 1024, 3, 1, 1),
+            ConvBlock(1024, 512, 1, 1, 0),
+            ConvBlock(512, 1024, 3, 1, 1),
+            ConvBlock(1024, 512, 1, 1, 0)            
+        )
+
+        # connection for route3 to route2
+        self.neck3_2 = nn.Sequential(
+            ConvBlock(512, 256, 1, 1, 0),
+            nn.Upsample(scale_factor=2, mode='nearest')
+        )
+
+        # feature pyramid for route2 (medium object)
+        self.fp2 = nn.Sequential(            
+            ConvBlock(768, 256, 1, 1, 0),
+            ConvBlock(256, 512, 3, 1, 1),
+            ConvBlock(512, 256, 1, 1, 0),
+            ConvBlock(256, 512, 3, 1, 1),
+            ConvBlock(512, 256, 1, 1, 0)
+        )
+
+        # connection for route2 to route1
+        self.neck2_1 = nn.Sequential(
+            ConvBlock(256, 128, 1, 1, 0),
+            nn.Upsample(scale_factor=2, mode='nearest')
+        )
+
+        # feature pyramid for route1 (small object)
+        self.fp1 = nn.Sequential(
+            ConvBlock(384, 128, 1, 1, 0),
+            ConvBlock(128, 256, 3, 1, 1),
+            ConvBlock(256, 128, 1, 1, 0),
+            ConvBlock(128, 256, 3, 1, 1),
+            ConvBlock(256, 128, 1, 1, 0)
+        )
+
+        # head for route3 (large object)
+        self.head3 = nn.Sequential(
+            ConvBlock(512, 1024, 3, 1, 1),
+            ConvBlock(1024, 3 * (5 + self.num_classes), 1, 1, 0)
+        )
+
+        # head for route2 (medium object)
+        self.head2 = nn.Sequential(
+            ConvBlock(256, 512, 3, 1, 1),
+            ConvBlock(512, 3 * (5 + self.num_classes), 1, 1, 0)
+        )
+
+        # head for route1 (small object)
+        self.head1 = nn.Sequential(
+            ConvBlock(128, 256, 3, 1, 1),
+            ConvBlock(256, 3 * (5 + self.num_classes), 1, 1, 0)
+        )
+
+    def forward(self, x):
+        r1, r2, r3 = self.backbone(x)
+
+        # feature pyramid for route3
+        large = self.fp3(r3)
+        medium = self.neck3_2(large)
+        large = self.head3(large)
+
+        # feature pyramid for route2
+        medium = torch.cat([medium, r2], dim=1)
+        medium = self.fp2(medium)
+        small = self.neck2_1(medium)
+        medium = self.head2(medium)
+
+        # feature pyramid for route1
+        small = torch.cat([small, r1], dim=1)
+        small = self.fp1(small)
+        small = self.head1(small)
+
+        return small, medium, large
+
+
 if __name__ == '__main__':
-    model = Darknet53()
-    model.load_pretrained_weights('darknet53.conv.74')
-    save_bin(model, 'pre_trained_darknet53.bin')
+    # create yolo v3 model
+    model = YoloV3(5, 'darknet53.conv.74')
+
+    # inference logic test for yolo v3
+    X = torch.ones((1, 3, 416, 416))
+    y_small, y_medium, y_large = model(X)
+    print(y_small.shape, y_medium.shape, y_large.shape)
