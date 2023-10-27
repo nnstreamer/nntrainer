e53c91a07f24b3e78cc84a60dbc270a1cf7e6be8
skykongkong8 <ss.kong@samsung.com>
2023-10-27 16:41:03 +0900

    [layer] Support fp16 in embedding layer
    - Add _FP16 code block in calcGrad in embedding layer (it does not need it for forwarding)
    - Add unittest accordingly
    - Explicit code in gtest for embedding layer:
    	- In layer gtest, each test suite runs without knowing any context of its adjacent layers
    	- Thus, for atypical layers like Embedding layer (which layer that has different dataType in input and output) we should either refactorize the code or explicitly process it
    - Room for memory optimization
    
    **Self evaluation:**
    1. Build test:     [X]Passed [ ]Failed [ ]Skipped
    2. Run test:     [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: skykongkong8 <ss.kong@samsung.com>

--- test/unittest/layers/unittest_layers_embedding.cpp
+++ test/unittest/layers/unittest_layers_embedding.cpp
@@ -7,6 +7,7 @@
  * @brief Embedding Layer Test
  * @see	https://github.com/nnstreamer/nntrainer
  * @author Parichay Kapoor <pk.kapoor@samsung.com>
+ * @author Sungsik Kong <ss.kong@samsung.com>
  * @bug No known bugs except for NYI items
  */
 #include <tuple>
@@ -16,6 +17,8 @@
 #include <embedding.h>
 #include <layers_common_tests.h>
 
+auto skip_derivative_option = LayerGoldenTestParamOptions::SKIP_CALC_DERIV;
+
 auto semantic_embedding = LayerSemanticsParamType(
   nntrainer::createLayer<nntrainer::EmbeddingLayer>,
   nntrainer::EmbeddingLayer::type, {"out_dim=1", "in_dim=1"},
@@ -23,3 +26,37 @@ auto semantic_embedding = LayerSemanticsParamType(
 
 GTEST_PARAMETER_TEST(Embedding, LayerSemantics,
                      ::testing::Values(semantic_embedding));
+
+auto embedding_fp32 =
+  LayerGoldenTestParamType(nntrainer::createLayer<nntrainer::EmbeddingLayer>,
+                           {"out_dim=10", "in_dim=10"}, "1:1:1:10",
+                           "embedding_w32a32_single_batch.nnlayergolden",
+                           skip_derivative_option, "nchw", "fp32", "fp32");
+
+GTEST_PARAMETER_TEST(Embedding32, LayerGoldenTest,
+                     ::testing::Values(embedding_fp32));
+
+#ifdef ENABLE_FP16
+auto embedding_mixed_single_batch =
+  LayerGoldenTestParamType(nntrainer::createLayer<nntrainer::EmbeddingLayer>,
+                           {"out_dim=10", "in_dim=10"}, "1:1:1:10",
+                           "embedding_mixed_single_batch.nnlayergolden",
+                           skip_derivative_option, "nchw", "fp16", "fp16");
+
+auto embedding_mixed_double_batch =
+  LayerGoldenTestParamType(nntrainer::createLayer<nntrainer::EmbeddingLayer>,
+                           {"out_dim=10", "in_dim=20"}, "2:1:1:10",
+                           "embedding_mixed_double_batch.nnlayergolden",
+                           skip_derivative_option, "nchw", "fp16", "fp16");
+
+auto embedding_mixed_many =
+  LayerGoldenTestParamType(nntrainer::createLayer<nntrainer::EmbeddingLayer>,
+                           {"out_dim=64", "in_dim=1000"}, "5:1:1:10",
+                           "embedding_mixed_many.nnlayergolden",
+                           skip_derivative_option, "nchw", "fp16", "fp16");
+
+GTEST_PARAMETER_TEST(Embedding16, LayerGoldenTest,
+                     ::testing::Values(embedding_mixed_single_batch,
+                                       embedding_mixed_double_batch,
+                                       embedding_mixed_many));
+#endif
