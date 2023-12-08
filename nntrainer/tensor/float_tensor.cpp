0d0d7021c57e75e89898e8ba9519413af5fc1709
Donghyeon Jeong <dhyeon.jeong@samsung.com>
2023-12-08 09:23:43 +0900

    [Tensor] Add Float/HalfTensor Implementation
    In this PR, FloatTensor and HalfTensor's override methods are implemented.
    
    **Self-evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test:   [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Donghyeon Jeong <dhyeon.jeong@samsung.com>

--- nntrainer/tensor/float_tensor.cpp
+++ nntrainer/tensor/float_tensor.cpp
@@ -9,38 +9,164 @@
  * @bug		No known bugs except for NYI items
  */
 
+#include <iomanip>
+#include <iostream>
+
+#include <blas_interface.h>
 #include <float_tensor.h>
+#include <util_func.h>
 
 namespace nntrainer {
 
 FloatTensor::FloatTensor(std::string name_, Tformat fm) :
   TensorBase(name_, fm, Tdatatype::FP32) {}
 
-void FloatTensor::allocate() {}
+/// @todo support allocation by src_tensor
+void FloatTensor::allocate() {
+  if (empty() || data)
+    return;
 
-void FloatTensor::deallocate() {}
+  /// allocate new memory for the tensor data
+  MemoryData *mem_data;
 
-void *FloatTensor::getData() const { return (void *)0; }
+  mem_data = new MemoryData((void *)(new float[dim.getDataLen()]{}));
+  data = std::shared_ptr<MemoryData>(mem_data, [](auto *mem_data) {
+    delete[] mem_data->template getAddr<float>();
+    delete mem_data;
+  });
 
-void *FloatTensor::getData(size_t idx) const { return (void *)0; }
-
-void *FloatTensor::getAddress(unsigned int i) { return (void *)0; }
-
-const void *FloatTensor::getAddress(unsigned int i) const {
-  return (const void *)0;
+  offset = 0;
+  initialize();
 }
 
-void FloatTensor::setValue(float value) {}
+void FloatTensor::deallocate() {
+  data = nullptr;
+  offset = 0;
+}
+
+void *FloatTensor::getData() const {
+  if (!data)
+    return nullptr;
+
+  data->validate();
+  return data->getAddr<float>() + offset;
+}
+
+void *FloatTensor::getData(size_t idx) const {
+  if (!data)
+    return nullptr;
+
+  data->validate();
+  return data->getAddr<float>() + offset + idx;
+}
+
+void *FloatTensor::getAddress(unsigned int i) {
+  size_t index = getIndex(batch(), channel(), height(), width());
+  if (i > index) {
+    return nullptr;
+  }
+  return &((float *)getData())[i];
+}
+
+const void *FloatTensor::getAddress(unsigned int i) const {
+  size_t index = getIndex(batch(), channel(), height(), width());
+  if (i > index) {
+    return nullptr;
+  }
+  return &((float *)getData())[i];
+}
+
+void FloatTensor::setValue(float value) {
+  float *data = (float *)getData();
+  std::fill(data, data + size(), value);
+}
 
 void FloatTensor::setValue(unsigned int batch, unsigned int c, unsigned int h,
-                           unsigned int w, float value) {}
+                           unsigned int w, float value) {
+  ((float *)getData())[getIndex(batch, c, h, w)] = value;
+}
 
-void FloatTensor::setZero() {}
+void FloatTensor::setZero() {
+  if (contiguous) {
+    sscal(size(), 0, (float *)getData(), 1);
+  } else {
+    /// @todo implement apply_i
+    // apply_i<float>([](float val) -> float { return 0; });
+    setValue(0);
+  }
+}
 
-void FloatTensor::initialize() {}
+/// @todo support additional initializer
+void FloatTensor::initialize() {
+  if (empty() || !isAllocated())
+    return;
 
-void FloatTensor::initialize(Initializer init) {}
+  switch (initializer) {
+  case Initializer::ZEROS:
+    setZero();
+    break;
+  case Initializer::ONES:
+    setValue(1.0f);
+    break;
+  default:
+    break;
+  }
 
-void FloatTensor::print(std::ostream &out) const {}
+  putData();
+}
+
+void FloatTensor::initialize(Initializer init) {
+  initializer = init;
+  initialize();
+}
+
+void FloatTensor::print(std::ostream &out) const {
+  printInstance(out, this);
+  const float *data = (float *)getData();
+  unsigned int len = size();
+  out << "data addr: " << data << '\n';
+  out << dim;
+
+  if (len > 100) {
+    out << '[' << data[0] << ' ' << data[1] << ' ' << data[2] << " ... "
+        << data[len - 3] << ' ' << data[len - 2] << ' ' << data[len - 1] << ']'
+        << std::endl;
+    return;
+  }
+
+  std::ios init(NULL);
+  init.copyfmt(out);
+
+  if (getFormat() == Tformat::NCHW) {
+    for (unsigned int k = 0; k < batch(); k++) {
+      for (unsigned int l = 0; l < channel(); l++) {
+        for (unsigned int i = 0; i < height(); i++) {
+          for (unsigned int j = 0; j < width(); j++) {
+            out << std::setw(10) << std::setprecision(10)
+                << data[getIndex(k, l, i, j)] << " ";
+          }
+          out << std::endl;
+        }
+        out << std::endl;
+      }
+      out << "-------" << std::endl;
+    }
+  } else {
+    for (unsigned int k = 0; k < batch(); k++) {
+      for (unsigned int i = 0; i < height(); i++) {
+        for (unsigned int j = 0; j < width(); j++) {
+          for (unsigned int l = 0; l < channel(); l++) {
+            out << std::setw(10) << std::setprecision(10)
+                << data[getIndex(k, l, i, j)] << " ";
+          }
+          out << std::endl;
+        }
+        out << std::endl;
+      }
+      out << "-------" << std::endl;
+    }
+  }
+  out.copyfmt(init);
+}
 
 } // namespace nntrainer
--- nntrainer/tensor/half_tensor.cpp
+++ nntrainer/tensor/half_tensor.cpp
@@ -9,38 +9,166 @@
  * @bug		No known bugs except for NYI items
  */
 
+#include <iomanip>
+#include <iostream>
+
+#include <blas_interface.h>
 #include <half_tensor.h>
+#include <util_func.h>
 
 namespace nntrainer {
 
 HalfTensor::HalfTensor(std::string name_, Tformat fm) :
   TensorBase(name_, fm, Tdatatype::FP16) {}
 
-void HalfTensor::allocate() {}
+/// @todo support allocation by src_tensor
+void HalfTensor::allocate() {
+  if (empty() || data)
+    /// already allocated
+    return;
 
-void HalfTensor::deallocate() {}
+  /// allocate new memory for the tensor data
+  MemoryData *mem_data;
 
-void *HalfTensor::getData() const { return (void *)0; }
+  mem_data = new MemoryData((void *)(new _FP16[dim.getDataLen()]{}));
+  data = std::shared_ptr<MemoryData>(mem_data, [](auto *mem_data) {
+    delete[] mem_data->template getAddr<_FP16>();
+    delete mem_data;
+  });
 
-void *HalfTensor::getData(size_t idx) const { return (void *)0; }
-
-void *HalfTensor::getAddress(unsigned int i) { return (void *)0; }
-
-const void *HalfTensor::getAddress(unsigned int i) const {
-  return (const void *)0;
+  offset = 0;
+  initialize();
 }
 
-void HalfTensor::setValue(float value) {}
+void HalfTensor::deallocate() {
+  data = nullptr;
+  offset = 0;
+}
+
+void *HalfTensor::getData() const {
+  if (!data)
+    return nullptr;
+
+  data->validate();
+  return data->getAddr<_FP16>() + offset;
+}
+
+void *HalfTensor::getData(size_t idx) const {
+  if (!data)
+    return nullptr;
+
+  data->validate();
+  return data->getAddr<_FP16>() + offset + idx;
+}
+
+void *HalfTensor::getAddress(unsigned int i) {
+  size_t index = getIndex(batch(), channel(), height(), width());
+  if (i > index) {
+    return nullptr;
+  }
+  return &((_FP16 *)getData())[i];
+}
+
+const void *HalfTensor::getAddress(unsigned int i) const {
+  size_t index = getIndex(batch(), channel(), height(), width());
+  if (i > index) {
+    return nullptr;
+  }
+  return &((_FP16 *)getData())[i];
+}
+
+void HalfTensor::setValue(float value) {
+  _FP16 *data = (_FP16 *)getData();
+  std::fill(data, data + size(), static_cast<_FP16>(value));
+}
 
 void HalfTensor::setValue(unsigned int batch, unsigned int c, unsigned int h,
-                          unsigned int w, float value) {}
+                          unsigned int w, float value) {
+  ((_FP16 *)getData())[getIndex(batch, c, h, w)] = static_cast<_FP16>(value);
+}
 
-void HalfTensor::setZero() {}
+void HalfTensor::setZero() {
+  if (contiguous) {
+    sscal(size(), 0, (_FP16 *)getData(), 1);
+  } else {
+    /// @todo implement apply_i
+    // apply_i<_FP16>([](_FP16 val) -> _FP16 { return 0; });
+    setValue(0);
+  }
+}
 
-void HalfTensor::initialize() {}
+/// @todo support additional initializer
+void HalfTensor::initialize() {
+  if (empty() || !isAllocated())
+    return;
 
-void HalfTensor::initialize(Initializer init) {}
+  switch (initializer) {
+  case Initializer::ZEROS:
+    setZero();
+    break;
+  case Initializer::ONES:
+    setValue(1.0f);
+    break;
+  default:
+    break;
+  }
 
-void HalfTensor::print(std::ostream &out) const {}
+  putData();
+}
+
+void HalfTensor::initialize(Initializer init) {
+  initializer = init;
+  initialize();
+}
+
+void HalfTensor::print(std::ostream &out) const {
+  printInstance(out, this);
+  const _FP16 *data = (_FP16 *)getData();
+  unsigned int len = size();
+  out << "data addr: " << data << '\n';
+  out << dim;
+
+  if (len > 100) {
+    out << '[' << (float)data[0] << ' ' << (float)data[1] << ' '
+        << (float)data[2] << " ... " << (float)data[len - 3] << ' '
+        << (float)data[len - 2] << ' ' << (float)data[len - 1] << ']'
+        << std::endl;
+    return;
+  }
+
+  std::ios init(NULL);
+  init.copyfmt(out);
+
+  if (getFormat() == Tformat::NCHW) {
+    for (unsigned int k = 0; k < batch(); k++) {
+      for (unsigned int l = 0; l < channel(); l++) {
+        for (unsigned int i = 0; i < height(); i++) {
+          for (unsigned int j = 0; j < width(); j++) {
+            out << std::setw(10) << std::setprecision(10)
+                << (float)data[getIndex(k, l, i, j)] << " ";
+          }
+          out << std::endl;
+        }
+        out << std::endl;
+      }
+      out << "-------" << std::endl;
+    }
+  } else {
+    for (unsigned int k = 0; k < batch(); k++) {
+      for (unsigned int i = 0; i < height(); i++) {
+        for (unsigned int j = 0; j < width(); j++) {
+          for (unsigned int l = 0; l < channel(); l++) {
+            out << std::setw(10) << std::setprecision(10)
+                << (float)data[getIndex(k, l, i, j)] << " ";
+          }
+          out << std::endl;
+        }
+        out << std::endl;
+      }
+      out << "-------" << std::endl;
+    }
+  }
+  out.copyfmt(init);
+}
 
 } // namespace nntrainer
