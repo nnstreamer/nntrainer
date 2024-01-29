299873d6ff09670c3661dd6d399e0c7eacfa8979
Eunju Yang <ej.yang@samsung.com>
2024-01-29 15:57:15 +0900

    [LoRA]  add a new feat(lora) to fc layer
    This commit includes implementation of lora only for the FC layer, which means it is not the generalized version. It is required to be written as a seperate class in order to remove code duplicates for other layers
    
    Signed-off-by: Eunju Yang <ej.yang@samsung.com>

--- nntrainer/layers/fc_layer.cpp
+++ nntrainer/layers/fc_layer.cpp
@@ -37,10 +37,11 @@ namespace nntrainer {
 static constexpr size_t SINGLE_INOUT_IDX = 0;
 
 enum FCParams { weight, bias };
+enum LORAParams { loraA, loraB, loraW };
 
 FullyConnectedLayer::FullyConnectedLayer() :
   LayerImpl(),
-  fc_props(props::Unit()) {
+  fc_props(props::Unit(), props::LoraRank()){
   weight_idx.fill(std::numeric_limits<unsigned>::max());
 }
 
@@ -57,6 +58,7 @@ void FullyConnectedLayer::finalize(InitLayerContext &context) {
   auto &disable_bias = std::get<props::DisableBias>(*layer_impl_props);
 
   auto unit = std::get<props::Unit>(fc_props).get();
+  auto lora_rank = (std::get<props::LoraRank>(fc_props).empty())? 0 : std::get<props::LoraRank>(fc_props).get();
 
   NNTR_THROW_IF(context.getNumInputs() != 1, std::invalid_argument)
     << "Fully connected layer takes only one input";
@@ -102,6 +104,39 @@ void FullyConnectedLayer::finalize(InitLayerContext &context) {
       context.requestWeight(bias_dim, bias_initializer, WeightRegularizer::NONE,
                             1.0f, bias_decay, "bias", true);
   }
+
+  // create weights for LoRA
+  if(lora_rank){
+
+    /** set LoRA specifications */
+    // loraA is a row vector (in_dim.width, lora_rank), loraB is a col vector of weight (lora_rank, out_dim),
+    // where shape of (loraA @ loraB) = shape of (W)
+    TensorDim loraA_dim(
+      1, is_nchw ? 1 : lora_rank, is_nchw ? in_dim.width() : 1,
+      is_nchw ? lora_rank : in_dim.channel(),
+      TensorDim::TensorType(context.getFormat(), context.getWeightDataType()),
+      is_nchw ? 0b0011 : 0b0101);
+
+    TensorDim loraB_dim(
+      1, is_nchw ? 1 : unit, is_nchw ? lora_rank : 1,
+      is_nchw ? unit : in_dim.channel(),
+      TensorDim::TensorType(context.getFormat(), context.getWeightDataType()),
+      is_nchw ? 0b0011 : 0b0101);
+
+    lora_idx[LORAParams::loraA] = context.requestWeight(
+      loraA_dim, weight_initializer, weight_regularizer,
+      weight_regularizer_constant, weight_decay, "loraA", true);
+
+    lora_idx[LORAParams::loraB] = context.requestWeight(
+      loraB_dim, weight_initializer, weight_regularizer,
+      weight_regularizer_constant, weight_decay, "loraB", true);
+
+    // save weight_lora as a tensor to be updated by (loraA @ loraB)
+    lora_idx[LORAParams::loraW] = context.requestTensor(
+      weight_dim, "weight_lora", Tensor::Initializer::NONE, true, 
+      TensorLifespan::ITERATION_LIFESPAN);
+  }
+
 }
 
 void FullyConnectedLayer::exportTo(
@@ -113,6 +148,18 @@ void FullyConnectedLayer::exportTo(
 void FullyConnectedLayer::setProperty(const std::vector<std::string> &values) {
   auto remain_props = loadProperties(values, fc_props);
   LayerImpl::setProperty(remain_props);
+}
+
+/**
+ * @brief forwarding function for lora. 
+ * It would be called during `forwarding` of FC layer.
+*/
+void FullyConnectedLayer::forwarding_lora(RunLayerContext &context, Tensor &weight){
+  Tensor &loraA = context.getWeight(lora_idx[LORAParams::loraA]);
+  Tensor &loraB = context.getWeight(lora_idx[LORAParams::loraB]);
+  Tensor &weight_lora = context.getTensor(lora_idx[LORAParams::loraW]);
+  loraA.dot(loraB, weight_lora);   // weight_lora = loraA @ loraB
+  weight.add(weight_lora, weight); // weight += weight_lora
 }
 
 void FullyConnectedLayer::forwarding(RunLayerContext &context, bool training) {
@@ -133,8 +180,12 @@ void FullyConnectedLayer::forwarding(RunLayerContext &context, bool training) {
       context.getWeightObject(weight_idx[FCParams::weight]).getOutputAxis();
 
     weight.dequantize(weight_, axis);
+    if(!std::get<props::LoraRank>(fc_props).empty()) 
+      forwarding_lora(context, weight_);
     input_.dot(weight_, hidden_, false, false);
   } else {
+    if(!std::get<props::LoraRank>(fc_props).empty())
+      forwarding_lora(context, weight);
     input_.dot(weight, hidden_, false, false);
   }
 
@@ -186,6 +237,11 @@ void FullyConnectedLayer::incremental_forwarding(RunLayerContext &context,
   }
 }
 
+/**
+ * @note 
+ * [note for LoRA] implicit calcDerivative is implicitly applied. 
+ * The weight is already updated with the LoRA's (W = W + W_lora)
+*/
 void FullyConnectedLayer::calcDerivative(RunLayerContext &context) {
   Tensor &weight = context.getWeight(weight_idx[FCParams::weight]);
 
@@ -196,27 +252,54 @@ void FullyConnectedLayer::calcDerivative(RunLayerContext &context) {
 }
 
 void FullyConnectedLayer::calcGradient(RunLayerContext &context) {
-  Tensor &djdw = context.getWeightGrad(weight_idx[FCParams::weight]);
 
-  const Tensor &derivative_ = context.getIncomingDerivative(SINGLE_INOUT_IDX);
-  Tensor &input_ = context.getInput(SINGLE_INOUT_IDX);
+  // (baseline) calcGradient
+  if(std::get<props::LoraRank>(fc_props).empty()){
+    Tensor &djdw = context.getWeightGrad(weight_idx[FCParams::weight]);
 
-  if (auto &disable_bias = std::get<props::DisableBias>(*layer_impl_props);
-      disable_bias.empty() || disable_bias.get() == false) {
-    Tensor &djdb = context.getWeightGrad(weight_idx[FCParams::bias]);
+    const Tensor &derivative_ = context.getIncomingDerivative(SINGLE_INOUT_IDX);
+    Tensor &input_ = context.getInput(SINGLE_INOUT_IDX);
 
-    if (context.isGradientFirstAccess(weight_idx[FCParams::bias])) {
-      derivative_.sum({0, 1, 2}, djdb);
-    } else {
-      /// @todo optimize below by adding beta to Tensor::sum
-      Tensor t = derivative_.sum({0, 1, 2});
-      djdb.add_i(t);
+    if (auto &disable_bias = std::get<props::DisableBias>(*layer_impl_props);
+        disable_bias.empty() || disable_bias.get() == false) {
+      Tensor &djdb = context.getWeightGrad(weight_idx[FCParams::bias]);
+
+      if (context.isGradientFirstAccess(weight_idx[FCParams::bias])) {
+        derivative_.sum({0, 1, 2}, djdb);
+      } else {
+        /// @todo optimize below by adding beta to Tensor::sum
+        Tensor t = derivative_.sum({0, 1, 2});
+        djdb.add_i(t);
+      }
     }
-  }
 
-  input_.dot_deriv_wrt_2(
-    djdw, derivative_, false, false,
-    !context.isGradientFirstAccess(weight_idx[FCParams::weight]));
+    input_.dot_deriv_wrt_2(
+      djdw, derivative_, false, false,
+      !context.isGradientFirstAccess(weight_idx[FCParams::weight]));
+  } else {
+    // (LoRA) calcGradient
+    Tensor &djdla = context.getWeightGrad(lora_idx[LORAParams::loraA]); 
+    Tensor &djdlb = context.getWeightGrad(lora_idx[LORAParams::loraB]); 
+    Tensor &djdlora_w = context.getTensorGrad(lora_idx[LORAParams::loraW]); 
+
+    const Tensor &derivative_ = context.getIncomingDerivative(SINGLE_INOUT_IDX);
+    Tensor &input_ = context.getInput(SINGLE_INOUT_IDX);
+    Tensor &lora_A = context.getWeight(lora_idx[LORAParams::loraA]);
+    Tensor &lora_B = context.getWeight(lora_idx[LORAParams::loraB]);
+
+    // (cf) forward
+      // input_.dot(lora_weight, hidden) : hidden = input @ lora_weight
+      // lora_A.dot(lora_B, lora_weight) : lora_weight = lora_A @ lora_B
+    input_.dot_deriv_wrt_2(
+      djdlora_w, derivative_, false, false,
+      !context.isGradientFirstAccess(lora_idx[LORAParams::loraW]));
+    lora_A.dot_deriv_wrt_2(
+      djdlb, djdlora_w, false, false,
+      !context.isGradientFirstAccess(lora_idx[LORAParams::loraB]));
+    djdla.dot_batched_deriv_wrt_1(
+      lora_B, djdlora_w,false, false, 
+      !context.isGradientFirstAccess(lora_idx[LORAParams::loraA]));
+  }
 }
 
 } /* namespace nntrainer */
