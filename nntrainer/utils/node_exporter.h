9935417050c9b577de2d112e1fb339af5f658e1d
Eunju Yang <ej.yang@samsung.com>
2024-01-31 16:05:50 +0900

    [LoRA] update node_exporter of fully connected layer
    This commit updates TfLite node exporter of fully conntected layer. It adds new property (LoraRank) as additional input property of fullyconnected layer
    
    Signed-off-by: Eunju Yang <ej.yang@samsung.com>

--- nntrainer/utils/node_exporter.h
+++ nntrainer/utils/node_exporter.h
@@ -283,7 +283,7 @@ class FullyConnectedLayer;
  * Exporter::saveTflResult(const PropsType &props, const NodeType *self);
  */
 template <>
-void Exporter::saveTflResult(const std::tuple<props::Unit> &props,
+void Exporter::saveTflResult(const std::tuple<props::Unit, props::LoraRank> &props,
                              const FullyConnectedLayer *self);
 
 class ActivationLayer;
