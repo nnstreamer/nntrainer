869d3ac77ecd1ade9280d2985aaa8a8ffbff3790
skykongkong8 <ss.kong@samsung.com>
2024-02-13 15:47:09 +0900

    [ util ] Implement exp_i function
    - Add exponential inplace function
    
    **Self evaluation:**
    1. Build test:     [X]Passed [ ]Failed [ ]Skipped
    2. Run test:     [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: skykongkong8 <ss.kong@samsung.com>

--- nntrainer/utils/util_simd_neon.h
+++ nntrainer/utils/util_simd_neon.h
@@ -39,19 +39,16 @@ void calc_trigonometric_vals_dup(unsigned int N_half, float *angle, float *cos_,
  * @param Y float * for Vector Y
  * @param Z float * for Vector Z
  */
-<<<<<<< HEAD
 void swish(const unsigned int N, float *X, float *Y, float *Z);
-=======
-void swish_neon(const unsigned int N, float *X, float *Y, float *Z);
 
 /**
  * @brief returns maximum value of the vector X
- * 
+ *
  * @param N number of elements in X
  * @param X float * for Vector X
  * @return float maximum value of vector X
  */
-float max(const unsigned int N, float* X);
+float max(const unsigned int N, float *X);
 
 /**
  * @brief soft max function with neon y_i = exp(x_i) / sum( exp(x_i) )
@@ -61,7 +58,14 @@ float max(const unsigned int N, float* X);
  * @param Y  float * for Vector Y
  */
 void softmax(const unsigned int N, float *X, float *Y);
->>>>>>> [ util ] Implement softmax function in util_simd
+
+/**
+ * @brief exponential inplace function
+ *
+ * @param N number of elements in X
+ * @param X float * for Vector X
+ */
+void exp_i(const unsigned int N, float *X);
 #ifdef ENABLE_FP16
 /**
  * @brief Accelerating function for rotary embedding layer forwarding
@@ -85,19 +89,16 @@ void compute_rotary_embedding_value(unsigned int dim, unsigned int half_,
  * @param Y __fp16 * for Vector Y
  * @param Z __fp16 * for Vector Z
  */
-<<<<<<< HEAD
 void swish(const unsigned int N, __fp16 *X, __fp16 *Y, __fp16 *Z);
-=======
-void swish_neon(const unsigned int N, __fp16 *X, __fp16 *Y, __fp16 *Z);
 
 /**
  * @brief returns maximum value of the vector X
- * 
+ *
  * @param N number of elements in X
  * @param X __fp16 * for Vector X
  * @return __fp16 maximum value of vector X
  */
-__fp16 max(const unsigned int N, __fp16* X);
+__fp16 max(const unsigned int N, __fp16 *X);
 
 /**
  * @brief soft max function with neon y_i = exp(x_i) / sum( exp(x_i) )
@@ -109,7 +110,6 @@ __fp16 max(const unsigned int N, __fp16* X);
  * @param Y  __fp16 * for Vector Y
  */
 void softmax(const unsigned int N, __fp16 *X, __fp16 *Y);
->>>>>>> [ util ] Implement softmax function in util_simd
 #endif
 
 } // namespace nntrainer::neon
