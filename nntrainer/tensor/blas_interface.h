26b1ea1b20d30fe54f3b803b2362011205c0a864
skykongkong8 <ss.kong@samsung.com>
2024-02-16 14:40:10 +0900

    [ BLAS ] Add ele_sub and ele_div function structure
    - This commit introduces a basic structure
     of elementwise subtraction and division funct
    ion structure in blas_interface
    - Function implementation will be added in
     a later commit
    
    **Self evaluation:**
    1. Build test:     [X]Passed [ ]Failed [ ]Skipped
    2. Run test:     [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: skykongkong8 <ss.kong@samsung.com>

--- nntrainer/tensor/blas_interface.h
+++ nntrainer/tensor/blas_interface.h
@@ -162,7 +162,7 @@ void sgemv(CBLAS_ORDER order, CBLAS_TRANSPOSE TransA, const unsigned int M,
  * @param[in] beta scalar multiplier for output
  */
 void ele_mul(const unsigned int N, const _FP16 *X, const _FP16 *Y, _FP16 *Z,
-             float alpha = 1.f , float beta = 0.f);
+             float alpha = 1.f, float beta = 0.f);
 
 /**
  * @brief     elementwise vector addition : Z = X + alpha * Y + beta *
@@ -175,6 +175,32 @@ void ele_mul(const unsigned int N, const _FP16 *X, const _FP16 *Y, _FP16 *Z,
  * @param[in] beta scalar multiplier for output
  */
 void ele_add(const unsigned int N, const _FP16 *X, const _FP16 *Y, _FP16 *Z,
+             float alpha = 1.f, float beta = 0.f);
+/**
+ * @brief     elementwise vector subtraction with neon : Z = X - alpha * Y +
+ * beta * Z
+ * @param[in] N  length of the vector
+ * @param[in] X _FP16 * for Vector X
+ * @param[in] Y _FP16 * for Vector Y
+ * @param[in] Z _FP16 * for Vector Z
+ * @param[in] alpha scalar multiplier for input
+ * @param[in] beta scalar multiplier for output
+ */
+void ele_sub(const unsigned N, const _FP16 *X, const _FP16 *Y, _FP16 *Z,
+             float alpha = 1.f, float beta = 0.f);
+
+/**
+ * @brief     elementwise vector division with neon : Z = X / (alpha * Y) + beta
+ * * Z
+ * @note ZeroDivisionError is not guaranteed in this function
+ * @param[in] N  length of the vector
+ * @param[in] X _FP16 * for Vector X
+ * @param[in] Y _FP16 * for Vector Y
+ * @param[in] Z _FP16 * for Vector Z
+ * @param[in] alpha scalar multiplier for input
+ * @param[in] beta scalar multiplier for output
+ */
+void ele_div(const unsigned N, const _FP16 *X, const _FP16 *Y, _FP16 *Z,
              float alpha = 1.f, float beta = 0.f);
 
 /**
@@ -401,6 +427,32 @@ void ele_mul(const unsigned int N, const float *X, const float *Y, float *Z,
  * @param[in] beta scalar multiplier for output
  */
 void ele_add(const unsigned int N, const float *X, const float *Y, float *Z,
+             float alpha = 1.f, float beta = 0.f);
+/**
+ * @brief     elementwise vector subtraction with neon : Z = X - alpha * Y +
+ * beta * Z
+ * @param[in] N  length of the vector
+ * @param[in] X float * for Vector X
+ * @param[in] Y float * for Vector Y
+ * @param[in] Z float * for Vector Z
+ * @param[in] alpha scalar multiplier for input
+ * @param[in] beta scalar multiplier for output
+ */
+void ele_sub(const unsigned N, const float *X, const float *Y, float *Z,
+             float alpha = 1.f, float beta = 0.f);
+
+/**
+ * @brief     elementwise vector division with neon : Z = X / (alpha * Y) + beta
+ * * Z
+ * @note ZeroDivisionError is not guaranteed in this function
+ * @param[in] N  length of the vector
+ * @param[in] X float * for Vector X
+ * @param[in] Y float * for Vector Y
+ * @param[in] Z float * for Vector Z
+ * @param[in] alpha scalar multiplier for input
+ * @param[in] beta scalar multiplier for output
+ */
+void ele_div(const unsigned N, const float *X, const float *Y, float *Z,
              float alpha = 1.f, float beta = 0.f);
 } /* namespace nntrainer */
 #endif /* __cplusplus */
