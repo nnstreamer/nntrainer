0dd3c0a26f63d685ad75510a7f308140f99777c1
skykongkong8 <ss.kong@samsung.com>
2024-04-15 10:34:29 +0900

    [ hgemm ] Apply acc16 partial sum strategy and adaptive macro use in 8x8 kernel
    - Apply similar change made in commit#52a3c734 but in 8x8 kernel
    
    **Self evaluation:**
    1. Build test:     [X]Passed [ ]Failed [ ]Skipped
    2. Run test:     [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: skykongkong8 <ss.kong@samsung.com>

--- nntrainer/tensor/hgemm/hgemm_kernel_8x8.h
+++ nntrainer/tensor/hgemm/hgemm_kernel_8x8.h
@@ -17,7 +17,183 @@
 /// @note Following KERNELs are the combinations of accuracy-latency
 /// tradeoff. User can select which kernel to use by replacing them.
 
-// 1. Partial sum 512 digits : Worst accuracy, best latency
+// 1. Partial sum 1024 digits : Worst accuracy, best latency
+#define KERNEL_8x8_ACC16()                 \
+  v24 = vdupq_n_f16(0.F);                  \
+  v25 = vdupq_n_f16(0.F);                  \
+  v26 = vdupq_n_f16(0.F);                  \
+  v27 = vdupq_n_f16(0.F);                  \
+  v28 = vdupq_n_f16(0.F);                  \
+  v29 = vdupq_n_f16(0.F);                  \
+  v30 = vdupq_n_f16(0.F);                  \
+  v31 = vdupq_n_f16(0.F);                  \
+  va0 = vld1q_f16(a);                      \
+  v16 = vld1q_f16(b);                      \
+  v24 = vfmaq_laneq_f16(v24, v16, va0, 0); \
+  v25 = vfmaq_laneq_f16(v25, v16, va0, 1); \
+  v26 = vfmaq_laneq_f16(v26, v16, va0, 2); \
+  v27 = vfmaq_laneq_f16(v27, v16, va0, 3); \
+  v28 = vfmaq_laneq_f16(v28, v16, va0, 4); \
+  v29 = vfmaq_laneq_f16(v29, v16, va0, 5); \
+  v30 = vfmaq_laneq_f16(v30, v16, va0, 6); \
+  v31 = vfmaq_laneq_f16(v31, v16, va0, 7); \
+  va1 = vld1q_f16(a + 8);                  \
+  v17 = vld1q_f16(b + 8);                  \
+  v24 = vfmaq_laneq_f16(v24, v17, va1, 0); \
+  v25 = vfmaq_laneq_f16(v25, v17, va1, 1); \
+  v26 = vfmaq_laneq_f16(v26, v17, va1, 2); \
+  v27 = vfmaq_laneq_f16(v27, v17, va1, 3); \
+  v28 = vfmaq_laneq_f16(v28, v17, va1, 4); \
+  v29 = vfmaq_laneq_f16(v29, v17, va1, 5); \
+  v30 = vfmaq_laneq_f16(v30, v17, va1, 6); \
+  v31 = vfmaq_laneq_f16(v31, v17, va1, 7); \
+  va2 = vld1q_f16(a + 8 * 2);              \
+  v18 = vld1q_f16(b + 8 * 2);              \
+  v24 = vfmaq_laneq_f16(v24, v18, va2, 0); \
+  v25 = vfmaq_laneq_f16(v25, v18, va2, 1); \
+  v26 = vfmaq_laneq_f16(v26, v18, va2, 2); \
+  v27 = vfmaq_laneq_f16(v27, v18, va2, 3); \
+  v28 = vfmaq_laneq_f16(v28, v18, va2, 4); \
+  v29 = vfmaq_laneq_f16(v29, v18, va2, 5); \
+  v30 = vfmaq_laneq_f16(v30, v18, va2, 6); \
+  v31 = vfmaq_laneq_f16(v31, v18, va2, 7); \
+  va3 = vld1q_f16(a + 8 * 3);              \
+  v19 = vld1q_f16(b + 8 * 3);              \
+  v24 = vfmaq_laneq_f16(v24, v19, va3, 0); \
+  v25 = vfmaq_laneq_f16(v25, v19, va3, 1); \
+  v26 = vfmaq_laneq_f16(v26, v19, va3, 2); \
+  v27 = vfmaq_laneq_f16(v27, v19, va3, 3); \
+  v28 = vfmaq_laneq_f16(v28, v19, va3, 4); \
+  v29 = vfmaq_laneq_f16(v29, v19, va3, 5); \
+  v30 = vfmaq_laneq_f16(v30, v19, va3, 6); \
+  v31 = vfmaq_laneq_f16(v31, v19, va3, 7); \
+  va4 = vld1q_f16(a + 8 * 4);              \
+  v20 = vld1q_f16(b + 8 * 4);              \
+  v24 = vfmaq_laneq_f16(v24, v20, va4, 0); \
+  v25 = vfmaq_laneq_f16(v25, v20, va4, 1); \
+  v26 = vfmaq_laneq_f16(v26, v20, va4, 2); \
+  v27 = vfmaq_laneq_f16(v27, v20, va4, 3); \
+  v28 = vfmaq_laneq_f16(v28, v20, va4, 4); \
+  v29 = vfmaq_laneq_f16(v29, v20, va4, 5); \
+  v30 = vfmaq_laneq_f16(v30, v20, va4, 6); \
+  v31 = vfmaq_laneq_f16(v31, v20, va4, 7); \
+  va5 = vld1q_f16(a + 8 * 5);              \
+  v21 = vld1q_f16(b + 8 * 5);              \
+  v24 = vfmaq_laneq_f16(v24, v21, va5, 0); \
+  v25 = vfmaq_laneq_f16(v25, v21, va5, 1); \
+  v26 = vfmaq_laneq_f16(v26, v21, va5, 2); \
+  v27 = vfmaq_laneq_f16(v27, v21, va5, 3); \
+  v28 = vfmaq_laneq_f16(v28, v21, va5, 4); \
+  v29 = vfmaq_laneq_f16(v29, v21, va5, 5); \
+  v30 = vfmaq_laneq_f16(v30, v21, va5, 6); \
+  v31 = vfmaq_laneq_f16(v31, v21, va5, 7); \
+  va6 = vld1q_f16(a + 8 * 6);              \
+  v22 = vld1q_f16(b + 8 * 6);              \
+  v24 = vfmaq_laneq_f16(v24, v22, va6, 0); \
+  v25 = vfmaq_laneq_f16(v25, v22, va6, 1); \
+  v26 = vfmaq_laneq_f16(v26, v22, va6, 2); \
+  v27 = vfmaq_laneq_f16(v27, v22, va6, 3); \
+  v28 = vfmaq_laneq_f16(v28, v22, va6, 4); \
+  v29 = vfmaq_laneq_f16(v29, v22, va6, 5); \
+  v30 = vfmaq_laneq_f16(v30, v22, va6, 6); \
+  v31 = vfmaq_laneq_f16(v31, v22, va6, 7); \
+  va7 = vld1q_f16(a + 8 * 7);              \
+  v23 = vld1q_f16(b + 8 * 7);              \
+  v24 = vfmaq_laneq_f16(v24, v23, va7, 0); \
+  v25 = vfmaq_laneq_f16(v25, v23, va7, 1); \
+  v26 = vfmaq_laneq_f16(v26, v23, va7, 2); \
+  v27 = vfmaq_laneq_f16(v27, v23, va7, 3); \
+  v28 = vfmaq_laneq_f16(v28, v23, va7, 4); \
+  v29 = vfmaq_laneq_f16(v29, v23, va7, 5); \
+  v30 = vfmaq_laneq_f16(v30, v23, va7, 6); \
+  v31 = vfmaq_laneq_f16(v31, v23, va7, 7); \
+  va7 = vld1q_f16(a + 8 * 8);              \
+  v23 = vld1q_f16(b + 8 * 8);              \
+  v24 = vfmaq_laneq_f16(v24, v23, va7, 0); \
+  v25 = vfmaq_laneq_f16(v25, v23, va7, 1); \
+  v26 = vfmaq_laneq_f16(v26, v23, va7, 2); \
+  v27 = vfmaq_laneq_f16(v27, v23, va7, 3); \
+  v28 = vfmaq_laneq_f16(v28, v23, va7, 4); \
+  v29 = vfmaq_laneq_f16(v29, v23, va7, 5); \
+  v30 = vfmaq_laneq_f16(v30, v23, va7, 6); \
+  v31 = vfmaq_laneq_f16(v31, v23, va7, 7); \
+  va7 = vld1q_f16(a + 8 * 9);              \
+  v23 = vld1q_f16(b + 8 * 9);              \
+  v24 = vfmaq_laneq_f16(v24, v23, va7, 0); \
+  v25 = vfmaq_laneq_f16(v25, v23, va7, 1); \
+  v26 = vfmaq_laneq_f16(v26, v23, va7, 2); \
+  v27 = vfmaq_laneq_f16(v27, v23, va7, 3); \
+  v28 = vfmaq_laneq_f16(v28, v23, va7, 4); \
+  v29 = vfmaq_laneq_f16(v29, v23, va7, 5); \
+  v30 = vfmaq_laneq_f16(v30, v23, va7, 6); \
+  v31 = vfmaq_laneq_f16(v31, v23, va7, 7); \
+  va7 = vld1q_f16(a + 8 * 10);             \
+  v23 = vld1q_f16(b + 8 * 10);             \
+  v24 = vfmaq_laneq_f16(v24, v23, va7, 0); \
+  v25 = vfmaq_laneq_f16(v25, v23, va7, 1); \
+  v26 = vfmaq_laneq_f16(v26, v23, va7, 2); \
+  v27 = vfmaq_laneq_f16(v27, v23, va7, 3); \
+  v28 = vfmaq_laneq_f16(v28, v23, va7, 4); \
+  v29 = vfmaq_laneq_f16(v29, v23, va7, 5); \
+  v30 = vfmaq_laneq_f16(v30, v23, va7, 6); \
+  v31 = vfmaq_laneq_f16(v31, v23, va7, 7); \
+  va7 = vld1q_f16(a + 8 * 11);             \
+  v23 = vld1q_f16(b + 8 * 11);             \
+  v24 = vfmaq_laneq_f16(v24, v23, va7, 0); \
+  v25 = vfmaq_laneq_f16(v25, v23, va7, 1); \
+  v26 = vfmaq_laneq_f16(v26, v23, va7, 2); \
+  v27 = vfmaq_laneq_f16(v27, v23, va7, 3); \
+  v28 = vfmaq_laneq_f16(v28, v23, va7, 4); \
+  v29 = vfmaq_laneq_f16(v29, v23, va7, 5); \
+  v30 = vfmaq_laneq_f16(v30, v23, va7, 6); \
+  v31 = vfmaq_laneq_f16(v31, v23, va7, 7); \
+  va7 = vld1q_f16(a + 8 * 12);             \
+  v23 = vld1q_f16(b + 8 * 12);             \
+  v24 = vfmaq_laneq_f16(v24, v23, va7, 0); \
+  v25 = vfmaq_laneq_f16(v25, v23, va7, 1); \
+  v26 = vfmaq_laneq_f16(v26, v23, va7, 2); \
+  v27 = vfmaq_laneq_f16(v27, v23, va7, 3); \
+  v28 = vfmaq_laneq_f16(v28, v23, va7, 4); \
+  v29 = vfmaq_laneq_f16(v29, v23, va7, 5); \
+  v30 = vfmaq_laneq_f16(v30, v23, va7, 6); \
+  v31 = vfmaq_laneq_f16(v31, v23, va7, 7); \
+  va7 = vld1q_f16(a + 8 * 13);             \
+  v23 = vld1q_f16(b + 8 * 13);             \
+  v24 = vfmaq_laneq_f16(v24, v23, va7, 0); \
+  v25 = vfmaq_laneq_f16(v25, v23, va7, 1); \
+  v26 = vfmaq_laneq_f16(v26, v23, va7, 2); \
+  v27 = vfmaq_laneq_f16(v27, v23, va7, 3); \
+  v28 = vfmaq_laneq_f16(v28, v23, va7, 4); \
+  v29 = vfmaq_laneq_f16(v29, v23, va7, 5); \
+  v30 = vfmaq_laneq_f16(v30, v23, va7, 6); \
+  v31 = vfmaq_laneq_f16(v31, v23, va7, 7); \
+  va7 = vld1q_f16(a + 8 * 14);             \
+  v23 = vld1q_f16(b + 8 * 14);             \
+  v24 = vfmaq_laneq_f16(v24, v23, va7, 0); \
+  v25 = vfmaq_laneq_f16(v25, v23, va7, 1); \
+  v26 = vfmaq_laneq_f16(v26, v23, va7, 2); \
+  v27 = vfmaq_laneq_f16(v27, v23, va7, 3); \
+  v28 = vfmaq_laneq_f16(v28, v23, va7, 4); \
+  v29 = vfmaq_laneq_f16(v29, v23, va7, 5); \
+  v30 = vfmaq_laneq_f16(v30, v23, va7, 6); \
+  v31 = vfmaq_laneq_f16(v31, v23, va7, 7); \
+  va7 = vld1q_f16(a + 8 * 15);             \
+  v23 = vld1q_f16(b + 8 * 15);             \
+  v24 = vfmaq_laneq_f16(v24, v23, va7, 0); \
+  v25 = vfmaq_laneq_f16(v25, v23, va7, 1); \
+  v26 = vfmaq_laneq_f16(v26, v23, va7, 2); \
+  v27 = vfmaq_laneq_f16(v27, v23, va7, 3); \
+  v28 = vfmaq_laneq_f16(v28, v23, va7, 4); \
+  v29 = vfmaq_laneq_f16(v29, v23, va7, 5); \
+  v30 = vfmaq_laneq_f16(v30, v23, va7, 6); \
+  v31 = vfmaq_laneq_f16(v31, v23, va7, 7); \
+  __builtin_prefetch(b + 128, 0, 3);       \
+  __builtin_prefetch(a + 128, 0, 3);       \
+  l += 16;                                 \
+  b += 8 * 16;                             \
+  a += 8 * 16;
+
+// 2. Partial sum 512 digits : Medium accuracy, medium latency
 #define KERNEL_8x8_ACC8()                  \
   v24 = vdupq_n_f16(0.F);                  \
   v25 = vdupq_n_f16(0.F);                  \
@@ -113,7 +289,7 @@
   b += 8 * 8;                              \
   a += 8 * 8;
 
-// 2. Partial sum 256 digits : Medium accuracy, medium latency
+// 3. Partial sum 256 digits : Medium accuracy, medium latency
 #define KERNEL_8x8_ACC4()                  \
   v24 = vdupq_n_f16(0.F);                  \
   v25 = vdupq_n_f16(0.F);                  \
@@ -169,7 +345,7 @@
   b += 8 * 4;                              \
   a += 8 * 4;
 
-// 3. Partial sum 64 digits : Best accuracy, worst latency
+// 4. Partial sum 64 digits : Best accuracy, worst latency
 #define KERNEL_8x8_ACC1()                  \
   v24 = vdupq_n_f16(0.F);                  \
   v25 = vdupq_n_f16(0.F);                  \
@@ -194,6 +370,46 @@
   l += 1;                                  \
   b += 8 * 1;                              \
   a += 8 * 1;
+
+#define SAVE_KERNEL_8X8_F16_f32()                                            \
+  vst1q_f32(c, vaddq_f32(vld1q_f32(c), vcvt_f32_f16(vget_low_f16(v24))));    \
+  vst1q_f32(c + 4,                                                           \
+            vaddq_f32(vld1q_f32(c + 4), vcvt_f32_f16(vget_high_f16(v24))));  \
+                                                                             \
+  vst1q_f32(c + ldc,                                                         \
+            vaddq_f32(vld1q_f32(c + ldc), vcvt_f32_f16(vget_low_f16(v25)))); \
+  vst1q_f32(c + 4 + ldc, vaddq_f32(vld1q_f32(c + 4 + ldc),                   \
+                                   vcvt_f32_f16(vget_high_f16(v25))));       \
+                                                                             \
+  vst1q_f32(c + 2 * ldc, vaddq_f32(vld1q_f32(c + 2 * ldc),                   \
+                                   vcvt_f32_f16(vget_low_f16(v26))));        \
+  vst1q_f32(c + 4 + 2 * ldc, vaddq_f32(vld1q_f32(c + 4 + 2 * ldc),           \
+                                       vcvt_f32_f16(vget_high_f16(v26))));   \
+                                                                             \
+  vst1q_f32(c + 3 * ldc, vaddq_f32(vld1q_f32(c + 3 * ldc),                   \
+                                   vcvt_f32_f16(vget_low_f16(v27))));        \
+  vst1q_f32(c + 4 + 3 * ldc, vaddq_f32(vld1q_f32(c + 4 + 3 * ldc),           \
+                                       vcvt_f32_f16(vget_high_f16(v27))));   \
+                                                                             \
+  vst1q_f32(c + 4 * ldc, vaddq_f32(vld1q_f32(c + 4 * ldc),                   \
+                                   vcvt_f32_f16(vget_low_f16(v28))));        \
+  vst1q_f32(c + 4 + 4 * ldc, vaddq_f32(vld1q_f32(c + 4 + 4 * ldc),           \
+                                       vcvt_f32_f16(vget_high_f16(v28))));   \
+                                                                             \
+  vst1q_f32(c + 5 * ldc, vaddq_f32(vld1q_f32(c + 5 * ldc),                   \
+                                   vcvt_f32_f16(vget_low_f16(v29))));        \
+  vst1q_f32(c + 4 + 5 * ldc, vaddq_f32(vld1q_f32(c + 4 + 5 * ldc),           \
+                                       vcvt_f32_f16(vget_high_f16(v29))));   \
+                                                                             \
+  vst1q_f32(c + 6 * ldc, vaddq_f32(vld1q_f32(c + 6 * ldc),                   \
+                                   vcvt_f32_f16(vget_low_f16(v30))));        \
+  vst1q_f32(c + 4 + 6 * ldc, vaddq_f32(vld1q_f32(c + 4 + 6 * ldc),           \
+                                       vcvt_f32_f16(vget_high_f16(v30))));   \
+                                                                             \
+  vst1q_f32(c + 7 * ldc, vaddq_f32(vld1q_f32(c + 7 * ldc),                   \
+                                   vcvt_f32_f16(vget_low_f16(v31))));        \
+  vst1q_f32(c + 4 + 7 * ldc, vaddq_f32(vld1q_f32(c + 4 + 7 * ldc),           \
+                                       vcvt_f32_f16(vget_high_f16(v31))));
 
 /**
  * @brief hgemm 8x8 kernel sc = sa * sb
@@ -263,6 +479,9 @@ void hgemm_kernel_8x8(unsigned int M, unsigned int N, unsigned int K,
   __fp16 *a = sa, *b = sb;
   float *c = sc;
   unsigned int i, j, l;
+  unsigned int K4 = (K >> 2) << 2;
+  unsigned int K8 = (K >> 3) << 3;
+  unsigned int K16 = (K >> 4) << 4;
   for (i = 0; i < M; i += VL_FP16) {
     for (j = 0; j < N; j += VL_FP16) {
       __builtin_prefetch(b, 0, 3);
@@ -272,48 +491,21 @@ void hgemm_kernel_8x8(unsigned int M, unsigned int N, unsigned int K,
       float16x8_t v24, v25, v26, v27, v28, v29, v30, v31;
       float16x8_t va0, va1, va2, va3, va4, va5, va6, va7;
       l = 0;
-
-      for (; l < K;) {
+      for (; l < K16;) {
+        KERNEL_8x8_ACC16();
+        SAVE_KERNEL_8X8_F16_f32();
+      }
+      for (; l < K8;) {
         KERNEL_8x8_ACC8();
-
-        vst1q_f32(c, vaddq_f32(vld1q_f32(c), vcvt_f32_f16(vget_low_f16(v24))));
-        vst1q_f32(
-          c + 4, vaddq_f32(vld1q_f32(c + 4), vcvt_f32_f16(vget_high_f16(v24))));
-
-        vst1q_f32(c + ldc, vaddq_f32(vld1q_f32(c + ldc),
-                                     vcvt_f32_f16(vget_low_f16(v25))));
-        vst1q_f32(c + 4 + ldc, vaddq_f32(vld1q_f32(c + 4 + ldc),
-                                         vcvt_f32_f16(vget_high_f16(v25))));
-
-        vst1q_f32(c + 2 * ldc, vaddq_f32(vld1q_f32(c + 2 * ldc),
-                                         vcvt_f32_f16(vget_low_f16(v26))));
-        vst1q_f32(c + 4 + 2 * ldc, vaddq_f32(vld1q_f32(c + 4 + 2 * ldc),
-                                             vcvt_f32_f16(vget_high_f16(v26))));
-
-        vst1q_f32(c + 3 * ldc, vaddq_f32(vld1q_f32(c + 3 * ldc),
-                                         vcvt_f32_f16(vget_low_f16(v27))));
-        vst1q_f32(c + 4 + 3 * ldc, vaddq_f32(vld1q_f32(c + 4 + 3 * ldc),
-                                             vcvt_f32_f16(vget_high_f16(v27))));
-
-        vst1q_f32(c + 4 * ldc, vaddq_f32(vld1q_f32(c + 4 * ldc),
-                                         vcvt_f32_f16(vget_low_f16(v28))));
-        vst1q_f32(c + 4 + 4 * ldc, vaddq_f32(vld1q_f32(c + 4 + 4 * ldc),
-                                             vcvt_f32_f16(vget_high_f16(v28))));
-
-        vst1q_f32(c + 5 * ldc, vaddq_f32(vld1q_f32(c + 5 * ldc),
-                                         vcvt_f32_f16(vget_low_f16(v29))));
-        vst1q_f32(c + 4 + 5 * ldc, vaddq_f32(vld1q_f32(c + 4 + 5 * ldc),
-                                             vcvt_f32_f16(vget_high_f16(v29))));
-
-        vst1q_f32(c + 6 * ldc, vaddq_f32(vld1q_f32(c + 6 * ldc),
-                                         vcvt_f32_f16(vget_low_f16(v30))));
-        vst1q_f32(c + 4 + 6 * ldc, vaddq_f32(vld1q_f32(c + 4 + 6 * ldc),
-                                             vcvt_f32_f16(vget_high_f16(v30))));
-
-        vst1q_f32(c + 7 * ldc, vaddq_f32(vld1q_f32(c + 7 * ldc),
-                                         vcvt_f32_f16(vget_low_f16(v31))));
-        vst1q_f32(c + 4 + 7 * ldc, vaddq_f32(vld1q_f32(c + 4 + 7 * ldc),
-                                             vcvt_f32_f16(vget_high_f16(v31))));
+        SAVE_KERNEL_8X8_F16_f32();
+      }
+      for (; l < K4;) {
+        KERNEL_8x8_ACC4();
+        SAVE_KERNEL_8X8_F16_f32();
+      }
+      for (; l < K;) {
+        KERNEL_8x8_ACC1();
+        SAVE_KERNEL_8X8_F16_f32();
       }
 
       c += 8;
