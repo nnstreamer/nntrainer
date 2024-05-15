270c45567bbdf4f58bc4db0e3050ed6a11b72a89
Debadri Samaddar <s.debadri@samsung.com>
2024-05-15 16:08:04 +0530

    [bugfix] Renamed variables in unittest of FC Layer
    Renamed global variables in unittest_layers_fully_connected_cl.cpp to fix duplicate declaration error
    
    Signed-off-by: Debadri Samaddar <s.debadri@samsung.com>

--- test/unittest/layers/unittest_layers_fully_connected_cl.cpp
+++ test/unittest/layers/unittest_layers_fully_connected_cl.cpp
@@ -16,29 +16,29 @@
 #include <fc_layer_cl.h>
 #include <layers_common_tests.h>
 
-auto semantic_fc = LayerSemanticsParamType(
+auto semantic_fc_gpu = LayerSemanticsParamType(
   nntrainer::createLayer<nntrainer::FullyConnectedLayerCl>,
   nntrainer::FullyConnectedLayerCl::type, {"unit=1"},
   LayerCreateSetPropertyOptions::AVAILABLE_FROM_APP_CONTEXT, false, 1);
 
 GTEST_PARAMETER_TEST(FullyConnectedGPU, LayerSemantics,
-                     ::testing::Values(semantic_fc));
+                     ::testing::Values(semantic_fc_gpu));
 
-auto fc_basic_plain = LayerGoldenTestParamType(
+auto fc_gpu_plain = LayerGoldenTestParamType(
   nntrainer::createLayer<nntrainer::FullyConnectedLayerCl>, {"unit=5"},
   "3:1:1:10", "fc_plain.nnlayergolden", LayerGoldenTestParamOptions::DEFAULT,
   "nchw", "fp32", "fp32");
-auto fc_basic_single_batch = LayerGoldenTestParamType(
+auto fc_gpu_single_batch = LayerGoldenTestParamType(
   nntrainer::createLayer<nntrainer::FullyConnectedLayerCl>, {"unit=4"},
   "1:1:1:10", "fc_single_batch.nnlayergolden",
   LayerGoldenTestParamOptions::DEFAULT, "nchw", "fp32", "fp32");
-auto fc_basic_no_decay = LayerGoldenTestParamType(
+auto fc_gpu_no_decay = LayerGoldenTestParamType(
   nntrainer::createLayer<nntrainer::FullyConnectedLayerCl>,
   {"unit=5", "weight_decay=0.0", "bias_decay=0.0"}, "3:1:1:10",
   "fc_plain.nnlayergolden", LayerGoldenTestParamOptions::DEFAULT, "nchw",
   "fp32", "fp32");
 
-auto fc_basic_plain_nhwc = LayerGoldenTestParamType(
+auto fc_gpu_plain_nhwc = LayerGoldenTestParamType(
   nntrainer::createLayer<nntrainer::FullyConnectedLayerCl>, {"unit=5"},
   "3:10:1:1", "fc_plain.nnlayergolden",
   LayerGoldenTestParamOptions::SKIP_CALC_DERIV |
@@ -46,14 +46,14 @@ auto fc_basic_plain_nhwc = LayerGoldenTestParamType(
     LayerGoldenTestParamOptions::USE_INC_FORWARD,
   "nhwc", "fp32", "fp32");
 
-auto fc_basic_single_batch_nhwc = LayerGoldenTestParamType(
+auto fc_gpu_single_batch_nhwc = LayerGoldenTestParamType(
   nntrainer::createLayer<nntrainer::FullyConnectedLayerCl>, {"unit=4"},
   "1:10:1:1", "fc_single_batch.nnlayergolden",
   LayerGoldenTestParamOptions::SKIP_CALC_DERIV |
     LayerGoldenTestParamOptions::SKIP_CALC_GRAD,
   "nhwc", "fp32", "fp32");
 
-auto fc_basic_no_decay_nhwc = LayerGoldenTestParamType(
+auto fc_gpu_no_decay_nhwc = LayerGoldenTestParamType(
   nntrainer::createLayer<nntrainer::FullyConnectedLayerCl>,
   {"unit=5", "weight_decay=0.0", "bias_decay=0.0"}, "3:10:1:1",
   "fc_plain.nnlayergolden",
@@ -62,7 +62,7 @@ auto fc_basic_no_decay_nhwc = LayerGoldenTestParamType(
   "nhwc", "fp32", "fp32");
 
 GTEST_PARAMETER_TEST(FullyConnectedGPU, LayerGoldenTest,
-                     ::testing::Values(fc_basic_plain, fc_basic_single_batch,
-                                       fc_basic_no_decay, fc_basic_plain_nhwc,
-                                       fc_basic_single_batch_nhwc,
-                                       fc_basic_no_decay_nhwc));
+                     ::testing::Values(fc_gpu_plain, fc_gpu_single_batch,
+                                       fc_gpu_no_decay, fc_gpu_plain_nhwc,
+                                       fc_gpu_single_batch_nhwc,
+                                       fc_gpu_no_decay_nhwc));
