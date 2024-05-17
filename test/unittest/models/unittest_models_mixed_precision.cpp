edd5778f9fc0e4188da3e5b4efd67ddced0fde3a
Donghak PARK <donghak.park@samsung.com>
2024-05-17 18:35:25 +0900

    [unittest] Add mixed precision unit test
    Add 2 case of mixed precision unit test
    - case1 : FC-FC-FC
    - case2 : Flatten - FC
    
    i will add more mixed precision case (conv, lstm, etc..)
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	 [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Donghak PARK <donghak.park@samsung.com>

--- test/unittest/models/unittest_models_mixed_precision.cpp
+++ test/unittest/models/unittest_models_mixed_precision.cpp
@@ -41,10 +41,51 @@ static std::unique_ptr<NeuralNetwork> fc_mixed_training() {
   return nn;
 }
 
+static std::unique_ptr<NeuralNetwork> multi_fc_mixed_training() {
+  std::unique_ptr<NeuralNetwork> nn(new NeuralNetwork());
+  nn->setProperty(
+    {"batch_size=2", "model_tensor_type=FP16-FP16", "loss_scale=128"});
+
+  auto graph = makeGraph({
+    {"input", {"name=in", "input_shape=1:28:28"}},
+    {"Fully_connected", {"name=fc1", "input_layers=in", "unit=100"}},
+    {"Fully_connected", {"name=fc2", "input_layers=fc1", "unit=50"}},
+    {"Fully_connected", {"name=fc3", "input_layers=fc2", "unit=10"}},
+    {"mse", {"name=loss", "input_layers=fc3"}},
+  });
+  for (auto &node : graph) {
+    nn->addLayer(node);
+  }
+  nn->setOptimizer(ml::train::createOptimizer("adam", {"learning_rate=0.1"}));
+  return nn;
+}
+
+static std::unique_ptr<NeuralNetwork> flatten_fc_mixed_training() {
+  std::unique_ptr<NeuralNetwork> nn(new NeuralNetwork());
+  nn->setProperty(
+    {"batch_size=2", "model_tensor_type=FP16-FP16", "loss_scale=128"});
+
+  auto graph = makeGraph({
+    {"input", {"name=in", "input_shape=1:28:28"}},
+    {"flatten", {"name=flatten", "input_layers=in"}},
+    {"Fully_connected", {"name=fc", "input_layers=flatten", "unit=10"}},
+    {"mse", {"name=loss", "input_layers=fc"}},
+  });
+  for (auto &node : graph) {
+    nn->addLayer(node);
+  }
+  nn->setOptimizer(ml::train::createOptimizer("adam", {"learning_rate=0.1"}));
+  return nn;
+}
+
 GTEST_PARAMETER_TEST(
   MixedPrecision, nntrainerModelTest,
   ::testing::ValuesIn({
     mkModelTc_V2(fc_mixed_training, "fc_mixed_training",
+                 ModelTestOption::NO_THROW_RUN_V2),
+    mkModelTc_V2(multi_fc_mixed_training, "multi_fc_mixed_training",
+                 ModelTestOption::NO_THROW_RUN_V2),
+    mkModelTc_V2(flatten_fc_mixed_training, "flatten_fc_mixed_training",
                  ModelTestOption::NO_THROW_RUN_V2),
     /** ModelTestOption::ALL_V2),
      * Disabled for now to check
