1943e70442b4275d6a02899412c77335773739cd
Debadri Samaddar <s.debadri@samsung.com>
2024-05-21 16:59:36 +0530

    [unittest/gpu] Added LayerSemanticsGpu test suite
    Added semantics test for layers on GPU.
    Removed redundant run_context compute engine setter call.
    
    Signed-off-by: Debadri Samaddar <s.debadri@samsung.com>

--- test/unittest/layers/layers_standalone_common_tests.cpp
+++ test/unittest/layers/layers_standalone_common_tests.cpp
@@ -7,6 +7,7 @@
  * @brief Common test for nntrainer layers (Param Tests)
  * @see	https://github.com/nnstreamer/nntrainer
  * @author Jihoon Lee <jhoon.it.lee@samsung.com>
+ * @author Debadri Samaddar <s.debadri@samsung.com>
  * @bug No known bugs except for NYI items
  */
 
@@ -80,3 +81,63 @@ TEST_P(LayerSemantics, setBatchValidate_p) {
                  nntrainer::exception::not_supported);
   }
 }
+
+#ifdef ENABLE_OPENCL
+TEST_P(LayerSemanticsGpu, setProperties_n) {
+  /** must not crash */
+  EXPECT_THROW(layer->setProperty({"unknown_props=2"}), std::invalid_argument);
+}
+
+TEST_P(LayerSemanticsGpu, gettersValidate_p) {
+  std::string type;
+
+  EXPECT_NO_THROW(type = layer->getType());
+  EXPECT_GT(type.size(), size_t(0));
+  EXPECT_NO_THROW(layer->supportInPlace());
+  EXPECT_NO_THROW(layer->requireLabel());
+  EXPECT_NO_THROW(layer->supportBackwarding());
+}
+
+TEST_P(LayerSemanticsGpu, finalizeValidate_p) {
+  ml::train::TensorDim in_dim({1, 1, 1, 1});
+  std::vector<ml::train::TensorDim> input_dims(num_inputs, in_dim);
+  nntrainer::InitLayerContext init_context =
+    nntrainer::InitLayerContext(input_dims, {true}, false, "layer");
+  EXPECT_EQ(init_context.validate(), true);
+
+  // set necessary properties only
+  EXPECT_NO_THROW(layer->setProperty(valid_properties));
+
+  if (!must_fail) {
+    EXPECT_NO_THROW(layer->finalize(init_context));
+
+    for (auto const &spec : init_context.getOutSpecs())
+      EXPECT_GT(spec.variable_spec.dim.getDataLen(), size_t(0));
+    for (auto const &ws : init_context.getWeightsSpec())
+      EXPECT_GT(std::get<0>(ws).getDataLen(), size_t(0));
+    for (auto const &ts : init_context.getTensorsSpec())
+      EXPECT_GT(std::get<0>(ts).getDataLen(), size_t(0));
+  } else {
+    EXPECT_THROW(layer->finalize(init_context),
+                 nntrainer::exception::not_supported);
+  }
+}
+
+TEST_P(LayerSemanticsGpu, setBatchValidate_p) {
+  ml::train::TensorDim in_dim({1, 1, 1, 1});
+  std::vector<ml::train::TensorDim> input_dims(num_inputs, in_dim);
+  nntrainer::InitLayerContext init_context =
+    nntrainer::InitLayerContext(input_dims, {true}, false, "layer");
+  EXPECT_EQ(init_context.validate(), true);
+
+  // set necessary properties only
+  EXPECT_NO_THROW(layer->setProperty(valid_properties));
+
+  if (!must_fail) {
+    EXPECT_NO_THROW(layer->finalize(init_context));
+  } else {
+    EXPECT_THROW(layer->finalize(init_context),
+                 nntrainer::exception::not_supported);
+  }
+}
+#endif
