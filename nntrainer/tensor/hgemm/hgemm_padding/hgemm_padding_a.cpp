cd1e0034d80489a9d894b18d8c82d49fe36d0281
skykongkong8 <ss.kong@samsung.com>
2024-07-15 18:41:43 +0900

    [ hgemm ] Implement NYI functions from matrix A/B hgemm_padding
    - Missing implementations might trigger unittest fails on Android.
    - This patch will now support padding function for all combinations of following conditions : matrix A / B, trans/noTrans, M/K/N direction
    
    **Self evaluation:**
    1. Build test:     [X]Passed [ ]Failed [ ]Skipped
    2. Run test:     [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: skykongkong8 <ss.kong@samsung.com>

--- nntrainer/tensor/hgemm/hgemm_padding/hgemm_padding_a.cpp
+++ nntrainer/tensor/hgemm/hgemm_padding/hgemm_padding_a.cpp
@@ -94,7 +94,7 @@ void hgemm_padding_A_noTrans_wrt_MK(const __fp16 *A, __fp16 *Ap, unsigned int M,
   float16x8_t ZEROS = vmovq_n_f16(0.F);
 
   for (unsigned int m = 0; m < M; ++m) {
-    for (unsigned int k = 0; k < K8_low; ++k) {
+    for (unsigned int k = 0; k < K8_low; k += 8) {
       vst1q_f16(&Ap[m * K8 + k], vld1q_f16(&A[m * K + k]));
     }
     for (unsigned int k = K8_low; k < K; ++k) {
@@ -105,8 +105,8 @@ void hgemm_padding_A_noTrans_wrt_MK(const __fp16 *A, __fp16 *Ap, unsigned int M,
     }
   }
   for (unsigned int m = M; m < M8; ++m) {
-    for (unsigned int k = K; k < K8; ++k) {
-      Ap[m * K8 + k] = ZEROS;
+    for (unsigned int k = 0; k < K8; k += 8) {
+      vst1q_f16(&Ap[m * K8 + k], ZEROS);
     }
   }
 }
@@ -115,16 +115,15 @@ void hgemm_padding_A_Trans_wrt_M(const __fp16 *A, __fp16 *Ap, unsigned int M,
                                  unsigned int K, unsigned int M8,
                                  unsigned int K8) {
   const unsigned int M8_low = (M >> 3) << 3;
-
   for (unsigned int k = 0; k < K; ++k) {
     for (unsigned int m = 0; m < M8_low; m += 8) {
-      vst1q_f16(&Ap[k * M + m], vld1q_f16(&A[k * M + m]));
+      vst1q_f16(&Ap[k * M8 + m], vld1q_f16(&A[k * M + m]));
     }
     for (unsigned int m = M8_low; m < M; ++m) {
-      Ap[k * M + m] = A[k * M + m];
+      Ap[k * M8 + m] = A[k * M + m];
     }
     for (unsigned int m = M; m < M8; ++m) {
-      Ap[k * M + m] = 0.F;
+      Ap[k * M8 + m] = 0.F;
     }
   }
 }
@@ -132,11 +131,38 @@ void hgemm_padding_A_Trans_wrt_M(const __fp16 *A, __fp16 *Ap, unsigned int M,
 void hgemm_padding_A_Trans_wrt_K(const __fp16 *A, __fp16 *Ap, unsigned int M,
                                  unsigned int K, unsigned int M8,
                                  unsigned int K8) {
-  std::cerr << "Error : hgemm_padding_A_Trans_wrt_K NYI!\n";
+  float16x8_t ZEROS = vmovq_n_f16(0.F);
+  for (unsigned int k = 0; k < K; ++k) {
+    for (unsigned int m = 0; m < M; m += 8) {
+      vst1q_f16(&Ap[k * M8 + m], vld1q_f16(&A[k * M + m]));
+    }
+  }
+  for (unsigned int k = K; k < K8; ++k) {
+    for (unsigned int m = 0; m < M; m += 8) {
+      vst1q_f16(&Ap[k * M8 + m], ZEROS);
+    }
+  }
 }
 
 void hgemm_padding_A_Trans_wrt_MK(const __fp16 *A, __fp16 *Ap, unsigned int M,
                                   unsigned int K, unsigned int M8,
                                   unsigned int K8) {
-  std::cerr << "Error : hgemm_padding_A_Trans_wrt_MK NYI!\n";
+  float16x8_t ZEROS = vmovq_n_f16(0.F);
+  const unsigned int M8_low = (M >> 3) << 3;
+  for (unsigned int k = 0; k < K; ++k) {
+    for (unsigned int m = 0; m < M8_low; m += 8) {
+      vst1q_f16(&Ap[k * M8 + m], vld1q_f16(&A[k * M + m]));
+    }
+    for (unsigned int m = M8_low; m < M; ++m) {
+      Ap[k * M8 + m] = A[k * M + m];
+    }
+    for (unsigned int m = M; m < M8; ++m) {
+      Ap[k * M8 + m] = 0.F;
+    }
+  }
+  for (unsigned int k = K; k < K8; ++k) {
+    for (unsigned int m = 0; m < M8; m += 8) {
+      vst1q_f16(&Ap[k * M8 + m], ZEROS);
+    }
+  }
 }
--- nntrainer/tensor/hgemm/hgemm_padding/hgemm_padding_b.cpp
+++ nntrainer/tensor/hgemm/hgemm_padding/hgemm_padding_b.cpp
@@ -20,9 +20,9 @@ void hgemm_padding_B(const __fp16 *B, __fp16 *Bp, unsigned int K,
                      unsigned int N, unsigned int K8, unsigned int N16,
                      bool transB) {
   if (transB) {
-    hgemm_padding_B_Trans(B, Bp, K, N, K8, N16);
+    return hgemm_padding_B_Trans(B, Bp, K, N, K8, N16);
   } else {
-    hgemm_padding_B_noTrans(B, Bp, K, N, K8, N16);
+    return hgemm_padding_B_noTrans(B, Bp, K, N, K8, N16);
   }
 }
 
@@ -30,11 +30,11 @@ void hgemm_padding_B_noTrans(const __fp16 *B, __fp16 *Bp, unsigned int K,
                              unsigned int N, unsigned int K8,
                              unsigned int N16) {
   if (K != K8 && N != N16) {
-    hgemm_padding_B_noTrans_wrt_KN(B, Bp, K, N, K8, N16);
+    return hgemm_padding_B_noTrans_wrt_KN(B, Bp, K, N, K8, N16);
   } else if (K != K8) {
-    hgemm_padding_B_noTrans_wrt_K(B, Bp, K, N, K8, N16);
+    return hgemm_padding_B_noTrans_wrt_K(B, Bp, K, N, K8, N16);
   } else if (N != N16) {
-    hgemm_padding_B_noTrans_wrt_N(B, Bp, K, N, K8, N16);
+    return hgemm_padding_B_noTrans_wrt_N(B, Bp, K, N, K8, N16);
   } else {
     std::cerr << "Error : No room for matrix B padding\n";
   }
@@ -43,11 +43,11 @@ void hgemm_padding_B_noTrans(const __fp16 *B, __fp16 *Bp, unsigned int K,
 void hgemm_padding_B_Trans(const __fp16 *B, __fp16 *Bp, unsigned int K,
                            unsigned int N, unsigned int K8, unsigned int N16) {
   if (K != K8 && N != N16) {
-    hgemm_padding_B_Trans_wrt_KN(B, Bp, K, N, K8, N16);
+    return hgemm_padding_B_Trans_wrt_KN(B, Bp, K, N, K8, N16);
   } else if (K != K8) {
-    hgemm_padding_B_Trans_wrt_K(B, Bp, K, N, K8, N16);
+    return hgemm_padding_B_Trans_wrt_K(B, Bp, K, N, K8, N16);
   } else if (N != N16) {
-    hgemm_padding_B_Trans_wrt_N(B, Bp, K, N, K8, N16);
+    return hgemm_padding_B_Trans_wrt_N(B, Bp, K, N, K8, N16);
   } else {
     std::cerr << "Error : No room for matrix B padding\n";
   }
@@ -56,7 +56,18 @@ void hgemm_padding_B_Trans(const __fp16 *B, __fp16 *Bp, unsigned int K,
 void hgemm_padding_B_noTrans_wrt_N(const __fp16 *B, __fp16 *Bp, unsigned int K,
                                    unsigned int N, unsigned int K8,
                                    unsigned int N16) {
-  std::cerr << "Error : hgemm_padding_B_noTrans_wrt_N NYI!\n";
+  const unsigned int N8_low = (N >> 3) << 3;
+  for (unsigned int k = 0; k < K; ++k) {
+    for (unsigned int n = 0; n < N8_low; n += 8) {
+      vst1q_f16(&Bp[k * N16 + n], vld1q_f16(&B[k * N + n]));
+    }
+    for (unsigned int n = N8_low; n < N; ++n) {
+      Bp[k * N16 + n] = B[k * N + n];
+    }
+    for (unsigned int n = N; n < N16; ++n) {
+      Bp[k * N16 + n] = 0.F;
+    }
+  }
 }
 
 void hgemm_padding_B_noTrans_wrt_K(const __fp16 *B, __fp16 *Bp, unsigned int K,
@@ -79,13 +90,41 @@ void hgemm_padding_B_noTrans_wrt_K(const __fp16 *B, __fp16 *Bp, unsigned int K,
 void hgemm_padding_B_noTrans_wrt_KN(const __fp16 *B, __fp16 *Bp, unsigned int K,
                                     unsigned int N, unsigned int K8,
                                     unsigned int N16) {
-  std::cerr << "Error : hgemm_padding_B_noTrans_wrt_KN NYI!\n";
+  unsigned int N8_low = (N >> 3) << 3;
+  float16x8_t ZEROS = vmovq_n_f16(0.F);
+  for (unsigned int k = 0; k < K; ++k) {
+    for (unsigned int n = 0; n < N8_low; n += 8) {
+      vst1q_f16(&Bp[k * N16 + n], vld1q_f16(&B[k * N + n]));
+    }
+    for (unsigned int n = N8_low; n < N; ++n) {
+      Bp[k * N16 + n] = B[k * N + n];
+    }
+    for (unsigned int n = N; n < N16; ++n) {
+      Bp[k * N16 + n] = 0.F;
+    }
+  }
+  for (unsigned int k = K; k < K8; ++k) {
+    for (unsigned int n = 0; n < N16; n += 8) {
+      vst1q_f16(&Bp[k * N16 + n], ZEROS);
+    }
+  }
 }
 
 void hgemm_padding_B_Trans_wrt_N(const __fp16 *B, __fp16 *Bp, unsigned int K,
                                  unsigned int N, unsigned int K8,
                                  unsigned int N16) {
-  std::cerr << "Error : hgemm_padding_B_Trans_wrt_N NYI!\n";
+  float16x8_t ZEROS = vmovq_n_f16(0.F);
+
+  for (unsigned int n = 0; n < N; ++n) {
+    for (unsigned int k = 0; k < K; k += 8) {
+      vst1q_f16(&Bp[n * K8 + k], vld1q_f16(&B[n * K + k]));
+    }
+  }
+  for (unsigned int n = N; n < N16; ++n) {
+    for (unsigned int k = 0; k < K; k += 8) {
+      vst1q_f16(&Bp[n * K8 + k], ZEROS);
+    }
+  }
 }
 
 void hgemm_padding_B_Trans_wrt_K(const __fp16 *B, __fp16 *Bp, unsigned int K,
@@ -110,5 +149,23 @@ void hgemm_padding_B_Trans_wrt_K(const __fp16 *B, __fp16 *Bp, unsigned int K,
 void hgemm_padding_B_Trans_wrt_KN(const __fp16 *B, __fp16 *Bp, unsigned int K,
                                   unsigned int N, unsigned int K8,
                                   unsigned int N16) {
-  std::cerr << "Error : hgemm_padding_B_Trans_wrt_KN NYI!\n";
+  unsigned int K8_low = (K >> 3) << 3;
+  float16x8_t ZEROS = vmovq_n_f16(0.F);
+
+  for (unsigned int n = 0; n < N; ++n) {
+    for (unsigned int k = 0; k < K8_low; k += 8) {
+      vst1q_f16(&Bp[n * K8 + k], vld1q_f16(&B[n * K + k]));
+    }
+    for (unsigned int k = K8_low; k < K; ++k) {
+      Bp[n * K8 + k] = B[n * K + k];
+    }
+    for (unsigned int k = K; k < K8; ++k) {
+      Bp[n * K8 + k] = 0.F;
+    }
+  }
+  for (unsigned int n = N; n < N16; ++n) {
+    for (unsigned int k = 0; k < K8; k += 8) {
+      vst1q_f16(&Bp[n * K8 + k], ZEROS);
+    }
+  }
 }
