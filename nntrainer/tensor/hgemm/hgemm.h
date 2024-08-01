4f3ae915e1b93cd37e5fb74462aaf5eda69bcc37
skykongkong8 <ss.kong@samsung.com>
2024-08-01 20:29:39 +0900

    [ hgemm ] Implement hgemm_small
    - Forcibly adding zero-padding made small dim index quite clumsy and redundant.
    - Implement explicit hgemm small function to cover M<8, N<16, K<16 case
    
    **Self evaluation:**
    1. Build test:     [X]Passed [ ]Failed [ ]Skipped
    2. Run test:     [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: skykongkong8 <ss.kong@samsung.com>

--- nntrainer/tensor/hgemm/hgemm.h
+++ nntrainer/tensor/hgemm/hgemm.h
@@ -29,6 +29,22 @@ void hgemm(const __fp16 *A, const __fp16 *B, __fp16 *C, unsigned int M,
            bool TransB);
 
 /**
+ * @brief     hgemm computation with neon but with small dim without padding : Y
+ * = alpha*op(A)*op(B) + beta*C, where op(X) is one of X or X**T
+ * @param[in] A __fp16 * for Matrix A
+ * @param[in] B __fp16 * for Matrix B
+ * @param[in] C __fp16 * for Matrix C
+ * @param[in] M number of op(A)'s and C's row
+ * @param[in] N number of op(B)'s and C's columns
+ * @param[in] K number of op(A)'s and columns and op(B)'s rows
+ * @param[in] alpha float number
+ * @param[in] beta float number
+ */
+void hgemm_small(const __fp16 *A, const __fp16 *B, __fp16 *C, unsigned int M,
+                 unsigned int N, unsigned int K, float alpha, float beta,
+                 bool TransA, bool TransB);
+
+/**
  * @brief     Checking function for whether matrix A or B needs padding for
  * optimal performance of fixed blocking-kernel sequence
  * @param[in] A __fp16 * for Matrix A
--- nntrainer/tensor/hgemm/hgemm_util.h
+++ nntrainer/tensor/hgemm/hgemm_util.h
@@ -10,6 +10,7 @@
  * @brief  This is for util functions for half-precision GEMM
  */
 
+#include <arm_neon.h>
 #include <assert.h>
 #include <stdlib.h>
 
@@ -19,9 +20,14 @@
  * @param sz amount of data to allocate
  * @return __fp16* addr of allocated memory
  */
-static inline __fp16 *alignedMalloc(int sz) {
-  void *addr = 0;
-  int iRet = posix_memalign(&addr, 64, sz * sizeof(__fp16));
-  assert(0 == iRet);
-  return (__fp16 *)addr;
-}
+__fp16 *alignedMalloc(unsigned int sz);
+
+unsigned int get_next_mltpl_of_n(unsigned int x, unsigned int n);
+
+unsigned int get_prev_mltpl_of_2p_n(unsigned int x, unsigned int n);
+
+void copy_C_to_C32(__fp16 *C, float *C32, unsigned int M, unsigned int N,
+                   float beta = 0.F);
+
+void copy_C32_to_C(float *C32, __fp16 *C, unsigned int M, unsigned int N,
+                   float beta = 0.F);
