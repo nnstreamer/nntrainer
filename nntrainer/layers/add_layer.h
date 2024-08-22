f7e2646db0ff4cf86bf6068e905222287c097643
Seungbaek Hong <sb92.hong@samsung.com>
2024-08-22 21:18:52 +0900

    [Layer] add "add layer"
    - added "add layer"
    - added a "model unit test" for add layer. Since 'model-level unit test'
    havn't been run for a long time, I diabled some test cases that causing
    issues when running model unit test.
    
    Many people gave great feedback, so I've improved the structure accordingly.
    - An upper class called "OperationLayer" was added to reduce redundant code.
    - Based on the number of input tensors, the behavior of "OperationLayer" has been classified into two types: unary and binary operations.
    - Various additional code cleanups have also taken place.
    
    **Self evaluation:**
    1. Build test:   [X]Passed [X]Failed [ ]Skipped
    2. Run test:     [X]Passed [X]Failed [ ]Skipped
    
    Signed-off-by: Seungbaek Hong <sb92.hong@samsung.com>

---
+++ nntrainer/layers/add_layer.h
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2024 SeungBaek Hong <sb92.hong@samsung.com>
+ *
+ * @file   add_layer.h
+ * @date   7 Oct 2024
+ * @see    https://github.com/nnstreamer/nntrainer
+ * @author SeungBaek Hong <sb92.hong@samsung.com>
+ * @bug    No known bugs except for NYI items
+ * @brief  This is add layer class (operation layer)
+ *
+ */
+
+#ifndef __ADD_LAYER_H__
+#define __ADD_LAYER_H__
+#ifdef __cplusplus
+
+#include <common_properties.h>
+#include <layer_devel.h>
+#include <operation_layer.h>
+
+namespace nntrainer {
+
+/**
+ * @class Add Layer
+ * @brief Add Layer
+ */
+class AddLayer : public BinaryOperationLayer {
+public:
+  /**
+   * @brief Destructor of Add Layer
+   */
+  ~AddLayer() {}
+
+  /**
+   * @brief Constructor of Add Layer
+   */
+  AddLayer() : BinaryOperationLayer(), add_props(props::Print()) {}
+
+  /**
+   *  @brief  Move constructor of Add Layer.
+   *  @param[in] AddLayer &&
+   */
+  AddLayer(AddLayer &&rhs) noexcept = default;
+
+  /**
+   * @brief  Move assignment operator.
+   * @parma[in] rhs AddLayer to be moved.
+   */
+  AddLayer &operator=(AddLayer &&rhs) = default;
+
+  /**
+   * @copydoc Layer::finalize(InitLayerContext &context)
+   */
+  void finalize(InitLayerContext &context) final;
+
+  /**
+   * @brief forwarding operation for add
+   *
+   * @param input0 input tensor 0
+   * @param input1 input tensor 1
+   * @param hidden tensor to store the result of addition
+   */
+  void forwarding_operation(const Tensor &input0, const Tensor &input1,
+                            Tensor &hidden) final;
+
+  /**
+   * @copydoc Layer::calcDerivative(RunLayerContext &context)
+   */
+  void calcDerivative(RunLayerContext &context) final;
+
+  /**
+   * @copydoc bool supportBackwarding() const
+   */
+  bool supportBackwarding() const final { return true; };
+
+  /**
+   * @copydoc Layer::exportTo(Exporter &exporter, ml::train::ExportMethods
+   * method)
+   */
+  void exportTo(Exporter &exporter,
+                const ml::train::ExportMethods &method) const final {}
+
+  /**
+   * @copydoc Layer::setProperty(const std::vector<std::string> &values)
+   */
+  void setProperty(const std::vector<std::string> &values) final;
+
+  /**
+   * @copydoc Layer::getType()
+   */
+  const std::string getType() const final { return AddLayer::type; }
+
+  std::tuple<props::Print> add_props;
+
+  inline static const std::string type = "add";
+};
+
+} // namespace nntrainer
+
+#endif /* __cplusplus */
+#endif /* __ADD_LAYER_H__ */
