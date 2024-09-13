1f6a2c0f92c0a83edc1df7c3c70b7bf5bc929222
Debadri Samaddar <s.debadri@samsung.com>
2024-09-13 13:23:30 +0530

    [enhance/gpu] Removing layer_context dependency
    Removed layer_context dependency from blas OpenCL kernels.
    Temporarily commented out cl_layers to avoid build failure.
    
    Signed-off-by: Debadri Samaddar <s.debadri@samsung.com>
    (cherry picked from commit 7ee9294c18e1bcba6d1a0fb3d3a7b7fabc0073e3)

--- nntrainer/tensor/cl_operations/blas_kernels.h
+++ nntrainer/tensor/cl_operations/blas_kernels.h
@@ -15,24 +15,14 @@
 #define __BLAS_KERNELS_H__
 
 #include <cl_context.h>
-#include <layer_context.h>
 #include <opencl_buffer.h>
 #include <opencl_kernel.h>
 #include <string>
 
 namespace nntrainer {
 
-/**
- * @brief declaring global kernel objects
- */
-extern opencl::Kernel kernel_sgemv;
-extern opencl::Kernel kernel_sgemm_noTrans;
-extern opencl::Kernel kernel_sgemm_transAB;
-extern opencl::Kernel kernel_sgemm_transA;
-extern opencl::Kernel kernel_sgemm_transB;
-extern opencl::Kernel kernel_dot;
-extern opencl::Kernel kernel_addition;
-extern opencl::Kernel kernel_sscal;
+// get global cl_context to use in kernels
+extern ClContext &cl_context_ref;
 
 /**
  * @brief     sgemv computation : Y = A*X + Y
@@ -45,8 +35,7 @@ extern opencl::Kernel kernel_sscal;
  * @param[in] context RunLayerContext reference
  */
 void sgemv_cl(const float *matAdata, const float *vecXdata, float *vecYdata,
-              unsigned int dim1, unsigned int dim2, unsigned int lda,
-              RunLayerContext &context);
+              unsigned int dim1, unsigned int dim2, unsigned int lda);
 
 /**
  * @brief     dot computation : sum of all X * Y
@@ -56,8 +45,7 @@ void sgemv_cl(const float *matAdata, const float *vecXdata, float *vecYdata,
  * @param[in] context RunLayerContext reference
  * @return    float dot product result
  */
-float dot_cl(const float *vecAdata, const float *vecXdata, unsigned int dim1,
-             RunLayerContext &context);
+float dot_cl(const float *vecAdata, const float *vecXdata, unsigned int dim1);
 
 /**
  * @brief     sgemm computation : Y = op(A)*op(B) + C,
@@ -77,8 +65,7 @@ float dot_cl(const float *vecAdata, const float *vecXdata, unsigned int dim1,
  */
 void sgemm_cl(bool TransA, bool TransB, const float *A, const float *B,
               float *C, unsigned int M, unsigned int N, unsigned int K,
-              unsigned int lda, unsigned int ldb, unsigned int ldc,
-              RunLayerContext &context);
+              unsigned int lda, unsigned int ldb, unsigned int ldc);
 
 /**
  * @brief     addition : sum of all input vectors
@@ -87,8 +74,7 @@ void sgemm_cl(bool TransA, bool TransB, const float *A, const float *B,
  * @param[in] size number of elements in input vector
  * @param[in] context RunLayerContext reference
  */
-void addition_cl(const float *input, float *res, unsigned int size,
-                 RunLayerContext &context);
+void addition_cl(const float *input, float *res, unsigned int size);
 
 /**
  * @brief     sscal value element by element immediately
@@ -97,21 +83,9 @@ void addition_cl(const float *input, float *res, unsigned int size,
  * @param[in] alpha float multiplier
  * @param[in] context RunLayerContext reference
  */
-void sscal_cl(float *X, const unsigned int N, const float alpha,
-              RunLayerContext &context);
+void sscal_cl(float *X, const unsigned int N, const float alpha);
 
 #ifdef ENABLE_FP16
-/**
- * @brief declaring global fp16 kernel objects
- */
-extern opencl::Kernel kernel_sgemv_fp16;
-extern opencl::Kernel kernel_sgemm_noTrans_fp16;
-extern opencl::Kernel kernel_sgemm_transAB_fp16;
-extern opencl::Kernel kernel_sgemm_transA_fp16;
-extern opencl::Kernel kernel_sgemm_transB_fp16;
-extern opencl::Kernel kernel_dot_fp16;
-extern opencl::Kernel kernel_addition_fp16;
-extern opencl::Kernel kernel_sscal_fp16;
 
 /**
  * @brief     fp16 sgemv computation : Y = A*X + Y
@@ -124,8 +98,7 @@ extern opencl::Kernel kernel_sscal_fp16;
  * @param[in] context RunLayerContext reference
  */
 void sgemv_cl(const __fp16 *matAdata, const __fp16 *vecXdata, __fp16 *vecYdata,
-              unsigned int dim1, unsigned int dim2, unsigned int lda,
-              RunLayerContext &context);
+              unsigned int dim1, unsigned int dim2, unsigned int lda);
 
 /**
  * @brief     fp16 dot computation : sum of all X * Y
@@ -135,8 +108,8 @@ void sgemv_cl(const __fp16 *matAdata, const __fp16 *vecXdata, __fp16 *vecYdata,
  * @param[in] context RunLayerContext reference
  * @return    fp16 dot product result
  */
-__fp16 dot_cl(const __fp16 *vecAdata, const __fp16 *vecXdata, unsigned int dim1,
-              RunLayerContext &context);
+__fp16 dot_cl(const __fp16 *vecAdata, const __fp16 *vecXdata,
+              unsigned int dim1);
 
 /**
  * @brief     fp16 sgemm computation : Y = op(A)*op(B) + C,
@@ -156,8 +129,7 @@ __fp16 dot_cl(const __fp16 *vecAdata, const __fp16 *vecXdata, unsigned int dim1,
  */
 void sgemm_cl(bool TransA, bool TransB, const __fp16 *A, const __fp16 *B,
               __fp16 *C, unsigned int M, unsigned int N, unsigned int K,
-              unsigned int lda, unsigned int ldb, unsigned int ldc,
-              RunLayerContext &context);
+              unsigned int lda, unsigned int ldb, unsigned int ldc);
 
 /**
  * @brief     fp16 addition : sum of all input vectors
@@ -166,8 +138,7 @@ void sgemm_cl(bool TransA, bool TransB, const __fp16 *A, const __fp16 *B,
  * @param[in] size number of elements in input vector
  * @param[in] context RunLayerContext reference
  */
-void addition_cl(const __fp16 *input, __fp16 *res, unsigned int size,
-                 RunLayerContext &context);
+void addition_cl(const __fp16 *input, __fp16 *res, unsigned int size);
 
 /**
  * @brief     fp16 sscal value element by element immediately
@@ -176,8 +147,7 @@ void addition_cl(const __fp16 *input, __fp16 *res, unsigned int size,
  * @param[in] alpha float multiplier
  * @param[in] context RunLayerContext reference
  */
-void sscal_cl(__fp16 *X, const unsigned int N, const float alpha,
-              RunLayerContext &context);
+void sscal_cl(__fp16 *X, const unsigned int N, const float alpha);
 #endif
 
 } // namespace nntrainer
