14d1e343313949189ed4ed25810d22eddd6ccaf4
Donghak PARK <donghak.park@samsung.com>
2024-10-16 13:46:18 +0900

    [UnitTest] Add kld-loss unit test
    Currently, there is no unit test for the KLD loss. Therefore, I am adding a new unit test to increase code coverage.
    - use pre defined unit test
    - (LayerSemanticsParamType)
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	 [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Donghak PARK <donghak.park@samsung.com>

--- test/unittest/layers/unittest_layers_loss.cpp
+++ test/unittest/layers/unittest_layers_loss.cpp
@@ -17,6 +17,7 @@
 #include <cross_entropy_loss_layer.h>
 #include <cross_entropy_sigmoid_loss_layer.h>
 #include <cross_entropy_softmax_loss_layer.h>
+#include <kld_loss_layer.h>
 #include <layers_common_tests.h>
 #include <mse_loss_layer.h>
 
@@ -35,6 +36,10 @@ auto semantic_loss_mse = LayerSemanticsParamType(
   nntrainer::MSELossLayer::type, {},
   LayerCreateSetPropertyOptions::AVAILABLE_FROM_APP_CONTEXT, false, 1);
 
+auto semantic_loss_kld = LayerSemanticsParamType(
+  nntrainer::createLayer<nntrainer::KLDLossLayer>,
+  nntrainer::KLDLossLayer::type, {}, 0, false, 1);
+
 auto semantic_loss_constant_derivative = LayerSemanticsParamType(
   nntrainer::createLayer<nntrainer::ConstantDerivativeLossLayer>,
   nntrainer::ConstantDerivativeLossLayer::type, {},
@@ -48,4 +53,5 @@ GTEST_PARAMETER_TEST(LossCross, LayerSemantics,
                      ::testing::Values(semantic_loss_cross, semantic_loss_mse,
                                        semantic_loss_cross_softmax,
                                        semantic_loss_cross_sigmoid,
-                                       semantic_loss_constant_derivative));
+                                       semantic_loss_constant_derivative,
+                                       semantic_loss_kld));
