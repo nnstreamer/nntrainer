331e5a3e01e2a6d69225309fa40a8f10db1ab53e
Donghak PARK <donghak.park@samsung.com>
2024-10-16 13:50:47 +0900

    [UnitTest] Add integration_tests on unit test
    In the case of NNTrainer, there are many instances where one function works in response to another function, so we have created integration tests for unit testing.
    - There are exceptional cases depending on the activation when it comes to losses.
    - For instance with BN (Batch Normalization), you can compare accuracy after conducting several epochs of testing.
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	 [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Donghak PARK <donghak.park@samsung.com>

---
+++ test/unittest/integration_tests/integration_tests.cpp
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2024 Donghak Park <donghak.park@samsung.com>
+ *
+ * @file unittest_loss_crossentropy.cpp
+ * @date 16 Oct 2024
+ * @brief CrossEntropy loss Layer Test
+ * @see	https://github.com/nnstreamer/nntrainer
+ * @author Donghak Park <donghak.park@samsung.com>
+ * @bug No known bugs except for NYI items
+ */
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+  int result = -1;
+
+  try {
+    testing::InitGoogleTest(&argc, argv);
+  } catch (...) {
+    std::cerr << "Error during InitGoogleTest" << std::endl;
+    return 0;
+  }
+
+  try {
+    result = RUN_ALL_TESTS();
+  } catch (...) {
+    std::cerr << "Error during RUN_ALL_TESTS()" << std::endl;
+  }
+
+  return result;
+}
