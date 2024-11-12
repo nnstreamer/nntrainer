a75b1de8831d596336a8f14b93463a5cc657c7aa
lhw414 <dlgusdn0414@snu.ac.kr>
2024-11-13 01:04:16 +0900

    [lr] add linearlr scheduler
    Add LinearLR scheduler source code
    - make lr_scheduler_linear.h, lr_scheduler_linear.cpp
    - modify other files to enable the use of LinearLR scheduler
    
    **evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: lhw414 <dlgusdn0414@snu.ac.kr>

---
+++ nntrainer/optimizers/lr_scheduler_linear.h
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2024 Hyunwoo LEE <dlgusdn0414@snu.ac.kr>
+ *
+ * @file   lr_scheduler_linear.h
+ * @date   11 November 2024
+ * @brief  This is Linear Learning Rate Scheduler class
+ * @see    https://github.com/nnstreamer/nntrainer
+ * @author Hyunwoo LEE <dlgusdn0414@snu.ac.kr>
+ * @bug    No known bugs except for NYI items
+ *
+ */
+
+#ifndef __LEARNING_RATE_SCHEDULER_LINEAR__
+#define __LEARNING_RATE_SCHEDULER_LINEAR__
+#ifdef __cplusplus
+
+#include <string>
+
+#include <common_properties.h>
+#include <lr_scheduler.h>
+
+namespace nntrainer {
+
+/**
+ * @class   Linear Learning Rate Scheduler class
+ * @brief   class for Linear Learning Rate Schedulers
+ */
+class LinearLearningRateScheduler : public LearningRateScheduler {
+
+public:
+  /**
+   * @brief Construct a new Linear Learning Rate Scheduler object
+   */
+  LinearLearningRateScheduler();
+
+  /**
+   * @copydoc LearningRateScheduler::getLearningRate(size_t iteration) const
+   */
+  virtual double getLearningRate(size_t iteration) override;
+
+  /**
+   * @copydoc LearningRateScheduler::finalize()
+   */
+  virtual void finalize() override;
+
+  /**
+   * @copydoc LearningRateScheduler::exportTo(Exporter &exporter, const
+   * ml::train::ExportMethods& method)
+   */
+  void exportTo(Exporter &exporter,
+                const ml::train::ExportMethods &method) const override;
+
+  /**
+   * @copydoc LearningRateScheduler::setProperty(const std::vector<std::string>
+   * &values)
+   */
+  void setProperty(const std::vector<std::string> &values) override;
+
+  /**
+   * @copydoc LearningRateScheduler::getType() const
+   */
+  const std::string getType() const override {
+    return LinearLearningRateScheduler::type;
+  }
+
+  inline static const std::string type = "linear";
+
+private:
+  std::tuple<props::MaxLearningRate, props::MinLearningRate, props::DecaySteps>
+    lr_props;
+};
+
+} // namespace nntrainer
+
+#endif /* __cplusplus */
+#endif /* __LEARNING_RATE_SCHEDULER_LINEAR__ */
