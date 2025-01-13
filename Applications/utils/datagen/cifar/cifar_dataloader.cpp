6add803eff44a3e9bc22caed8df19f9048f92c57
Grzegorz Kisala <gkisala@gmail.com>
2025-01-13 17:27:04 +0100

    [Windows] Use portable version of strerror
    Replace using GNU specific function strerror_r with portable version std::strerror
    
    **Self-evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test:   [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Grzegorz Kisala <gkisala@gmail.com>

--- Applications/utils/datagen/cifar/cifar_dataloader.cpp
+++ Applications/utils/datagen/cifar/cifar_dataloader.cpp
@@ -137,11 +137,8 @@ void Cifar100DataLoader::next(float **input, float **label, bool *last) {
   /// @note below logic assumes a single input and the fine label is used
 
   auto fill_one_sample = [this](float *input_, float *label_, int index) {
-    const size_t error_buflen = 102;
-    char error_buf[error_buflen];
     NNTR_THROW_IF(!file.good(), std::invalid_argument)
-      << "file is not good, reason: "
-      << strerror_r(errno, error_buf, error_buflen);
+      << "file is not good, reason: " << std::strerror(errno);
     file.seekg(index * Cifar100DataLoader::SampleSize, std::ios_base::beg);
 
     uint8_t current_label;
