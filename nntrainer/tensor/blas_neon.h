e4e7c6be17ba1c51ecbff41502d986cd49a9e77a
skykongkong8 <ss.kong@samsung.com>
2025-01-21 20:11:19 +0900

    [ neon ] Implement copy_fp32_s16
    - load from f32, convert to s32, narrow to s16
    
    **Self evaluation:**
    1. Build test:     [X]Passed [ ]Failed [ ]Skipped
    2. Run test:     [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: skykongkong8 <ss.kong@samsung.com>

--- nntrainer/tensor/blas_neon.h
+++ nntrainer/tensor/blas_neon.h
@@ -100,6 +100,14 @@ void copy_s16_fp32(const unsigned int N, const int16_t *X, float *Y);
 /**
  * @brief     copy function : Y = X
  * @param[in] N number of elements in X
+ * @param[in] X float * for Vector X
+ * @param[in] Y int16_t * for Vector Y
+ */
+void copy_fp32_s16(const unsigned int N, const float *X, int16_t *Y);
+
+/**
+ * @brief     copy function : Y = X
+ * @param[in] N number of elements in X
  * @param[in] X int16_t * for Vector X
  * @param[in] Y int16_t * for Vector Y
  */
