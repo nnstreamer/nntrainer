5960b578e7e38b0170bcd6ff7de2710a664cca40
jijoong.moon <jijoong.moon@samsung.com>
2025-02-08 12:33:17 +0900

    [ Model ] add Model Property for mixed type
    Add QINT8-UINT16, UNIT16-UINT16 type for Weight and Activation
    computation type.
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	 [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: jijoong.moon <jijoong.moon@samsung.com>

--- nntrainer/models/model_common_properties.h
+++ nntrainer/models/model_common_properties.h
@@ -192,15 +192,20 @@ struct ModelTensorDataTypeInfo {
     W16A16,
     W16A32,
     W32A16,
-    W32A32
+    W32A32,
+    WQ16AQ16,
+    WU16AU16,
+    W8AU16
   };
   static constexpr std::initializer_list<Enum> EnumList = {
-    Enum::W3A32,  Enum::W4A16,  Enum::W4A32,  Enum::W8A16, Enum::W8A32,
-    Enum::W16A16, Enum::W16A32, Enum::W32A16, Enum::W32A32};
+    Enum::W3A32,  Enum::W4A16,    Enum::W4A32,    Enum::W8A16,
+    Enum::W8A32,  Enum::W16A16,   Enum::W16A32,   Enum::W32A16,
+    Enum::W32A32, Enum::WQ16AQ16, Enum::WU16AU16, Enum::W8AU16};
 
   static constexpr const char *EnumStr[] = {
-    "BCQ-FP32",  "QINT4-FP16", "QINT4-FP32", "QINT8-FP16", "QINT8-FP32",
-    "FP16-FP16", "FP16-FP32",  "FP32-FP16",  "FP32-FP32"};
+    "BCQ-FP32",   "QINT4-FP16",    "QINT4-FP32",    "QINT8-FP16",
+    "QINT8-FP32", "FP16-FP16",     "FP16-FP32",     "FP32-FP16",
+    "FP32-FP32",  "QINT16-QINT16", "UINT16-UINT16", "QINT8-UINT16"};
 };
 
 /**
