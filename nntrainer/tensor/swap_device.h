21e80c417ef5395cef3221ec23e8a09a377da797
Donghak PARK <donghak.park@samsung.com>
2025-02-25 15:28:03 +0900

    [FSU] Update swap device & cache element
    Add memory ptr for allocate shared mem
    - add mem_ptr
    - add unmap - array for manage unmapped ptr
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	 [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Donghak PARK <donghak.park@samsung.com>

--- nntrainer/tensor/swap_device.h
+++ nntrainer/tensor/swap_device.h
@@ -17,6 +17,7 @@
 #include <fcntl.h>
 #include <map>
 #include <memory>
+#include <nntrainer_error.h>
 #include <string>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -87,7 +88,8 @@ public:
    * @return The pointer of the swap space
    *
    */
-  void *getBuffer(off_t offset, size_t size, bool alloc_only = false);
+  void *getBuffer(off_t offset, size_t size, void *memory_ptr,
+                  bool alloc_only = false);
 
   /**
    * @brief Deallocate and put data
@@ -126,6 +128,31 @@ public:
    */
   unsigned int getNumLoadedTensors();
 
+  /**
+   * @brief Check if address is unmapped or not.
+   *
+   * @param address
+   * @return bool true if address is unmapped, otherwise false.
+   */
+  bool address_unmapped(void *address) {
+    NNTR_THROW_IF(is_unmapped.find(address) == is_unmapped.end(),
+                  std::runtime_error)
+      << "Couldn't find address";
+    return is_unmapped[address];
+  }
+
+  /**
+   * @brief Set address as unmapped.
+   *
+   * @param address
+   */
+  void set_unmapped(void *address) {
+    NNTR_THROW_IF(is_unmapped.find(address) == is_unmapped.end(),
+                  std::runtime_error)
+      << "Couldn't find address";
+    is_unmapped[address] = true;
+  }
+
 private:
   const std::string dev_path; /**< device path */
   int fd;                     /**< device file description */
@@ -134,6 +161,7 @@ private:
 #ifdef USE_MMAP
   std::map<void *, std::tuple<void *, size_t, off_t, ssize_t>>
     mapped; /**< <pointer, <orig_pointer, size, offset, origianl size>> */
+  std::map<void *, bool> is_unmapped;
 #else
   std::map<void *, std::pair<off_t, ssize_t>>
     allocated; /**< <pointer, <offset, size>> */
