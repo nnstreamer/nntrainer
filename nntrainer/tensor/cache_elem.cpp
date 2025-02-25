405959fe4befa6c930837b8aed772d09ab516ffa
Donghak PARK <donghak.park@samsung.com>
2025-02-25 15:28:03 +0900

    [FSU] Update swap device & cache element
    Add memory ptr for allocate shared mem
    - add mem_ptr
    - add unmap - array for manage unmapped ptr
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	 [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Donghak PARK <donghak.park@samsung.com>

--- nntrainer/tensor/cache_elem.cpp
+++ nntrainer/tensor/cache_elem.cpp
@@ -54,7 +54,7 @@ void CacheElem::swapIn(Options opt) {
 
   opt = static_cast<Options>(opt | initial_opt);
   bool alloc_only = checkAllocOnly(policy, opt);
-  void *buf = device->getBuffer(offset, length, alloc_only);
+  void *buf = device->getBuffer(offset, length, memory_ptr, alloc_only);
 
   initial_opt = static_cast<Options>(initial_opt & ~Options::FIRST_ACCESS);
   mem_data->setAddr((void *)buf);
