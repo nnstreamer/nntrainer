df0a3666b249261e233397fb8c1f73f47951d98e
Donghak PARK <donghak.park@samsung.com>
2025-03-10 17:13:25 +0900

    [FSU] aligned_alloc for MMAP PAGE Aligne
    Allocate Memory space using Aligned_alloc
    - MMAP with FIXED options need aligned address & Page size read
    - Add AllocateFSU function to allocate as aligned mem address
    - Fix some application
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	 [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Donghak PARK <donghak.park@samsung.com>

--- nntrainer/tensor/cache_elem.cpp
+++ nntrainer/tensor/cache_elem.cpp
@@ -75,11 +75,7 @@ void CacheElem::swapOut(Options opt) {
   void *buf = (void *)mem_data->getAddr();
 
   initial_opt = static_cast<Options>(initial_opt & ~Options::FIRST_WRITE);
-  auto is_unmapped = device->address_unmapped(buf);
-  if (is_unmapped == false) {
-    device->putBuffer(buf, dealloc_only);
-    device->set_unmapped(buf);
-  }
+  device->putBuffer(buf, dealloc_only);
   mem_data->setAddr(nullptr);
   mem_data->setValid(false);
   active = false;
