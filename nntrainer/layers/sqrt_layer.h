4bd6d39f29318c2d50f4b8649e8373f7d3b61910
Seungbaek Hong <sb92.hong@samsung.com>
2025-03-18 20:48:29 +0900

    [layer] add sqrt layer structure
    implemented the structure of the sqrt layer excluding
    the forwarding and backwarding implementations.
    
    **Self evaluation:**
    1. Build test: [*]Passed [ ]Failed [ ]Skipped
    2. Run test: [ ]Passed [ ]Failed [*]Skipped
    
    Signed-off-by: Seungbaek Hong <sb92.hong@samsung.com>

---
+++ nntrainer/layers/sqrt_layer.h
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2025 SeungBaek Hong <sb92.hong@samsung.com>
+ *
+ * @file   sqrt_layer.h
+ * @date   18 March 2025
+ * @see    https://github.com/nnstreamer/nntrainer
+ * @author SeungBaek Hong <sb92.hong@samsung.com>
+ * @bug    No known bugs except for NYI items
+ * @brief  This is sqrt layer class (operation layer)
+ *
+ */
+
+#ifndef __SQRT_LAYER_H__
+#define __SQRT_LAYER_H__
+#ifdef __cplusplus
+
+#include <common_properties.h>
+#include <layer_devel.h>
+#include <operation_layer.h>
+
+namespace nntrainer {
+
+/**
+ * @class SQRT Layer
+ * @brief SQRT Layer
+ */
+class SQRTLayer : public UnaryOperationLayer {
+public:
+  /**
+   * @brief Constructor of SQRT Layer
+   */
+  SQRTLayer() :
+    UnaryOperationLayer(),
+    sqrt_props(props::Print(), props::InPlaceProp()),
+    support_backwarding(true) {}
+
+  /**
+   * @brief Destructor of SQRT Layer
+   */
+  ~SQRTLayer(){};
+
+  /**
+   *  @brief  Move constructor of SQRT Layer.
+   *  @param[in] SQRTLayer &&
+   */
+  SQRTLayer(SQRTLayer &&rhs) noexcept = default;
+
+  /**
+   * @brief  Move assignment operator.
+   * @parma[in] rhs SQRTLayer to be moved.
+   */
+  SQRTLayer &operator=(SQRTLayer &&rhs) = default;
+
+  /**
+   * @copydoc Layer::finalize(InitLayerContext &context)
+   */
+  void finalize(InitLayerContext &context) final;
+
+  /**
+   * @brief forwarding operation for SQRT
+   *
+   * @param input input tensor
+   * @param hidden tensor to store the result value
+   */
+  void forwarding_operation(const Tensor &input, Tensor &hidden) final;
+
+  /**
+   * @copydoc Layer::calcDerivative(RunLayerContext &context)
+   */
+  void calcDerivative(RunLayerContext &context) final;
+
+  /**
+   * @copydoc bool supportBackwarding() const
+   */
+  bool supportBackwarding() const final { return support_backwarding; };
+
+  /**
+   * @brief Initialize the in-place settings of the layer
+   * @return InPlaceType
+   */
+  InPlaceType initializeInPlace() final {
+    if (std::get<props::InPlaceProp>(sqrt_props).empty() ||
+        !std::get<props::InPlaceProp>(sqrt_props).get()) {
+      is_inplace = false;
+      support_backwarding = true;
+    } else {
+      is_inplace = true;
+      support_backwarding = false;
+    }
+
+    if (!supportInPlace())
+      return InPlaceType::NONE;
+    else
+      return InPlaceType::NON_RESTRICTING;
+  }
+
+  /**
+   * @copydoc Layer::exportTo(Exporter &exporter, ml::train::ExportMethods
+   * method)
+   */
+  void exportTo(Exporter &exporter,
+                const ml::train::ExportMethods &method) const final {}
+
+  /**
+   * @copydoc Layer::setProperty(const std::vector<std::string> &values)
+   */
+  void setProperty(const std::vector<std::string> &values) final;
+
+  /**
+   * @copydoc Layer::getType()
+   */
+  const std::string getType() const final { return SQRTLayer::type; };
+
+  std::tuple<props::Print, props::InPlaceProp> sqrt_props;
+  bool support_backwarding; /**< support backwarding */
+
+  inline static const std::string type = "sqrt";
+};
+
+} // namespace nntrainer
+
+#endif /* __cplusplus */
+#endif /* __SQRT_LAYER_H__ */
