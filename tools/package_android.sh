9bf7d6fc3b0e0ad98c8bd08f3ef8e4b0b9440f37
jijoong.moon <jijoong.moon@samsung.com>
2025-03-25 12:07:21 +0900

    [ Android ] update to support additional argument
    This pr enables to support additional arguments for android in build
    script.
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	 [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: jijoong.moon <jijoong.moon@samsung.com>

--- tools/package_android.sh
+++ tools/package_android.sh
@@ -6,25 +6,39 @@ TARGET=$1
 [ -z $1 ] && TARGET=$(pwd)
 echo $TARGET
 
+
 if [ ! -d $TARGET ]; then
-  echo $TARGET is not a directory. please put project root of nntrainer
-  exit 1
+    if [[ $1 == -D* ]]; then
+	TARGET=$(pwd)
+	echo $TARGET
+    else
+	echo $TARGET is not a directory. please put project root of nntrainer
+	exit 1
+    fi
 fi
 
 pushd $TARGET
+
+filtered_args=()
+
+for arg in "$@"; do
+    if [[ $arg == -D* ]]; then
+	filtered_args+=("$arg")
+    fi
+done
 
 if [ ! -d builddir ]; then
     #default value of openblas num threads is 1 for android
     #enable-tflite-interpreter=false is just temporally until ci system is stabel
     #enable-opencl=true will compile OpenCL related changes or remove this option to exclude OpenCL compilations.
-  meson builddir -Dplatform=android -Dopenblas-num-threads=1 -Denable-tflite-interpreter=false -Denable-tflite-backbone=false -Denable-fp16=true -Domp-num-threads=1 -Denable-opencl=true -Dhgemm-experimental-kernel=false
+  meson builddir -Dplatform=android -Dopenblas-num-threads=1 -Denable-tflite-interpreter=false -Denable-tflite-backbone=false -Denable-fp16=true -Domp-num-threads=1 -Denable-opencl=true -Dhgemm-experimental-kernel=false ${filtered_args[@]}
 else
   echo "warning: $TARGET/builddir has already been taken, this script tries to reconfigure and try building"
   pushd builddir
     #default value of openblas num threads is 1 for android
     #enable-tflite-interpreter=false is just temporally until ci system is stabel  
     #enable-opencl=true will compile OpenCL related changes or remove this option to exclude OpenCL compilations.
-    meson configure -Dplatform=android -Dopenblas-num-threads=1 -Denable-tflite-interpreter=false -Denable-tflite-backbone=false -Denable-fp16=true -Domp-num-threads=1 -Denable-opencl=true -Dhgemm-experimental-kernel=false
+    meson configure -Dplatform=android -Dopenblas-num-threads=1 -Denable-tflite-interpreter=false -Denable-tflite-backbone=false -Denable-fp16=true -Domp-num-threads=1 -Denable-opencl=true -Dhgemm-experimental-kernel=false ${filtered_args[@]}
     meson --wipe
   popd
 fi
