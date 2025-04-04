b399532c72c058535b2349ebe83a7bf42aaa27ce
skykongkong8 <ss.kong@samsung.com>
2025-04-04 13:18:00 +0900

    [ ggml ] Remove GGML_COMMON_DECL_C
    - For coherence, remove C-related files. Unify them with CPP.
    
    **Self evaluation:**
    1. Build test:     [X]Passed [ ]Failed [ ]Skipped
    2. Run test:     [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: skykongkong8 <ss.kong@samsung.com>

--- nntrainer/tensor/cpu_backend/ggml_interface/ggml_cpu_impl.h
+++ nntrainer/tensor/cpu_backend/ggml_interface/ggml_cpu_impl.h
@@ -50,6 +50,21 @@ void ggml_q4_K_8x8_q8_K_repack(const unsigned int M, const unsigned int N, void*
  */
 size_t ggml_quantize_q4_K(const float * src, void * dst, int64_t nrow, int64_t n_per_row, const float * quant_weights);
 
+/**
+ * @brief 
+ * 
+ * @param x_raw 
+ * @param y 
+ * @param k 
+ */
 void ggml_dequantize_row_q4_K(const void * x_raw, float * y, int64_t k);
+
+/**
+ * @brief 
+ * 
+ * @param x 
+ * @param y 
+ * @param k 
+ */
 void ggml_dequantize_row_q8_K(const void * x, float * y, int64_t k);
 
