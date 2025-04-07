fedc8a112206a1c28bd03242f63a639e45cec78a
Seungbaek Hong <sb92.hong@samsung.com>
2025-04-07 15:16:12 +0900

    [layer] add cast layer
    added cast layer (type casting).
    
    - there is no model-unittest yet.
    - In some cases, optimization may be considered (such as fusing only one
    layer in front), but for now, only the basic implementation is included.
    optimization will be applied through ONNX graph optimization work,
    later.
    
    **Self evaluation:**
    1. Build test: [*]Passed [ ]Failed [ ]Skipped
    2. Run test: [ ]Passed [ ]Failed [*]Skipped
    
    Signed-off-by: Seungbaek Hong <sb92.hong@samsung.com>

---
+++ test/unittest/layers/unittest_layers_cast.cpp
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2025 SeungBaek Hong <sb92.hong@samsung.com>
+ *
+ * @file unittest_layers_cast.cpp
+ * @date 7 April 2025
+ * @brief Cast Layer Test
+ * @see	https://github.com/nnstreamer/nntrainer
+ * @author SeungBaek Hong <sb92.hong@samsung.com>
+ * @bug No known bugs except for NYI items
+ */
+#include <tuple>
+
+#include <gtest/gtest.h>
+
+#include <cast_layer.h>
+#include <layers_common_tests.h>
+
+auto semantic_cast = LayerSemanticsParamType(
+  nntrainer::createLayer<nntrainer::CastLayer>, nntrainer::CastLayer::type,
+  {"tensor_dtype=FP16"},
+  LayerCreateSetPropertyOptions::AVAILABLE_FROM_APP_CONTEXT, false, 2);
+
+auto semantic_cast_multi = LayerSemanticsParamType(
+  nntrainer::createLayer<nntrainer::CastLayer>, nntrainer::CastLayer::type,
+  {"tensor_dtype=FP16"},
+  LayerCreateSetPropertyOptions::AVAILABLE_FROM_APP_CONTEXT, false, 2);
+
+GTEST_PARAMETER_TEST(Divide, LayerSemantics,
+                     ::testing::Values(semantic_cast, semantic_cast_multi));
