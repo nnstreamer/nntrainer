89f5f03ccbe565dea5168119436c5a97cd047676
Maciej Nalewaj <m.nalewaj@samsung.com>
2025-04-15 16:10:20 +0200

    Add draft code - implementation gemm_q4_K using ggml - not working - only it works on floats
    Signed-off-by: Maciej Nalewaj <m.nalewaj@samsung.com>

--- nntrainer/tensor/cpu_backend/ggml_interface/ggml_interface.h
+++ nntrainer/tensor/cpu_backend/ggml_interface/ggml_interface.h
@@ -14,7 +14,47 @@
 #define __GGML_INTERFACE_H__
 #ifdef __cplusplus
 
-namespace nntrainer {}
+#include <stdint.h>
+#include <stdlib.h>
+
+
+namespace nntrainer {
+
+ /**
+  * @brief Quantize float to q4_K Quantization format
+  * 
+  * @param src 
+  * @param dst 
+  * @param nrow 
+  * @param n_per_row 
+  * @param quant_weights 
+  * @return size_t 
+  */
+ size_t nntr_quantize_q4_K(const float * src, void * dst, int64_t nrow, int64_t n_per_row, const float * quant_weights);
+
+/**
+ * @brief 
+ * 
+ * @param M 
+ * @param N 
+ * @param K 
+ * @param A 
+ * @param lda 
+ * @param B 
+ * @param ldb 
+ * @param C 
+ * @param ldc 
+ */
+void nntr_q4_K_8x8_q8_K_GEMM(const unsigned int M, const unsigned int N, const unsigned int K,
+    const float *A, const unsigned int lda, const void *B,
+    const unsigned int ldb, float *C, const unsigned int ldc);
+
+void nntr_dequantize_row_q4_K(const void * x_raw, float * y, int64_t k);
+void nntr_dequantize_row_q8_K(const void * x, float * y, int64_t k);
+
+void nntr_repack_q4_K_to_q8_K(void* W, void* repacked_W, size_t data_size, const unsigned int M, const unsigned int N);
+
+}
 
 #endif
 #endif
