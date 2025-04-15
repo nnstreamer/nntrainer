ecde98f5a7574a3bd586f3c7689bba0e5afd086b
mwlasiuk <testmailsmtp12345@gmail.com>
2025-04-15 13:55:41 +0200

    Add doxygen
    * Added doxygen author comment to new files
    
    Signed-off-by: mwlasiuk <testmailsmtp12345@gmail.com>

--- nntrainer/tensor/cpu_backend/ggml_interface/ggml_interface.cpp
+++ nntrainer/tensor/cpu_backend/ggml_interface/ggml_interface.cpp
@@ -1,3 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2025 Michal Wlasiuk <testmailsmtp12345@gmail.com>
+ *
+ * @file   ggml_interface.h
+ * @date   15 April 2025
+ * @see    https://github.com/nnstreamer/nntrainer
+ * @author Michal Wlasiuk <testmailsmtp12345@gmail.com>
+ * @bug    No known bugs except for NYI items
+ * @brief  Function interface to use ggml lib from cpu_backend
+ */
+
 #include <ggml.h>
 #include <ggml_interface.h>
 
--- nntrainer/tensor/cpu_backend/ggml_interface/ggml_interface.h
+++ nntrainer/tensor/cpu_backend/ggml_interface/ggml_interface.h
@@ -1,3 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2025 Michal Wlasiuk <testmailsmtp12345@gmail.com>
+ *
+ * @file   ggml_interface.h
+ * @date   15 April 2025
+ * @see    https://github.com/nnstreamer/nntrainer
+ * @author Michal Wlasiuk <testmailsmtp12345@gmail.com>
+ * @bug    No known bugs except for NYI items
+ * @brief  Function interface to use ggml lib from cpu_backend
+ */
+
 #ifndef __GGML_INTERFACE_H__
 #define __GGML_INTERFACE_H__
 #ifdef __cplusplus
