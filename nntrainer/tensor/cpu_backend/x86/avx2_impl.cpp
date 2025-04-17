b6c8ba2995f65fa4fbac4ab1c23c9d609777f316
Piotr Rak <piotr.rak@samsung.com>
2025-04-17 13:48:00 +0200

    fix negation of exp(-x)
    Signed-off-by: Piotr Rak <piotr.rak@samsung.com>

--- nntrainer/tensor/cpu_backend/x86/avx2_impl.cpp
+++ nntrainer/tensor/cpu_backend/x86/avx2_impl.cpp
@@ -175,7 +175,7 @@ inline auto avx2_approx_exp_malossi_p7(__m256 xs) noexcept -> __m256
 [[gnu::always_inline, msvc::forceinline]]
 auto avx2_approx_swiglu(__m256 x, __m256 s) noexcept -> __m256
 {
-  auto neg_x = _mm256_xor_ps(x, _mm256_set1_ps(-1.0f));
+  auto neg_x = _mm256_sub_ps(_mm256_setzero_ps(), x);
   auto inv_sigmoid = _mm256_add_ps(avx2_approx_exp_malossi_p7(neg_x), _mm256_set1_ps(1.0f));
   auto swiglu_nonscaled = _mm256_div_ps(x, inv_sigmoid);
   return _mm256_mul_ps(swiglu_nonscaled, s);
