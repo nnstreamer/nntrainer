c9cc61583b294a9814cbd634d44cfffb69d544b4
Maciej Nalewaj <m.nalewaj@samsung.com>
2025-04-30 16:55:04 +0200

    [ANDROID] Add ggml subproject to Android build
    - Add enable-ggml=true flag to Android build
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Maciej Nalewaj <m.nalewaj@samsung.com>

---
+++ jni/prepare_ggml.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+set -e
+GGML_SRC=$1
+
+REVISION=489716ba99ecd51164f79e8c6fec0b5bf634eac9
+PATCH_PATH="${GGML_SRC}"/../packagefiles/ggml/0001-Export-some-internal-methods.patch
+
+echo "[GGML] PREPARING GGML..."
+
+pushd "${GGML_SRC}"
+git checkout "${REVISION}"
+git restore include
+git restore src
+git apply "${PATCH_PATH}"
+cp ./src/ggml-cpu/ggml-cpu.c ./src/ggml-cpu/ggml-cpu_c.c
+popd
+
+echo "[GGML] PREPARING GGML FINISHED"
