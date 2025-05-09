33ebc7abe96cca6a5c33183c5914718d78842d18
Donghyeon Jeong <dhyeon.jeong@samsung.com>
2025-05-09 15:59:55 +0900

    [bugfix] Android build error
    This PR resolves the Android build error when compiling the tensor unit test. This patch also changes the Android package script to enable OpenCL.
    
    **Self-evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test:   [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Donghyeon Jeong <dhyeon.jeong@samsung.com>

--- tools/package_android.sh
+++ tools/package_android.sh
@@ -31,14 +31,14 @@ if [ ! -d builddir ]; then
     #default value of openblas num threads is 1 for android
     #enable-tflite-interpreter=false is just temporally until ci system is stabel
     #enable-opencl=true will compile OpenCL related changes or remove this option to exclude OpenCL compilations.
-  meson builddir -Dplatform=android -Dopenblas-num-threads=1 -Denable-tflite-interpreter=false -Denable-tflite-backbone=false -Denable-fp16=true -Domp-num-threads=1 -Denable-opencl=false -Dhgemm-experimental-kernel=false ${filtered_args[@]}
+  meson builddir -Dplatform=android -Dopenblas-num-threads=1 -Denable-tflite-interpreter=false -Denable-tflite-backbone=false -Denable-fp16=true -Domp-num-threads=1 -Denable-opencl=true -Dhgemm-experimental-kernel=false ${filtered_args[@]}
 else
   echo "warning: $TARGET/builddir has already been taken, this script tries to reconfigure and try building"
   pushd builddir
     #default value of openblas num threads is 1 for android
     #enable-tflite-interpreter=false is just temporally until ci system is stabel  
     #enable-opencl=true will compile OpenCL related changes or remove this option to exclude OpenCL compilations.
-    meson configure -Dplatform=android -Dopenblas-num-threads=1 -Denable-tflite-interpreter=false -Denable-tflite-backbone=false -Denable-fp16=true -Domp-num-threads=1 -Denable-opencl=false -Dhgemm-experimental-kernel=false ${filtered_args[@]}
+    meson configure -Dplatform=android -Dopenblas-num-threads=1 -Denable-tflite-interpreter=false -Denable-tflite-backbone=false -Denable-fp16=true -Domp-num-threads=1 -Denable-opencl=true -Dhgemm-experimental-kernel=false ${filtered_args[@]}
     meson --wipe
   popd
 fi
