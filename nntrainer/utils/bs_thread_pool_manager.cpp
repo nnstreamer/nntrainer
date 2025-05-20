267cd0618b27094b4ac508c3e599f8e6970531b4
skykongkong8 <ss.kong@samsung.com>
2025-05-20 10:56:53 +0900

    [ utils ] Add bs threadpool manager
    - Before : Use bs threadpool from tensor
        -> This makes a cyclic dependency when trying to use threadpool from cpu_backend level.
    - After : Use through bs_thread_pool_manager to not only instantiate
    threadpool only once, but also use from distinctive nntrainer structure
    levels without tangled dependency.
    
    **Self evaluation:**
    1. Build test:     [X]Passed [ ]Failed [ ]Skipped
    2. Run test:     [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: skykongkong8 <ss.kong@samsung.com>

---
+++ nntrainer/utils/bs_thread_pool_manager.cpp
@@ -0,0 +1,10 @@
+#ifndef THREAD_POOL_MANAGER_CPP
+#define THREAD_POOL_MANAGER_CPP
+
+#include "bs_thread_pool_manager.hpp"
+
+namespace nntrainer {
+BS::thread_pool<> ThreadPoolManager::pool(std::thread::hardware_concurrency());
+}
+
+#endif // THREAD_POOL_MANAGER_CPP
---
+++ nntrainer/utils/bs_thread_pool_manager.hpp
@@ -0,0 +1,28 @@
+#ifndef THREAD_POOL_MANAGER_HPP
+#define THREAD_POOL_MANAGER_HPP
+
+#pragma once
+#include "bs_thread_pool.h"
+
+namespace nntrainer {
+class ThreadPoolManager {
+protected:
+  static BS::thread_pool<> pool;
+
+public:
+  // Delete copy and move constructors and assignment operators
+  ThreadPoolManager(const ThreadPoolManager &) = delete;
+  ThreadPoolManager &operator=(const ThreadPoolManager &) = delete;
+  ThreadPoolManager(ThreadPoolManager &&) = delete;
+  ThreadPoolManager &operator=(ThreadPoolManager &&) = delete;
+
+  // Static method to access the single instance
+  static BS::thread_pool<> &getInstance() { return pool; }
+
+private:
+  ThreadPoolManager() = default;
+  ~ThreadPoolManager() = default;
+};
+} // namespace nntrainer
+
+#endif // THREAD_POOL_MANAGER_HPP
