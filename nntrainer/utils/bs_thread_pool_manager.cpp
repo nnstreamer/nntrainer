2964597a175b5818f6ad3079b5254c2306fc3500
skykongkong8 <ss.kong@samsung.com>
2025-05-20 11:22:36 +0900

    [ doxygen ] Add doxygen tag for BSThreadPoolManager class
    **Self evaluation:**
    1. Build test:     [X]Passed [ ]Failed [ ]Skipped
    2. Run test:     [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: skykongkong8 <ss.kong@samsung.com>

--- nntrainer/utils/bs_thread_pool_manager.cpp
+++ nntrainer/utils/bs_thread_pool_manager.cpp
@@ -1,10 +1,27 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2025 Sungsik Kong <ss.kong@samsung.com>
+ *
+ * @file   bs_threadpool_manager.cpp
+ * @date   20 May 2025
+ * @see    https://github.com/nnstreamer/nntrainer
+ * @author Sungsik Kong <ss.kong@samsung.com>
+ * @bug    No known bugs except for NYI items
+ * @brief  BS threadpool manager class source file
+ */
+
 #ifndef THREAD_POOL_MANAGER_CPP
 #define THREAD_POOL_MANAGER_CPP
 
 #include "bs_thread_pool_manager.hpp"
 
 namespace nntrainer {
+/**
+ * @brief Instantiate thread pool with the number of hardware concurrency.
+ *
+ * @return BS::thread_pool<>
+ */
 BS::thread_pool<> ThreadPoolManager::pool(std::thread::hardware_concurrency());
-}
+} // namespace nntrainer
 
 #endif // THREAD_POOL_MANAGER_CPP
--- nntrainer/utils/bs_thread_pool_manager.hpp
+++ nntrainer/utils/bs_thread_pool_manager.hpp
@@ -1,3 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2025 Sungsik Kong <ss.kong@samsung.com>
+ *
+ * @file   bs_threadpool_manager.hpp
+ * @date   20 May 2025
+ * @see    https://github.com/nnstreamer/nntrainer
+ * @author Sungsik Kong <ss.kong@samsung.com>
+ * @bug    No known bugs except for NYI items
+ * @brief  BS threadpool manager class header file
+ */
+
 #ifndef THREAD_POOL_MANAGER_HPP
 #define THREAD_POOL_MANAGER_HPP
 
@@ -5,22 +17,45 @@
 #include "bs_thread_pool.h"
 
 namespace nntrainer {
+/**
+ * @brief ThreadPoolManager is a singleton class that manages a thread pool
+ *
+ */
 class ThreadPoolManager {
 protected:
   static BS::thread_pool<> pool;
 
 public:
   // Delete copy and move constructors and assignment operators
+  /**
+   * @brief Construct a new Thread Pool Manager object
+   *
+   */
   ThreadPoolManager(const ThreadPoolManager &) = delete;
-  ThreadPoolManager &operator=(const ThreadPoolManager &) = delete;
-  ThreadPoolManager(ThreadPoolManager &&) = delete;
-  ThreadPoolManager &operator=(ThreadPoolManager &&) = delete;
 
-  // Static method to access the single instance
+  /**
+   * @brief Construct a new Thread Pool Manager object
+   *
+   */
+  ThreadPoolManager(ThreadPoolManager &&) = delete;
+
+  /**
+   * @brief Static method to access the single instance
+   *
+   * @return BS::thread_pool<>&
+   */
   static BS::thread_pool<> &getInstance() { return pool; }
 
 private:
+  /**
+   * @brief Construct a new Thread Pool Manager object
+   *
+   */
   ThreadPoolManager() = default;
+  /**
+   * @brief Destroy the Thread Pool Manager object
+   *
+   */
   ~ThreadPoolManager() = default;
 };
 } // namespace nntrainer
