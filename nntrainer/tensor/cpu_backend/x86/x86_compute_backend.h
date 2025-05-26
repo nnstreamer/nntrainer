6f4ae81eba55662f255411183d787c5c539b6b7b
skykongkong8 <ss.kong@samsung.com>
2025-05-26 17:05:32 +0900

    [ q6K ] Add q6K function interface on cpu_backend
    - Add missing implemenation on fallback, and arm
    
    **Self evaluation:**
    1. Build test:     [X]Passed [ ]Failed [ ]Skipped
    2. Run test:     [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: skykongkong8 <ss.kong@samsung.com>

--- nntrainer/tensor/cpu_backend/x86/x86_compute_backend.h
+++ nntrainer/tensor/cpu_backend/x86/x86_compute_backend.h
@@ -803,6 +803,16 @@ size_t quantize_q4_0(const float *src, void *dst, int64_t nrow,
  */
 size_t quantize_q4_K(const float *src, void *dst, int64_t nrow,
                      int64_t n_per_row, const float *quant_weights);
+/**
+ * @brief 
+ * 
+ * @param src 
+ * @param dst 
+ * @param nrow 
+ * @param n_per_row 
+ * @param quant_weights 
+ * @return size_t 
+ */
 size_t quantize_q6_K(const float *src, void *dst, int64_t nrow,
                      int64_t n_per_row, const float *quant_weights);
 
