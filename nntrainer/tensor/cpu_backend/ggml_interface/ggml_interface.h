a2f4cb2a4e2b61dfce6fd21a9976ed017106e410
skykongkong8 <ss.kong@samsung.com>
2025-05-27 09:46:01 +0900

    [ trivial ] Apply clang-format for repeated C++ format checker CI fail
    **Self evaluation:**
    1. Build test:     [X]Passed [ ]Failed [ ]Skipped
    2. Run test:     [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: skykongkong8 <ss.kong@samsung.com>

--- nntrainer/tensor/cpu_backend/ggml_interface/ggml_interface.h
+++ nntrainer/tensor/cpu_backend/ggml_interface/ggml_interface.h
@@ -54,14 +54,14 @@ size_t __ggml_quantize_q4_0(const float *src, void *dst, int64_t nrow,
 size_t __ggml_quantize_q4_K(const float *src, void *dst, int64_t nrow,
                             int64_t n_per_row, const float *quant_weights);
 /**
- * @brief 
- * 
- * @param src 
- * @param dst 
- * @param nrow 
- * @param n_per_row 
- * @param quant_weights 
- * @return size_t 
+ * @brief
+ *
+ * @param src
+ * @param dst
+ * @param nrow
+ * @param n_per_row
+ * @param quant_weights
+ * @return size_t
  */
 size_t __ggml_quantize_q6_K(const float *src, void *dst, int64_t nrow,
                             int64_t n_per_row, const float *quant_weights);
