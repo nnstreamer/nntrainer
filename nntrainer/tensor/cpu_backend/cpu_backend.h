61a6ddfeaf14a0b590f9b6d0901e0cc45670d08c
mwlasiuk <testmailsmtp12345@gmail.com>
2025-05-28 11:24:02 +0200

    Fixing CI errors
    - format
    - fix return type
    - remove duplicates
    - format using clang-format 14
    - remove diplicates in ggml
    - remove duplicates from arm backend
    
    Signed-off-by: mwlasiuk <testmailsmtp12345@gmail.com>

--- nntrainer/tensor/cpu_backend/cpu_backend.h
+++ nntrainer/tensor/cpu_backend/cpu_backend.h
@@ -636,23 +636,7 @@ extern void gemm_q4_K(const unsigned int M, const unsigned int N,
                       const unsigned int K, const float *A,
                       const unsigned int lda, const void *B,
                       const unsigned int ldb, float *C, const unsigned int ldc);
-/**
- * @brief
- *
- * @param M
- * @param N
- * @param K
- * @param A
- * @param lda
- * @param B
- * @param ldb
- * @param C
- * @param ldc
- */
-extern void gemm_q6_K(const unsigned int M, const unsigned int N,
-                      const unsigned int K, const float *A,
-                      const unsigned int lda, const void *B,
-                      const unsigned int ldb, float *C, const unsigned int ldc);
+
 /**
  * @brief
  *
