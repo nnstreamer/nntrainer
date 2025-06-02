cbe7a9391d002d6c61eada8f1d4a6dc1958af6e4
Donghak PARK <donghak.park@samsung.com>
2025-06-02 18:00:40 +0900

    [Load] Load from FileOffset
    Load From FileOffset
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	 [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Donghak PARK <donghak.park@samsung.com>

--- nntrainer/models/neuralnet.cpp
+++ nntrainer/models/neuralnet.cpp
@@ -681,7 +681,6 @@ void NeuralNetwork::load(const std::string &file_path,
       size_t size = weight->getVariable().getMemoryBytes();
       auto tensor_data_type = weight->getDim().getDataType();
       weight->getVariableRef().setFileOffset(start_from);
-      weight->getVariable().getOffset();
       ///@todo instead of checking the data type,
       /// we may need to create a common parent class for
       /// quantized tensors, requiring qparam to be saved
