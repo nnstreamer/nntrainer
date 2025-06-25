49da48439c473cdebf6ea4dcb83cc0fbfabb6d59
mwlasiuk <testmailsmtp12345@gmail.com>
2025-06-25 14:39:45 +0200

    Add testcases - Q6-F32 testcases to search for fail point
    Signed-off-by: mwlasiuk <testmailsmtp12345@gmail.com>

--- nntrainer/cl_context.cpp
+++ nntrainer/cl_context.cpp
@@ -154,6 +154,8 @@ void ClContext::initBlasClKernels() {
   registerClKernel(getAdditionClKernel(), "addition_cl");
   registerClKernel(getSscalClKernel(), "sscal_cl");
   registerClKernel(getQ6KSgemvClKernel(), "kernel_mul_mv_q6_K_f32");
+  registerClKernel(getQ6KQ81SgemvClKernel(), "kernel_mul_mv_q6_K_q8_1");
+  registerClKernel(getQ4KQ81SgemvClKernel(), "kernel_mul_mv_q4_K_q8_1");
 
 #ifdef ENABLE_FP16
   registerClKernel(getHgemvClKernel(), "sgemv_cl_fp16");
