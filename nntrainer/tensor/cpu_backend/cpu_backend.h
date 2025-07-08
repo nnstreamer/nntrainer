83a38c81c9d6d5841397c4eab64a9b2403b3887e
Maciej Nalewaj <m.nalewaj@samsung.com>
2025-07-08 17:14:04 +0200

    Add backend operations for multi head attention layer
    **Self evaluation:**
    1. Build test:     [X]Passed [ ]Failed [ ]Skipped
    2. Run test:     [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Maciej Nalewaj <m.nalewaj@samsung.com>

--- nntrainer/tensor/cpu_backend/cpu_backend.h
+++ nntrainer/tensor/cpu_backend/cpu_backend.h
@@ -716,5 +716,77 @@ extern void dequantize_row_q6_K(const void *x, float *y, int64_t k);
  * @param k number of elements in x
  */
 extern void dequantize_row_q8_K(const void *x, float *y, int64_t k);
+
+/**
+ * @brief
+ * @param qk_out float * input/output values
+ * @param start_row start row number
+ * @param end_row end row number
+ * @param num_heads heads number
+ */
+extern void softmax_row_inplace(float *qk_out, size_t start_row, size_t end_row,
+                                size_t num_heads);
+
+/**
+ * @brief
+ * @param qk_out float * input/output values
+ * @param start_row start row number
+ * @param end_row end row number
+ * @param num_heads heads number
+ */
+extern void softmax_row(float *qk_out, size_t start_row, size_t end_row,
+                        size_t num_heads);
+
+/**
+ * @brief
+ * @param iter
+ * @param in
+ * @param vcache
+ * @param output
+ * @param seq
+ * @param num_cache_head
+ * @param gqa_size
+ * @param head_dim
+ * @param process_all
+ */
+extern void compute_fp16vcache_fp32_transposed(int iter, const float *in,
+                                               const uint16_t *vcache,
+                                               float *output, int seq,
+                                               int num_cache_head, int gqa_size,
+                                               int head_dim, bool process_all);
+
+/**
+ * @brief
+ * @tparam BType
+ * @param A
+ * @param B
+ * @param output
+ * @param num_rows
+ * @param N
+ * @param chunk_size
+ * @param group_size
+ * @param tile_size
+ */
+template <typename BType>
+void compute_kcaches(const float *A, const BType *B, float *output,
+                     int num_rows, int N, int chunk_size, int group_size,
+                     int tile_size);
+
+/**
+ * @brief
+ * @param width
+ * @param dim
+ * @param half_
+ * @param inout
+ * @param output
+ * @param cos_
+ * @param sin_
+ * @param only_convert_to_fp16
+ */
+void compute_rotary_emb_value(unsigned int width, unsigned int dim,
+                              unsigned int half_, float *inout, void *output,
+                              const float *cos_, const float *sin_,
+                              bool only_convert_to_fp16);
+
 #endif
 #endif
