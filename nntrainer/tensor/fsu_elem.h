ca44239e81679f29545fdc5b70de6ec73616823e
Donghak PARK <donghak.park@samsung.com>
2025-07-10 16:47:09 +0900

    [FSU][Refactoring] Implement fsu_elem.cpp
    - Implement fsu_elem.cpp
    - it support only FSU (Flash Storage Utilization)
    - it support Batch load of weight
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	 [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Donghak PARK <donghak.park@samsung.com>

--- nntrainer/tensor/fsu_elem.h
+++ nntrainer/tensor/fsu_elem.h
@@ -34,7 +34,11 @@ using ssize_t = std::make_signed_t<size_t>;
 
 namespace nntrainer {
 
-struct ElementInfo {
+/**
+ * @brief FSU Element information for process load
+ *
+ */
+typedef struct ElementInfo {
   unsigned int id;
   void *memory_ptr;
   bool active;
@@ -49,37 +53,98 @@ struct ElementInfo {
  */
 class FsuElem {
 public:
+  /**
+   * @brief FsuElem Constructor
+   */
   explicit FsuElem() : load_task_id(-1), unload_task_id(-1) {}
 
-   ~FsuElem() = default;
+  /**
+   * @brief FsuElem Destructor
+   */
+  ~FsuElem() = default;
 
-  // Cache Elem Functions
+  /**
+   * @brief fsuStart : open fd with given file_path
+   */
   void fsuStart();
+
+  /**
+   * @brief fsuFinish : close fd and release memory mapped buffer
+   */
   void fsuFinish();
 
+  /**
+   * @brief Load Weight from given wieght file path
+   *
+   * @param ids requested weight's id
+   */
   void loadFromFile(std::vector<unsigned int> ids);
 
+  /**
+   * @brief get element active status
+   *
+   * @param id weight id
+   * @return active status
+   */
   bool isActive(unsigned int id) { return elements[id].active; }
 
+  /**
+   * @brief set element's active to false
+   *
+   * @param id weight id
+   */
   void inActive(unsigned int id) { elements[id].active = false; }
 
+  /**
+   * @brief set LoadTaskID
+   *
+   * @param id weight id
+   */
   void setLoadTaskID(int id) { load_task_id = id; }
 
+  /**
+   * @brief get LoadTaskID
+   *
+   * @return LoadTaskID
+   */
   int getLoadTaskID() { return load_task_id; }
 
+  /**
+   * @brief set UnloadTaskID
+   *
+   * @param id weight id
+   */
   void setUnloadTaskID(int id) { unload_task_id = id; }
 
+  /**
+   * @brief get UnLoadTaskID
+   *
+   * @return unload_task_id
+   */
   int getUnloadTaskID() { return unload_task_id; }
 
-  // swap_device functions
+  /**
+   * @brief get weight_file_path
+   *
+   * @return weight_file_path
+   */
+  const std::string getWeightFilePath() const { return weight_file_path; }
 
+  /**
+   * @brief setWeightFilePath
+   *
+   * @param file_path file_path that contains weights to be loaded into memory
+   */
+  void setWeightFilePath(std::string file_path) {
+    weight_file_path = file_path;
+  }
 
- void getBuffer(std::vector<unsigned int> ids);
-
-  const std::string getDevicePath() const { return dev_path; }
-
-  void setDevicePath(std::string file_path) { dev_path = file_path; }
-
+  /**
+   * @brief set {start_off, weight_len} to elements[id]
+   *
+   * @param id weight id
+   * @param offset {start_off, weight_len}
+   */
   void setWeightOffset(unsigned int id,
                        const std::pair<size_t, size_t> &offset) {
     elements[id].start_offset = offset.first;
@@ -87,13 +152,12 @@ public:
   }
 
 private:
-  int load_task_id;
-  int unload_task_id;
-  std::unordered_map<unsigned int, ElementInfo> elements;
-
-  // Swap Device's private variable
-  std::string dev_path;
-  int fd;
+  int load_task_id;   /**< load task id */
+  int unload_task_id; /**< unload task id */
+  std::unordered_map<unsigned int, ElementInfo>
+    elements;                   /**< Element info dict */
+  std::string weight_file_path; /**< Weight file path */
+  int fd;                       /**< File fd */
 };
 } // namespace nntrainer
 #endif // CACHE_ELEM_V2_H
