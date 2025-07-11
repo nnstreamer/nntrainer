0eddfc7de367687ccdf03673d23245e7bf941bb2
Donghak PARK <donghak.park@samsung.com>
2025-07-11 16:40:53 +0900

    [FSU][Refactoring] Refactoring Inference & FSU Logic
    Refactoring swap to fsu logic (inference & fsu enable case)
    - integrating all function to fsu_weight_pool
    - remove fsu_elem.*
    - only for inference fsu logic
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	 [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Donghak PARK <donghak.park@samsung.com>

---
+++ nntrainer/tensor/fsu_weight_pool.h
@@ -0,0 +1,132 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2025 Donghak Park <donghak.park@samsung.com>
+ *
+ * @file   fsu_weight_pool.h
+ * @date   10 July 2025
+ * @see    https://github.com/nnstreamer/nntrainer
+ * @author Donghak Park <donghak.park@samsung.com>
+ * @bug    No known bugs except for NYI items
+ * @brief  FSU Weight Pool
+ *
+ */
+
+#ifndef FSU_WEIGHT_POOL_H
+#define FSU_WEIGHT_POOL_H
+
+#include <cache_loader.h>
+#include <task_executor.h>
+#include <memory_data.h>
+#include <memory_pool.h>
+
+#include <future>
+#include <memory>
+#include <mutex>
+#include <shared_mutex>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#if defined(_WIN32)
+#include "utils/mman_windows.h"
+#include <io.h>
+using ssize_t = std::make_signed_t<size_t>;
+#define O_SYNC 0UL
+#else
+#include <sys/mman.h>
+#include <unistd.h>
+#endif
+
+
+namespace nntrainer {
+/**
+ * @brief FSU Element information for process load
+ *
+ */
+struct ElementInfo {
+  unsigned int id{};
+  void *memory_ptr{};
+  bool active{};
+  size_t start_offset{};
+  size_t weight_len{};
+  std::shared_ptr<MemoryData> mem_data;
+  int load_task_id{};
+  LoadState load_state;
+};
+
+/**
+ * @class   FsuWeightPool
+ * @brief   Flash Storage Utilization Pool
+ */
+class FsuWeightPool : public MemoryPool {
+
+public:
+  using ExecIds = std::set<unsigned int>;
+
+  explicit FsuWeightPool();
+
+  ~FsuWeightPool() override;
+
+  void weightFileOpen();
+
+  void weightFileClose();
+
+  void allocate() override;
+
+  void deallocate() override;
+
+  void inActive(unsigned int order);
+
+  unsigned int requestMemory(
+    size_t bytes, unsigned int start_time, unsigned int end_time,
+    std::vector<unsigned int> exec_order = std::vector<unsigned int>(),
+    TensorLifespan lifespan = TensorLifespan::MAX_LIFESPAN,
+    bool is_wgrad = false) override;
+
+  std::shared_ptr<MemoryData> getMemory(unsigned int id) override;
+
+  void clear() override;
+
+  void loadTensor(unsigned int order);
+
+  void setFsuWeightPath(std::string path) override {weight_file_path = path};
+
+  /**
+   * @brief set {start_off, weight_len} to elements[id]
+
+   * @param id weight id
+   * @param offset {start_off, weight_len}
+   */
+  void addElement(unsigned int id, const std::pair<size_t, size_t> &offset,
+                  std::shared_ptr<MemoryData> data, void *ptr) {
+    /**
+      unsigned int id{};
+      void *memory_ptr{};
+      bool active{};
+      size_t start_offset{};
+      size_t weight_len{};
+      std::shared_ptr<MemoryData> mem_data;
+    */
+    elements[id] = {id, ptr, false, offset.first, offset.second, data};
+  }
+  bool checkAllLoadComplete(unsigned int order);
+  void loadFromFile(std::vector<unsigned int> ids);
+  bool loadAllinOrder(unsigned int order);
+
+protected:
+  void validate(unsigned int id);
+
+private:
+  std::string weight_file_path; /**< Weight file path */
+  int fd;                       /**< File fd */
+  std::unordered_map<unsigned int, ElementInfo>
+    elements;                         /**< Element info dict */
+  TaskExecutor *load_task_executor;   /**< task executor */
+  std::unordered_map<unsigned int, ExecIds> order_to_exec_ids;
+  std::vector<unsigned int> id_bank;
+  unsigned int batch_size;
+  std::mutex mod_mutex;
+  std::mutex state_mutex;
+};
+} // namespace nntrainer
+#endif // FSU_WEIGHT_POOL_H
