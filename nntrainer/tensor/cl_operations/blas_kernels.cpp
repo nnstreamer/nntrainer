7ee4494fbb0a42eb688580543c570dc7007b72ba
Donghyeon Jeong <dhyeon.jeong@samsung.com>
2025-07-17 15:40:45 +0900

    Fix M Non-Multiples of 4 in Q4_K GEMM and Adds Dynamic Quantization
    This pull request (PR) addresses an issue where the variable M is not a multiple of 4.
    Since the q4_K_8x8_q8_K GEMM operation requires M to be a multiple of 4, we need to use GEMV for the remainder when it is not.
    The current patch implements the CPU version of GEMV until the GPU kernel becomes available.
    Additionally, this patch introduces dynamic quantization for float input.
    
    **Self-evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test:   [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Donghyeon Jeong <dhyeon.jeong@samsung.com>

--- nntrainer/tensor/cl_operations/blas_kernels.cpp
+++ nntrainer/tensor/cl_operations/blas_kernels.cpp
@@ -15,6 +15,7 @@
 #include <blas_kernels.h>
 
 #include "clblast.h"
+#include "ggml.h"
 
 namespace nntrainer {
 
@@ -212,6 +213,37 @@ void sgemm_q4_k_cl(const unsigned int M, const unsigned int N,
     return;
   }
 
+  /// @note Dynamic quatization for matBdata
+  const size_t qk_k = 256;
+  const size_t sizeof_block_q8_K = 292;
+
+  const unsigned int blocks_per_4_rows = (K + qk_k - 1) / qk_k;
+  const unsigned int qb_4_rows_size = sizeof_block_q8_K * 4 * blocks_per_4_rows;
+  const size_t qb_row_size = (sizeof_block_q8_K * K) / qk_k;
+  const unsigned int qb_size = qb_4_rows_size * (((M >> 2) << 2) / 4 + 1);
+  const unsigned int M4 = ((M - M % 4) / 4);
+  const int B_step = 144 * (K / qk_k);
+
+  void *quantBdata = blas_cc->context_inst_.createSVMRegion(qb_size);
+
+  if (M == 1) {
+    nntrainer::quantize_row_q8_K((float *)matBdata, quantBdata, K);
+  } else {
+    for (int i = 0; i < static_cast<int>(M4); i++) {
+      ::ggml_quantize_mat_q8_K_4x8(
+        ((float *)matBdata) + 4 * i * K,
+        reinterpret_cast<char *>(quantBdata) + i * qb_4_rows_size, K);
+    }
+
+    for (unsigned int i = M4 * 4; i < M; i++) {
+      nntrainer::quantize_row_q8_K(((float *)matBdata) + i * K,
+                                   (reinterpret_cast<char *>(quantBdata) +
+                                    (M4 * qb_4_rows_size) +
+                                    (i - M4 * 4) * qb_row_size),
+                                   K);
+    }
+  }
+
   if (!kernel->SetKernelArguments(0, &K, sizeof(int))) {
     printf("Failed to set kernel argument 1 for mat_mul_q4_K_8x8_q8_K");
     return;
@@ -232,7 +264,7 @@ void sgemm_q4_k_cl(const unsigned int M, const unsigned int N,
     return;
   }
 
-  if (!kernel->SetKernelSVMArguments(4, matBdata)) {
+  if (!kernel->SetKernelSVMArguments(4, quantBdata)) {
     printf("Failed to set kernel SVM argument 3 for mat_mul_q4_K_8x8_q8_K");
     return;
   }
@@ -262,6 +294,32 @@ void sgemm_q4_k_cl(const unsigned int M, const unsigned int N,
     printf("Failed to map output buffer for mat_mul_q4_K_8x8_q8_K\n");
     return;
   }
+
+  /// @note Use CPU multithreaded GEMV for the leftover rows
+  int n_threads = 4;
+  for (unsigned int pb = M4 * 4; pb < M; pb++) {
+#pragma omp parallel for num_threads(n_threads)
+    for (int thread_idx = 0; thread_idx < n_threads; ++thread_idx) {
+      unsigned int M_step_start = (thread_idx * N) / n_threads;     // = 0
+      unsigned int M_step_end = ((thread_idx + 1) * N) / n_threads; // ne01 = N
+
+      M_step_start = (M_step_start % 8) ? M_step_start + 8 - (M_step_start % 8)
+                                        : M_step_start;
+      M_step_end =
+        (M_step_end % 8) ? M_step_end + 8 - (M_step_end % 8) : M_step_end;
+
+      ::ggml_gemv_q4_K_8x8_q8_K(
+        K,
+        (float *)((matCdata + ((pb - M4 * 4) * N) + (M4 * 4 * N)) +
+                  M_step_start),
+        N, (void *)((char *)matAdata + M_step_start * B_step),
+        (char *)quantBdata + (M4 * qb_4_rows_size) +
+          (pb - M4 * 4) * qb_row_size,
+        1, M_step_end - M_step_start);
+    }
+  }
+
+  blas_cc->context_inst_.releaseSVMRegion(quantBdata);
 }
 
 void sgemv_cl(const float *matAdata, const float *vecXdata, float *vecYdata,
