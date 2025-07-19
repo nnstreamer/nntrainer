8a2abf9f2b9a46d3de1e551005236d7bb19cb816
Donghak PARK <donghak.park@samsung.com>
2025-07-19 09:36:36 +0900

    [Export] Add dropout Realizer & rename realizer
    - Add dropout Realizer
    - Rename loss_realizer to tflite_export_realizer
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	 [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Donghak PARK <donghak.park@samsung.com>

--- nntrainer/compiler/tflite_export_realizer.h
+++ nntrainer/compiler/tflite_export_realizer.h
@@ -2,15 +2,16 @@
 /**
  * Copyright (C) 2022 seongwoo <mhs4670go@naver.com>
  *
- * @file loss_realizer.h
- * @date 4 May 2022
+ * @file tflite_export_realizer.h
+ * @date 18 July 2025
  * @brief NNTrainer graph realizer which remove loss layer for inference
  * @see	https://github.com/nnstreamer/nntrainer
  * @author seongwoo <mhs4670go@naver.com>
+ * @author donghak park <donghak.park@samsung.com>
  * @bug No known bugs except for NYI items
  */
-#ifndef __LOSS_REALIZER_H__
-#define __LOSS_REALIZER_H__
+#ifndef __TFLITE_EXPORT_REALIZER_H__
+#define __TFLITE_EXPORT_REALIZER_H__
 
 #include <vector>
 
@@ -23,19 +24,19 @@ namespace nntrainer {
  * @note This assumes the number of input / output connection of loss layer == 1
  *
  */
-class LossRealizer final : public GraphRealizer {
+class TfliteExportRealizer final : public GraphRealizer {
 public:
   /**
    * @brief Construct a new Loss Realizer object
    *
    */
-  LossRealizer() = default;
+  TfliteExportRealizer() = default;
 
   /**
    * @brief Destroy the Graph Realizer object
    *
    */
-  ~LossRealizer() = default;
+  ~TfliteExportRealizer() = default;
 
   /**
    * @brief graph realizer creates a shallow copied graph based on the reference
@@ -44,8 +45,17 @@ public:
    *
    */
   GraphRepresentation realize(const GraphRepresentation &reference) override;
+
+  /**
+     * @brief graph realizer creates a shallow copied graph based on the reference
+     * @note drop_out realizer removes drop_out layers from GraphRepresentation
+     * @param reference GraphRepresentation to be realized
+     *
+  */
+  GraphRepresentation realize_dropout(const GraphRepresentation &reference);
+
 };
 
 } // namespace nntrainer
 
-#endif // __LOSS_REALIZER_H__
+#endif // __TFLITE_EXPORT_REALIZER_H__
