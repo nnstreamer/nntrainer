81ac965de4fd682f138e6c1b4c9162aea49be6cf
skykongkong8 <ss.kong@samsung.com>
2025-07-22 17:12:23 +0900

    [ cblas ] Make an option to include cblas or not
    - In specific circumstances, it is better to build without cblas option.
    - trivial) Add option for ggml_interface.h to choose ggml related files or not.
    
    **Self evaluation:**
    1. Build test:     [X]Passed [ ]Failed [ ]Skipped
    2. Run test:     [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: skykongkong8 <ss.kong@samsung.com>

--- test/unittest/unittest_nntrainer_cpu_backend.cpp
+++ test/unittest/unittest_nntrainer_cpu_backend.cpp
@@ -272,9 +272,8 @@ float test_gemm_q4_0(const uint32_t M, const uint32_t K, const uint32_t N,
   // Step2. Repack Weight to q4_K_8x8 layout (This happens when you load the
   // model weights. It's a one-time operation)
   std::vector<char> q4_0_repacked_qWeight = std::vector<char>(q4_0_data_size);
-  nntrainer::repack_q4_0(q4_0_repacked_qWeight.data(),
-                                   q4_0_offline_qWeight_ptr, q4_0_data_size, N,
-                                   K);
+  nntrainer::repack_q4_0(q4_0_repacked_qWeight.data(), q4_0_offline_qWeight_ptr,
+                         q4_0_data_size, N, K);
 
   // Step3. Run GEMM! (Online activation quantization + kernel routine + return
   // float)
