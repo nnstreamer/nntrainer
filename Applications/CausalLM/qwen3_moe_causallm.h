c01392ddf6a090ff2ab852aaec394a076800fee3
Eunju Yang <ej.yang@samsung.com>
2025-07-22 11:08:44 +0900

    [CuasalLM] Qwen3-MoE is added
    - This patch adds Qwen3-MoE as an example of CausalLM
    - This patch include weight converter and nntr_config for Qwen3-moe
    
    Signed-off-by: Eunju Yang <ej.yang@samsung.com>

---
+++ Applications/CausalLM/qwen3_moe_causallm.h
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2025 Eunju Yang <ej.yang@samsung.com>
+ *
+ * @file   qwen3_moe_causallm.h
+ * @date   15 July 2025
+ * @see    https://github.com/nnstreamer/nntrainer
+ * @author Eunju Yang <ej.yang@samsung.com>
+ * @bug    No known bugs except for NYI items
+ * @note   Please refer to the following code :
+ *
+ */
+
+#ifndef __QWEN_MOE_CAUSAL_LM_H__
+#define __QWEN_MOE_CAUSAL_LM_H__
+
+#include <causal_lm.h>
+#include <qwen3_causallm.h>
+
+namespace causallm {
+
+class Qwen3MoECausalLM : public Qwen3CausalLM {
+
+public:
+  static constexpr const char *architectures = "Qwen3MoeForCausalLM";
+
+  Qwen3MoECausalLM(json &cfg, json &generation_cfg, json &nntr_cfg) :
+    Qwen3CausalLM(cfg, generation_cfg, nntr_cfg) {
+    setupParameters(cfg, generation_cfg, nntr_cfg);
+  }
+
+  ~Qwen3MoECausalLM() = default;
+
+  std::vector<LayerHandle> createMlp(const int layer_id, int dim,
+                                     int hidden_dim,
+                                     std::string input_name) override;
+
+  void setupParameters(json &cfg, json &generation_cfg,
+                       json &nntr_cfg) override;
+
+  void registerCustomLayers() override;
+
+private:
+  unsigned int NUM_EXPERTS;
+  unsigned int NUM_EXPERTS_PER_TOK;
+};
+}; // namespace causallm
+
+#endif /* __QWEN_MOE_CAUSAL_LM_H__ */
