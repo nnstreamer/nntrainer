24177991df345a48f7d9019f72e31b638970e6af
Eunju Yang <ej.yang@samsung.com>
2025-07-22 11:08:44 +0900

    [CuasalLM] Qwen3-MoE is added
    - This patch adds Qwen3-MoE as an example of CausalLM
    - This patch include weight converter and nntr_config for Qwen3-moe
    
    Signed-off-by: Eunju Yang <ej.yang@samsung.com>

---
+++ Applications/CausalLM/layers/qwen_moe_layer.cpp
@@ -0,0 +1,416 @@
+/**
+ * Copyright (C) 2020 Samsung Electronics Co., Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * @file	moe_layer.cpp
+ * @date	09 June 2025
+ * @brief	This is a Mixture of Expert Layer Class for Neural Network
+ * @see		https://github.com/nnstreamer/
+ * @author	Eunju Yang <ej.yang@samsung.com>
+ * @bug		No known bugs except for NYI items
+ *
+ */
+
+#include <acti_func.h>
+#include <cmath>
+#include <node_exporter.h>
+#include <omp.h>
+#include <qwen_moe_layer.h>
+#include <stdexcept>
+
+namespace causallm {
+
+static constexpr size_t SINGLE_INOUT_IDX = 0;
+
+MoELayer::MoELayer() :
+  LayerImpl(),
+  num_experts(0),
+  topk(0),
+  moe_props(props::NumExperts(), props::NumExpertsPerToken(),
+            nntrainer::props::Unit(), props::MoEActivation()),
+  expert_gate_proj_indices({}),
+  expert_up_proj_indices({}),
+  expert_down_proj_indices({}),
+  gate_idx(std::numeric_limits<unsigned>::max()),
+  router_logits_idx(std::numeric_limits<unsigned>::max()),
+  expert_mask_idx(std::numeric_limits<unsigned>::max()) {}
+
+void MoELayer::finalize(nntrainer::InitLayerContext &context) {
+
+  // 1. Validate input/output dimensions
+  NNTR_THROW_IF(context.getNumInputs() != 1, std::invalid_argument)
+    << "MoE layer only supports single input";
+
+  auto &weight_regularizer =
+    std::get<nntrainer::props::WeightRegularizer>(*layer_impl_props);
+  auto &weight_regularizer_constant =
+    std::get<nntrainer::props::WeightRegularizerConstant>(*layer_impl_props);
+  auto &weight_initializer =
+    std::get<nntrainer::props::WeightInitializer>(*layer_impl_props);
+  auto &weight_decay =
+    std::get<nntrainer::props::WeightDecay>(*layer_impl_props);
+
+  // 2. Set output dimensions (same as input)
+  const auto &in_dim = context.getInputDimensions()[SINGLE_INOUT_IDX];
+  const bool is_nchw = context.getFormat() == nntrainer::Tformat::NCHW;
+  std::vector<nntrainer::TensorDim> output_dims(1);
+  output_dims[SINGLE_INOUT_IDX] = in_dim;
+  context.setOutputDimensions(output_dims);
+
+  // 3. Get MoE properties
+  num_experts = std::get<props::NumExperts>(moe_props).get();
+  topk = std::get<props::NumExpertsPerToken>(moe_props).get();
+  const unsigned int intermediate_size =
+    std::get<nntrainer::props::Unit>(moe_props).get();
+  const unsigned int hidden_size = in_dim.width(); // Feature dimension
+
+  // activation function
+  if (std::get<props::MoEActivation>(moe_props).empty()) {
+    throw std::runtime_error("Activation type is not set for MoE layer");
+  }
+  switch (context.getActivationDataType()) {
+  case ml::train::TensorDim::DataType::FP32:
+    acti_func.setActiFunc<float>(
+      std::get<props::MoEActivation>(moe_props).get());
+    break;
+  default:
+    throw std::runtime_error("Unsupported activation data type for MoE layer");
+  }
+
+  // 4. Initialie gate layer (router)
+  nntrainer::TensorDim gate_dim(
+    1, is_nchw ? 1 : num_experts, is_nchw ? hidden_size : 1,
+    is_nchw ? num_experts : hidden_size,
+    nntrainer::TensorDim::TensorType(context.getFormat(),
+                                     context.getWeightDataType()),
+    is_nchw ? 0b0011 : 0b0101);
+
+  gate_idx = context.requestWeight(
+    gate_dim, weight_initializer, weight_regularizer,
+    weight_regularizer_constant, weight_decay, "gate", true);
+
+  // 5. Initializer expert weights
+  expert_gate_proj_indices.reserve(num_experts);
+  expert_up_proj_indices.reserve(num_experts);
+  expert_down_proj_indices.reserve(num_experts);
+
+  nntrainer::TensorDim expert_gate_dim(
+    1, is_nchw ? 1 : intermediate_size, is_nchw ? hidden_size : 1,
+    is_nchw ? intermediate_size : hidden_size,
+    nntrainer::TensorDim::TensorType(context.getFormat(),
+                                     context.getWeightDataType()),
+    is_nchw ? 0b0011 : 0b0101);
+
+  nntrainer::TensorDim expert_down_dim(
+    1, is_nchw ? 1 : hidden_size, is_nchw ? intermediate_size : 1,
+    is_nchw ? hidden_size : intermediate_size,
+    nntrainer::TensorDim::TensorType(context.getFormat(),
+                                     context.getWeightDataType()),
+    is_nchw ? 0b0011 : 0b0101);
+
+  for (unsigned int i = 0; i < num_experts; ++i) {
+    // Up projection
+    expert_up_proj_indices.push_back(context.requestWeight(
+      expert_gate_dim, // Same dimensions as gate projection
+      weight_initializer, weight_regularizer, weight_regularizer_constant,
+      weight_decay, "expert_up_" + std::to_string(i), false));
+
+    // Gate projection
+    expert_gate_proj_indices.push_back(context.requestWeight(
+      expert_gate_dim, weight_initializer, weight_regularizer,
+      weight_regularizer_constant, weight_decay,
+      "expert_gate_" + std::to_string(i), true));
+
+    // Down projection
+    expert_down_proj_indices.push_back(context.requestWeight(
+      expert_down_dim, weight_initializer, weight_regularizer,
+      weight_regularizer_constant, weight_decay,
+      "expert_down_" + std::to_string(i), false));
+  }
+
+  // 6. Request intermediate tensors
+  const unsigned batch_size = in_dim.batch();
+  const unsigned seq_len = in_dim.height();
+  const unsigned total_tokens = batch_size * seq_len;
+
+  // Router logits :  [batch * seq, num_experts]
+  router_logits_idx =
+    context.requestTensor({total_tokens, 1, 1, num_experts}, "router_logits",
+                          nntrainer::Initializer::NONE, false,
+                          nntrainer::TensorLifespan::FORWARD_FUNC_LIFESPAN);
+
+  // Expert mask: [num_experts, batch*seq]
+  expert_mask_idx =
+    context.requestTensor({num_experts, 1, topk, total_tokens}, "expert_mask",
+                          nntrainer::Initializer::ZEROS, false,
+                          nntrainer::TensorLifespan::FORWARD_FUNC_LIFESPAN);
+}
+
+void MoELayer::forwarding(nntrainer::RunLayerContext &context, bool training) {
+  nntrainer::Tensor &input = context.getInput(SINGLE_INOUT_IDX);
+  nntrainer::Tensor &output = context.getOutput(SINGLE_INOUT_IDX);
+
+  nntrainer::Tensor &router_logits = context.getTensor(router_logits_idx);
+  nntrainer::Tensor &expert_mask = context.getTensor(expert_mask_idx);
+
+  const unsigned batch_size = input.batch();
+  const unsigned seq_len = input.height();
+  const unsigned hidden_size = input.width();
+  const unsigned total_tokens = batch_size * seq_len;
+
+  // reshape input: [B,1,S,H] -> [B*S,1,1,H]
+  input.reshape({total_tokens, 1, 1, hidden_size});
+
+  // reshape output: [B,1,S,H] -> [B*S,1,1,H]
+  output.reshape({total_tokens, 1, 1, hidden_size});
+  output.setZero();
+
+  // routing
+  nntrainer::Tensor &gate_weights = context.getWeight(gate_idx);
+  input.dot(gate_weights, router_logits);
+  router_logits.apply(nntrainer::ActiFunc::softmax<float>, router_logits);
+  auto topk_result = router_logits.topK(topk);
+  auto topk_values = std::get<0>(topk_result);
+  auto topk_indices = std::get<1>(topk_result);
+
+  const uint32_t *indices_data = topk_indices.getData<uint32_t>();
+#pragma omp parallel for collapse(2)
+  for (int i = 0; i < static_cast<int>(total_tokens); ++i) {
+    for (int k = 0; k < static_cast<int>(topk); ++k) {
+      expert_mask.setValue(indices_data[i * topk + k], 0, k, i, 1.0f);
+    }
+  }
+
+// expert forwarding
+#pragma omp parallel
+  {
+#pragma omp for schedule(dynamic)
+    for (int expert_idx = 0; expert_idx < static_cast<int>(num_experts);
+         ++expert_idx) {
+      std::vector<unsigned> token_indices;
+      std::vector<float> topk_values_vector;
+
+      // get token indices for this expert
+      for (int i = 0; i < static_cast<int>(total_tokens); ++i) {
+        for (int k = 0; k < static_cast<int>(topk); ++k) {
+          if (expert_mask.getValue<float>(expert_idx, 0, k, i) > 0.5f) {
+            token_indices.push_back(i);
+            topk_values_vector.push_back(
+              topk_values.getValue<float>(i, 0, 0, k));
+          }
+        }
+      }
+      if (token_indices.empty())
+        continue;
+
+      // slicing and computing expert forward pass
+      nntrainer::Tensor expert_output = compute_expert_forward(
+        input.getBatchSlice(token_indices), topk_values_vector,
+        context.getWeight(expert_gate_proj_indices[expert_idx]),
+        context.getWeight(expert_up_proj_indices[expert_idx]),
+        context.getWeight(expert_down_proj_indices[expert_idx]));
+
+// acuumulate expert output into the main output tensor
+#pragma omp critical
+      {
+        for (int i = 0; i < static_cast<int>(token_indices.size()); ++i) {
+          unsigned idx = token_indices[i];
+          auto tgt_output = output.getBatchSlice(idx, 1);
+          // tgt_output.add_i(expert_output.getBatchSlice(i, 1));
+          tgt_output.copyData(expert_output.getBatchSlice(i, 1));
+        }
+      }
+    }
+  }
+
+  // reshape output: [B*S,1,1,H] -> [B,1,S,H]
+  output.reshape({batch_size, 1, seq_len, hidden_size});
+}
+
+inline nntrainer::Tensor MoELayer::compute_expert_forward(
+  const nntrainer::Tensor &input, const std::vector<float> &weights,
+  const nntrainer::Tensor &gate_proj, const nntrainer::Tensor &up_proj,
+  const nntrainer::Tensor &down_proj) {
+
+  const unsigned tokens = input.batch();
+  const unsigned hidden_size = input.width();
+  const unsigned intermediate_size = gate_proj.width();
+
+  // Gate projection: [tokens,1,1,H] x [H,I] -> [tokens,1,1,I]
+  nntrainer::Tensor gate_out(tokens, 1, 1, intermediate_size);
+  nntrainer::Tensor acti_out(tokens, 1, 1, intermediate_size);
+  input.dot(gate_proj, gate_out);
+
+  // Apply activation (silu)
+  ///@todo acti_func.run_fn(gate_out, gate_out); -> this inplace operation
+  /// doesn't work
+  acti_func.run_fn(gate_out, acti_out);
+
+  // Up projection: [tokens,1,1,H] x [H,I] -> [tokens,1,1,I]
+  nntrainer::Tensor up_out(tokens, 1, 1, intermediate_size);
+  input.dot(up_proj, up_out);
+
+  // Multiply: silu(gate_out) * up_out
+  acti_out.multiply_i(up_out);
+
+  // Down projection: [tokens,1,1,I] x [I,H] -> [tokens,1,1,H]
+  nntrainer::Tensor expert_output(tokens, 1, 1, hidden_size);
+  acti_out.dot(down_proj, expert_output);
+
+  // Weight by routing scores (broadcast multiply)
+  for (unsigned i = 0; i < tokens; ++i) {
+    float weight_val = weights[i];
+    auto weighted_expert_output = expert_output.getBatchSlice(i, 1);
+    weighted_expert_output.multiply_i(weight_val);
+  }
+
+  return expert_output;
+}
+
+void MoELayer::incremental_forwarding(nntrainer::RunLayerContext &context,
+                                      unsigned int from, unsigned int to,
+                                      bool training) {
+  if (from) {
+    NNTR_THROW_IF(to - from != 1, std::invalid_argument)
+      << "incremental step size is not 1";
+    from = 0;
+    to = 1;
+  }
+
+  nntrainer::Tensor &input_ = context.getInput(SINGLE_INOUT_IDX);
+  nntrainer::Tensor &output_ = context.getOutput(SINGLE_INOUT_IDX);
+
+  nntrainer::Tensor &router_logits_ = context.getTensor(router_logits_idx);
+  nntrainer::Tensor &expert_mask = context.getTensor(expert_mask_idx);
+
+  nntrainer::TensorDim input_step_dim = input_.getDim();
+  nntrainer::TensorDim output_step_dim = output_.getDim();
+  nntrainer::TensorDim router_logits_step_dim = router_logits_.getDim();
+
+  input_step_dim.batch(1);
+  output_step_dim.batch(1);
+  router_logits_step_dim.batch(to - from);
+
+  input_step_dim.height(to - from);
+  output_step_dim.height(to - from);
+
+  for (unsigned int b = 0; b < input_.batch(); ++b) {
+
+    auto input = input_.getSharedDataTensor(
+      input_step_dim, b * input_step_dim.getFeatureLen(), true);
+    auto output = output_.getSharedDataTensor(
+      output_step_dim, b * output_step_dim.getFeatureLen(), true);
+    auto router_logits =
+      router_logits_.getSharedDataTensor(router_logits_step_dim, 0, true);
+
+    const unsigned batch_size = input.batch();
+    const unsigned seq_len = input.height();
+    const unsigned hidden_size = input.width();
+    const unsigned total_tokens = batch_size * seq_len;
+
+    // reshape input: [B,1,S,H] -> [B*S,1,1,H]
+    input.reshape({total_tokens, 1, 1, hidden_size});
+
+    // reshape output: [B,1,S,H] -> [B*S,1,1,H]
+    output.reshape({total_tokens, 1, 1, hidden_size});
+    output.setZero();
+    expert_mask.setZero();
+
+    // routing
+    nntrainer::Tensor &gate_weights = context.getWeight(gate_idx);
+    input.dot(gate_weights, router_logits);
+    router_logits.apply(nntrainer::ActiFunc::softmax<float>, router_logits);
+    auto topk_result = router_logits.topK(topk);
+    auto topk_values = std::get<0>(topk_result);
+    auto topk_indices = std::get<1>(topk_result);
+
+    // norm_topk_prob
+    topk_values.divide_i(topk_values.sum(3));
+
+    const uint32_t *indices_data = topk_indices.getData<uint32_t>();
+    // #pragma omp parallel for collapse(2)
+    for (int i = 0; i < static_cast<int>(total_tokens); ++i) {
+      for (int k = 0; k < static_cast<int>(topk); ++k) {
+        expert_mask.setValue(indices_data[i * topk + k], 0, k, i, 1.0f);
+      }
+    }
+
+    // expert forwarding
+    // #pragma omp parallel
+    {
+      // #pragma omp for schedule(dynamic)
+      for (int expert_idx = 0; expert_idx < static_cast<int>(num_experts);
+           ++expert_idx) {
+        std::vector<unsigned> token_indices;
+        std::vector<float> topk_values_vector;
+
+        // get token indices for this expert
+        for (int i = 0; i < static_cast<int>(total_tokens); ++i) {
+          for (int k = 0; k < static_cast<int>(topk); ++k) {
+            if (expert_mask.getValue<float>(expert_idx, 0, k, i) > 0.5f) {
+              token_indices.push_back(i);
+              topk_values_vector.push_back(
+                topk_values.getValue<float>(i, 0, 0, k));
+            }
+          }
+        }
+        if (token_indices.empty())
+          continue;
+
+        // slicing and computing expert forward pass
+        nntrainer::Tensor expert_output = compute_expert_forward(
+          input.getBatchSlice(token_indices), topk_values_vector,
+          context.getWeight(expert_gate_proj_indices[expert_idx]),
+          context.getWeight(expert_up_proj_indices[expert_idx]),
+          context.getWeight(expert_down_proj_indices[expert_idx]));
+
+        // acuumulate expert output into the main output tensor
+        // #pragma omp critical
+        {
+          for (int i = 0; i < static_cast<int>(token_indices.size()); ++i) {
+            unsigned idx = token_indices[i];
+            auto tgt_output = output.getBatchSlice(idx, 1);
+            tgt_output.add_i(expert_output.getBatchSlice(i, 1));
+            // tgt_output.copyData(expert_output.getBatchSlice(i, 1));
+          }
+        }
+      }
+    }
+
+    // reshape output: [B*S,1,1,H] -> [B,1,S,H]
+    output.reshape({batch_size, 1, seq_len, hidden_size});
+  }
+}
+
+void MoELayer::setProperty(const std::vector<std::string> &values) {
+  auto remain_props = loadProperties(values, moe_props);
+  nntrainer::LayerImpl::setProperty(remain_props);
+}
+
+void MoELayer::calcDerivative(nntrainer::RunLayerContext &context) {
+  // MoE layer does not support derivative calculation
+  throw std::runtime_error("MoE layer does not support derivative calculation");
+}
+
+void MoELayer::calcGradient(nntrainer::RunLayerContext &context) {
+  // MoE layer does not support gradient calculation
+  throw std::runtime_error("MoE layer does not support gradient calculation");
+}
+
+void MoELayer::exportTo(nntrainer::Exporter &exporter,
+                        const ml::train::ExportMethods &method) const {
+  nntrainer::LayerImpl::exportTo(exporter, method);
+  exporter.saveResult(moe_props, method, this); // Save MoE specific properties
+}
+
+} // namespace causallm
