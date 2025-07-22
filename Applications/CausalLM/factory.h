ce87f604fbcc61ae4e2b0a3e81af3a751f3d6185
Eunju Yang <ej.yang@samsung.com>
2025-07-22 10:38:57 +0900

    [CausalLM] basic causallm application
    - This patch adds an example of causallm with nntrainer
    - This patch doesn't contain tokenizer, even it is written under
      assumption that we have `huggingface_tokenizer`.
    - This patch should be merged with tokenizer patch.
    
    Signed-off-by: Eunju Yang <ej.yang@samsung.com>

---
+++ Applications/CausalLM/factory.h
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2025 Eunju Yang <ej.yang@samsung.com>
+ *
+ * @file   causal_lm.h
+ * @date   22 July 2025
+ * @see    https://github.com/nnstreamer/nntrainer
+ * @author Eunju Yang <ej.yang@samsung.com>
+ * @bug    No known bugs except for NYI items
+ * @brief  CausalLM Factory to support registration and creation of various
+ * CausalLM models
+ */
+
+#ifndef __CAUSALLM_FACTORY_H__
+#define __CAUSALLM_FACTORY_H__
+
+#include <causal_lm.h>
+#include <unordered_map>
+
+namespace causallm {
+
+class Factory {
+public:
+  using Creator =
+    std::function<std::unique_ptr<CausalLM>(json &, json &, json &)>;
+
+  static Factory &Instance() {
+    static Factory factory;
+    return factory;
+  }
+
+  void registerModel(const std::string &key, Creator creator) {
+    creators[key] = creator;
+  }
+
+  std::unique_ptr<CausalLM> create(const std::string &key, json &cfg,
+                                   json &generation_cfg, json &nntr_cfg) const {
+    auto it = creators.find(key);
+    if (it != creators.end()) {
+      return (it->second)(cfg, generation_cfg, nntr_cfg);
+    }
+    return nullptr;
+  }
+
+private:
+  std::unordered_map<std::string, Creator> creators;
+};
+
+} // namespace causallm
+
+#endif
