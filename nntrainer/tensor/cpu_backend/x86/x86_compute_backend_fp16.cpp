8dc73ff4f8b6f9bca493b1028d79846bd07530c5
skykongkong8 <ss.kong@samsung.com>
2025-07-23 17:06:37 +0900

    [ cpu_backend ] Add function interface for all-archs in cpu_backend
    - quantize_q8_0
    - dequantize_row_q8_0
    - gemm_q4_0<float> and gemm_q4_0<_FP16>
    
    **Self evaluation:**
    1. Build test:     [X]Passed [ ]Failed [ ]Skipped
    2. Run test:     [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: skykongkong8 <ss.kong@samsung.com>

--- nntrainer/tensor/cpu_backend/x86/x86_compute_backend_fp16.cpp
+++ nntrainer/tensor/cpu_backend/x86/x86_compute_backend_fp16.cpp
@@ -20,6 +20,9 @@
 #ifdef USE_BLAS
 #include <cblas_interface.h>
 #endif
+#ifdef ENABLE_GGML
+#include <ggml_interface.h>
+#endif
 
 #define ROW_MAJOR 0
 #define COL_MAJOR 1
@@ -214,6 +217,34 @@ _FP16 max_val(const unsigned int N, _FP16 *X) { return __fallback_max(N, X); }
 
 void softmax(const unsigned int N, _FP16 *X, _FP16 *Y) {
   __fallback_softmax(N, X, Y);
+}
+
+void dequantize_row_q8_0(const void *x_raw, _FP16 *y, int64_t k) {
+#ifdef ENABLE_GGML
+  __nntr_dequantize_row_q8_0(x_raw, y, k);
+#else
+  __fallback_dequantize_row_q8_0(x_raw, y, k);
+#endif
+}
+
+size_t quantize_q8_0(const _FP16 *src, void *dst, int64_t nrow,
+                     int64_t n_per_row, const float *quant_weights) {
+#ifdef ENABLE_GGML
+  return __nntr_quantize_q8_0(src, dst, nrow, n_per_row, quant_weights);
+#else
+  return __fallback_quantize_q8_0(src, dst, nrow, n_per_row, quant_weights);
+#endif
+}
+
+template <>
+void gemm_q4_0(const unsigned int M, const unsigned int N, const unsigned int K,
+               const _FP16 *A, const unsigned int lda, const void *B,
+               const unsigned int ldb, _FP16 *C, const unsigned int ldc) {
+#ifdef ENABLE_GGML
+  return __ggml_q4_0_4x8_q8_0_GEMM<_FP16>(M, N, K, A, lda, B, ldb, C, ldc);
+#else
+  return __fallback_gemm_q4_0(M, N, K, A, lda, B, ldb, C, ldc);
+#endif
 }
 
 } /* namespace nntrainer */
