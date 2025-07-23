17bf60852caa4cca3ed1b062dedd47640f4ebf44
skykongkong8 <ss.kong@samsung.com>
2025-07-23 17:06:37 +0900

    [ cpu_backend ] Add function interface for all-archs in cpu_backend
    - quantize_q8_0
    - dequantize_row_q8_0
    - gemm_q4_0<float> and gemm_q4_0<_FP16>
    
    **Self evaluation:**
    1. Build test:     [X]Passed [ ]Failed [ ]Skipped
    2. Run test:     [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: skykongkong8 <ss.kong@samsung.com>

--- nntrainer/tensor/cpu_backend/fallback/fallback.h
+++ nntrainer/tensor/cpu_backend/fallback/fallback.h
@@ -112,6 +112,39 @@ void hsgemv(const unsigned int TStorageOrder, bool TransA, const unsigned int M,
             const unsigned int N, const float alpha, const _FP16 *A,
             const unsigned int lda, const float *X, const unsigned int incX,
             const float beta, float *Y, const unsigned int incY);
+
+/**
+ * @brief Quantize float to q6_K Quantization format
+ *
+ * @param src float* src to be quantized
+ * @param dst void* dst to store quantized data
+ * @param k number of elements in src
+ */
+void quantize_row_q8_0(const _FP16 *__restrict src, void *__restrict dst,
+                       int64_t k);
+
+/**
+ * @brief Quantize _FP16 to q8_0 Quantization format
+ *
+ * @param src input src to be quantized
+ * @param dst output destination for quantized data
+ * @param nrow number of row
+ * @param n_per_row number of elements per row
+ * @param quant_weights additional information for quantization. Currently in no
+ * use.
+ * @return size_t total size of quantized data
+ */
+size_t quantize_q8_0(const _FP16 *src, void *dst, int64_t nrow,
+                     int64_t n_per_row, const float *quant_weights);
+/**
+ * @brief q8_0 to _FP16 dequantize
+ *
+ * @param x_raw input src to be dequantized
+ * @param y output destination for dequantized data
+ * @param k data length
+ */
+void dequantize_row_q8_0(const void *x_raw, _FP16 *y, int64_t k);
+
 /**
  * @brief Accelerating function for rotary embedding layer forwarding
  *
