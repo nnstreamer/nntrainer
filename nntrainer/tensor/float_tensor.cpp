6cdba38c0fb9df563f08059e7fc8c0bd8e53cb54
Eunju Yang <ej.yang@samsung.com>
2025-07-23 09:17:31 +0900

    [Q4_0] Register Q4_0 as weight & attach dot op
    - This patch registers Q4_0 as a weight type
    - This patch aims to support Q4_0-FP32
    - This patch integrates Q4_0 tensor and Q4_0(FP32) kernel
    
    **Self evaluation:**
    
    Build test: [X]Passed [ ]Failed [ ]Skipped
    Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Eunju Yang <ej.yang@samsung.com>

--- nntrainer/tensor/float_tensor.cpp
+++ nntrainer/tensor/float_tensor.cpp
@@ -698,9 +698,10 @@ Tensor &FloatTensor::dot(Tensor const &input, Tensor &output, bool trans,
   case Tdatatype::FP16:
     dotFloat32Float16(input, output, trans, trans_in, beta);
     break;
-  /** applying gemm_q4_k / gemm_q6_k */
+  /** applying gemm_q4_k / gemm_q6_k / gemm_q4_0 */
   case Tdatatype::Q4_K:
   case Tdatatype::Q6_K:
+  case Tdatatype::Q4_0:
     dotQnK(input, output, trans, trans_in, beta, input.getDataType());
     break;
   default:
@@ -863,6 +864,13 @@ Tensor &FloatTensor::dotQnK(Tensor const &input, Tensor &output, bool trans,
     gemm_q6_K(M, N, K, data, K, (void *)mdata, N, rdata, N);
 #endif
     break;
+  case Tdatatype::Q4_0:
+    M = getDim().height();
+    K = getDim().width();
+    N = input.getDim().width();
+    gemm_q4_0(M, N, K, data, K, (void *)mdata, N, rdata, N);
+    break;
+
   default:
     throw std::invalid_argument("Error: unsupported datatype");
   }
