00a25ffa54a6abae83d9891c9853e99b26662c35
Eunju Yang <ej.yang@samsung.com>
2025-07-23 10:33:00 +0900

    [CausalLM] fix doxygen issues
    - This patch updates doxygen-related warnings.
    - This patch adds doxygen-related tags and explanations to the CausalLM
      files
    
    Signed-off-by: Eunju Yang <ej.yang@samsung.com>

--- Applications/CausalLM/layers/causallm_common_properties.h
+++ Applications/CausalLM/layers/causallm_common_properties.h
@@ -1,3 +1,25 @@
+/**
+ * Copyright (C) 2025 Samsung Electronics Co., Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * @file	causallm_common_properties.h
+ * @date	23 July 2025
+ * @brief	This defines a qwen3 causal language model.
+ * @see		https://github.com/nnstreamer/
+ * @author	Eunju Yang <ej.yang@samsung.com>
+ * @bug		No known bugs except for NYI items
+ *
+ */
 #ifndef __CAUSALLM_COMMON_PROPERTIES_H__
 #define __CAUSALLM_COMMON_PROPERTIES_H__
 
