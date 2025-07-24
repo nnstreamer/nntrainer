324761f64da559a9934f1ac37c0bb4a0910be742
skykongkong8 <ss.kong@samsung.com>
2025-07-24 10:34:18 +0900

    [ cpu_backend ] Refactor FP16-related ggml functions to function templates
    - dequantize_row_q8_K
    - quantize_row_q8_K
    - gemm_q6_K
    
    **Self evaluation:**
    1. Build test:     [X]Passed [ ]Failed [ ]Skipped
    2. Run test:     [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: skykongkong8 <ss.kong@samsung.com>

--- nntrainer/tensor/cpu_backend/x86/x86_compute_backend.h
+++ nntrainer/tensor/cpu_backend/x86/x86_compute_backend.h
@@ -852,7 +852,7 @@ bool is_valid(const unsigned int N, const float *X);
  * @param C float* output
  * @param ldc Leading dimension of C
  */
-template <typename T>
+template <typename T = float>
 void gemm_q4_0(const unsigned int M, const unsigned int N, const unsigned int K,
                const T *A, const unsigned int lda, const void *B,
                const unsigned int ldb, T *C, const unsigned int ldc);
@@ -887,9 +887,10 @@ void gemm_q4_K(const unsigned int M, const unsigned int N, const unsigned int K,
  * @param C float* output
  * @param ldc Leading dimension of C
  */
+template <typename T = float>
 void gemm_q6_K(const unsigned int M, const unsigned int N, const unsigned int K,
-               const float *A, const unsigned int lda, const void *B,
-               const unsigned int ldb, float *C, const unsigned int ldc);
+               const T *A, const unsigned int lda, const void *B,
+               const unsigned int ldb, T *C, const unsigned int ldc);
 
 /**
  * @brief (1xK)*(Kx1) dot product for q6_K and q8_K vectors
@@ -960,11 +961,12 @@ void quantize_row_q6_K(const float *src, void *dst, int64_t k);
 /**
  * @brief Quantize float to q6_K Quantization format
  *
- * @param src float* src to be quantized
+ * @param src T* src to be quantized
  * @param dst void* dst to store quantized data
  * @param k number of elements in src
  */
-void quantize_row_q8_K(const float *src, void *dst, int64_t k);
+template <typename T = float>
+void quantize_row_q8_K(const T *src, void *dst, int64_t k);
 
 /**
  * @brief dequantize row of q4_K data to float
@@ -985,13 +987,14 @@ void dequantize_row_q4_K(const void *x, float *y, int64_t k);
 void dequantize_row_q6_K(const void *x, float *y, int64_t k);
 
 /**
- * @brief dequantize row of q8_K data to float
+ * @brief dequantize row of q8_K data to T
  *
- * @param x input to be dequantized from q8_K to float
+ * @param x input to be dequantized from q8_K to T
  * @param y dequantized data output
  * @param k number of elements in x
  */
-void dequantize_row_q8_K(const void *x, float *y, int64_t k);
+template <typename T = float>
+void dequantize_row_q8_K(const void *x, T *y, int64_t k);
 
 /**
  * @brief repack q40 to q40x8
