810b6e08300d7a05469079f9f9c680b6d9b43f9e
hyeonseok <hs89.lee@samsung.com>
2025-07-28 17:50:53 +0900

    [model] remove unused functions
     - Remove addLayerNode, getUnsortedLayers, setLoss, rotate_180, logFloat, exp_util
    
    Signed-off-by: hyeonseok <hs89.lee@samsung.com>

--- test/unittest/unittest_util_func.cpp
+++ test/unittest/unittest_util_func.cpp
@@ -56,22 +56,6 @@ TEST(nntrainer_util_func, logFloat_01_p) {
   }
 }
 
-// TEST(nntrainer_util_func, rotate_180_p) {
-//   nntrainer::TensorDim dim(1, 1, 2, 3);
-
-//   float data[6] = {1, 2, 3, 4, 5, 6};
-//   float rotated_data[6] = {6, 5, 4, 3, 2, 1};
-
-//   nntrainer::Tensor tensor1(dim, data);
-//   nntrainer::Tensor tensor2 = nntrainer::rotate_180(tensor1);
-
-//   for (unsigned int i = 0, cnt = 0; i < dim.batch(); ++i)
-//     for (unsigned int j = 0; j < dim.channel(); ++j)
-//       for (unsigned int k = 0; k < dim.height(); ++k)
-//         for (unsigned int l = 0; l < dim.width(); ++l)
-//           EXPECT_EQ(tensor2.getValue(i, j, k, l), rotated_data[cnt++]);
-// }
-
 TEST(nntrainer_util_func, checkedRead_n) {
   std::ifstream file("not existing file");
   char array[5];
