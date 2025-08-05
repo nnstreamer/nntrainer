2f7a6a774dd32b1642e3b488428730792dfa6eb9
Eunju Yang <ej.yang@samsung.com>
2025-08-05 17:24:40 +0900

    [ParalldlQKV] Add parallel QKV and its model
    - This patch implements parallel qkv layer.
    - This patch adds an exmple model (NNTRQwen3CausalLM) using the parallel
      qkv layer.
    
    Signed-off-by: Eunju Yang <ej.yang@samsung.com>

--- Applications/CausalLM/main.cpp
+++ Applications/CausalLM/main.cpp
@@ -30,6 +30,7 @@
 #include <factory.h>
 
 #include "causal_lm.h"
+#include "nntr_qwen3_causallm.h"
 #include "qwen3_causallm.h"
 #include "qwen3_moe_causallm.h"
 #include "qwen3_slim_moe_causallm.h"
@@ -121,6 +122,11 @@ int main(int argc, char *argv[]) {
     [](json cfg, json generation_cfg, json nntr_cfg) {
       return std::make_unique<causallm::Qwen3SlimMoECausalLM>(
         cfg, generation_cfg, nntr_cfg);
+    });
+  causallm::Factory::Instance().registerModel(
+    "NNTRQwen3ForCausalLM", [](json cfg, json generation_cfg, json nntr_cfg) {
+      return std::make_unique<causallm::NNTRQwen3CausalLM>(cfg, generation_cfg,
+                                                           nntr_cfg);
     });
 
   // Validate arguments
