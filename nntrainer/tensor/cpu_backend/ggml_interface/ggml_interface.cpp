c223c6994117bce93aed96b0df9b14605315a8b5
skykongkong8 <ss.kong@samsung.com>
2025-08-12 15:29:42 +0900

    [ ggml ] Add ggml_interface.cpp
    - ggml_interface_omp.cpp : contains ONLY omp-parallelized functions
    - ggml_interface_bstp.cpp : contains ONLY bs threadpool-parallelized functions
    - ggml_interface_mixed.cpp : contains BOTH omp and bstp, best performance on Windows GB
    - ggml_interface.cpp : contains non-multithreaded functions
    
    **Self evaluation:**
    1. Build test:     [X]Passed [ ]Failed [ ]Skipped
    2. Run test:     [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: skykongkong8 <ss.kong@samsung.com>

--- nntrainer/tensor/cpu_backend/ggml_interface/ggml_interface.cpp
+++ nntrainer/tensor/cpu_backend/ggml_interface/ggml_interface.cpp
@@ -14,103 +14,103 @@
 
 namespace nntrainer {
     
-// void __ggml_init() {
-//   // needed to initialize f16 tables
-//   struct ggml_init_params params = {0, NULL, false};
-//   struct ggml_context *ctx = ggml_init(params);
-//   ggml_free(ctx);
-// }
+void __ggml_init() {
+  // needed to initialize f16 tables
+  struct ggml_init_params params = {0, NULL, false};
+  struct ggml_context *ctx = ggml_init(params);
+  ggml_free(ctx);
+}
 
-// size_t __ggml_quantize_q4_0(const float *src, void *dst, int64_t nrow,
-//                             int64_t n_per_row, const float *quant_weights) {
-//   return ::quantize_q4_0(src, dst, nrow, n_per_row, quant_weights);
-// }
+size_t __ggml_quantize_q4_0(const float *src, void *dst, int64_t nrow,
+                            int64_t n_per_row, const float *quant_weights) {
+  return ::quantize_q4_0(src, dst, nrow, n_per_row, quant_weights);
+}
 
-// size_t __ggml_quantize_q4_K(const float *src, void *dst, int64_t nrow,
-//                             int64_t n_per_row, const float *quant_weights) {
-//   return ::quantize_q4_K(src, dst, nrow, n_per_row, quant_weights);
-// }
+size_t __ggml_quantize_q4_K(const float *src, void *dst, int64_t nrow,
+                            int64_t n_per_row, const float *quant_weights) {
+  return ::quantize_q4_K(src, dst, nrow, n_per_row, quant_weights);
+}
 
-// size_t __ggml_quantize_q6_K(const float *src, void *dst, int64_t nrow,
-//                             int64_t n_per_row, const float *quant_weights) {
-//   return ::quantize_q6_K(src, dst, nrow, n_per_row, quant_weights);
-// }
+size_t __ggml_quantize_q6_K(const float *src, void *dst, int64_t nrow,
+                            int64_t n_per_row, const float *quant_weights) {
+  return ::quantize_q6_K(src, dst, nrow, n_per_row, quant_weights);
+}
 
-// void __ggml_quantize_row_q6_K(const float *src, void *dst, int64_t k) {
-//   ::quantize_q6_K(src, dst, 1, k, nullptr);
-// }
+void __ggml_quantize_row_q6_K(const float *src, void *dst, int64_t k) {
+  ::quantize_q6_K(src, dst, 1, k, nullptr);
+}
 
-// template <>
-// void __ggml_quantize_row_q8_K(const float *src, void *dst, int64_t k) {
-//   ::quantize_row_q8_K(src, dst, k);
-// }
-
-
-// float __nntr_vec_dot_q6_K_q8_K(const unsigned int K,
-//                                const void *__restrict v_q6_K,
-//                                const void *__restrict v_q8_K) {
-//   float result;
-//   int bs = 1, bx = 1, by = 1,
-//       nrc = 1; // unused variables in ggml_vec_dot_q6_K_q8_K
-//   ::ggml_vec_dot_q6_K_q8_K(K, &result, bs, v_q6_K, bx, v_q8_K, by, nrc);
-//   return result;
-// }
-
-// float __ggml_vec_dot_q6_K_f32(const unsigned int K, const void *v_q6_K,
-//                               const float *f) {
-//   // Quantization of activations
-//   int blocks_per_row = (K + QK_K - 1) / QK_K;
-//   int q8_K_activation_size = sizeof(block_q8_K) * blocks_per_row;
-//   std::vector<char> v_q8_activation = std::vector<char>(q8_K_activation_size);
-//   ::quantize_row_q8_K(f, v_q8_activation.data(), K);
-
-//   return __nntr_vec_dot_q6_K_q8_K(K, v_q6_K, v_q8_activation.data());
-// }
-
-// float __ggml_vec_dot_q6_K(const unsigned int K,
-//                           const void *__restrict v_q6_K,
-//                           const float *__restrict activation) {
-//   float result;
-//   int bs = 1, bx = 1, by = 1,
-//       nrc = 1; // unused variables in ggml_vec_dot_q6_K_q8_K
-
-//   int blocks_per_row = (K + QK_K - 1) / QK_K;
-//   int q8_K_activation_size = sizeof(block_q8_K) * blocks_per_row;
-//   std::vector<char> v_q8_activation = std::vector<char>(q8_K_activation_size);
-//   __ggml_quantize_row_q8_K(activation, v_q8_activation.data(), K);
-
-//   ::ggml_vec_dot_q6_K_q8_K(K, &result, bs, v_q6_K, bx, v_q8_activation.data(), by,
-//                          nrc);
-//   return result;
-// }
+template <>
+void __ggml_quantize_row_q8_K(const float *src, void *dst, int64_t k) {
+  ::quantize_row_q8_K(src, dst, k);
+}
 
 
-// void __ggml_dequantize_row_q4_K(const void *x_raw, float *y, int64_t k) {
-//   ::dequantize_row_q4_K((const block_q4_K *)x_raw, y, k);
-// }
+float __nntr_vec_dot_q6_K_q8_K(const unsigned int K,
+                               const void *__restrict v_q6_K,
+                               const void *__restrict v_q8_K) {
+  float result;
+  int bs = 1, bx = 1, by = 1,
+      nrc = 1; // unused variables in ggml_vec_dot_q6_K_q8_K
+  ::ggml_vec_dot_q6_K_q8_K(K, &result, bs, v_q6_K, bx, v_q8_K, by, nrc);
+  return result;
+}
 
-// void __ggml_dequantize_row_q6_K(const void *x, float *y, int64_t k) {
-//   ::dequantize_row_q6_K((const block_q6_K *)x, y, k);
-// }
+float __ggml_vec_dot_q6_K_f32(const unsigned int K, const void *v_q6_K,
+                              const float *f) {
+  // Quantization of activations
+  int blocks_per_row = (K + QK_K - 1) / QK_K;
+  int q8_K_activation_size = sizeof(block_q8_K) * blocks_per_row;
+  std::vector<char> v_q8_activation = std::vector<char>(q8_K_activation_size);
+  ::quantize_row_q8_K(f, v_q8_activation.data(), K);
 
-// template <>
-// void __ggml_dequantize_row_q8_K(const void *x, float *y, int64_t k) {
-//   ::dequantize_row_q8_K((const block_q8_K *)x, y, k);
-// }
+  return __nntr_vec_dot_q6_K_q8_K(K, v_q6_K, v_q8_activation.data());
+}
 
-// void __ggml_repack_q4_0_to_q4_0_4(void *W, void *repacked_W, size_t data_size,
-//                                   const unsigned int M, const unsigned int N) {
-//   ::ggml_repack_q4_0_to_q4_0_4_bl(W, 8, repacked_W, data_size, M, N);
-// }
+float __ggml_vec_dot_q6_K(const unsigned int K,
+                          const void *__restrict v_q6_K,
+                          const float *__restrict activation) {
+  float result;
+  int bs = 1, bx = 1, by = 1,
+      nrc = 1; // unused variables in ggml_vec_dot_q6_K_q8_K
 
-// void __ggml_repack_q4_0_to_q4_0_8(void *W, void *repacked_W, size_t data_size,
-//                                   const unsigned int M, const unsigned int N) {
-//   ::ggml_repack_q4_0_to_q4_0_8_bl(W, 8, repacked_W, data_size, M, N);
-// }
+  int blocks_per_row = (K + QK_K - 1) / QK_K;
+  int q8_K_activation_size = sizeof(block_q8_K) * blocks_per_row;
+  std::vector<char> v_q8_activation = std::vector<char>(q8_K_activation_size);
+  __ggml_quantize_row_q8_K(activation, v_q8_activation.data(), K);
 
-// void __ggml_repack_q4_K_to_q4_K_8(void *W, void *repacked_W, size_t data_size,
-//                                   const unsigned int M, const unsigned int N) {
-//   ::ggml_repack_q4_K_to_q4_K_8_bl(W, 8, repacked_W, data_size, M, N);
-// }
+  ::ggml_vec_dot_q6_K_q8_K(K, &result, bs, v_q6_K, bx, v_q8_activation.data(), by,
+                         nrc);
+  return result;
+}
+
+
+void __ggml_dequantize_row_q4_K(const void *x_raw, float *y, int64_t k) {
+  ::dequantize_row_q4_K((const block_q4_K *)x_raw, y, k);
+}
+
+void __ggml_dequantize_row_q6_K(const void *x, float *y, int64_t k) {
+  ::dequantize_row_q6_K((const block_q6_K *)x, y, k);
+}
+
+template <>
+void __ggml_dequantize_row_q8_K(const void *x, float *y, int64_t k) {
+  ::dequantize_row_q8_K((const block_q8_K *)x, y, k);
+}
+
+void __ggml_repack_q4_0_to_q4_0_4(void *W, void *repacked_W, size_t data_size,
+                                  const unsigned int M, const unsigned int N) {
+  ::ggml_repack_q4_0_to_q4_0_4_bl(W, 8, repacked_W, data_size, M, N);
+}
+
+void __ggml_repack_q4_0_to_q4_0_8(void *W, void *repacked_W, size_t data_size,
+                                  const unsigned int M, const unsigned int N) {
+  ::ggml_repack_q4_0_to_q4_0_8_bl(W, 8, repacked_W, data_size, M, N);
+}
+
+void __ggml_repack_q4_K_to_q4_K_8(void *W, void *repacked_W, size_t data_size,
+                                  const unsigned int M, const unsigned int N) {
+  ::ggml_repack_q4_K_to_q4_K_8_bl(W, 8, repacked_W, data_size, M, N);
+}
 
 }
