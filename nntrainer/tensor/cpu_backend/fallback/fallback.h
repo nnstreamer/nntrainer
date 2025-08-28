44d5a68b8dfe181a73d40dedc9595677456e5f67
Maciej Nalewaj <m.nalewaj@samsung.com>
2025-08-28 17:23:13 +0200

    [Backend] Restore tile_size in compute_kcaches()
    **Self-evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test:   [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Maciej Nalewaj <m.nalewaj@samsung.com>

--- nntrainer/tensor/cpu_backend/fallback/fallback.h
+++ nntrainer/tensor/cpu_backend/fallback/fallback.h
@@ -1061,12 +1061,13 @@ void compute_fp16vcache_fp32_transposed(int row_num, const float *in,
  * @param[in] num_cache_head number head of cache
  * @param[in] head_dim head dimension
  * @param[in] gqa_size size of group
+ * @param[in] tile_size size of tile
  * @param[in] local_window_size windows size for local attention
  */
 template <typename BType>
 void compute_kcaches(const float *in, const BType *kcache, float *output,
                      int num_rows, int num_cache_head, int head_dim,
-                     int gqa_size, size_t local_window_size = UINT_MAX);
+                     int gqa_size, int tile_size, size_t local_window_size = UINT_MAX);
 
 /**
  * @brief Compute rotary embedding value
