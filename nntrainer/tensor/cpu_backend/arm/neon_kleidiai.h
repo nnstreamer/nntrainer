3d2fdfd1a9efa6e7282146d4c2f8218eb35b31f4
skykongkong8 <ss.kong@samsung.com>
2025-09-16 10:08:45 +0900

    [ arm ] Introduce kleidiai and kernels
    - This commit introduces simd optimized kleidiai kernel
    - This commit will be a foundational reference for introducing more kernels from kleidiai
    - This time, I added qai8dxp-qsi4cxp Matmul and qs4cx_f32 quantization
    
    **Self evaluation:**
    1. Build test:     [X]Passed [ ]Failed [ ]Skipped
    2. Run test:     [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: skykongkong8 <ss.kong@samsung.com>

---
+++ nntrainer/tensor/cpu_backend/arm/neon_kleidiai.h
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2024 Arm Limited and/or its affiliates
+ * Copyright (C) 2024 Sungsik Kong <ss.kong@samsung.com>
+ *
+ * @file   neon_kleidiai.h
+ * @date   15 September 2025
+ * @see    https://github.com/nnstreamer/nntrainer
+ * @author Sungsik Kong
+ *
+ * @brief  Modified computational backend components of
+ * matmul_clamp_f32_qai8dxp_qsi4cxp. Portions of this file are derived from Arm
+ * Limited code licensed under the Apache License, Version 2.0, with
+ * modifications
+ *
+ * @note   Licensed under the Apache License, Version 2.0 (the "License");
+ *         you may not use this file except in compliance with the License.
+ *         You may obtain a copy of the License at
+ *             http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @modifications
+ *   - [2025-09-15] Integrated and adapted Arm-provided code into
+ *     nntrainer CPU backend
+ *
+ * @bug    No known bugs except for NYI items
+ */
+
+#include <cstdint>
+#include <stddef.h>
+
+void nntr_gemm_qai8dxp_qsi4cxp_rtp(size_t m, size_t n, size_t k,
+                               void *lhs_native_mtx_f32,
+                               void *rhs_native_mtx_qs4cx, void *rhs_scales_f32,
+                               float *dst_act_mtx_f32, bool transB,
+                               float lower_bound, float upper_bound);
