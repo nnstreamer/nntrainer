f33e29c3e0f1ea98e1809e86142f48457643261e
Piotr Pokorski <p.pokorski@samsung.com>
2025-09-16 17:36:47 +0200

    Implementation of OpenVINO int4 gemm
    This PR implements the OpenVINO INT4 GEMM OpenCL kernel.
    
    **Self-evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test:   [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Piotr Pokorski <p.pokorski@samsung.com>

--- nntrainer/tensor/cl_operations/blas_kernels.h
+++ nntrainer/tensor/cl_operations/blas_kernels.h
@@ -75,6 +75,10 @@ void gemm_q4_0_async_cl(std::vector<void *> matAdata, float *matBdata,
 void gemm_q4_0_cl(void *matAdata, float *matBdata, float *matCdata,
                   unsigned int M, unsigned int N, unsigned int K);
 
+void openvino_gemm_cl(void *input, void *weights, void *scales, void *output,
+                      unsigned int M, unsigned int N, unsigned int K,
+                      unsigned int quantization_group_size = 128);
+
 /**
  * @brief     Q6_K sgemv computation : Y = A*X
  * @param[in] matAdata void * for Matrix A
