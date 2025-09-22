c9d7150c5662bbea71b4acf2b2e0e52d618a5aaf
Donghyeon Jeong <dhyeon.jeong@samsung.com>
2025-09-22 11:39:38 +0900

    [OpenCL] Async INT4 GEMM kernel
    This commit adds async kernel execution of the OpenVINO INT4 GEMM
    
    **Self-evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test:   [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Donghyeon Jeong <dhyeon.jeong@samsung.com>

--- nntrainer/cl_buffer_manager.h
+++ nntrainer/cl_buffer_manager.h
@@ -59,6 +59,7 @@ private:
   void *data_input = nullptr;
   std::vector<void *> scale_vec;
   std::vector<void *> quant_vec;
+  std::vector<void *> output_vec;
 
 public:
   /**
@@ -100,6 +101,18 @@ public:
    * @brief Get the SVM pointer to data_input
    */
   void *getSVMInput() { return data_input; }
+
+  /**
+   * @brief Get the SVM pointer to data_input
+   *
+   * @note remove this when fp16 is enabled on Windows
+   */
+  void *getSVMOutput(unsigned int idx = 0) {
+    if (idx >= output_vec.size())
+      return nullptr;
+
+    return output_vec[idx];
+  }
 
   /**
    * @brief Get the SVM pointer to data_input
