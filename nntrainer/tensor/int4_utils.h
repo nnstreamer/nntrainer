5dba502d8d7969ea672ec3f7cc0cb4c59b7c1f8e
Maciej Nalewaj <m.nalewaj@samsung.com>
2025-10-15 10:53:03 +0200

    Doxygen documentation added for int4/q4_0 utils Move Q4_0 methods to q4_0_utils.h/.cpp files Get definition block_q4_0 from one place
    **Self evaluation:**
    1. Build test:     [X]Passed [ ]Failed [ ]Skipped
    2. Run test:     [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Maciej Nalewaj <m.nalewaj@samsung.com>

--- nntrainer/tensor/int4_utils.h
+++ nntrainer/tensor/int4_utils.h
@@ -1,3 +1,13 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * @file	int4_utils.h
+ * @date	15 October 2025
+ * @brief	This is Int4Utils class for some utils for INT4 quantization format.
+ * @see		https://github.com/nnstreamer/nntrainer
+ * @author	Grzegorz Kisala <gkisala@gmail.com>
+ * @bug		No known bugs
+ */
+
 #ifndef __NNTRAINER_INT4_UTILS_H__
 #define __NNTRAINER_INT4_UTILS_H__
 
@@ -5,74 +15,111 @@
 #include <cstdint>
 #include <vector>
 
-// TODO those structures should be defined in one place
-#define QK4_0 32
-
-struct block_q4_0x8 {
-  uint16_t d[8];   // 16B
-  uint8_t qs[128]; // 16 x u64
-};
-
-struct block_q4_0 {
-  uint16_t d;            // delta
-  uint8_t qs[QK4_0 / 2]; // nibbles / quants
-};
-
 namespace nntrainer {
 
+/**
+ * @class Int4Utils class
+ * @brief Int4Utils class with helpers for 4-bit integers calculation,
+ * quantization and dequantization methods for osv32_isv2 layout of data
+ */
 class Int4Utils {
 public:
+  /// @brief Block size used in the osv32_isv2 layout
   static constexpr const size_t ROW_BLOCK_SIZE = 32;
+
+  /// @brief Numbers of element in one byte of date in the osv32_isv2 layout
   static constexpr const size_t COLUMN_BLOCK_SIZE = 2;
 
+  /**
+   * @brief     Compute scale for input weights
+   * @param[in] group_weights float * inout vector of weights
+   * @param[in] group_size group size (32 or 64 or 128)
+   * @return computed scale
+   */
   static float computeScaleForGroup(const float *group_weights,
                                     const size_t group_size);
 
+  /**
+   * @brief     Compute scales for float* matrix weghts
+   * @param[in] weights float * input matrix
+   * @param[in] rows_count number of rows of input matrix
+   * @param[in] columns_count number of columns of input matrix
+   * @param[in] group_size group size (32 or 64 or 128)
+   * @param[out] scales float vector output scales
+   */
   static void computeScales(const float *weights, const size_t rows_count,
                             const size_t columns_count, const size_t group_size,
                             std::vector<float> &scales);
 
-  static void quantize(const float *weights, const size_t rows_count,
-                       const size_t columns_count, const size_t group_size,
-                       std::vector<uint8_t> &out_weights,
-                       std::vector<uint16_t> &out_scales);
-
+  /**
+   * @brief     Pack one weight from position (row_id, column_id) into 4-bits
+   * value
+   * @param[in] weights float * input matrix
+   * @param[in] scales float * input vector os scales
+   * @param[in] row_id number of row
+   * @param[in] column_id number of column
+   * @param[in] groups_per_row number of groups pre row
+   * @param[in] group_size group size (32 or 64 or 128)
+   * @param[in] rows_count number of rows of input matrix
+   * @param[in] columns_count number of columns of input matrix
+   * @return
+   */
   static uint8_t pack(const float *weights, const float *scales,
                       const size_t row_id, const size_t column_id,
                       const size_t groups_per_row, const size_t group_size,
                       const size_t rows_count, const size_t columns_count);
 
+  /**
+   * @brief Quantize weights float* matrix to OpenVINO layout:
+   * OS_IS_YX_OSV32_ISV2, osv32_isv2 layout for int4 packed weight:
+   *
+   * y0_x0x1 | y1_x0x1 | ....  | y15_x0x1|| y16_x0x1 | y17_x0x1 | ... | y31_x0x1
+   * y0_x2x3 | y1_x2x3 | ....  | y15_x2x3|| y16_x2x3 | y17_x2x3 | ... | y31_x2x3
+   * ...
+   * @param weights float * input matrix
+   * @param rows_count number of rows of input matrix
+   * @param columns_count number of columns of input matrix
+   * @param group_size group size (32 or 64 or 128)
+   * @param out_weights output quantized weights in layout osv**_isv2
+   * @param out_scales output scales
+   */
   static void quantizeAndRepack(const float *weights, const size_t rows_count,
                                 const size_t columns_count,
                                 const size_t group_size,
                                 std::vector<uint8_t> &out_weights,
                                 std::vector<uint16_t> &out_scales);
 
+  /**
+   * @brief     Quantize one float value to 4-bits integer
+   * @param[in] weight input weight
+   * @param[in] scale input scale
+   * @return 4-bit integer
+   */
   static uint8_t quantizeToInt4(const float weight, const float scale);
 
+  /**
+   * @brief     Convert 4-bit integer value to 32-bit integer
+   * @param[in] int4_value input 4-bit signed integer value
+   * @return output int value
+   */
   static int convertInt4ToInt(const uint8_t int4_value);
 
+  /**
+   * @brief     Dequantize weights in osv32_isv2 layout and scales to float
+   * weights
+   * @param[in] weights input matrix with quantized weights in osv32_isv2 layout
+   * @param[in] scales fp16 vector input scales
+   * @param[in] rows_count number of rows of data
+   * @param[in] columns_count number of columns of data
+   * @param[in] group_size group size (32 or 64 or 128)
+   * @param[out] dequantized_weights float vector of dequantized_weights
+   */
   static void dequantizePacked(const std::vector<uint8_t> &weights,
                                const std::vector<uint16_t> &scales,
                                const size_t rows_count,
                                const size_t columns_count,
                                const size_t group_size,
                                std::vector<float> &dequantized_weights);
-
-  // TODO begin
-  // temporary put Q4_0 depack here in order to be able to run it both in tests
-  // and llm inference
-  static void nntr_depack_block_q4_0x8(const block_q4_0x8 *in, block_q4_0 *dst,
-                                       unsigned int blck_size_interleave);
-
-  static void nntr_depack_q4_0_8_bl_to_q4_0(void *__restrict dst,
-                                            const void *__restrict data,
-                                            size_t data_size, size_t nrow,
-                                            size_t k);
-
-  static void dequantize_q4_0(void *q4_weight_repack, float *weight_fp32_out,
-                              int N, int K);
-  // TODO end
 };
 
 } // namespace nntrainer
