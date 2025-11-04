f7caa1c7c2d5475b44be4178d41588dfc9eecee0
Donghyeon Jeong <dhyeon.jeong@samsung.com>
2025-11-04 21:33:52 +0900

    Fix for ARMv9-a Build Error
    This commit resolves the build error when the architecture is ARMv9-a.
    
    **Self-evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test:   [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Donghyeon Jeong <dhyeon.jeong@samsung.com>

--- nntrainer/tensor/cpu_backend/arm/kai/matmul_clamp_f32_qai8dxp_qsi4cxp/kai_matmul_clamp_f32_qai8dxp1x8_qsi4cxp8x8_1x8x32_neon_dotprod.c
+++ nntrainer/tensor/cpu_backend/arm/kai/matmul_clamp_f32_qai8dxp_qsi4cxp/kai_matmul_clamp_f32_qai8dxp1x8_qsi4cxp8x8_1x8x32_neon_dotprod.c
@@ -127,9 +127,9 @@ kai_get_dst_size_matmul_clamp_f32_qai8dxp1x8_qsi4cxp8x8_1x8x32_neon_dotprod(
 }
 
 void kai_run_matmul_clamp_f32_qai8dxp1x8_qsi4cxp8x8_1x8x32_neon_dotprod(
-  size_t m, size_t n, size_t k, const void *restrict lhs_packed,
-  const void *restrict rhs_packed,
-  float *restrict dst, // NOLINT(readability-non-const-parameter)
+  size_t m, size_t n, size_t k, const void * lhs_packed,
+  const void * rhs_packed,
+  float * dst, // NOLINT(readability-non-const-parameter)
   size_t dst_stride_row, size_t dst_stride_col, float scalar_min,
   float scalar_max) {
   KAI_ASSERT(dst_stride_col == sizeof(float));
