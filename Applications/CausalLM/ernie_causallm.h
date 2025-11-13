bc2ffd1aa7f1e8a23d976ef1f2a78d7784f23476
Donghak PARK <donghak.park@samsung.com>
2025-11-13 16:53:46 +0900

    [LLM][Application] Add Ernie Model to Causallm
    Add Ernie 4.5 Model to Causallm
    - 21B-A3B Model
    - Text Model
    - This Model has Shared Experts (2) + TopK Experts (6)
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	 [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Donghak PARK <donghak.park@samsung.com>

---
+++ Applications/CausalLM/ernie_causallm.h
@@ -0,0 +1,60 @@
+//
+// Created by donghak on 25. 11. 13..
+//
+
+#ifndef __ERNIE_CAUSALLM_H__
+#define __ERNIE_CAUSALLM_H__
+#include <causal_lm.h>
+
+namespace causallm {
+
+class Ernie4_5_MoeForCausalLM : public CausalLM {
+public:
+  static constexpr const char *architecture = "Ernie4_5_MoeForCausalLM";
+  Ernie4_5_MoeForCausalLM(json &cfg, json &generation_cfg, json &nntr_cfg) :
+    CausalLM(cfg, generation_cfg, nntr_cfg) {
+    setupParameters(cfg, generation_cfg, nntr_cfg);
+  }
+
+  virtual ~Ernie4_5_MoeForCausalLM() = default;
+
+  /**
+   * @brief createAttention
+   */
+  std::vector<LayerHandle> createAttention(const int layer_id, int seq_len,
+                                           int n_heads, int head_dim,
+                                           std::string query_name,
+                                           std::string key_name,
+                                           std::string value_name) override;
+
+  /**
+   * @brief MoE layer
+   */
+  std::vector<LayerHandle> createMlp(const int layer_id, int dim,
+                                     int hidden_dim,
+                                     std::string input_name) override;
+
+  /**
+   * @brief setupParameters
+   */
+  void setupParameters(json &cfg, json &generation_cfg,
+                       json &nntr_cfg) override;
+
+  /**
+   * @brief registerCutomLayers
+   */
+  void registerCustomLayers() override;
+
+private:
+  unsigned int NUM_EXPERTS;
+  unsigned int NUM_EXPERTS_PER_TOK;
+  unsigned int NUM_SHARED_EXPERTS;
+
+  std::vector<std::string> LAYER_TYPES;
+  float ATTENTION_ROPE_SCALING_FACTOR;
+
+};
+
+} // namespace causallm
+
+#endif // __ERNIE_CAUSALLM_H__
