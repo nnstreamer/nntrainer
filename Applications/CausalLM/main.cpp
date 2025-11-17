c4a14a6584bc1f78acd8610319165290ae232fa8
Donghak PARK <donghak.park@samsung.com>
2025-11-17 16:05:27 +0900

    [Ernie][Application] Implement Ernie 4.5 at nntrainer
    Implement Ernie 4.5 at nntrainer
    - modify main & options at nntrainer
    
    **Self evaluation:**
    1. Build test:	 [X]Passed [ ]Failed [ ]Skipped
    2. Run test:	 [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Donghak PARK <donghak.park@samsung.com>

--- Applications/CausalLM/main.cpp
+++ Applications/CausalLM/main.cpp
@@ -38,6 +38,9 @@
 #include "qwen3_causallm.h"
 #include "qwen3_moe_causallm.h"
 #include "qwen3_slim_moe_causallm.h"
+
+#include "ernie_causallm.h"
+
 #include <sys/resource.h>
 
 #include <atomic>
@@ -152,6 +155,13 @@ int main(int argc, char *argv[]) {
     "GptOssCachedSlimCausalLM",
     [](json cfg, json generation_cfg, json nntr_cfg) {
       return std::make_unique<causallm::GptOssCachedSlimCausalLM>(
+        cfg, generation_cfg, nntr_cfg);
+    });
+
+  causallm::Factory::Instance().registerModel(
+    "Ernie4_5_MoeForCausalLM",
+    [](json cfg, json generation_cfg, json nntr_cfg) {
+      return std::make_unique<causallm::Ernie4_5_MoeForCausalLM>(
         cfg, generation_cfg, nntr_cfg);
     });
 
