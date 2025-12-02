eb23faa6d2d789fe104a53a08524e1a2b6fbb31f
Donghak PARK <donghak.park@samsung.com>
2025-12-02 11:41:26 +0900

    123123123

---
+++ Applications/CausalLM/layers/qwen_moe_layer_cached.h
@@ -0,0 +1,161 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2025 Eunju Yang <ej.yang@samsung.com>
+ *
+ * @file   qwen_moe_layer_fsu.h
+ * @date   09 June 2025
+ * @brief  This is Mixture of Expert Layer Class of Neural Network
+ * @see    https://github.com/nnstreamer/nntrainer
+ * @author Eunju Yang <ej.yang@samsung.com>
+ * @bug    No known bugs except for NYI items
+ * @note   This file is part of the Mixture of Expert Layer implementation.
+ *         It does not support shared experts.
+ *         This layer is implemented based on the LLama-MoE.
+ *         For more information, please refer to the following link:
+ *         https://arxiv.org/pdf/2406.16554
+ * @todo   This layer does not support backwarding yet.
+ */
+
+#ifndef __MOE_LAYER_H__
+#define __MOE_LAYER_H__
+#ifdef __cplusplus
+
+#include <acti_func.h>
+#include <causallm_common_properties.h>
+#include <common_properties.h>
+#include <layer_impl.h>
+#include <list>
+
+namespace causallm {
+
+/**
+ * @class   SlimMoELayer
+ * @brief   Mixture of Expert Layer
+ */
+class CachedSlimMoELayer : public nntrainer::LayerImpl {
+public:
+  /**
+   * @brief     Constructor of Mixture of Expert Layer
+   */
+  CachedSlimMoELayer();
+
+  /**
+   * @brief     Destructor of Mixture of Expert Layer
+   */
+  ~CachedSlimMoELayer() = default;
+
+  /**
+   * @brief  Move constructor.
+   *  @param[in] CachedSlimMoELayer &&
+   */
+  CachedSlimMoELayer(CachedSlimMoELayer &&rhs) noexcept = default;
+
+  /**
+   * @brief  Move assignment operator.
+   * @param[in] rhs CachedSlimMoELayer to be moved.
+   */
+  CachedSlimMoELayer &operator=(CachedSlimMoELayer &&rhs) = default;
+
+  /**
+   * @copydoc Layer::finalize(InitLayerContext &context)
+   */
+  void finalize(nntrainer::InitLayerContext &context) override;
+
+  /**
+   * @copydoc Layer::forwarding(RunLayerContext &context, bool training)
+   */
+  void forwarding(nntrainer::RunLayerContext &context, bool training) override;
+
+  /**
+   * @copydoc Layer::incremental_forwarding(RunLayerContext &context, unsigned)
+   */
+  void incremental_forwarding(nntrainer::RunLayerContext &context,
+                              unsigned int from, unsigned int to,
+                              bool training) override;
+
+  /**
+   * @copydoc Layer::calcDerivative(RunLayerContext &context)
+   */
+  void calcDerivative(nntrainer::RunLayerContext &context) override;
+
+  /**
+   * @copydoc Layer::calcGradient(RunLayerContext &context)
+   */
+  void calcGradient(nntrainer::RunLayerContext &context) override;
+
+  /**
+   * @copydoc Layer::setProperty(const std::vector<std::string> &values)
+   */
+  void setProperty(const std::vector<std::string> &values) override;
+
+  /**
+   * @copydoc Layer::exportTo(Exporter &exporter, const ml::train::ExportMethods
+   * &methods)
+   */
+  void exportTo(nntrainer::Exporter &exporter,
+                const ml::train::ExportMethods &method) const override;
+
+  /**
+   * @copydoc Layer::getType()
+   */
+  const std::string getType() const override {
+    return CachedSlimMoELayer::type;
+  };
+
+  /**
+   * @brief Layer::supportBackwarding()
+   */
+  bool supportBackwarding() const override { return false; }
+
+  WIN_EXPORT void updateTensorsByInputDimensions(
+    nntrainer::RunLayerContext &context,
+    std::vector<nntrainer::TensorDim> input_dimensions) override;
+
+  static constexpr const char *type =
+    "moe_cached_slim"; /**< type of the layer */
+
+private:
+  unsigned int num_experts;      /**< number of experts */
+  unsigned int topk;             /**< number of experts per token, i.e., topk */
+  nntrainer::ActiFunc acti_func; /**< activation function for the expert */
+  std::tuple<props::NumExperts, props::NumExpertsPerToken,
+             nntrainer::props::Unit, props::MoEActivation>
+    moe_props;
+
+  // weight indeices
+  std::vector<unsigned int> expert_gate_proj_indices;
+  std::vector<unsigned int> expert_up_proj_indices;
+  std::vector<unsigned int> expert_down_proj_indices;
+
+  std::list<int> loaded_expert_deque;
+  std::unordered_map<int, std::list<int>::iterator> iteration_map;
+  std::unordered_map<int, double> expert_predict_scores;
+  std::vector<bool> need_load;
+  std::mutex cache_mutex;
+
+  unsigned int gate_idx;
+
+  // Intermediate tensor indices
+  unsigned int router_logits_idx;
+  unsigned int expert_mask_idx;
+  /**
+   * @brief expert forward computation without memory copies
+   * @param input Input tensor (reshaped to [total_tokens, 1, 1, hidden_size])
+   * @param output Output tensor to accumulate results
+   * @param token_assignments Vector of (token_index, weight) pairs for this
+   * expert
+   * @param gate_proj Gate projection weight tensor
+   * @param up_proj Up projection weight tensor
+   * @param down_proj Down projection weight tensor
+   * @param hidden_size Hidden dimension size
+   */
+  inline void compute_expert_forward(
+    const nntrainer::Tensor &input, nntrainer::Tensor &output,
+    const std::vector<std::pair<unsigned, float>> &token_assignments,
+    const nntrainer::Tensor &gate_proj, const nntrainer::Tensor &up_proj,
+    const nntrainer::Tensor &down_proj, unsigned int hidden_size);
+};
+} // namespace causallm
+
+#endif /* __cplusplus */
+#endif /* __MOE_LAYER_H__ */
